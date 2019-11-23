1818b952ba4cc06baaa719782b1381ff4a57d1b9
dunia-anime <adityha.on@gmail.com>
2019-11-23 17:29:40 +0800

    Update

---
+++ Anim-js/-AnS/91ds.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* 91 Days',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/J6TbQmz7JGgfLc2XXQzIZrVyXO9XhvDdJOTVh7kaxhecNQmIHW4gxsp4oQr9mIoX-mkrUR0WN0YsZ6JIDFkGshpnTbg0DdD7AoJPxa0kR_zvFnE3L8gtpRssAyhD36xcdtxfFT4UgQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/J6TbQmz7JGgfLc2XXQzIZrVyXO9XhvDdJOTVh7kaxhecNQmIHW4gxsp4oQr9mIoX-mkrUR0WN0YsZ6JIDFkGshpnTbg0DdD7AoJPxa0kR_zvFnE3L8gtpRssAyhD36xcdtxfFT4UgQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/mh8_Muz158z689lO4SK8qTliBwPp_pc12BbO_WguLBGjo60IyQMkjIz85l47tAw2yMYNy3Rlm0QrSsoAvGmPJdKnwIsmzikgYfBGDm1gx3vMOBWn3oJHPUqGK6ZM68MG5GquCN25sA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/mh8_Muz158z689lO4SK8qTliBwPp_pc12BbO_WguLBGjo60IyQMkjIz85l47tAw2yMYNy3Rlm0QrSsoAvGmPJdKnwIsmzikgYfBGDm1gx3vMOBWn3oJHPUqGK6ZM68MG5GquCN25sA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_iX-bxhSjPtwTIqlafhOIdJuEa2pumg5tzvpdkS679iwGrYYWifFioBXwuKN5Z2-BhVW0RF0mp9IDs3VBRUGePbrv1NyF5LtItuqkgR7AuEkyj-ItLV_hywyrtt_oN_hDEdmwnHIsQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_iX-bxhSjPtwTIqlafhOIdJuEa2pumg5tzvpdkS679iwGrYYWifFioBXwuKN5Z2-BhVW0RF0mp9IDs3VBRUGePbrv1NyF5LtItuqkgR7AuEkyj-ItLV_hywyrtt_oN_hDEdmwnHIsQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/mgJ0yJSxxYzK2FGTUFz_W-ZJPmXmDAVbhnLF884QKeQ6By8fl7VYAfl83wSHkMUXe-o53Fq9ICAnTxCYqm1GpXORa8uhmZsQqOhFz9WuFsjIjNuZS78Qnsank5m0lynaR-iNyhm4UQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/mgJ0yJSxxYzK2FGTUFz_W-ZJPmXmDAVbhnLF884QKeQ6By8fl7VYAfl83wSHkMUXe-o53Fq9ICAnTxCYqm1GpXORa8uhmZsQqOhFz9WuFsjIjNuZS78Qnsank5m0lynaR-iNyhm4UQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/21auzR-jqaVNXQlujWeP8X_TKw-zUVhf0F-aoiihoZUOOh478im2XeMRtf9dstgx0cU9WUfyb17UzxFP2x1nSoKFkIKqRl3jpEq_lp5AaMAHcFwu3k721qGkNsRXckKSSd_NgdyhmQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/21auzR-jqaVNXQlujWeP8X_TKw-zUVhf0F-aoiihoZUOOh478im2XeMRtf9dstgx0cU9WUfyb17UzxFP2x1nSoKFkIKqRl3jpEq_lp5AaMAHcFwu3k721qGkNsRXckKSSd_NgdyhmQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SXBLTfdWrP2NZ63vDhesVM2idk_zghBf53F5Nf99bcql2ODYOdGEqY6WxI9kjV2C6AmuS1MG90yvIn5l-Qsi4XqPrKXS_xa1khO6BPijuP3KEbRGEaVjUlKH-htEqfhfGdH_nvnbMA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SXBLTfdWrP2NZ63vDhesVM2idk_zghBf53F5Nf99bcql2ODYOdGEqY6WxI9kjV2C6AmuS1MG90yvIn5l-Qsi4XqPrKXS_xa1khO6BPijuP3KEbRGEaVjUlKH-htEqfhfGdH_nvnbMA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/FsoFeEwpaB_bFLftnfuEONUj7HwmPqGd0HcsC8Ex1CAF9wLQBLMgp-ewNOJHvzM5PraW-oSCUKfb89MeGAyde1BBdp1ka5z0EcGKDIhcv0p09dVDO5u6K0QGlxF4x9eoSbfe_Oqrbg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/FsoFeEwpaB_bFLftnfuEONUj7HwmPqGd0HcsC8Ex1CAF9wLQBLMgp-ewNOJHvzM5PraW-oSCUKfb89MeGAyde1BBdp1ka5z0EcGKDIhcv0p09dVDO5u6K0QGlxF4x9eoSbfe_Oqrbg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/yIdUNfSFEj2WaKZg9N7x-9X516bEcZqyPrjSOBNDd1kneZdEPgN6pNSU_cQc9k-qRMsEe3vzhd3RrohgRIWOyEvDHaLmzsYOBzSS5A2rLD7lKwPGOL1dSy_GDJs3Mk9t-hvglnz96Q=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/yIdUNfSFEj2WaKZg9N7x-9X516bEcZqyPrjSOBNDd1kneZdEPgN6pNSU_cQc9k-qRMsEe3vzhd3RrohgRIWOyEvDHaLmzsYOBzSS5A2rLD7lKwPGOL1dSy_GDJs3Mk9t-hvglnz96Q=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/kUVfMu0iGpWZpyc_z6NC_FAroGJhYn1-w8oXyEITyaTaCnYxCtQkoSbHyUl1q6zsFD5Nko8ull3yZsLlWs8z8PEbXN9BFV9nwbRQUOO3f5M2BMUxWtIPh5o8fpczWvmWClCZa3Fu0g=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/kUVfMu0iGpWZpyc_z6NC_FAroGJhYn1-w8oXyEITyaTaCnYxCtQkoSbHyUl1q6zsFD5Nko8ull3yZsLlWs8z8PEbXN9BFV9nwbRQUOO3f5M2BMUxWtIPh5o8fpczWvmWClCZa3Fu0g=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/rwoigaFu52xjhQXJMaUtGaWxEqmbBocbakK967KE0M6JfTNuB51esyc3YW5o5CyEASMzC8nuoK5q6fI_nhysj1JZgeixdfPcixINfyQLf57icLCJGMTuk8XwelaNnMgc8paKZHX0kA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/rwoigaFu52xjhQXJMaUtGaWxEqmbBocbakK967KE0M6JfTNuB51esyc3YW5o5CyEASMzC8nuoK5q6fI_nhysj1JZgeixdfPcixINfyQLf57icLCJGMTuk8XwelaNnMgc8paKZHX0kA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/fC0sDnoboQvZsF1lO-2GZAfnqLGP5KC4j4v5J4yRz8u8ogERHYMw_6C06UbHsOMri7zsaUyaD-p9M1E5qqYSZMGKhzEESGPIHVBk6uK5dOvWKQ5Q7MGFts4x1jQs_rOEMwLS8AttuA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/fC0sDnoboQvZsF1lO-2GZAfnqLGP5KC4j4v5J4yRz8u8ogERHYMw_6C06UbHsOMri7zsaUyaD-p9M1E5qqYSZMGKhzEESGPIHVBk6uK5dOvWKQ5Q7MGFts4x1jQs_rOEMwLS8AttuA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/1Jr9eqAYGaL3z2ocs32HClY-67AAAsJvrDwtk_ZYi0yT3vYzh3_kdtNlVjyiw80OtOGEopYdB0VZQyO89Ol4BhQY96P91BID7GwhjLcUap6WPzK7lg7V23A2gjy5hUky7oT9ds4dYg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/1Jr9eqAYGaL3z2ocs32HClY-67AAAsJvrDwtk_ZYi0yT3vYzh3_kdtNlVjyiw80OtOGEopYdB0VZQyO89Ol4BhQY96P91BID7GwhjLcUap6WPzK7lg7V23A2gjy5hUky7oT9ds4dYg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* 91 Days',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/pen7C5v8pji6NQpdqeUGSZ2jIMv7y41unCJKo9anqfdt_10Ld99JANXibav5aAA2wX0yXIwy58t8wGkDfgA0Wc6hMXMgI8q5m4f2zk3UKURki3Q85yseW-0IdyTsV3g72FUKRRc5Dw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/pen7C5v8pji6NQpdqeUGSZ2jIMv7y41unCJKo9anqfdt_10Ld99JANXibav5aAA2wX0yXIwy58t8wGkDfgA0Wc6hMXMgI8q5m4f2zk3UKURki3Q85yseW-0IdyTsV3g72FUKRRc5Dw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
