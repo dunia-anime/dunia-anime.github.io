68fc4da937e9cefbaa4b7c6cf5357aa757b8d59a
dunia-anime <adityha.on@gmail.com>
2019-11-26 18:32:52 +0800

    Create DrunTouTS1.js

---
+++ Anim-js/D/DrunTouTS1.js
@@ -0,0 +1,289 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/-zTQbX3mbvPSJ1_uZ4g_nPsIIni8YNokLccfAReE0tzQ84mcjzCoIRt3kI7nudi3y9DKJRbBQ8LrufWTmL3LDCjOglINjF-tIkUVhV2UHMOgysHPO-fhPho0dFNJoKqrnkaHVFCWKA=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/09BNbYLw-sYHnJVsQxNXEUunGtRn2ak4_WEpxy28eyMBrQRhu8in3Pp6RBEHGwqnTU7gktOVn-LsO2dODQs9r19-YWc86BYv2zEYVFpTcx4hya_H1TT4YmZKeW-wsfiZj6iTfXv0tg=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/soYFt-mffFH9FaGXqZHa_ZYW5ifRYVsKo8eEfJG-tC3PQ-fqSGSw-tEMGmw9fhMuuib113ht3J7nGRn0VKaxTyKHwXZABf89gGp9XvjfAdvgDkD1ZF7Se_dtarPvymHN5GFOLxhjnQ=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/N3flM3ehwvKrqBubqS47Bfxv89yCqxR81gjnz2a7go4QYuWSQcWyd4hGkFCp47C4YtBC4Gee1CTmq6Ys_k6tWvWgHE_FyyBhNYBmfv1vIEYbst96k9kA5E-yn4jCXljL_fyhnOW1-g=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/Vyw9y9nZhqzSnFEmSsbNxeg2f-Ee50JNa8MrBvad5Tna8AQTtBE3xiPVNDQElAaJwTRT5A_IRSYiNN7iFeIWkVb8tqutZNmIbDUE-SGM1FFKEKC-inFpnVsWvmrdx2Gn9nmCpj4fAw=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/YVFL6itEdzRX989NPs23biZTdhawbJtiJOagHjK8jYHuCOZDmdiaYWRn2GGjnoqeZRcF1sasUk7MSJyQ7kZvcg6nB6kghN_lxsDavLnWshfle6Cl5ezP_qEaLZc6y3lgNcb1o5AMcg=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/y-VltLsdvPeG4fY858C2_r5CjVRqSO-qvr4scJApU26czBRVPO_zfmJYnfKQZC_DBPcvTAFfHsiVdsTbmVcpIcodJzgb3WuFcBN7nzdROUUQNSVubiZPPpno0V4JtyYOjBNBn4NPJQ=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/i1Cc98eDym0yrZl8axoZ5jKE4WTS-m2NbpAT2VvqU2MuzmC20anji8etxL-vNlstAK028ZbzpMvmbOay9FsNxbJ4gUtmT6OavPGN0X038AGNs_QaLg33R-yN2BzTubJRJroVjerWkA=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/utXL23xdnESeSEoZ-V4_aKNBdHF_rx7dnTpjAsWy8OOq-Hu5HPkTkZLwlf66FP_ARCCaABaET-J4Y1pjfZz8NmsaBcUPFCvJIV3Kb_wiJJKhKbrjFZiNZxMw9qOcMJYpsa1QLPcECw=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/roSfrO8Fq1_r7DrYmSaS17B-pJBAXZYsiLMH3z_22o4ivhnLbp3JAK_HtQ-MAZjlA-7eNn0Hzpj4eRjbrzdHz8LMIi_pMNYWqu8zjok_7Rm4bupcy24CRqTqGAoY-2AqEdSruf9KvQ=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/8szlf0xyIhon4NF_FfPcOdSJKB_bKKdWierS7jUc4k6wr0EE1DN-UzadMmChx8Bne0TSwynBO6YdGbLKpJFeeUM966Vf4Si_urMSPitH28j1UJKhby6EYhIDmKsmhuAh3Apm4Pv4iw=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/DZ7iGRKw6b4CBh7x5alkIHtaujhfNAiQ874Z5moA_M2vQHWrKEaLI9TdRxSugYPagKiyM2T-D2Z7mtb45LqoCM8DsZ3h-GJrLub0Xzeu1JyU14_Q0mM6Niubu6s0CbuiJXIPM0qi5A=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Druaga no Tou S1',
+        episode: '* Episode Special',
+        name: 'Episode Special',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/1A-t3XylhYzMrr_6Pm2kzpmZjPhsU79dLP3cC5Q6d-_m9Mep5wylX937_srQ9cFT689Dluqu15748ZsYB-y6GJ0qfuSNaVQYwwv_efHFE3-yomhHCPgS9KhbmTslBfWFFBq0XMy52g=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
