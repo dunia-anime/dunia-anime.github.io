299c2f818876c44fac3eb59ab8ca5c889e6aee28
dunia-anime <adityha.on@gmail.com>
2019-11-27 18:46:35 +0800

    Create riter.js

---
+++ Anim-js/D/riter.js
@@ -0,0 +1,408 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Drifters',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/KVY5_JuK1YBmxxaAwf8WeiMIu5u99vbvs1s1QcQPgfhuOE0OfhsPdVZC_a7KozY6FRXNYpyvAcEB_oY8_2-rdV0Nj54XnuGJGtmc8fH7a862SDcWVbmVHVSh4jYwk75pLaxJGINOPJQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/KVY5_JuK1YBmxxaAwf8WeiMIu5u99vbvs1s1QcQPgfhuOE0OfhsPdVZC_a7KozY6FRXNYpyvAcEB_oY8_2-rdV0Nj54XnuGJGtmc8fH7a862SDcWVbmVHVSh4jYwk75pLaxJGINOPJQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3k8edzySFaLTEkWDITGgRMHA1UNgDsYNR0SSr33UfytPutlznATLGdyr7hu4XgPp81jx6PXzl7Z0GE1zAPsjRRO6gJ2htX98tLWtU0n-zvcbUWbPu0UiL6f3hddU4K-bs5_Wlkgtx9k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3k8edzySFaLTEkWDITGgRMHA1UNgDsYNR0SSr33UfytPutlznATLGdyr7hu4XgPp81jx6PXzl7Z0GE1zAPsjRRO6gJ2htX98tLWtU0n-zvcbUWbPu0UiL6f3hddU4K-bs5_Wlkgtx9k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Ly6_yvCbqnPVWjfyMGN6qmdT2MsLdOIaqAa8CREkfcrtiM4mfxCDta818-wgHFDYLwwwqS0kotz0AiA3vE7SE24O7X4_ue9AnjQKLKl4HAAGl9_Y4NPQSRZzYx5R5_vdGTsUNZRTDLk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Ly6_yvCbqnPVWjfyMGN6qmdT2MsLdOIaqAa8CREkfcrtiM4mfxCDta818-wgHFDYLwwwqS0kotz0AiA3vE7SE24O7X4_ue9AnjQKLKl4HAAGl9_Y4NPQSRZzYx5R5_vdGTsUNZRTDLk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/dCW82kgztSGyZwMKsHCRwuAzxkMvjl0jsHDGO95wWNx-pqejzmdOKuO9quJBSqfKE-zVB6pSqu2lqDd6ObhmykdfxS72bdhwzs7RpJ-3dJ3hcm5f-54lX6e-YV60zGpl20Z2bYFFxyU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/dCW82kgztSGyZwMKsHCRwuAzxkMvjl0jsHDGO95wWNx-pqejzmdOKuO9quJBSqfKE-zVB6pSqu2lqDd6ObhmykdfxS72bdhwzs7RpJ-3dJ3hcm5f-54lX6e-YV60zGpl20Z2bYFFxyU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/HxFtvNMlRYP_bmPFbFOejTRVUWObsTAo1-QE0qSpCP7DLER5WeuDRlMKp0eQAvVoNidHhlDqXN99lBaqPGbUEfqWS-EDKwYCFPYD-oE1GV5C2FyjPv42sS8s3wdGyzbCet-Kp-daT4w=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/HxFtvNMlRYP_bmPFbFOejTRVUWObsTAo1-QE0qSpCP7DLER5WeuDRlMKp0eQAvVoNidHhlDqXN99lBaqPGbUEfqWS-EDKwYCFPYD-oE1GV5C2FyjPv42sS8s3wdGyzbCet-Kp-daT4w=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/uudFoJ7SJuVHealn5Ji2-dlShxanbivXuN9upJciuT1n_HrUAbWv_iBpcrW4eOIr9TV8C52PgaFLQP17rURXnJ7Pb5P7baHJ6W2jDVZWoFAyKFyQ1LKaM_8jfhDz48ZmUjg2rye54nw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/uudFoJ7SJuVHealn5Ji2-dlShxanbivXuN9upJciuT1n_HrUAbWv_iBpcrW4eOIr9TV8C52PgaFLQP17rURXnJ7Pb5P7baHJ6W2jDVZWoFAyKFyQ1LKaM_8jfhDz48ZmUjg2rye54nw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SLr0W5aFfDzkfVNjoZ7YEbEvLQMN9vVr-oqRy7FPdlm1XQcleyEZZpU0FUxQQoIxH-auyXo2EI8sMo8B8ATbag_DugKuAJm-1NzJTXEdbclAZWuLD4Ib-OVovQ8RElra6Cg5C3jMEOI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SLr0W5aFfDzkfVNjoZ7YEbEvLQMN9vVr-oqRy7FPdlm1XQcleyEZZpU0FUxQQoIxH-auyXo2EI8sMo8B8ATbag_DugKuAJm-1NzJTXEdbclAZWuLD4Ib-OVovQ8RElra6Cg5C3jMEOI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/bDD6P7kC4XhbKkKTebZKdaM3Kjx4XauJPH9FvUBvhmAe6E--6a-1MyIqoKhFVvFYnNTy1WQ_ic01AZic21fewDoTmXBhRQC2xBnjE3HUAy-5Eq-sHU7mrw1IgCSrtPuqaaNjZZcYpag=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/bDD6P7kC4XhbKkKTebZKdaM3Kjx4XauJPH9FvUBvhmAe6E--6a-1MyIqoKhFVvFYnNTy1WQ_ic01AZic21fewDoTmXBhRQC2xBnjE3HUAy-5Eq-sHU7mrw1IgCSrtPuqaaNjZZcYpag=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/k-CkCqhpPLyFo3alWtG4wOgv1Vj94WI_-ekEpg4KlkJF9ckAT0hWxWltKb5cbPk-l_bKR9FF84ugDlDT-yQh8xUuMxbOQllGDVQJa0VdX4o_yakmFjCoNWF29s6KjBB7v6YM1rqi6AI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/k-CkCqhpPLyFo3alWtG4wOgv1Vj94WI_-ekEpg4KlkJF9ckAT0hWxWltKb5cbPk-l_bKR9FF84ugDlDT-yQh8xUuMxbOQllGDVQJa0VdX4o_yakmFjCoNWF29s6KjBB7v6YM1rqi6AI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/uZLOD9IgNHtqZqOmPrTlkvy8BVYxn7Iqwd1MxrxuDYRSnVc1zjlgAaktJQ_D6bZqk112ob3R1WIdXAz70v4MDgEt1wqze6HxbgfhPfiq5MQNmAPI8EqAaBg4kHKZ-PZtXx5ZJWGkjDQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/uZLOD9IgNHtqZqOmPrTlkvy8BVYxn7Iqwd1MxrxuDYRSnVc1zjlgAaktJQ_D6bZqk112ob3R1WIdXAz70v4MDgEt1wqze6HxbgfhPfiq5MQNmAPI8EqAaBg4kHKZ-PZtXx5ZJWGkjDQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/2Qi0WLA0Q9dAYVKMNlipHMvzz_lPJZAcOAgViLVKYj6huD3ZZ_Pu1N53_SvdEnCeL4_Lm0R9iiP9SFS0jpFDf-tUQDjp_4vmi1zQ9wK68HHRnnA6Zx_fDPJjm-dTuSQ-oiFcJxV_RQU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/2Qi0WLA0Q9dAYVKMNlipHMvzz_lPJZAcOAgViLVKYj6huD3ZZ_Pu1N53_SvdEnCeL4_Lm0R9iiP9SFS0jpFDf-tUQDjp_4vmi1zQ9wK68HHRnnA6Zx_fDPJjm-dTuSQ-oiFcJxV_RQU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3jbqhr9N4QYs3GyvVWiJD95NoD2b_4EFrxcXdGhsi7NLdlGmt51aKxsn2P24VPRXEw5P73X0gd_4dGkavlJe9mDCRD1aMrDFLmwUOqKtTpA0Md3NjscazXOx415QdrBKU4Pnz0B-5Rk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3jbqhr9N4QYs3GyvVWiJD95NoD2b_4EFrxcXdGhsi7NLdlGmt51aKxsn2P24VPRXEw5P73X0gd_4dGkavlJe9mDCRD1aMrDFLmwUOqKtTpA0Md3NjscazXOx415QdrBKU4Pnz0B-5Rk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode OVA 1',
+        name: 'Episode OVA 1',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/z7x1lRf80lMdlVYM00fEZjag2DuPHq3MraIbQd79n731h_MJUMIxWbSwb3rlhCQKl-k_igLMH6boPBfqDVggjREeDxN6GXEgWfAmUYs93sFQOHuiwIM1D56SeF7P6Pq4oDXSODtb2Es=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/z7x1lRf80lMdlVYM00fEZjag2DuPHq3MraIbQd79n731h_MJUMIxWbSwb3rlhCQKl-k_igLMH6boPBfqDVggjREeDxN6GXEgWfAmUYs93sFQOHuiwIM1D56SeF7P6Pq4oDXSODtb2Es=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Drifters',
+        episode: '* Episode OVA 2',
+        name: 'Episode OVA 2',
+        deskrip1: '* Rating : 8.01',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/k6GJvlH6GiC1QD4XpBDAwbD0p9_PAlE3mF3gB1z2zIGOdE6sUrdGsp9DIhIryZ28XpG_k31kbGc6TaTfMlBXgfiqFuVpQfsRR9g277Mx5VC0iXXcZR_BqxT6W5SQiZUhr5mnmKGG0Zs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/k6GJvlH6GiC1QD4XpBDAwbD0p9_PAlE3mF3gB1z2zIGOdE6sUrdGsp9DIhIryZ28XpG_k31kbGc6TaTfMlBXgfiqFuVpQfsRR9g277Mx5VC0iXXcZR_BqxT6W5SQiZUhr5mnmKGG0Zs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
