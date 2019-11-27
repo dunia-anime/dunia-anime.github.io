d80745d34fb80130cef53b56a74a67abedd8cc80
dunia-anime <adityha.on@gmail.com>
2019-11-27 18:59:39 +0800

    Create ninHiToAe.js

---
+++ Anim-js/D/ninHiToAe.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/QNhgGyoSmgRO9GF0TuvyZB76ODdAjnvYqjpK_lNgCrlgQ2lJYU2vBOI2fNNyTEf3Tk4NxVIq-8CSWJ8qp1DnoFW6asKBDwUXLzpVoQTP8KkCZ5Yni34VjTY17oFn5xKQgcrgwFR1xCU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/QNhgGyoSmgRO9GF0TuvyZB76ODdAjnvYqjpK_lNgCrlgQ2lJYU2vBOI2fNNyTEf3Tk4NxVIq-8CSWJ8qp1DnoFW6asKBDwUXLzpVoQTP8KkCZ5Yni34VjTY17oFn5xKQgcrgwFR1xCU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/FNljlSKY27oyEamJ-kOAGfmREV-YPwiFTNjtAE0hpfX05i1oi76UbzjdrIdavD9I2o2aH8eAMXczd9olJFQEOdWNDmjSiAQdiT3-Uv38p_G2lyeHM_6KCKG2POYbaMn7JRPISxqU-B0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/FNljlSKY27oyEamJ-kOAGfmREV-YPwiFTNjtAE0hpfX05i1oi76UbzjdrIdavD9I2o2aH8eAMXczd9olJFQEOdWNDmjSiAQdiT3-Uv38p_G2lyeHM_6KCKG2POYbaMn7JRPISxqU-B0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/oAEliioMQ1NKraWV8-8TlX4NPEDIQpv562hlLS9xpKyiOMByvaoFjxqK-dXsCptea6zeJCFec7gxKkctY1TphliI8cyjCOptyOCQYReyU82GE6C9_kXyNTw4O0xqJq7f6j3lVcYi5Dk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/oAEliioMQ1NKraWV8-8TlX4NPEDIQpv562hlLS9xpKyiOMByvaoFjxqK-dXsCptea6zeJCFec7gxKkctY1TphliI8cyjCOptyOCQYReyU82GE6C9_kXyNTw4O0xqJq7f6j3lVcYi5Dk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/whZlCuGL9TaGJrmRLBascYGhL8JWCl8-mO-foUoLmB5iRUSiECsCh2qltxDqM92_gHu_X5lHIAwv-qiSBQyVG2k28qoAWEmEr1p13tSgx5bXf5JrkZ2gylvZw1yLowecqfQv7Z4DGJg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/whZlCuGL9TaGJrmRLBascYGhL8JWCl8-mO-foUoLmB5iRUSiECsCh2qltxDqM92_gHu_X5lHIAwv-qiSBQyVG2k28qoAWEmEr1p13tSgx5bXf5JrkZ2gylvZw1yLowecqfQv7Z4DGJg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/oH9Dmloxxsii4TfVIbjvAwSl3iJ5_c8KZskwQiOgdllyxKqneawCxS4jodmW7J1tvzx1aFHrAS5b6W2nXySPGaM2BxwHzhvgAJ-HSmMkYiXMIXXPLEFRF8S2I9QJSe4blSk5Xj1dNsA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/oH9Dmloxxsii4TfVIbjvAwSl3iJ5_c8KZskwQiOgdllyxKqneawCxS4jodmW7J1tvzx1aFHrAS5b6W2nXySPGaM2BxwHzhvgAJ-HSmMkYiXMIXXPLEFRF8S2I9QJSe4blSk5Xj1dNsA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/aqucf2eT7-PrghtlcPlTlvFSmWu0DLRoJfxGZgL8azctw53jEcM2BnB1O3CMYFxxjKVwzMF_OFbw98J804CXWkIJdE8fkgSAKaxsm7UIeDK23bgxZqkb2LiZSYIc5njsTAjvcv7007Q=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/aqucf2eT7-PrghtlcPlTlvFSmWu0DLRoJfxGZgL8azctw53jEcM2BnB1O3CMYFxxjKVwzMF_OFbw98J804CXWkIJdE8fkgSAKaxsm7UIeDK23bgxZqkb2LiZSYIc5njsTAjvcv7007Q=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/JIlZ3Ml_5q_4qj4xFeLYm04RY8nQpmmjwMrWgSjr2vjCusugi3iVvIR8bzRovt98r9xNVNv3PYTDaTgtHnKRFip67a6NWa_toqWV9Ze5ySYWuKYPTkbSDy3YNZ3e-bjN7UDpzbKjHOU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/JIlZ3Ml_5q_4qj4xFeLYm04RY8nQpmmjwMrWgSjr2vjCusugi3iVvIR8bzRovt98r9xNVNv3PYTDaTgtHnKRFip67a6NWa_toqWV9Ze5ySYWuKYPTkbSDy3YNZ3e-bjN7UDpzbKjHOU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/omquDlLALLGH9W-xUd70-c4tFlgDYhccce4K2Zk4NDEy26F_cFDDqn1voSrGfMed2NCBihAJyq37WhIpNjh4xXwBSdGP3JiCPPpa26TksTJJ15j7Yp9PtR1HqRBKm1mRuKolpmIG7L0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/omquDlLALLGH9W-xUd70-c4tFlgDYhccce4K2Zk4NDEy26F_cFDDqn1voSrGfMed2NCBihAJyq37WhIpNjh4xXwBSdGP3JiCPPpa26TksTJJ15j7Yp9PtR1HqRBKm1mRuKolpmIG7L0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AaJkslzzSPns7n6Ll3JD6NOgjCShwfbAZpaEbEssTElhfr-wiMk6HVxGcbscKIX8eK94xbafgOxESxmykfadzsAHYhpefD82FVV2ULNOskx608KxlrAoqtOQH38Hu-jx8O3S0KYb2dc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AaJkslzzSPns7n6Ll3JD6NOgjCShwfbAZpaEbEssTElhfr-wiMk6HVxGcbscKIX8eK94xbafgOxESxmykfadzsAHYhpefD82FVV2ULNOskx608KxlrAoqtOQH38Hu-jx8O3S0KYb2dc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/iBRfVR8ULl3fl4bDunlG4Sb3Jy9g3IFWaA-l7tzbQcCU9WW8FGegok_D0qQEkpsmZkFFq8ErtY_LbmW1ACq8KZ19AeLqidM0AFySx1mlJXPWoy-g3xHJ2gFKVwPxLkgwlJVnvSo0yhs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/iBRfVR8ULl3fl4bDunlG4Sb3Jy9g3IFWaA-l7tzbQcCU9WW8FGegok_D0qQEkpsmZkFFq8ErtY_LbmW1ACq8KZ19AeLqidM0AFySx1mlJXPWoy-g3xHJ2gFKVwPxLkgwlJVnvSo0yhs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/7-EMgedUQxcD7lJIMZrIrNEOxLk7nIUgVhL7uu7lRHFTv0NShFd6FGmDeH60nhY2HKBhG8JI_xblJxd8CMzoD11YIf6RkvnB93FnkfGRGErl723iz_XuQbtF6TjbPVA1mEqOz12_HwE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/7-EMgedUQxcD7lJIMZrIrNEOxLk7nIUgVhL7uu7lRHFTv0NShFd6FGmDeH60nhY2HKBhG8JI_xblJxd8CMzoD11YIf6RkvnB93FnkfGRGErl723iz_XuQbtF6TjbPVA1mEqOz12_HwE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Doukyonin wa Hiza TAU',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.86',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WVaayJsK7-07avIGd4mFYgmr9APpB4tr3gM0QyPCx5hf3kzJW8tT0_R_SvIhjmw4vYrmmzEAhd7Fc3ONuAWMs5k4cEjjjvpUzct1pvla6nbJJ49ef52tjFMXmOnSEOv_hQPfNCJzST8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WVaayJsK7-07avIGd4mFYgmr9APpB4tr3gM0QyPCx5hf3kzJW8tT0_R_SvIhjmw4vYrmmzEAhd7Fc3ONuAWMs5k4cEjjjvpUzct1pvla6nbJJ49ef52tjFMXmOnSEOv_hQPfNCJzST8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
