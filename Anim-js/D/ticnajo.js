5db3c8dfd396614350bbe89efa0dd556e6eb517e
dunia-anime <adityha.on@gmail.com>
2019-11-27 21:10:59 +0800

    Create ticnajo.js

---
+++ Anim-js/D/ticnajo.js
@@ -0,0 +1,324 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/lJfKEgE16aEuLOL6UR26yURV4KJUVOfxARfITiDN9EOOIN8qMNhUyV7woXGfbpzpHGZa0aYr9b5e4AEDbFHGmd3wIWDSn9RFVWyr7zWEXWqvzC8h6XSZ1ij6xlRf9f_S-wzpYB81kzo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/lJfKEgE16aEuLOL6UR26yURV4KJUVOfxARfITiDN9EOOIN8qMNhUyV7woXGfbpzpHGZa0aYr9b5e4AEDbFHGmd3wIWDSn9RFVWyr7zWEXWqvzC8h6XSZ1ij6xlRf9f_S-wzpYB81kzo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/QOaH_phMDH0eMI95doxQmAkfWKtp7PWz51byCacwRZBGbyKR2bBWrY6sLq0KoudtDBFSaAxjofqLhCEfPShFsODd-WeK3djkP0ssWOgwzmHKRJPIHG0nnhEAune-d76IW8OaFiBMFb0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/QOaH_phMDH0eMI95doxQmAkfWKtp7PWz51byCacwRZBGbyKR2bBWrY6sLq0KoudtDBFSaAxjofqLhCEfPShFsODd-WeK3djkP0ssWOgwzmHKRJPIHG0nnhEAune-d76IW8OaFiBMFb0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Nlm7goeLuzudU79HHWWgb5grqZGLTp-rBAIk61sd8djuDtpkgyZhdsqlDD_jhiw8_eBVItOWLYE-q7HocwTHLgvQrPiK2r2A9cThweW1q11su5KABbiNvPFKeW6EVQiHiBBIc5IxUxk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Nlm7goeLuzudU79HHWWgb5grqZGLTp-rBAIk61sd8djuDtpkgyZhdsqlDD_jhiw8_eBVItOWLYE-q7HocwTHLgvQrPiK2r2A9cThweW1q11su5KABbiNvPFKeW6EVQiHiBBIc5IxUxk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/R567ln1mXzcF4K7dd3eH9T1QfDVI6jxmNu59ZudcH_03sas-hbhaPSKlDHwudNSlMz_G503QqmNdBAAOA1nbkG23KTDuVW_7nyN18X4HxWIWHeziVQF9NFFRNZ01UT2fOp4LI1jHXxg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/R567ln1mXzcF4K7dd3eH9T1QfDVI6jxmNu59ZudcH_03sas-hbhaPSKlDHwudNSlMz_G503QqmNdBAAOA1nbkG23KTDuVW_7nyN18X4HxWIWHeziVQF9NFFRNZ01UT2fOp4LI1jHXxg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/At4VLwiXJmU6-2_u_Xb9FEfZWKTeJSNdrnbnVBKt9KdWIVrHbNdk3twCUbnUfLRWxt-S2f9bymi6OtEbWBU5zA_1VbNxMYtRGEvd5nAlVTcSu4vUn3uMndVA_IV7DJtBhZQAP9JIRV8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/At4VLwiXJmU6-2_u_Xb9FEfZWKTeJSNdrnbnVBKt9KdWIVrHbNdk3twCUbnUfLRWxt-S2f9bymi6OtEbWBU5zA_1VbNxMYtRGEvd5nAlVTcSu4vUn3uMndVA_IV7DJtBhZQAP9JIRV8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/41XoVzQiQ2OiJwKwl4ys1GB7pDoA0FbCxJfFj5k0P6N3XWhdernk0pP7kNXend4IHpbviKydX2eVEniD-xpfwn-IHHDZxmjNQGo-7K2_gr2Zu1LMfYQ5v3sib7fdvZrpRWOAkuyMWOI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/41XoVzQiQ2OiJwKwl4ys1GB7pDoA0FbCxJfFj5k0P6N3XWhdernk0pP7kNXend4IHpbviKydX2eVEniD-xpfwn-IHHDZxmjNQGo-7K2_gr2Zu1LMfYQ5v3sib7fdvZrpRWOAkuyMWOI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/X8vTr4N9yMPdhoF0966J4aupZLXpjTWRpawd2Pv6NotSFt5RAEroxxeWSrhszbADCBWNN7cba338LFYYSQfBmx1wggzQI9m9fL-Vc_M4v7dUqLnPzaWMITv9XSZEX5PKn4d71IGmoYk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/X8vTr4N9yMPdhoF0966J4aupZLXpjTWRpawd2Pv6NotSFt5RAEroxxeWSrhszbADCBWNN7cba338LFYYSQfBmx1wggzQI9m9fL-Vc_M4v7dUqLnPzaWMITv9XSZEX5PKn4d71IGmoYk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/YVFSdvj1Y-UZfp7ZSIFyHbLLw2cFdQmze4bAZZBJTNu8tV8DVejWD1nNBw4_JmxZpNLsHpldVc2qXiNh4npx245MZlGgeLos-7R0qmUr3uqQ0OAMxwF0nWXqiFSx2s9t7WrgUypaLhw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/YVFSdvj1Y-UZfp7ZSIFyHbLLw2cFdQmze4bAZZBJTNu8tV8DVejWD1nNBw4_JmxZpNLsHpldVc2qXiNh4npx245MZlGgeLos-7R0qmUr3uqQ0OAMxwF0nWXqiFSx2s9t7WrgUypaLhw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Gq2BJkRqdlLH2bZzqMS6zdhCRdjKmUC03Mluk-XzZp5Kabh87vq3XFoy7wvfmZyew_ekC0yhzjPePiGs0T93OVkVhUYaNQAPy75ibeMa4Dmahct0MyC1O92kW_lbnQG_ItUaqk03xlE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Gq2BJkRqdlLH2bZzqMS6zdhCRdjKmUC03Mluk-XzZp5Kabh87vq3XFoy7wvfmZyew_ekC0yhzjPePiGs0T93OVkVhUYaNQAPy75ibeMa4Dmahct0MyC1O92kW_lbnQG_ItUaqk03xlE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/RhWiRHRG_FVjluqUdrx1vKzG7MfvYs121IdtsrbbAhLClO8DuDfuY9Sx8hAQOFHqZOsXc1vM4fg4hRmef7s0aV9pn14usWgKYBhdd6uR1UtEwaoZI8rDpAw69Kkxr61gDfxSc9PgdHU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/RhWiRHRG_FVjluqUdrx1vKzG7MfvYs121IdtsrbbAhLClO8DuDfuY9Sx8hAQOFHqZOsXc1vM4fg4hRmef7s0aV9pn14usWgKYBhdd6uR1UtEwaoZI8rDpAw69Kkxr61gDfxSc9PgdHU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Domestic na Kanojo',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.13',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/wZUqljPvz9HkTZ4KRuyVAogZdaDxAX5xMD7ldIljD-v1s8GNhFtp2XLxMGfAc4W5qsAIqcBohxuPM7Ooyz9HWH7KnkXZ_gDnn24a3a98c-mBPDAdxJC6F-3lyw6qxEHMFU9pixJVmvA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/wZUqljPvz9HkTZ4KRuyVAogZdaDxAX5xMD7ldIljD-v1s8GNhFtp2XLxMGfAc4W5qsAIqcBohxuPM7Ooyz9HWH7KnkXZ_gDnn24a3a98c-mBPDAdxJC6F-3lyw6qxEHMFU9pixJVmvA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
