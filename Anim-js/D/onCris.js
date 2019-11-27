dccaf843ac15488570011ce4dfaae52281415665
dunia-anime <adityha.on@gmail.com>
2019-11-27 18:52:10 +0800

    Create onCris.js

---
+++ Anim-js/D/onCris.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Dragon Crisis',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/oy9jpGaUjna07dR9U_hinU-j34MdROKqTUALOUCIk2y_FjKyomBNzwFS2VdDh8oziJQpN3pqGOQkih5-xkjYtoVuSJuqetUFus9qd0dO0L7oLFL9q-WTCBjp4rmvLKg31wC1xtGNkEQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/oy9jpGaUjna07dR9U_hinU-j34MdROKqTUALOUCIk2y_FjKyomBNzwFS2VdDh8oziJQpN3pqGOQkih5-xkjYtoVuSJuqetUFus9qd0dO0L7oLFL9q-WTCBjp4rmvLKg31wC1xtGNkEQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/DNSnzU0RAjaJ-z7fbqTLTWPngZp3RPoZGJGskAtfJ9b67IEYIPfk778wtnOxvihwFoZJe1cP6uHev7FgpzqpQoXWcOLrvYQxL6xW0PNM2-t6UM9lWC22jRItwbgg6gtM2M7Ro7zqj8Q=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/DNSnzU0RAjaJ-z7fbqTLTWPngZp3RPoZGJGskAtfJ9b67IEYIPfk778wtnOxvihwFoZJe1cP6uHev7FgpzqpQoXWcOLrvYQxL6xW0PNM2-t6UM9lWC22jRItwbgg6gtM2M7Ro7zqj8Q=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Hgy8z6R4FesnteAW0k-pMaj76gY0oH-mR8vE1R_K5Ggt60RXg5QAKF2WWjna26LKHnGUD3nWHvRIzTVAC_cplBXIroSQDVO9s-gGmfxst_UphEjo5_0wldjfs5vQFFfEv4NlbDPKHQ0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Hgy8z6R4FesnteAW0k-pMaj76gY0oH-mR8vE1R_K5Ggt60RXg5QAKF2WWjna26LKHnGUD3nWHvRIzTVAC_cplBXIroSQDVO9s-gGmfxst_UphEjo5_0wldjfs5vQFFfEv4NlbDPKHQ0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/sVigsSrCZhPT-fm3bWtPyKrylpRfKp7wVyPrjR3rKgWJUAbXAshNuA9DZPGAWevL6zGQmmfWGhxTLObxtdfVqVYTBsBApbYu7hV7r6JNNN11llqY5uzOfzU1KwcyGgyH3KbtLnnh90o=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/sVigsSrCZhPT-fm3bWtPyKrylpRfKp7wVyPrjR3rKgWJUAbXAshNuA9DZPGAWevL6zGQmmfWGhxTLObxtdfVqVYTBsBApbYu7hV7r6JNNN11llqY5uzOfzU1KwcyGgyH3KbtLnnh90o=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/axg2pV8CWptV61aq8lArSnjflnnaPlZ2WGWFg12ev4WTVgVHIVF8ELhuuYZGwa2nJkjup0ftfF9Ej_pZbMkdlJGbm6FaQflYcP-_83uEdBo651wk5DHShaKyAWUkLofgGx33x33_vD8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/axg2pV8CWptV61aq8lArSnjflnnaPlZ2WGWFg12ev4WTVgVHIVF8ELhuuYZGwa2nJkjup0ftfF9Ej_pZbMkdlJGbm6FaQflYcP-_83uEdBo651wk5DHShaKyAWUkLofgGx33x33_vD8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WeHstC1ckL3e1uxIhBlAAEQEvbNDrweOHzOJTdSCzR7Og6c2qFJU6KJYFXLzXk3N4Mjt78qKJMIglqgP-ccNIa6krk3Wpbizix-qwvadjW_JbQoYs_hg4kLwa1hYiHtwE64PxL-Yytw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WeHstC1ckL3e1uxIhBlAAEQEvbNDrweOHzOJTdSCzR7Og6c2qFJU6KJYFXLzXk3N4Mjt78qKJMIglqgP-ccNIa6krk3Wpbizix-qwvadjW_JbQoYs_hg4kLwa1hYiHtwE64PxL-Yytw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/NCUj8gh5cvrxOAHRrAM4WrBsB-AIiBZ5dJ-nccKJlfSLMTcnkSE709pKKqGDYG3aNSIQaacZ8otGN5Egic9Gio2zBxbqZ9LZGUuBEbBtagJY1aPFDuyKyT-CkDGY7HDK8O3LJf0gF7w=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/NCUj8gh5cvrxOAHRrAM4WrBsB-AIiBZ5dJ-nccKJlfSLMTcnkSE709pKKqGDYG3aNSIQaacZ8otGN5Egic9Gio2zBxbqZ9LZGUuBEbBtagJY1aPFDuyKyT-CkDGY7HDK8O3LJf0gF7w=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/603FLqZjp0ckZF2NPolo-NK0EDnnt6lCCYSF3zvRZTSIllVZD4XRhI5MiZ5RGLTGOL4Rdr03XuhPvoQqFH78btK2wbktMe0EITXWg09hwguVb09fKjTCJJLDyCNDTKeTvXMibpC60Kk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/603FLqZjp0ckZF2NPolo-NK0EDnnt6lCCYSF3zvRZTSIllVZD4XRhI5MiZ5RGLTGOL4Rdr03XuhPvoQqFH78btK2wbktMe0EITXWg09hwguVb09fKjTCJJLDyCNDTKeTvXMibpC60Kk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SC_UICNp2Nc13-ZZukq6STie6IVolbEEfarwWsD13W1bY48MuhmKvtf_seu5Gqs4D0YGpc_KtMGvhF9BePe7O74WKBgtDQEBiKVoA5PQ6JJiOzUl10IBzldsQvxmz4LUF9MspIooGn8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SC_UICNp2Nc13-ZZukq6STie6IVolbEEfarwWsD13W1bY48MuhmKvtf_seu5Gqs4D0YGpc_KtMGvhF9BePe7O74WKBgtDQEBiKVoA5PQ6JJiOzUl10IBzldsQvxmz4LUF9MspIooGn8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/NesC1mFAGXaw2xy-4gLtrXSvMf-UABjCMgTG0rihEiG9h47tsxft7AnN9IhEyOK4awM4SmK5E6Bmbx-cgHJMJm_h0Q2Sohu1Rch4IF9EAvsIMVyKVjuJojYAEgmeeO_6EvqybNO1c9A=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/NesC1mFAGXaw2xy-4gLtrXSvMf-UABjCMgTG0rihEiG9h47tsxft7AnN9IhEyOK4awM4SmK5E6Bmbx-cgHJMJm_h0Q2Sohu1Rch4IF9EAvsIMVyKVjuJojYAEgmeeO_6EvqybNO1c9A=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9XpeB33D3i4PyHyhZvdOFXq1t10oGiNL7OacyTqb6VsoNfvCGRmfMWvHk4Ig7lx-hVkxK0M0efkWCFR3oguSaI-o9shb1vZxLoUc8yOESSJOuzSGvOjZoShqU_CsQzYrKWOuFC51lLc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9XpeB33D3i4PyHyhZvdOFXq1t10oGiNL7OacyTqb6VsoNfvCGRmfMWvHk4Ig7lx-hVkxK0M0efkWCFR3oguSaI-o9shb1vZxLoUc8yOESSJOuzSGvOjZoShqU_CsQzYrKWOuFC51lLc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dragon Crisis',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.80',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/paWRYfABY5peWN5c7Cz0MZa8geuPpFzqIaACpYt3cx55kfC4Iersv3xcT6KwjrQVF2KkA5XLyxPZTW-4owOmhxKeURj6CEO_7XaTG-Es78ZNePzEX0_YhucKgBHJf9VU5tBmpgPQlQ0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/paWRYfABY5peWN5c7Cz0MZa8geuPpFzqIaACpYt3cx55kfC4Iersv3xcT6KwjrQVF2KkA5XLyxPZTW-4owOmhxKeURj6CEO_7XaTG-Es78ZNePzEX0_YhucKgBHJf9VU5tBmpgPQlQ0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
