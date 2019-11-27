2f9928fa7afc174282b9848be979d699b2a4ea1c
dunia-anime <adityha.on@gmail.com>
2019-11-27 22:02:11 +0800

    Create ive.js

---
+++ Anim-js/D/ive.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Dive!!',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/DFzgv3h85MhbyDRstUV3HB_kQOOYvPu1Uo3tuinkjn-_tnpiEjSftlUagRltpofM1JslpQosEkomkS2pf-XI2-o5R_KIg6pRQSKkz2hrOO1x-5lnndHxwxCii63OZr0lMjnaZwjs_98=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/DFzgv3h85MhbyDRstUV3HB_kQOOYvPu1Uo3tuinkjn-_tnpiEjSftlUagRltpofM1JslpQosEkomkS2pf-XI2-o5R_KIg6pRQSKkz2hrOO1x-5lnndHxwxCii63OZr0lMjnaZwjs_98=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/dEk7SNBsU6FegCioPkcAFFwWPVK9_e206WfbCAQUPaQx2eKzQCouDQuJ7gZitAUsy1C4yWLexBDigwgPNcIJ2d0nnAdd7lVOncJerQ8AHqW6NhBmSMEqPNialFM8_jnFVXedQJF7m5o=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/dEk7SNBsU6FegCioPkcAFFwWPVK9_e206WfbCAQUPaQx2eKzQCouDQuJ7gZitAUsy1C4yWLexBDigwgPNcIJ2d0nnAdd7lVOncJerQ8AHqW6NhBmSMEqPNialFM8_jnFVXedQJF7m5o=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/cUEWTOdcJ0vQebbm1X8VfGr4ZtaG7WZO2SSoFctftRJkGnQ2CowA18bbp9NU84zwI-KahoC2uOK9-L2A8cLFenHKKnQeaU4WwxHAdSd9q9Y05xykNFUn43GwafU7jE3yvvy8L6hcjnQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/cUEWTOdcJ0vQebbm1X8VfGr4ZtaG7WZO2SSoFctftRJkGnQ2CowA18bbp9NU84zwI-KahoC2uOK9-L2A8cLFenHKKnQeaU4WwxHAdSd9q9Y05xykNFUn43GwafU7jE3yvvy8L6hcjnQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/7-rLdhxkkbSl0EFJkzbjSlzCKuInmXNGYLu5DLTChINwawhyLHrDS5XiRD653qJi-rHlkWBmF-Ykw_xmu4mA8avZqI5uJ15NP3p_aCOeTrbF0hGg-n8ksEJ5nDogbRSrjEYFJeYumYY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/7-rLdhxkkbSl0EFJkzbjSlzCKuInmXNGYLu5DLTChINwawhyLHrDS5XiRD653qJi-rHlkWBmF-Ykw_xmu4mA8avZqI5uJ15NP3p_aCOeTrbF0hGg-n8ksEJ5nDogbRSrjEYFJeYumYY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/dwLw_zbtGnTckfwN6M6lIZOeJpru2clEDno1xhQ9MbkOu283S8C4GeB6xFfbM2i0sCdO8Ui5K55syh2B_-ak6_1IMOGwbFOkNbmntWmdBgd-a5AUGApwmR8t7uTl8EuFX483A21RT8M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/dwLw_zbtGnTckfwN6M6lIZOeJpru2clEDno1xhQ9MbkOu283S8C4GeB6xFfbM2i0sCdO8Ui5K55syh2B_-ak6_1IMOGwbFOkNbmntWmdBgd-a5AUGApwmR8t7uTl8EuFX483A21RT8M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/QqlnO_lEyOKWYFHbT86y__K5bXclN0o_AVVtBEhOI1YqeKmfqDPMrbuEOLcr8QF6i2Sj7mvGOAGpZLBfk0YMoSLZw4WYEVAENXPUzU_zvLOJY-ga-sSMZdSUrcKeZuFl4YpXLKPn94o=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/QqlnO_lEyOKWYFHbT86y__K5bXclN0o_AVVtBEhOI1YqeKmfqDPMrbuEOLcr8QF6i2Sj7mvGOAGpZLBfk0YMoSLZw4WYEVAENXPUzU_zvLOJY-ga-sSMZdSUrcKeZuFl4YpXLKPn94o=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/51LE1afleezOlK8XdpbSzZpCx6dY1p9jukbpoJ7bkR46qr7rMIe-8RiJmCuv6SHXkKggTj6M2T_KOMkrO5cpR7gLrSFLemYz8cmpfisuGvzeTsnk5iRqcKQSQg5Yf0eMBxFcMsUzNq4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/51LE1afleezOlK8XdpbSzZpCx6dY1p9jukbpoJ7bkR46qr7rMIe-8RiJmCuv6SHXkKggTj6M2T_KOMkrO5cpR7gLrSFLemYz8cmpfisuGvzeTsnk5iRqcKQSQg5Yf0eMBxFcMsUzNq4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/e4Cn_0NE8IO4BB60uudZZLhvdbUZ9x48O6dJvJiR3cg2UrWXQeckZTe4ErlQ2qwKJ2qj1z-n1u306I415eYhs7c4H7cg0-FL-NROoSFw2ABcXFFN4qlwPS8LvrM3fVOuB_RpG6w6dO0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/e4Cn_0NE8IO4BB60uudZZLhvdbUZ9x48O6dJvJiR3cg2UrWXQeckZTe4ErlQ2qwKJ2qj1z-n1u306I415eYhs7c4H7cg0-FL-NROoSFw2ABcXFFN4qlwPS8LvrM3fVOuB_RpG6w6dO0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/zPEe8KiEczI1W8GD2CTLsUl0xbPbWgoYs_z_NtMkT_gqsFwlNqwTMjI2RaMsdHMhbnr87G0xNQ-4kepUS8_yDXr7A4cuRo2FtfwnAPcym4LNIpoeixLZGlDPoTUz70myv570ZQ6Ml48=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/zPEe8KiEczI1W8GD2CTLsUl0xbPbWgoYs_z_NtMkT_gqsFwlNqwTMjI2RaMsdHMhbnr87G0xNQ-4kepUS8_yDXr7A4cuRo2FtfwnAPcym4LNIpoeixLZGlDPoTUz70myv570ZQ6Ml48=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/goLoCD4EQTWaxWtPiKC3W038NHqZQcZrcW415jOJXJrf2uy2q1nU32SjTEDqrNFkm9LrBimGEYFnow-wB7hsRr-hrMq-ehmSjpYZ4XsT9qbf9cRjSQ9qLHKi45b3nRf97AxNzoJuKc8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/goLoCD4EQTWaxWtPiKC3W038NHqZQcZrcW415jOJXJrf2uy2q1nU32SjTEDqrNFkm9LrBimGEYFnow-wB7hsRr-hrMq-ehmSjpYZ4XsT9qbf9cRjSQ9qLHKi45b3nRf97AxNzoJuKc8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/PPmLW9KS24DUMw86recWTewSGxNxg3BBqQt3X0QCxaKukGCML-9kHg25mQM5Asqzx7s3DN7622Tie769dKA3P0B6uGPrMXZ8lz4FABeUz8AyhmOLttQp2MTAMydMoXcxZI3cCxH9Ny4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/PPmLW9KS24DUMw86recWTewSGxNxg3BBqQt3X0QCxaKukGCML-9kHg25mQM5Asqzx7s3DN7622Tie769dKA3P0B6uGPrMXZ8lz4FABeUz8AyhmOLttQp2MTAMydMoXcxZI3cCxH9Ny4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dive!!',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.15',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/zq-64C4Zph2K6B9_qyZz7XnH7CGqWrIwzgv9miMSJ4K8mtr93X2Y9vdSDQYulK16Ym7UVrkkFuntpH7cP4kY540fpduJz-jGxErl6p3aScQh-e7H613JGFQ_j39PWcOgmbz-_6szkhs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/zq-64C4Zph2K6B9_qyZz7XnH7CGqWrIwzgv9miMSJ4K8mtr93X2Y9vdSDQYulK16Ym7UVrkkFuntpH7cP4kY540fpduJz-jGxErl6p3aScQh-e7H613JGFQ_j39PWcOgmbz-_6szkhs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
