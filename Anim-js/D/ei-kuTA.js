9fb32e9c2567750ee994bc2010b6b74dc6f6d5df
dunia-anime <adityha.on@gmail.com>
2019-11-27 21:05:54 +0800

    Create ei-kuTA.js

---
+++ Anim-js/D/ei-kuTA.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/CXxOHoyUsQTn-pNxb5m7YFmmi--rIFMH6xbNshY-2te4rPJ6fNWG9oGGisTzZ5RdcPU-3V2OwSPiKpdHvLrBhXjJDN2L5Tx-yQJvYIaRkhEakMvUQ7G0nqAYntWwikEmdq1xpafdUTQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/CXxOHoyUsQTn-pNxb5m7YFmmi--rIFMH6xbNshY-2te4rPJ6fNWG9oGGisTzZ5RdcPU-3V2OwSPiKpdHvLrBhXjJDN2L5Tx-yQJvYIaRkhEakMvUQ7G0nqAYntWwikEmdq1xpafdUTQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/1KeC4fKL13NLkdR5mehExrxmSCUvRhrOOX_dlFA357Y0UCjY25sHaG5NpxvdNwp7Z2QIMR7JYzgqL5t2Weh2MAtro_Tx0gKMZfnD8M2Jw0Mo-cnhjNiIuapzAdFn-XlJriAsa_u4110=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/1KeC4fKL13NLkdR5mehExrxmSCUvRhrOOX_dlFA357Y0UCjY25sHaG5NpxvdNwp7Z2QIMR7JYzgqL5t2Weh2MAtro_Tx0gKMZfnD8M2Jw0Mo-cnhjNiIuapzAdFn-XlJriAsa_u4110=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_4qWyJJYq-FZIdJr_6xEc7YmD7vHvevp-SdjR6TK4-dmyLx760jBq_zl9Jm0CGPjNk6JaDuHI4kapO8rHbX6pgfgxtX0OqZ_nFG3dlKqVPGDhskM33duqGm-d4HabITt8Z7UpSKi2jg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_4qWyJJYq-FZIdJr_6xEc7YmD7vHvevp-SdjR6TK4-dmyLx760jBq_zl9Jm0CGPjNk6JaDuHI4kapO8rHbX6pgfgxtX0OqZ_nFG3dlKqVPGDhskM33duqGm-d4HabITt8Z7UpSKi2jg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/I46-bUOG7TF0pCAv-82TboGDxwGn6ZqwiyVLjyGMlQ-tReZM5SQoZFbaj8cTJ8bIJ_6R7hPN1du4fxW8jX602acmdzlSwC8gkicccotUA-XYpV3rzO6CQ7Xzhfx_khkoobFC3OVfQRQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/I46-bUOG7TF0pCAv-82TboGDxwGn6ZqwiyVLjyGMlQ-tReZM5SQoZFbaj8cTJ8bIJ_6R7hPN1du4fxW8jX602acmdzlSwC8gkicccotUA-XYpV3rzO6CQ7Xzhfx_khkoobFC3OVfQRQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZDhEZsbqSgUGmxr-vvECiVKHwQZdgtnT1rnMOZ0HNY5QYsdZ8WWEJtzY9TpT5Koy9NxvWwUQKfISgtU8d5RsLgZM8bmjIpQeyB079MeAJpzvD-w2tGw_Tqddt13nmpz3YuP2WWtqDyo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZDhEZsbqSgUGmxr-vvECiVKHwQZdgtnT1rnMOZ0HNY5QYsdZ8WWEJtzY9TpT5Koy9NxvWwUQKfISgtU8d5RsLgZM8bmjIpQeyB079MeAJpzvD-w2tGw_Tqddt13nmpz3YuP2WWtqDyo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jux21nYAS-i5FLWuBn-S8bQYXIBdUN_uyA3R1z2E-VQ3jSXUrkMbGsxa9EvpccUW8h7pirAaOjkDsF_tugfTo09zFs5TL_qT-i3svYKuPrPVuOvHalSINx8HrBYZeUs3GBfmrArkyeI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jux21nYAS-i5FLWuBn-S8bQYXIBdUN_uyA3R1z2E-VQ3jSXUrkMbGsxa9EvpccUW8h7pirAaOjkDsF_tugfTo09zFs5TL_qT-i3svYKuPrPVuOvHalSINx8HrBYZeUs3GBfmrArkyeI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/OBGj-Jd9c1Dsp1x1vTRnBcELLOyil7L2ElLvnQ6cthVteEqHZBnSGuIGiY1trJfUna6gSsIcVUhD7xcalJsevPte-6Ve3nj7DMqKKH2d3BIEyqxjN3hw5XPx8fPj6-GF1ZriIpE4oYw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/OBGj-Jd9c1Dsp1x1vTRnBcELLOyil7L2ElLvnQ6cthVteEqHZBnSGuIGiY1trJfUna6gSsIcVUhD7xcalJsevPte-6Ve3nj7DMqKKH2d3BIEyqxjN3hw5XPx8fPj6-GF1ZriIpE4oYw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/j1sNn1NkmuSmbovW4LVZaQVxj5dAJrE1pAzZq_3WLmJVsfuXtxqUXE01yG0h32uN4lhsP9bX3__7r8vFiPhu-UY4F_a_lpcU5kqXHuHODc214TlPpqtAdbnqxb5nvQ_3z6JxMImyplg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/j1sNn1NkmuSmbovW4LVZaQVxj5dAJrE1pAzZq_3WLmJVsfuXtxqUXE01yG0h32uN4lhsP9bX3__7r8vFiPhu-UY4F_a_lpcU5kqXHuHODc214TlPpqtAdbnqxb5nvQ_3z6JxMImyplg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_JnvSaEysMwyJd87puIlLXBMe1U8ODoFujWehRPnnSPN5rDWYwQqSzU68I7GihwQCA6B-awAnpEknl4mV_DoIVBX43w-5-UEfESDLxOg5LlfDNJ290eCYgdNHcRUlKQwHW_qh_8Kzt0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_JnvSaEysMwyJd87puIlLXBMe1U8ODoFujWehRPnnSPN5rDWYwQqSzU68I7GihwQCA6B-awAnpEknl4mV_DoIVBX43w-5-UEfESDLxOg5LlfDNJ290eCYgdNHcRUlKQwHW_qh_8Kzt0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/phTDqrQiKSJFIrUA6rDh5oKVwCd3kfFrAiPCcVfCnOqdYiwQcXwGc2TXavaGQCcxly3KZZ5Bd7BZdGRnUG3l6I_n4wgQp4sTscBTVgealMThZAHoBWbM1DLS4D8TmOJ_EyFZ-6KVMHE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/phTDqrQiKSJFIrUA6rDh5oKVwCd3kfFrAiPCcVfCnOqdYiwQcXwGc2TXavaGQCcxly3KZZ5Bd7BZdGRnUG3l6I_n4wgQp4sTscBTVgealMThZAHoBWbM1DLS4D8TmOJ_EyFZ-6KVMHE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/54iBCVa0fBsGifynkpyyPLbvyBkaFPifSpaT9g3JRA6qFt0dC7W_GpZEHIp-ZDAjCICYIMLmZ_t5Iyo0ZxhY5ETlTmmGWMxcMLfrcaKFe1EIF2A6VQSsN3dIV2-pY_19lNupiJVjRRU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/54iBCVa0fBsGifynkpyyPLbvyBkaFPifSpaT9g3JRA6qFt0dC7W_GpZEHIp-ZDAjCICYIMLmZ_t5Iyo0ZxhY5ETlTmmGWMxcMLfrcaKFe1EIF2A6VQSsN3dIV2-pY_19lNupiJVjRRU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dorei-ku The Animation',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SWDGmTxSysg_ISkqz2f9jJSb8jDWg-DLq3ElC6jRctslu9v6a0vu9_GKojM4Swoe241Ykuqu8SYprb20yMzRUkHciEkHir1fTv7hwACXC_9Nghg2f6wXRHQ_7NLPrYo7UcYnNWdb6mM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SWDGmTxSysg_ISkqz2f9jJSb8jDWg-DLq3ElC6jRctslu9v6a0vu9_GKojM4Swoe241Ykuqu8SYprb20yMzRUkHciEkHir1fTv7hwACXC_9Nghg2f6wXRHQ_7NLPrYo7UcYnNWdb6mM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
