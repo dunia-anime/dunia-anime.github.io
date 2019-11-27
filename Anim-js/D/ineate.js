0cb7f92f4c6aae8de931c9959df014fdda9c53a1
dunia-anime <adityha.on@gmail.com>
2019-11-27 21:55:14 +0800

    Create ineate.js

---
+++ Anim-js/D/ineate.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Divine Gate',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/onl8b8Vw6mxmmuUeliWW0lLPYbKxKI9T8jXLimvOXkCQosnOiuKHQJJxYl-ra1I5kfWslX0X6RFMG9QVqZaG7tTOJZOoGzFprIYnBj9aq2U-cqY76oSTlSrO2C--Wvlo6fqYPXZbjMY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/onl8b8Vw6mxmmuUeliWW0lLPYbKxKI9T8jXLimvOXkCQosnOiuKHQJJxYl-ra1I5kfWslX0X6RFMG9QVqZaG7tTOJZOoGzFprIYnBj9aq2U-cqY76oSTlSrO2C--Wvlo6fqYPXZbjMY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ttVr4du5r4ceMPNxtqHQRaR4QPTVfvqHdIU_5MF0ZEi_MY8ylKK-kt7gOO-_rgNBKlSrkcAcyRvrSDrYRZfEN3FqSVENexEl5h19t9xVvU4GL8f8Q-tCbZhpxORE8LQ0GVtEh54OKWo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ttVr4du5r4ceMPNxtqHQRaR4QPTVfvqHdIU_5MF0ZEi_MY8ylKK-kt7gOO-_rgNBKlSrkcAcyRvrSDrYRZfEN3FqSVENexEl5h19t9xVvU4GL8f8Q-tCbZhpxORE8LQ0GVtEh54OKWo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/c51syZqOTfAsnd6r-SlGvrWy-AEZrp9IMWZkCzbwOxdim5QJf5me1AHvNyets1xpGWqyf6qgG7HnhtN0gVDQz1PsziOCVBUgFT0qs4V-W0xJcRkWnBu7vfvL1G7FTuZxtCavmjPCfw0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/c51syZqOTfAsnd6r-SlGvrWy-AEZrp9IMWZkCzbwOxdim5QJf5me1AHvNyets1xpGWqyf6qgG7HnhtN0gVDQz1PsziOCVBUgFT0qs4V-W0xJcRkWnBu7vfvL1G7FTuZxtCavmjPCfw0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/aIf0MfQdSHRvKWCBpszG9mdoTC_oA7TUo33POMdJui_mw8Qh1S24E0Zry4GOO9xocIBUYzGzm39zvd1oN47Y5YzC2mbIlpQsp4K3VSeFg_D8fFurYte8_UxQ_GlvO9yoZRV4trnj9Bs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/aIf0MfQdSHRvKWCBpszG9mdoTC_oA7TUo33POMdJui_mw8Qh1S24E0Zry4GOO9xocIBUYzGzm39zvd1oN47Y5YzC2mbIlpQsp4K3VSeFg_D8fFurYte8_UxQ_GlvO9yoZRV4trnj9Bs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ummXmozdFsHq4-Sxg8dslTl5yqRE7jCqFSiwLOFZ6wpqPkpFKQszR6aXfm-BGt6wbMSTBHxOdxZMaiE_Ix37katJ7DYCGL47mXHt5WdmfoCl-qQrPO_7rji_ive8imf34m3ZjMp6gKk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ummXmozdFsHq4-Sxg8dslTl5yqRE7jCqFSiwLOFZ6wpqPkpFKQszR6aXfm-BGt6wbMSTBHxOdxZMaiE_Ix37katJ7DYCGL47mXHt5WdmfoCl-qQrPO_7rji_ive8imf34m3ZjMp6gKk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/rs7tSINBA3wxGgDrN0rtHjj3-HXN0-fhG_EFY3pfP6ouK3sNe_X_lU4IiRuG_JNtaC7g2jzaBLopEa-MeBtKJ0N_YQDSqdAnacfzevXaSofs5xJ9riZpAwFm6VtuFGQFJD6Uck1RcZs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/rs7tSINBA3wxGgDrN0rtHjj3-HXN0-fhG_EFY3pfP6ouK3sNe_X_lU4IiRuG_JNtaC7g2jzaBLopEa-MeBtKJ0N_YQDSqdAnacfzevXaSofs5xJ9riZpAwFm6VtuFGQFJD6Uck1RcZs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/dgObbgGisCbbWi7p5DLFiDDp9sDtReBg27pJxDIx6sXjDM-RRYuWRXNwf3sxd824gM2IVAZ3Tv3M178ig1v6ZnF5nPfyDYadNM4QHSts-BdZ7UlWEc52gZPQEpfcnoZ6wSsXS2oc8I4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/dgObbgGisCbbWi7p5DLFiDDp9sDtReBg27pJxDIx6sXjDM-RRYuWRXNwf3sxd824gM2IVAZ3Tv3M178ig1v6ZnF5nPfyDYadNM4QHSts-BdZ7UlWEc52gZPQEpfcnoZ6wSsXS2oc8I4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6ccmmBRigq7ywUJgNJ1W3995DGdKLOIhPNprPzER1J9C_M2zREzhlfDCCgLzR0v73gI-p08sJeJvvzTWfOgQ_BUa_6eYB5eQsR3xUPqR3tOhcTwNnbp_vAKprhYBTXcrr4sFh5xduy4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6ccmmBRigq7ywUJgNJ1W3995DGdKLOIhPNprPzER1J9C_M2zREzhlfDCCgLzR0v73gI-p08sJeJvvzTWfOgQ_BUa_6eYB5eQsR3xUPqR3tOhcTwNnbp_vAKprhYBTXcrr4sFh5xduy4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/seOSEVOVQR1366hRcN3QjbR5Q7WJ4UOBlyZPXTjUsm-OBB23-vwVHo1ggTr4aEnATbLwxXOyZg77gq6j1r-cOOuFTKHCsQdsO3V7v7NSHw2ghkqM5HPL-RHQwYb32vv-qooDAvoFrsQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/seOSEVOVQR1366hRcN3QjbR5Q7WJ4UOBlyZPXTjUsm-OBB23-vwVHo1ggTr4aEnATbLwxXOyZg77gq6j1r-cOOuFTKHCsQdsO3V7v7NSHw2ghkqM5HPL-RHQwYb32vv-qooDAvoFrsQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/5U4W9dd5i97slj4icOwO6yBJ9ME-EwkSk8uxgLZ8wabPmsnJfrHE1yDk3jdjBIOHoEVN5HBj7JCjkyYPUUFdTrKz3Xe4YVx6k4-IR_4kBpQw_WToRzYyE1DdcOTkQV1se0qeGa0QPa4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/5U4W9dd5i97slj4icOwO6yBJ9ME-EwkSk8uxgLZ8wabPmsnJfrHE1yDk3jdjBIOHoEVN5HBj7JCjkyYPUUFdTrKz3Xe4YVx6k4-IR_4kBpQw_WToRzYyE1DdcOTkQV1se0qeGa0QPa4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/klQ3QGTUPQZI1uAqsdWkF61hSxeQS3xnEyUzF-Du2f53LxI_3HK6kvdGxKzwhllvExSNwo0AFR_W3x_hih1AU11iKvZ6UbIhYev4ZWaylNrKHgcM_tRvLAYT64rT8e6JFkqsqRf0HPQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/klQ3QGTUPQZI1uAqsdWkF61hSxeQS3xnEyUzF-Du2f53LxI_3HK6kvdGxKzwhllvExSNwo0AFR_W3x_hih1AU11iKvZ6UbIhYev4ZWaylNrKHgcM_tRvLAYT64rT8e6JFkqsqRf0HPQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Divine Gate',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 5.73',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/VD0_L5P3Htrl0z9GWB4xrYow5WCPM98VMQualD4nM6669xOy-6wPuBYz9OiGjKwFN2q_khRZ0aRtCN8nF84OO96WwOypqZwCH-0Uf6t7zs6A0yVM0HdcGSaTJABazeXLasZev30Zl94=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/VD0_L5P3Htrl0z9GWB4xrYow5WCPM98VMQualD4nM6669xOy-6wPuBYz9OiGjKwFN2q_khRZ0aRtCN8nF84OO96WwOypqZwCH-0Uf6t7zs6A0yVM0HdcGSaTJABazeXLasZev30Zl94=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
