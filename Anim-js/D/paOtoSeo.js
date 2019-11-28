aa2ebe73af9a1fc3dac7e3e8c972450ff6bd4a87
dunia-anime <adityha.on@gmail.com>
2019-11-28 10:14:21 +0800

    Create paOtoSeo.js

---
+++ Anim-js/D/paOtoSeo.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/dzx8lYmiUHZRrq4xT6hzhlkvZaYFwOd0KFBqbV0z1kamCMGwkXJTuwaDLJ2oeP5dHpkrrnegLSQKUzYRQl_2dWaN838hev5jOJWg5Hq55MZa3Ud5H_MvwEJ4RYUNY4AOqrIsWpkFdVc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/dzx8lYmiUHZRrq4xT6hzhlkvZaYFwOd0KFBqbV0z1kamCMGwkXJTuwaDLJ2oeP5dHpkrrnegLSQKUzYRQl_2dWaN838hev5jOJWg5Hq55MZa3Ud5H_MvwEJ4RYUNY4AOqrIsWpkFdVc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/j3pLv5bzX2_qVl9cfFkPnHDu2owF1YXu3q8XreiPwfN2KXXqOtiEZqwKyIO16OcN-jpTqQlDQGFM5xCcTd0oq3TlQAb2J5fAFg-WfxH0h9EzI6C-yLqEks6lfDXmQp5Z_jGyq2zgmqw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/j3pLv5bzX2_qVl9cfFkPnHDu2owF1YXu3q8XreiPwfN2KXXqOtiEZqwKyIO16OcN-jpTqQlDQGFM5xCcTd0oq3TlQAb2J5fAFg-WfxH0h9EzI6C-yLqEks6lfDXmQp5Z_jGyq2zgmqw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jahGQHJDU2TUkfYPQZPJeLHtW8ZsOr4tHCMfiedG7nMOCsCLhwBRiS8CgRuspgshWAFOPPW4Zcma2M1PHvaf7OizT2saCaKmHdTZ6EnVAW3p0nFZnqPByyGTLy4f_WoKKMs0-m08T9w=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jahGQHJDU2TUkfYPQZPJeLHtW8ZsOr4tHCMfiedG7nMOCsCLhwBRiS8CgRuspgshWAFOPPW4Zcma2M1PHvaf7OizT2saCaKmHdTZ6EnVAW3p0nFZnqPByyGTLy4f_WoKKMs0-m08T9w=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Zwkj9Qsi_c-6rIstLauEiqH1Vo4ZJ02OqGdv6SXBx7vXU1OUiUgPMaHnrREugAcCThYLIIXG770b4NcTixTwF5Tv2uxOrXIhSndExSDnJ4rkcLMrUJzJhP9gKprpYc7XQjnKW6_zORg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Zwkj9Qsi_c-6rIstLauEiqH1Vo4ZJ02OqGdv6SXBx7vXU1OUiUgPMaHnrREugAcCThYLIIXG770b4NcTixTwF5Tv2uxOrXIhSndExSDnJ4rkcLMrUJzJhP9gKprpYc7XQjnKW6_zORg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/rSJhn8LIIsbTUvThfaLJmOcDVAiTaQxs_JRGzrRI0ieKPwKePdAq4US9cn7Z701yrrYVprUOOU6kBkkQ7Y_fkxZvW8d3KCliWLNsZda381d4JGUOlom7mGCbOl2okDQW8wVODWwxSl4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/rSJhn8LIIsbTUvThfaLJmOcDVAiTaQxs_JRGzrRI0ieKPwKePdAq4US9cn7Z701yrrYVprUOOU6kBkkQ7Y_fkxZvW8d3KCliWLNsZda381d4JGUOlom7mGCbOl2okDQW8wVODWwxSl4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/VA3MgtupiZy-yxn5o4Mvz4wjufkQBfL-eS1py5vqMmqvnEhyhNoD5kdFl3iI-FZ9CNKa4hEjg3SnDu8Ua31y62AEnHy2F4nk6n28QGkH9tztYSAB0E0jqn3eLP_3kjq1dOiTCZrGRbc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/VA3MgtupiZy-yxn5o4Mvz4wjufkQBfL-eS1py5vqMmqvnEhyhNoD5kdFl3iI-FZ9CNKa4hEjg3SnDu8Ua31y62AEnHy2F4nk6n28QGkH9tztYSAB0E0jqn3eLP_3kjq1dOiTCZrGRbc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/tevdzdVSxfdzothx_BwieRjtwutFDeJOyL-fTaKNcJQx2lFDdXqHhJcTAbW_AHvKWr8DiHRPOSDG9bnAdCgDP8d7WK3NT6Arjday3f5cw4hSd49_c3LBWhBm8wUgFqqgtQSvcU4V-2Y=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/tevdzdVSxfdzothx_BwieRjtwutFDeJOyL-fTaKNcJQx2lFDdXqHhJcTAbW_AHvKWr8DiHRPOSDG9bnAdCgDP8d7WK3NT6Arjday3f5cw4hSd49_c3LBWhBm8wUgFqqgtQSvcU4V-2Y=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/TM3uIQQ9mEBxyA2TdFnNTRXIK_vMvI_gIYOYCKz3DDkS4Rvtrv3tb8F1f2gMscFRySWP8Swj4DyjE3ndcMISPZT8Eqw_2Mz6QDuaQBxFRUTbs-4zk0aXJCJxq6cIVZ5sIZqcyv2rZ2o=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/TM3uIQQ9mEBxyA2TdFnNTRXIK_vMvI_gIYOYCKz3DDkS4Rvtrv3tb8F1f2gMscFRySWP8Swj4DyjE3ndcMISPZT8Eqw_2Mz6QDuaQBxFRUTbs-4zk0aXJCJxq6cIVZ5sIZqcyv2rZ2o=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/FmULDEU7JM0ds_r6uBAy3Sa9bbTDnWvngaW8sXZ0HXIxylivYAkKevPSwtUti3nR983X9T7avPiFhKh8WLumQOjRx1PwjiYjRBV808HTYH8kaZuKgzGwKZdfAfjk-_zZpuzLuWH4j7c=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/FmULDEU7JM0ds_r6uBAy3Sa9bbTDnWvngaW8sXZ0HXIxylivYAkKevPSwtUti3nR983X9T7avPiFhKh8WLumQOjRx1PwjiYjRBV808HTYH8kaZuKgzGwKZdfAfjk-_zZpuzLuWH4j7c=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/qPS85Nh3XaOOmwkKTY2DB-NPBYbosg7-oxFihqo_8sMAjcjEoDNc19l-8bWQc-XpqC-6q8S61P_BTARr60cPXXX-Or5ULMaC_UPeTBA2jNeaHirpwlkLlV8NbOHPP4IPDzLQLE171uY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/qPS85Nh3XaOOmwkKTY2DB-NPBYbosg7-oxFihqo_8sMAjcjEoDNc19l-8bWQc-XpqC-6q8S61P_BTARr60cPXXX-Or5ULMaC_UPeTBA2jNeaHirpwlkLlV8NbOHPP4IPDzLQLE171uY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/JE49YmqDhOhpogoLO9jpphYR2MF_CUTj-0zw7oIpv3QITwfLa9VxEpApmU8q53o1FlZKCAX2ypWyMkZSz_JozJaVDAn3jUdYY4wFdZaRBTDzap1mHkl5nRszGhMWIpPS8shcp5ydoGc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/JE49YmqDhOhpogoLO9jpphYR2MF_CUTj-0zw7oIpv3QITwfLa9VxEpApmU8q53o1FlZKCAX2ypWyMkZSz_JozJaVDAn3jUdYY4wFdZaRBTDzap1mHkl5nRszGhMWIpPS8shcp5ydoGc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/EnD5ymBtmk0WQWmu9YjbjGuwxNml81wA-W43NNlG86iqgbjPFLHqShc7tulLO-hJlltbM42OXMdnTsak67WrNuHUOgqqucF-VjeKbLpo_pPO3WjZDVR4q1xV7jfTyPCY1dykGOyCRXU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/EnD5ymBtmk0WQWmu9YjbjGuwxNml81wA-W43NNlG86iqgbjPFLHqShc7tulLO-hJlltbM42OXMdnTsak67WrNuHUOgqqucF-VjeKbLpo_pPO3WjZDVR4q1xV7jfTyPCY1dykGOyCRXU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Denpa Onna to SO',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.24',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/MXUHhPLmZRdjpD2HLIOnCyMMT_dMO3U0bUQjD8QkFEvcDJgNWUL40k3DJZC-_66CCfYJ_dvJPm3h8glWNJi0cr9Svr1emDTS8epz94Ca5OUuxL9kLxYaoWv6Jno3Fag1TBGrB7l0t9I=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/MXUHhPLmZRdjpD2HLIOnCyMMT_dMO3U0bUQjD8QkFEvcDJgNWUL40k3DJZC-_66CCfYJ_dvJPm3h8glWNJi0cr9Svr1emDTS8epz94Ca5OUuxL9kLxYaoWv6Jno3Fag1TBGrB7l0t9I=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
