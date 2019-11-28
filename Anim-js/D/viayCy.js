1fbd960e0ac8bdbb1868a919d91dec628bc52ed3
dunia-anime <adityha.on@gmail.com>
2019-11-28 09:44:25 +0800

    Create viayCy.js

---
+++ Anim-js/D/viayCy.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Devil May Cry',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/JMwW2wM6xYLwOCOLzm6PyK128Cn9uetqzV4hXwCXk8kVadNg8JYy5HHXA3rd_kvxE6h0qOSBQIN_zWChk6jd2oQiao81f61HBYBLruoz2xNvf0euT1V6SYyFVge4fLKAAHHaYzKhYYU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/JMwW2wM6xYLwOCOLzm6PyK128Cn9uetqzV4hXwCXk8kVadNg8JYy5HHXA3rd_kvxE6h0qOSBQIN_zWChk6jd2oQiao81f61HBYBLruoz2xNvf0euT1V6SYyFVge4fLKAAHHaYzKhYYU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3iVoSgwU_z3KKEWEOGSeODR2hQSZywFBodBXwklE0t4tk7k3LXmRrGSW7BsXMPFuzAW61S79If6Pzw3ja5YtiCKhUakULVlRWaG-47wSU-PaiPwd8UQpJSGtc97RXRgv1fPGIQui_4M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3iVoSgwU_z3KKEWEOGSeODR2hQSZywFBodBXwklE0t4tk7k3LXmRrGSW7BsXMPFuzAW61S79If6Pzw3ja5YtiCKhUakULVlRWaG-47wSU-PaiPwd8UQpJSGtc97RXRgv1fPGIQui_4M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/owuazMYpGHvBFP6t-OEpKM7dNeXnRnt4riir5D9ub3X5L9aPCQloHsCm24cxa1VTz-ruStDtqARxc5pnCL2iNrLXtTMMvGntz0-mejHoEf2jZP17wJoNifnGLKNV0Fp9lAYOX4nHGzc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/owuazMYpGHvBFP6t-OEpKM7dNeXnRnt4riir5D9ub3X5L9aPCQloHsCm24cxa1VTz-ruStDtqARxc5pnCL2iNrLXtTMMvGntz0-mejHoEf2jZP17wJoNifnGLKNV0Fp9lAYOX4nHGzc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/GKJEAo25n-gNnjrzFMiofL4Pw61MENXSJ09-WAD34qKq5tYRG-4TM5PcVymaSTQhtHCLvrUjPFFtbxAo54cAtI0qPwbs0P-u5npcG0Xtc7wZKCOPSwLCZVSpSBgbXqXq-7g2rWTtN7E=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/GKJEAo25n-gNnjrzFMiofL4Pw61MENXSJ09-WAD34qKq5tYRG-4TM5PcVymaSTQhtHCLvrUjPFFtbxAo54cAtI0qPwbs0P-u5npcG0Xtc7wZKCOPSwLCZVSpSBgbXqXq-7g2rWTtN7E=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/4Vq7j4F-FWcf0loF1eB3-FSOhkIqvdjqUeNc315PxMTjYqy-A3ElbLG6ZfAkM0y6GFxNbxhMxbAtGgYPo6YI4ayg3B0YpmomrzOSOQJsVopwxATFYW1TJTjWJoilpsSDNMF7dOvLZVQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/4Vq7j4F-FWcf0loF1eB3-FSOhkIqvdjqUeNc315PxMTjYqy-A3ElbLG6ZfAkM0y6GFxNbxhMxbAtGgYPo6YI4ayg3B0YpmomrzOSOQJsVopwxATFYW1TJTjWJoilpsSDNMF7dOvLZVQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SFYbEFSCV6KJzB9popERZTl-OV-J4MbH47jy7QWltdvl3EGR8QNAmr8xEidZno9hbhUDkOcnZ6R_dt2isw65dmXo8BkrkgZKKSIvZn6Y1FjlO8LM7iY_GXUmqT4rkDqGTBsYpMu6-LM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SFYbEFSCV6KJzB9popERZTl-OV-J4MbH47jy7QWltdvl3EGR8QNAmr8xEidZno9hbhUDkOcnZ6R_dt2isw65dmXo8BkrkgZKKSIvZn6Y1FjlO8LM7iY_GXUmqT4rkDqGTBsYpMu6-LM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Bc2RhyVYPwILe_pPE56xVy_sxqEzaBpYxwocKseVA_E-vJawoZ_yn6RwP-s5dR2uaccj2LFYn87efMGVUy4XvWcq8DeZcqbLz8xkx_KYZnqAk55tNgPz6wvuaat9hFJcwoQu6c_TxZQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Bc2RhyVYPwILe_pPE56xVy_sxqEzaBpYxwocKseVA_E-vJawoZ_yn6RwP-s5dR2uaccj2LFYn87efMGVUy4XvWcq8DeZcqbLz8xkx_KYZnqAk55tNgPz6wvuaat9hFJcwoQu6c_TxZQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/YR1kHEp_Ivw4DyTkaMLohVCf5kPNKv45_QKD8RL3I4E-w6s9QT0dCl6JTwKa0urQcp55RGd8DIhn54dn30Tjgk-LTJqrruOc4WI7fcrqB2PQgHx7KOfBS8-zWjrNBB-JTzEydvCW2Ns=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/YR1kHEp_Ivw4DyTkaMLohVCf5kPNKv45_QKD8RL3I4E-w6s9QT0dCl6JTwKa0urQcp55RGd8DIhn54dn30Tjgk-LTJqrruOc4WI7fcrqB2PQgHx7KOfBS8-zWjrNBB-JTzEydvCW2Ns=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3k65bEK3mMoR3zKZvJ_ibUigbT8DjmM5F5vrsAmRpDcmP-d731Qi-cpf9yUs7i3mns7espLCcnDC8PVybnUhZJG9fP6piXEGjMbMs1qCFpyfMRfxP6LKh3BTi6qjryDcQB4N59NNck0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3k65bEK3mMoR3zKZvJ_ibUigbT8DjmM5F5vrsAmRpDcmP-d731Qi-cpf9yUs7i3mns7espLCcnDC8PVybnUhZJG9fP6piXEGjMbMs1qCFpyfMRfxP6LKh3BTi6qjryDcQB4N59NNck0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6c8Tyt8Bk7_NN_0yqaYmWcn61cA5pJPxk9anX5Eu9I-BnPUipid5ZmhWVXfYHtkqHdE9PLGgt7WoUjzbgELCoCTweG9SGkcAB8ZKkwUMOyxWQ1hvP4eIdCOg4NVhC1t0Lxw9kCCP8FU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6c8Tyt8Bk7_NN_0yqaYmWcn61cA5pJPxk9anX5Eu9I-BnPUipid5ZmhWVXfYHtkqHdE9PLGgt7WoUjzbgELCoCTweG9SGkcAB8ZKkwUMOyxWQ1hvP4eIdCOg4NVhC1t0Lxw9kCCP8FU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/7rKaHH9TjhgleIXsTWkWy2aGndS2bXZ7z1tiL0dLilytR9S0c-FcS3Ib4G5Uy4ZjQXkjZL63Aj8ESvolTroUm1FRHylwVR8Tl7112zs3poecAg1Ac9bGnjk9XeyuoJUhNE731cvdM0k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/7rKaHH9TjhgleIXsTWkWy2aGndS2bXZ7z1tiL0dLilytR9S0c-FcS3Ib4G5Uy4ZjQXkjZL63Aj8ESvolTroUm1FRHylwVR8Tl7112zs3poecAg1Ac9bGnjk9XeyuoJUhNE731cvdM0k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil May Cry',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.07',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/hz7j8Db3KvK7hvUiP5zwdcJn9lomMPzMsrDyxG7eMCuuDSjbKqMbPfnaPrt6_iw5gN04FMKz6E7KCIziqu08KlK4tgBabZlOIpXUGDgQfyYqr7sTzNx2JcAEnN-_IqKvsO3AG0EYOCo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/hz7j8Db3KvK7hvUiP5zwdcJn9lomMPzMsrDyxG7eMCuuDSjbKqMbPfnaPrt6_iw5gN04FMKz6E7KCIziqu08KlK4tgBabZlOIpXUGDgQfyYqr7sTzNx2JcAEnN-_IqKvsO3AG0EYOCo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
