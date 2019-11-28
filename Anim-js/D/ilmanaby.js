c15b851e2bedf25f1a40dab17f4a778394ed0150
dunia-anime <adityha.on@gmail.com>
2019-11-28 09:36:20 +0800

    Create ilmanaby.js

---
+++ Anim-js/D/ilmanaby.js
@@ -0,0 +1,296 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/HHZ4gsz9er_KIGxpJ2bVpdTDJRsg60tQatRixYkM_3Qm_A6c5lGjMZEMBOGn3f9prKDPbqb-T3yebvMX4_GogYSqeks2U4OSFUxKyeyaW30BA1OkZjRFwuJsrCvh4BqGIRrv5CmZqeY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/HHZ4gsz9er_KIGxpJ2bVpdTDJRsg60tQatRixYkM_3Qm_A6c5lGjMZEMBOGn3f9prKDPbqb-T3yebvMX4_GogYSqeks2U4OSFUxKyeyaW30BA1OkZjRFwuJsrCvh4BqGIRrv5CmZqeY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/xLl9cq54SKcEIzVrAh2XcOJVH3Bx4wnwCAnriSXPAdv8U3_1a-FTvMnsSvb0AdKLXNJYgh03X47ZaJJWPq5mlhGqlbNBqGdYGTh7QVlbjAvN3vpSOFXrkY4vabuK8B2_wtA0kDUnC9s=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/xLl9cq54SKcEIzVrAh2XcOJVH3Bx4wnwCAnriSXPAdv8U3_1a-FTvMnsSvb0AdKLXNJYgh03X47ZaJJWPq5mlhGqlbNBqGdYGTh7QVlbjAvN3vpSOFXrkY4vabuK8B2_wtA0kDUnC9s=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/LEUADOqmI1x7Z6WoCCGbUwoOtFjKmMGFpuBnHTbGvR7TUnjX9hn2MQ7GoSY9YwSNevOkBeOZvDO6v9RdFWNIF8idwafVsFc8EZ_c6QgAPKNNFWTI682QVy_dqZtrZnPBpbl2_v4mOhY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/LEUADOqmI1x7Z6WoCCGbUwoOtFjKmMGFpuBnHTbGvR7TUnjX9hn2MQ7GoSY9YwSNevOkBeOZvDO6v9RdFWNIF8idwafVsFc8EZ_c6QgAPKNNFWTI682QVy_dqZtrZnPBpbl2_v4mOhY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/HYv8UZXV7Uw9nPJR-cAnhanGovz3nE7sZAkDIPbDkMn4O8V9Of18GOTM-VSpiIgImYvb74HttZXL-sOfVZ3MltoP3tCmNr5_U-fHhBeIWZcZaAFFr8iWG9bHpdm_CNl6TH4H5y8D0PU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/HYv8UZXV7Uw9nPJR-cAnhanGovz3nE7sZAkDIPbDkMn4O8V9Of18GOTM-VSpiIgImYvb74HttZXL-sOfVZ3MltoP3tCmNr5_U-fHhBeIWZcZaAFFr8iWG9bHpdm_CNl6TH4H5y8D0PU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/z7WWONg5xRyWRegSEWudsfFEfZxwLF3zXOE7PrC8DLam0K1oWvU3U9iDe65nEBuiy5-iBxoYFvqTmWV1eQkq_BEXaQ-RnfYYnToipHrv0UUPVkOSlB_YTIWFc9N36uSurxC1qbsTohw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/z7WWONg5xRyWRegSEWudsfFEfZxwLF3zXOE7PrC8DLam0K1oWvU3U9iDe65nEBuiy5-iBxoYFvqTmWV1eQkq_BEXaQ-RnfYYnToipHrv0UUPVkOSlB_YTIWFc9N36uSurxC1qbsTohw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nwrINeooclEg5MKbqerXW9PwKBPZ3GQWPgV4AqnHD9s4CO8LoU_l1m82GAiH_lgcKcBKndOLaVnDNq0JXOu8tpEji0bXOW2kYGdu70ZUcXTYYL0hghOUrmx-wPnDzIF9P6Nn_mLZjks=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nwrINeooclEg5MKbqerXW9PwKBPZ3GQWPgV4AqnHD9s4CO8LoU_l1m82GAiH_lgcKcBKndOLaVnDNq0JXOu8tpEji0bXOW2kYGdu70ZUcXTYYL0hghOUrmx-wPnDzIF9P6Nn_mLZjks=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/gJVOBi3r8nrMSCp2VuvDb84UPB3_BulUD9bDmGoEEaGdxoswQxZWgUmTLaAZJMyBiba7XYzdsi4HwlArBe3aICP5Xyytfo5fNColTPGY1KSnSovzFoPjZLL5Zp-_MdIjPdm2z0LkhtQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/gJVOBi3r8nrMSCp2VuvDb84UPB3_BulUD9bDmGoEEaGdxoswQxZWgUmTLaAZJMyBiba7XYzdsi4HwlArBe3aICP5Xyytfo5fNColTPGY1KSnSovzFoPjZLL5Zp-_MdIjPdm2z0LkhtQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/uFp8_yfVWQfJU1GmBn2BqDkZuR8_FrBNoc53ebXl33ad98-yAGvVm2BRgZGw_mbpdmyCwzfyFUBeYznhcKE7UoeUGwRxe98ItehNZr_6sl6dFWVoGqfPvF0kuNdbzZxYmHs4aDONS-M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/uFp8_yfVWQfJU1GmBn2BqDkZuR8_FrBNoc53ebXl33ad98-yAGvVm2BRgZGw_mbpdmyCwzfyFUBeYznhcKE7UoeUGwRxe98ItehNZr_6sl6dFWVoGqfPvF0kuNdbzZxYmHs4aDONS-M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/5jO46xNBNfTlgHfaeELZqM-xOOhi0Z1Ka9QQi9MdBNjA0zPj_DiQKaQVYzOXwO8YbZAKzNrRxcLVCluR6ATcJ55X1UU7wVoy43bkFzfdzCBGU7py67Dd-zSB89W8_7_w3Z68p4NNYi0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/5jO46xNBNfTlgHfaeELZqM-xOOhi0Z1Ka9QQi9MdBNjA0zPj_DiQKaQVYzOXwO8YbZAKzNrRxcLVCluR6ATcJ55X1UU7wVoy43bkFzfdzCBGU7py67Dd-zSB89W8_7_w3Z68p4NNYi0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devilman: Crybaby',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.94',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/C3lAdgRDEOPNKyloApiUEg3ef9fe__fXpnVOSYoBilcTOCHHok7p-jsQd8ifcKLfS5YdvMobUm4dUYzw5nICAYtTtKMHeHGnnmJB4FtgtX2n3iukf2qI8MH3vkafn-VzrefxT1K6ETg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/C3lAdgRDEOPNKyloApiUEg3ef9fe__fXpnVOSYoBilcTOCHHok7p-jsQd8ifcKLfS5YdvMobUm4dUYzw5nICAYtTtKMHeHGnnmJB4FtgtX2n3iukf2qI8MH3vkafn-VzrefxT1K6ETg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
