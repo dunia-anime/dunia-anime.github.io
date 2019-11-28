f42b07f63bfc6127f44c94db221d1c3862edf016
dunia-anime <adityha.on@gmail.com>
2019-11-28 18:43:47 +0800

    Update tynterMShukuPriteE.js

--- Anim-js/C/tynterMShukuPriteE.js
+++ Anim-js/C/tynterMShukuPriteE.js
@@ -1,48 +1,41 @@
- var player = videojs('my_video_1', {
-     sources: [{
-             src: 'https://lh3.googleusercontent.com/4xJKrcTDm-HuSGCX1ftq8xsUf0mewlTtWWqGCm3EheaobKxPwDzWTxDsUrL-QYRLNu7KPtBKQYtYzwtaQ1sVR5H9Ku3Y4qlmQQpvyMbgLBNSEIftp_Qe3Luj7shCz28zyHRjEi3VdcYACArylZLX10Ml2_YucqOQ7xUqeK3MVX9-2nvJN-NY6Veuh2Jebnb3z3M0ltHrpU9iHlHNBMngNVSypcOUMmR7GCw4BPsD25uV7Wkj_capoDorrblovQ4E4ZjDwLw8EDGV2kSylIgqNiadS1MWWla_H7R-VLYpurDKcy49o2AdsF3YvXZCkHjov1OPDHeUV36ELQkiUymyxXOD9YqJwjhP8xel9B8hoX0InxD6JrJ1eIT4LBOp-ydVUKdd2nQZUQYrTPqvK81_jEWJQqO8R3RLcnw_sTmlnTUOUk17o-J--Og-jVilS2cwtl5mF_CfOUT7Fk3zAuDt5yMeIs7fi6mr-2Zd45uo1OENDppaPs7oHWLFrRphtgfGV0h7vcZtP7RUvkinpXANwbpLrTTSqNs2FS8I4rCLXdOYEGQ9lk9N8UQ3dH4qdnKrarQBCOY49c90rk5ERPhYGX1oQzhaa1Qbdfb7RpLQ8wH5d1cCxwM2O6tQzibJiHV9wMm-6OG2NX2mYDHSr-mb8b_x0l3jW7noG6Dwb_KfIoT-BOa9ND-vDA=m37',
-             type: 'video/mp4',
-             label: '1080p',
-         },
-         {
-             src: 'https://lh3.googleusercontent.com/4xJKrcTDm-HuSGCX1ftq8xsUf0mewlTtWWqGCm3EheaobKxPwDzWTxDsUrL-QYRLNu7KPtBKQYtYzwtaQ1sVR5H9Ku3Y4qlmQQpvyMbgLBNSEIftp_Qe3Luj7shCz28zyHRjEi3VdcYACArylZLX10Ml2_YucqOQ7xUqeK3MVX9-2nvJN-NY6Veuh2Jebnb3z3M0ltHrpU9iHlHNBMngNVSypcOUMmR7GCw4BPsD25uV7Wkj_capoDorrblovQ4E4ZjDwLw8EDGV2kSylIgqNiadS1MWWla_H7R-VLYpurDKcy49o2AdsF3YvXZCkHjov1OPDHeUV36ELQkiUymyxXOD9YqJwjhP8xel9B8hoX0InxD6JrJ1eIT4LBOp-ydVUKdd2nQZUQYrTPqvK81_jEWJQqO8R3RLcnw_sTmlnTUOUk17o-J--Og-jVilS2cwtl5mF_CfOUT7Fk3zAuDt5yMeIs7fi6mr-2Zd45uo1OENDppaPs7oHWLFrRphtgfGV0h7vcZtP7RUvkinpXANwbpLrTTSqNs2FS8I4rCLXdOYEGQ9lk9N8UQ3dH4qdnKrarQBCOY49c90rk5ERPhYGX1oQzhaa1Qbdfb7RpLQ8wH5d1cCxwM2O6tQzibJiHV9wMm-6OG2NX2mYDHSr-mb8b_x0l3jW7noG6Dwb_KfIoT-BOa9ND-vDA=m22',
-             type: 'video/mp4',
-             label: '720p',
-         },
-         {
-             src: 'https://lh3.googleusercontent.com/4xJKrcTDm-HuSGCX1ftq8xsUf0mewlTtWWqGCm3EheaobKxPwDzWTxDsUrL-QYRLNu7KPtBKQYtYzwtaQ1sVR5H9Ku3Y4qlmQQpvyMbgLBNSEIftp_Qe3Luj7shCz28zyHRjEi3VdcYACArylZLX10Ml2_YucqOQ7xUqeK3MVX9-2nvJN-NY6Veuh2Jebnb3z3M0ltHrpU9iHlHNBMngNVSypcOUMmR7GCw4BPsD25uV7Wkj_capoDorrblovQ4E4ZjDwLw8EDGV2kSylIgqNiadS1MWWla_H7R-VLYpurDKcy49o2AdsF3YvXZCkHjov1OPDHeUV36ELQkiUymyxXOD9YqJwjhP8xel9B8hoX0InxD6JrJ1eIT4LBOp-ydVUKdd2nQZUQYrTPqvK81_jEWJQqO8R3RLcnw_sTmlnTUOUk17o-J--Og-jVilS2cwtl5mF_CfOUT7Fk3zAuDt5yMeIs7fi6mr-2Zd45uo1OENDppaPs7oHWLFrRphtgfGV0h7vcZtP7RUvkinpXANwbpLrTTSqNs2FS8I4rCLXdOYEGQ9lk9N8UQ3dH4qdnKrarQBCOY49c90rk5ERPhYGX1oQzhaa1Qbdfb7RpLQ8wH5d1cCxwM2O6tQzibJiHV9wMm-6OG2NX2mYDHSr-mb8b_x0l3jW7noG6Dwb_KfIoT-BOa9ND-vDA=m18',
-             type: 'video/mp4',
-             label: '480p',
-             selected: 'true'
-         }
-     ]
+  var player = videojs('my_video_1', {
+      sources: [{
+              src: 'https://lh3.googleusercontent.com/4xJKrcTDm-HuSGCX1ftq8xsUf0mewlTtWWqGCm3EheaobKxPwDzWTxDsUrL-QYRLNu7KPtBKQYtYzwtaQ1sVR5H9Ku3Y4qlmQQpvyMbgLBNSEIftp_Qe3Luj7shCz28zyHRjEi3VdcYACArylZLX10Ml2_YucqOQ7xUqeK3MVX9-2nvJN-NY6Veuh2Jebnb3z3M0ltHrpU9iHlHNBMngNVSypcOUMmR7GCw4BPsD25uV7Wkj_capoDorrblovQ4E4ZjDwLw8EDGV2kSylIgqNiadS1MWWla_H7R-VLYpurDKcy49o2AdsF3YvXZCkHjov1OPDHeUV36ELQkiUymyxXOD9YqJwjhP8xel9B8hoX0InxD6JrJ1eIT4LBOp-ydVUKdd2nQZUQYrTPqvK81_jEWJQqO8R3RLcnw_sTmlnTUOUk17o-J--Og-jVilS2cwtl5mF_CfOUT7Fk3zAuDt5yMeIs7fi6mr-2Zd45uo1OENDppaPs7oHWLFrRphtgfGV0h7vcZtP7RUvkinpXANwbpLrTTSqNs2FS8I4rCLXdOYEGQ9lk9N8UQ3dH4qdnKrarQBCOY49c90rk5ERPhYGX1oQzhaa1Qbdfb7RpLQ8wH5d1cCxwM2O6tQzibJiHV9wMm-6OG2NX2mYDHSr-mb8b_x0l3jW7noG6Dwb_KfIoT-BOa9ND-vDA=m37',
+              type: 'video/mp4',
+              label: '1080p',
+          },
+          {
+              src: 'https://lh3.googleusercontent.com/4xJKrcTDm-HuSGCX1ftq8xsUf0mewlTtWWqGCm3EheaobKxPwDzWTxDsUrL-QYRLNu7KPtBKQYtYzwtaQ1sVR5H9Ku3Y4qlmQQpvyMbgLBNSEIftp_Qe3Luj7shCz28zyHRjEi3VdcYACArylZLX10Ml2_YucqOQ7xUqeK3MVX9-2nvJN-NY6Veuh2Jebnb3z3M0ltHrpU9iHlHNBMngNVSypcOUMmR7GCw4BPsD25uV7Wkj_capoDorrblovQ4E4ZjDwLw8EDGV2kSylIgqNiadS1MWWla_H7R-VLYpurDKcy49o2AdsF3YvXZCkHjov1OPDHeUV36ELQkiUymyxXOD9YqJwjhP8xel9B8hoX0InxD6JrJ1eIT4LBOp-ydVUKdd2nQZUQYrTPqvK81_jEWJQqO8R3RLcnw_sTmlnTUOUk17o-J--Og-jVilS2cwtl5mF_CfOUT7Fk3zAuDt5yMeIs7fi6mr-2Zd45uo1OENDppaPs7oHWLFrRphtgfGV0h7vcZtP7RUvkinpXANwbpLrTTSqNs2FS8I4rCLXdOYEGQ9lk9N8UQ3dH4qdnKrarQBCOY49c90rk5ERPhYGX1oQzhaa1Qbdfb7RpLQ8wH5d1cCxwM2O6tQzibJiHV9wMm-6OG2NX2mYDHSr-mb8b_x0l3jW7noG6Dwb_KfIoT-BOa9ND-vDA=m22',
+              type: 'video/mp4',
+              label: '720p',
+          },
+          {
+              src: 'https://lh3.googleusercontent.com/4xJKrcTDm-HuSGCX1ftq8xsUf0mewlTtWWqGCm3EheaobKxPwDzWTxDsUrL-QYRLNu7KPtBKQYtYzwtaQ1sVR5H9Ku3Y4qlmQQpvyMbgLBNSEIftp_Qe3Luj7shCz28zyHRjEi3VdcYACArylZLX10Ml2_YucqOQ7xUqeK3MVX9-2nvJN-NY6Veuh2Jebnb3z3M0ltHrpU9iHlHNBMngNVSypcOUMmR7GCw4BPsD25uV7Wkj_capoDorrblovQ4E4ZjDwLw8EDGV2kSylIgqNiadS1MWWla_H7R-VLYpurDKcy49o2AdsF3YvXZCkHjov1OPDHeUV36ELQkiUymyxXOD9YqJwjhP8xel9B8hoX0InxD6JrJ1eIT4LBOp-ydVUKdd2nQZUQYrTPqvK81_jEWJQqO8R3RLcnw_sTmlnTUOUk17o-J--Og-jVilS2cwtl5mF_CfOUT7Fk3zAuDt5yMeIs7fi6mr-2Zd45uo1OENDppaPs7oHWLFrRphtgfGV0h7vcZtP7RUvkinpXANwbpLrTTSqNs2FS8I4rCLXdOYEGQ9lk9N8UQ3dH4qdnKrarQBCOY49c90rk5ERPhYGX1oQzhaa1Qbdfb7RpLQ8wH5d1cCxwM2O6tQzibJiHV9wMm-6OG2NX2mYDHSr-mb8b_x0l3jW7noG6Dwb_KfIoT-BOa9ND-vDA=m18',
+              type: 'video/mp4',
+              label: '480p',
+              selected: 'true'
+          }
+      ]
 
- });
- videojs("my_video_1", function () {
-     var player = this;
-     player.ass({
-         'src': ["https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/C/City%20Hunter%20Movie%20Shinjuku%20Private%20Eyes/%5Bdunia-anime%5D%20City%20Hunter%20Movie%20Shinjuku%20Private%20Eyes.ass"],
-         label: "Indoesia",
-         videoWidth: 640,
-         videoHeight: 360,
-         enableSvg: false
-     })
- });
- videojs('video').ready(function () {
-     this.hotkeys({
+  });
 
-     });
- });
+  videojs("my_video_1", {}, function () {
+      var player = this;
+      player.controlBar.addChild('QualitySelector');
+      player.landscapeFullscreen();
+  });
+  videojs("my_video_1", {}, function () {
+      var player = this;
+      player.ass({
+          'src': ["https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/C/City%20Hunter%20Movie%20Shinjuku%20Private%20Eyes/%5Bdunia-anime%5D%20City%20Hunter%20Movie%20Shinjuku%20Private%20Eyes.ass"],
+          label: "Indoesia",
+          videoWidth: 640,
+          videoHeight: 360,
+          enableSvg: false
+      })
+  });
+  videojs('video').ready(function () {
+      this.hotkeys({
 
- videojs("my_video_1", {}, function () {
-     var player = this;
-     player.controlBar.addChild('QualitySelector');
-     player.landscapeFullscreen();
-     player.ass({
-         'src': ["https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/C/City%20Hunter%20Movie%20Shinjuku%20Private%20Eyes/%5Bdunia-anime%5D%20City%20Hunter%20Movie%20Shinjuku%20Private%20Eyes.ass"],
-         label: "Indoesia",
-         videoWidth: 640,
-         videoHeight: 360,
-         enableSvg: false
-     })
- });
+      });
+  });
