31e5556279627022678feba05407664d927797f3
dunia-anime <adityha.on@gmail.com>
2019-11-28 09:11:33 +0800

    Create siow.js

---
+++ Anim-js/D/siow.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Dimension W',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/csGUvfT0VZZ1YlrlonqWwlw7cfQBexoupKX9aXVaIRxOSRJvzFxxOM30VTlTNPg1-PiAbaFXOlA2vT4D4O2LI9O7t0T5KO_T0ty_ZAuKO6TXUOef4hmSx7l7sje43a5futif6v-moKo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/csGUvfT0VZZ1YlrlonqWwlw7cfQBexoupKX9aXVaIRxOSRJvzFxxOM30VTlTNPg1-PiAbaFXOlA2vT4D4O2LI9O7t0T5KO_T0ty_ZAuKO6TXUOef4hmSx7l7sje43a5futif6v-moKo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/lk-rK0MCLlDYaqrnabLjT526o-hD15YfoyKN0EfjjZcQwrfTKGR9uQxkOo-wyYere8KBHc_b08H7rUx-cLUJCQBrW9SMOH7Ay9QWqTYxTnNnmDegfFCXBAUTmH0rwfrtnIAZoyo814E=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/lk-rK0MCLlDYaqrnabLjT526o-hD15YfoyKN0EfjjZcQwrfTKGR9uQxkOo-wyYere8KBHc_b08H7rUx-cLUJCQBrW9SMOH7Ay9QWqTYxTnNnmDegfFCXBAUTmH0rwfrtnIAZoyo814E=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/T0F_bq8wLxIYIguw13afO2A5e-egX3YdhXdJDuFIlLqi19RbjBYALNjCSgVDIs1DOBpLzq7YBsc9pRzocymCILsn7cEuOa5A813IHtNj4CHmUwnclnYCDvz19lOc3CiVzU7zIU0Q8-4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/T0F_bq8wLxIYIguw13afO2A5e-egX3YdhXdJDuFIlLqi19RbjBYALNjCSgVDIs1DOBpLzq7YBsc9pRzocymCILsn7cEuOa5A813IHtNj4CHmUwnclnYCDvz19lOc3CiVzU7zIU0Q8-4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jPIdjm1-zNSM5xDQA5tBp4wLnmTIaXQKKorynv2qnbNzQZTLX8KIqXrqIgJXVo2xOdyKt0NK3wZ4amH4z78DU9ZcPalnmZ6s6fhO3RC87SEcCUQMxnj-5c2EayCMHQT2WZtNRulAVbU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jPIdjm1-zNSM5xDQA5tBp4wLnmTIaXQKKorynv2qnbNzQZTLX8KIqXrqIgJXVo2xOdyKt0NK3wZ4amH4z78DU9ZcPalnmZ6s6fhO3RC87SEcCUQMxnj-5c2EayCMHQT2WZtNRulAVbU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/vIUdmU4bQoosJDjxajIbFBimWsIo4zZQvRraTBTLd_wZZHPvWFJZRMKEo1LwvLcEOFxS1uxW5tCm8uE2lTGYxeF_3i1v4RgBQ9QWLhnLqiDINasyQFBLJjZmMqKHzGHw-pUKNIr70yQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/vIUdmU4bQoosJDjxajIbFBimWsIo4zZQvRraTBTLd_wZZHPvWFJZRMKEo1LwvLcEOFxS1uxW5tCm8uE2lTGYxeF_3i1v4RgBQ9QWLhnLqiDINasyQFBLJjZmMqKHzGHw-pUKNIr70yQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/75xwnUoECirQUBoonehGjyr0XFLbVl9ZrVsXnqtPf3qmr1D6p3SHhcu2ZK0HLuMD5QeiPEri4P1iK9yWrrty0V62hqFICGq8iZD1NVF2B8_OK3slU3zb21GgVDbM0s3LBMKfFRdrG20=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/75xwnUoECirQUBoonehGjyr0XFLbVl9ZrVsXnqtPf3qmr1D6p3SHhcu2ZK0HLuMD5QeiPEri4P1iK9yWrrty0V62hqFICGq8iZD1NVF2B8_OK3slU3zb21GgVDbM0s3LBMKfFRdrG20=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/NcJQZIsEY3cicHqTxcDYn-WU-q-bjBJ3YI5c1ZK4dOcWxHDqq_uctyz2ZXYSMgSfRn_SUBz0R3bjE78CXMKGhENqjowUVN1GFk5uluxJCUbceb-dDCmE-FfPOQbMezQcyWmwcJJCVog=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/NcJQZIsEY3cicHqTxcDYn-WU-q-bjBJ3YI5c1ZK4dOcWxHDqq_uctyz2ZXYSMgSfRn_SUBz0R3bjE78CXMKGhENqjowUVN1GFk5uluxJCUbceb-dDCmE-FfPOQbMezQcyWmwcJJCVog=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/5cOIufU1wj1yCAxcFm-jIAd3ij0J57rdPoo7D9Zz9a29LVQ6UZEEKBh0pvn9yPO1iM0LIpgzk1UUO60fJP51O-LLVGFIp_S_iUfARtL9qZuQyQsqDyIZ6SqOiGMbQqP6zDhKK-QFK7o=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/5cOIufU1wj1yCAxcFm-jIAd3ij0J57rdPoo7D9Zz9a29LVQ6UZEEKBh0pvn9yPO1iM0LIpgzk1UUO60fJP51O-LLVGFIp_S_iUfARtL9qZuQyQsqDyIZ6SqOiGMbQqP6zDhKK-QFK7o=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZkG2hrK9TdgGbipSSL4_SaxSZIxhEY07o8iJhSvrD-rUI0ogOsZK5myEafksSsL-ztFlp4Vtf6LuNIgLr3lS7aRz89Wn9c5ulZUhBBcJXdzVla6RYrILizEByRAjmHCAY54r_nS3VZs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZkG2hrK9TdgGbipSSL4_SaxSZIxhEY07o8iJhSvrD-rUI0ogOsZK5myEafksSsL-ztFlp4Vtf6LuNIgLr3lS7aRz89Wn9c5ulZUhBBcJXdzVla6RYrILizEByRAjmHCAY54r_nS3VZs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3txwGuo7zF1FnmMmgTMW4-emTZnTMTqcEkNB-SOTFAMmd15RSr6z_eBx0KaSu_0i30KX4SBmWNUOKmWArnBJAj8Di9vKbcGfCoogXkcW-OlLSZ_nWEHWKjtkb4DXdTYu6Jtev_qLGbc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3txwGuo7zF1FnmMmgTMW4-emTZnTMTqcEkNB-SOTFAMmd15RSr6z_eBx0KaSu_0i30KX4SBmWNUOKmWArnBJAj8Di9vKbcGfCoogXkcW-OlLSZ_nWEHWKjtkb4DXdTYu6Jtev_qLGbc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/87M4RkPfqaN3RpYdDeSUoNHUJRnTWfdILOF6H-ytYFKWduPvtwrNYQ5vKwxke-VXoGsmXL1hl9fNmnf8cFDJ_tcFMnnYmZCh-xa2ijkGOjEULciVV-sZDxd9FDrQ-bNgk5D6P4uz8Rk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/87M4RkPfqaN3RpYdDeSUoNHUJRnTWfdILOF6H-ytYFKWduPvtwrNYQ5vKwxke-VXoGsmXL1hl9fNmnf8cFDJ_tcFMnnYmZCh-xa2ijkGOjEULciVV-sZDxd9FDrQ-bNgk5D6P4uz8Rk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Nt8XcufsjC_PCLRZPkCubUpR5Wbq-d2SNoHXIpojBmxPJ0YTw9f-pIkR79C-Ym1f4fKlC_qhH7w_tvFj4puIM7uQ7rBFiZwor9UWKjaWj-1fqqwvmI4lzhUpC51OZVJYWvjGmA5zbfc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Nt8XcufsjC_PCLRZPkCubUpR5Wbq-d2SNoHXIpojBmxPJ0YTw9f-pIkR79C-Ym1f4fKlC_qhH7w_tvFj4puIM7uQ7rBFiZwor9UWKjaWj-1fqqwvmI4lzhUpC51OZVJYWvjGmA5zbfc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dimension W',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.28',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/VNr683cYW4NtmjOxxr7FntIrhsfLMifqz2AOaYRaJHhh8O08wEDj4pUO346ST2mObvsTDT07VafK36goOq4KZXN26Yg5G4moGiWZjliw0BYUDT37XctOSAuV8cCYPWNhFyGWjr4zBAI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/VNr683cYW4NtmjOxxr7FntIrhsfLMifqz2AOaYRaJHhh8O08wEDj4pUO346ST2mObvsTDT07VafK36goOq4KZXN26Yg5G4moGiWZjliw0BYUDT37XctOSAuV8cCYPWNhFyGWjr4zBAI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
