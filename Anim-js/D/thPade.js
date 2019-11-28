da415c5c05acb998074c4f09a6cb713ab6f45d5e
dunia-anime <adityha.on@gmail.com>
2019-11-28 10:33:30 +0800

    Create thPade.js

---
+++ Anim-js/D/thPade.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Death Parade',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/FahYjKP9frpiXptzC7mLYSM_F8CEai6cFz14C8wbyLkADl0B05TiSOc7DyTLlbYMBFxKjYqXtyOMLUoXHG-Yf8Gftbezzd4A7Y3m8_DqB9XM7wrOffTCHVb3OZIRcST7H_Zjp76D62A=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/FahYjKP9frpiXptzC7mLYSM_F8CEai6cFz14C8wbyLkADl0B05TiSOc7DyTLlbYMBFxKjYqXtyOMLUoXHG-Yf8Gftbezzd4A7Y3m8_DqB9XM7wrOffTCHVb3OZIRcST7H_Zjp76D62A=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/-3Q2YWpfbEBqeuKeZyKpJmgg4klAtCiqy2Yw1PFNWQPzzdu-tJb-ABqSK-VgIWzGR9yzWS7dF9iOCetUwaQEE7z1X-cIpbPpS3DYtMWvHxjsDbppoIuhOK2k6hBBjyT1btM3ln-0sPE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/-3Q2YWpfbEBqeuKeZyKpJmgg4klAtCiqy2Yw1PFNWQPzzdu-tJb-ABqSK-VgIWzGR9yzWS7dF9iOCetUwaQEE7z1X-cIpbPpS3DYtMWvHxjsDbppoIuhOK2k6hBBjyT1btM3ln-0sPE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/5R3rRH-0yRGvF3P2E_D_GGlywcrDzocxwOpHApKUQ5MKIzFsF8gtIbMPn94d8TYMVpDHGMG2w0VXX49NFcyZK_VjbifFsvO4eh9oI_lI55nP-URTfU2F1v4qTVjxKMhBFK7H1y53eKQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/5R3rRH-0yRGvF3P2E_D_GGlywcrDzocxwOpHApKUQ5MKIzFsF8gtIbMPn94d8TYMVpDHGMG2w0VXX49NFcyZK_VjbifFsvO4eh9oI_lI55nP-URTfU2F1v4qTVjxKMhBFK7H1y53eKQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WIq0QHYDNceMoAuJtb6a8vah2WWx6A-lKRn85pThk8yNPOabGQnwK0X76QlnQ5BRPpttYaXmWZPymGIoo-Gb3MqndcABwnSh3-cyot3U04p8zpGl7GvA9BX5MDb6a3j0ujVTS0_TmcY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WIq0QHYDNceMoAuJtb6a8vah2WWx6A-lKRn85pThk8yNPOabGQnwK0X76QlnQ5BRPpttYaXmWZPymGIoo-Gb3MqndcABwnSh3-cyot3U04p8zpGl7GvA9BX5MDb6a3j0ujVTS0_TmcY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/gY6eFvh8-Bcs4UAz9eA1oUITF3vLvoiiBmyEYS04sFpyk-wvOupUEK76dRlQPQdZwQcRsWJy6m2DOJ7CbtWc9CTSPMihanbnCbzRSfEF-wGZgO2hrJcjcXkW3SEO9fACheT3czXl5KY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/gY6eFvh8-Bcs4UAz9eA1oUITF3vLvoiiBmyEYS04sFpyk-wvOupUEK76dRlQPQdZwQcRsWJy6m2DOJ7CbtWc9CTSPMihanbnCbzRSfEF-wGZgO2hrJcjcXkW3SEO9fACheT3czXl5KY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nB73RnF8Fpz94nZVj9JxSwvIlXm7PV9tuWHOWCnFw5_1eC-BHqBIbQfekyqacnwpYaI2xc8-LjOkgmEWXcQ4-djzVHE5Bq5SNLsUh1wRBcqanlIEP7HncaNDV5FrDk29jFqhjoLFLOQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nB73RnF8Fpz94nZVj9JxSwvIlXm7PV9tuWHOWCnFw5_1eC-BHqBIbQfekyqacnwpYaI2xc8-LjOkgmEWXcQ4-djzVHE5Bq5SNLsUh1wRBcqanlIEP7HncaNDV5FrDk29jFqhjoLFLOQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Ga51nQLmh76GUitjVbj_uEaJWhC9e8qcYh9ifczdHr_mo5y-Jeg9BLwSAQgHh_2IDUQGeUPAgxCt_q9CC1-Kw98wpcb5fqlfWYeI7NTAESA0PjSSfUsIca2qy3WVTgJ0ZCIv4fC9_II=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Ga51nQLmh76GUitjVbj_uEaJWhC9e8qcYh9ifczdHr_mo5y-Jeg9BLwSAQgHh_2IDUQGeUPAgxCt_q9CC1-Kw98wpcb5fqlfWYeI7NTAESA0PjSSfUsIca2qy3WVTgJ0ZCIv4fC9_II=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_ObvFzV98SNGAc7TrPkHnuEJOzaSNyOXTKi3kOu4ipeqIns9T9KEqs_JJEeMD4Vh2-EOCBK-XYZB5un-MWcjHcONxCnGZ9DAFJaO-9vQfxCMFUXhYYkhs7woaevTgSBXPsK65PlUV6M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_ObvFzV98SNGAc7TrPkHnuEJOzaSNyOXTKi3kOu4ipeqIns9T9KEqs_JJEeMD4Vh2-EOCBK-XYZB5un-MWcjHcONxCnGZ9DAFJaO-9vQfxCMFUXhYYkhs7woaevTgSBXPsK65PlUV6M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/32iz0cxuX1FB0I9rxol2a97GnjlAX2PztifWaIde-koRnqUPVcNAlSMEnPezn-AX5BSfOSMIUzzVf5B6JSKEX0XfiJKxzZQJFgj2cxuL8SRfZlZ0TUeHBCeWYUeLjERgCfQmpV_Wrrg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/32iz0cxuX1FB0I9rxol2a97GnjlAX2PztifWaIde-koRnqUPVcNAlSMEnPezn-AX5BSfOSMIUzzVf5B6JSKEX0XfiJKxzZQJFgj2cxuL8SRfZlZ0TUeHBCeWYUeLjERgCfQmpV_Wrrg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Sa_NxMXJ1SBBrE7yn-STmh5YI7PFQfQH1BxcltQWkY_0LGODbiQPihbi9O-ZP8QpUBA557c9X22B6jsIu5U6Ml0ILKib3pcpPINKyuGRaIhZnbL3U7uY-r7Z9Ykn97qDCyGFMRmIRAQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Sa_NxMXJ1SBBrE7yn-STmh5YI7PFQfQH1BxcltQWkY_0LGODbiQPihbi9O-ZP8QpUBA557c9X22B6jsIu5U6Ml0ILKib3pcpPINKyuGRaIhZnbL3U7uY-r7Z9Ykn97qDCyGFMRmIRAQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/fAjzfnS9qPUa5X_JQb2C9afgfP-snIrjfeVt6V1cR4MNKZfgOk9K8A4KjOjFUCvaNv0Xq90WPn7GWTOPQPe1FciwX9-AQgCPw7uGxPalLwfiW1Mm41MxWTiw9VvKZcsfyU9frAIPVQ8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/fAjzfnS9qPUa5X_JQb2C9afgfP-snIrjfeVt6V1cR4MNKZfgOk9K8A4KjOjFUCvaNv0Xq90WPn7GWTOPQPe1FciwX9-AQgCPw7uGxPalLwfiW1Mm41MxWTiw9VvKZcsfyU9frAIPVQ8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death Parade',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 8.22',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/2ahEAjmWFAZKrL1y-KavPhL4YCVYCCLqE0axivBDg02lEyeDE7gPZCMzr9vcrX_LYWblKRbYBTZ8K1bnBwYdkaeWkjVEMbdgkbmO4-BsA0mZtZzfuDoYc9wdWQ_AxUPldHbRXJK6iNo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/2ahEAjmWFAZKrL1y-KavPhL4YCVYCCLqE0axivBDg02lEyeDE7gPZCMzr9vcrX_LYWblKRbYBTZ8K1bnBwYdkaeWkjVEMbdgkbmO4-BsA0mZtZzfuDoYc9wdWQ_AxUPldHbRXJK6iNo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
