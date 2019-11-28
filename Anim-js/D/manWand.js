fa372de938a71943db2e43ec5c4bc7e4dae611d2
dunia-anime <adityha.on@gmail.com>
2019-11-28 11:17:08 +0800

    Create manWand.js

---
+++ Anim-js/D/manWand.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nub-vUulA75yQ0Yot9rzZNYaaRuIpvE21kmuN2KSP2OnTgkIUg9Nw2CDTwSAaKqk8HlztdXdtsvyK-ruXtwvuRBYo1PeF_EOeoQfRNYWwtnhmRz3GhxW0l6gW-hg2WGtxt0FgrKwBPo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nub-vUulA75yQ0Yot9rzZNYaaRuIpvE21kmuN2KSP2OnTgkIUg9Nw2CDTwSAaKqk8HlztdXdtsvyK-ruXtwvuRBYo1PeF_EOeoQfRNYWwtnhmRz3GhxW0l6gW-hg2WGtxt0FgrKwBPo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/59PhRzZ6Af-gpdKFFUzi_AaAg9xKaOK32WEvrRltFBMBcr6xIavWSGpCXnpjHxD9mthgyVcJzFeAKGFIZtxPbH9tLnHKypgNNv4y-Lq_kI_VuztZCqi9DP3rOWEFqulAoK0oXrK3yzA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/59PhRzZ6Af-gpdKFFUzi_AaAg9xKaOK32WEvrRltFBMBcr6xIavWSGpCXnpjHxD9mthgyVcJzFeAKGFIZtxPbH9tLnHKypgNNv4y-Lq_kI_VuztZCqi9DP3rOWEFqulAoK0oXrK3yzA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/UtBIVJcwApwkxpnwoWMQGrbS4Khs6JV8377uLzAY9FDqPvpbuWMiXfE-05br3ouwzoYP6vsz9Zr-zDDRZVFsRwiumIWWaqKpgYzPIMN4oRQvQ9PMn90o5Vu1szEIZGZzNV1gNK33sPc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/UtBIVJcwApwkxpnwoWMQGrbS4Khs6JV8377uLzAY9FDqPvpbuWMiXfE-05br3ouwzoYP6vsz9Zr-zDDRZVFsRwiumIWWaqKpgYzPIMN4oRQvQ9PMn90o5Vu1szEIZGZzNV1gNK33sPc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/P7sRhvxJ0uFI_OHSp8u_Tdpsar2wkPoGLab7nvZatnBDSQxrRjNaAQaP7VJwxC4Ld6gB5yIZe0ur2vKhJPBaTWymK2yyFwz-hT1_eJh6QkpniHBTnGMOYOgNmRCxCkE6bjOMxpOEJ5E=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/P7sRhvxJ0uFI_OHSp8u_Tdpsar2wkPoGLab7nvZatnBDSQxrRjNaAQaP7VJwxC4Ld6gB5yIZe0ur2vKhJPBaTWymK2yyFwz-hT1_eJh6QkpniHBTnGMOYOgNmRCxCkE6bjOMxpOEJ5E=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9jQOq8aUPKLrJQ8KHBnqzgmOogWLJ7EtxhFEr6rl-blxHFdaoE5HY5PP1njIJ8fDJS4MlrG1JoEqoIa9ImobY3W9AtBuI4F37uvrYKFx7R8o0x7nChpskWdlJFBel1EnaLmxlXYyIpg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9jQOq8aUPKLrJQ8KHBnqzgmOogWLJ7EtxhFEr6rl-blxHFdaoE5HY5PP1njIJ8fDJS4MlrG1JoEqoIa9ImobY3W9AtBuI4F37uvrYKFx7R8o0x7nChpskWdlJFBel1EnaLmxlXYyIpg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/hWSa_2FxHtqZdl3u5RLk8quQ2eRlW_5nRXDpm7A1JJHVXJLqVHr6aJMTWOib_sVswQO2ik13M3XKCI7M01hbLiRMPjgJjIX4G28lOkAlYTFsfgWJy0Y0QJgOWZyAX7qKv92YqQHWT9M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/hWSa_2FxHtqZdl3u5RLk8quQ2eRlW_5nRXDpm7A1JJHVXJLqVHr6aJMTWOib_sVswQO2ik13M3XKCI7M01hbLiRMPjgJjIX4G28lOkAlYTFsfgWJy0Y0QJgOWZyAX7qKv92YqQHWT9M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/xecZBg8k7DkYr5LpH7Kd9tcR6MWYJevMpretlnJltgnyp6KYVrGFMT_303FuC4p2uPmMJ5xd6bQ4qbDpBC4OBo9B1imavX4WsBnUvFSDyMDMh1xjCB6xvZ4iKgC9UdkC_qC5McpPr-8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/xecZBg8k7DkYr5LpH7Kd9tcR6MWYJevMpretlnJltgnyp6KYVrGFMT_303FuC4p2uPmMJ5xd6bQ4qbDpBC4OBo9B1imavX4WsBnUvFSDyMDMh1xjCB6xvZ4iKgC9UdkC_qC5McpPr-8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/iPyr1IjbQC9iPzdg5dOnROUpsrRSgPb7a29p6AHgx71aCtB2eGOl93kmE9IiJ82j1fS09adzV0s4ioUNajTovL-LatgFOQuf2SngAHJuihofqXWyrzpgxFbmxUQLsk9Pv0Ed5W2zhgw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/iPyr1IjbQC9iPzdg5dOnROUpsrRSgPb7a29p6AHgx71aCtB2eGOl93kmE9IiJ82j1fS09adzV0s4ioUNajTovL-LatgFOQuf2SngAHJuihofqXWyrzpgxFbmxUQLsk9Pv0Ed5W2zhgw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/qQfEbydpzkTPFmq7VVonDDkCg-JsROv4-Ai6b3jS4G7ivcSQ-vdOwVriPEiX7DvibDv8RPs9OzOhK61VuxfDFG_5XXJMfiN9PWhHbFjMEWipgq05yXT2Rge_yKpbl53IbBQK3ivsb6c=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/qQfEbydpzkTPFmq7VVonDDkCg-JsROv4-Ai6b3jS4G7ivcSQ-vdOwVriPEiX7DvibDv8RPs9OzOhK61VuxfDFG_5XXJMfiN9PWhHbFjMEWipgq05yXT2Rge_yKpbl53IbBQK3ivsb6c=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Bq4_3JXGPE56C-v2c0ecUE4xHC663td-RCuIc2wcT_z7qdDAvVg8vUW1BqeYPXKPuK35Z0Fs3TsLkhNc_xBztk7PbfdihkJAGVRA2HGb8U4Rp1nSV-F5an7jmgDmXFaql1ZctqK-2yg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Bq4_3JXGPE56C-v2c0ecUE4xHC663td-RCuIc2wcT_z7qdDAvVg8vUW1BqeYPXKPuK35Z0Fs3TsLkhNc_xBztk7PbfdihkJAGVRA2HGb8U4Rp1nSV-F5an7jmgDmXFaql1ZctqK-2yg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/t_OBdkIs48kyjMdL6PnXXuRGmTbWa_KhaIrPqsr9McquXmMXao6Irr6opIgLIdRRDwBW01h7v2xsSCEZaltTACu1U5XM2kxAm8LjsEJj7GrQw9ZnhcB15eZicDcJIaAdCvbXgRlsk7Q=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/t_OBdkIs48kyjMdL6PnXXuRGmTbWa_KhaIrPqsr9McquXmMXao6Irr6opIgLIdRRDwBW01h7v2xsSCEZaltTACu1U5XM2kxAm8LjsEJj7GrQw9ZnhcB15eZicDcJIaAdCvbXgRlsk7Q=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Deadman Wonderland',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/uFb1bn6rHdDrbXUbqLkZFxxp09hQDByrYXVPT0k4rRkPvdHpyYMCGQN5ddo-wfuF7n5a3q8cJfIY0sm4FWcBPk5ZoShRCE6FmA0d0BQtzsNxJ5DlXNApPkfzsBHznn7i2pNsv7lj9NY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/uFb1bn6rHdDrbXUbqLkZFxxp09hQDByrYXVPT0k4rRkPvdHpyYMCGQN5ddo-wfuF7n5a3q8cJfIY0sm4FWcBPk5ZoShRCE6FmA0d0BQtzsNxJ5DlXNApPkfzsBHznn7i2pNsv7lj9NY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
