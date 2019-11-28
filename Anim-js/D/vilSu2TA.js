923d3b65228f8f6887ef9614a436af84592d3d17
dunia-anime <adityha.on@gmail.com>
2019-11-28 09:40:45 +0800

    Create vilSu2TA.js

---
+++ Anim-js/D/vilSu2TA.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9xiOecikvOu6EvpaP7T5qhQKTCwuA-rwQEjXGLg8BhyS7hVEa3B7a9J_sbH3gQ2IJSx0l0m1wvTmhdJ5fUat7GyL-_Fo3_GwTGf69IZK-syJDbhyuGyn92E_jWhy0A5756pVkdxxWLU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9xiOecikvOu6EvpaP7T5qhQKTCwuA-rwQEjXGLg8BhyS7hVEa3B7a9J_sbH3gQ2IJSx0l0m1wvTmhdJ5fUat7GyL-_Fo3_GwTGf69IZK-syJDbhyuGyn92E_jWhy0A5756pVkdxxWLU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3I29JubCagf_ZJ4hGN9V9eiHu-SUNLi_YfGUL3aBkkJVB37JsotoEIOCp2Ss-HGkACGwiIiMq6ej5c-U4V9WUY3nYOAj2phWMW5Izq4zkr6qWjvjrM2Xf9U1vtW5XvyxLZcOZLCb-HQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3I29JubCagf_ZJ4hGN9V9eiHu-SUNLi_YfGUL3aBkkJVB37JsotoEIOCp2Ss-HGkACGwiIiMq6ej5c-U4V9WUY3nYOAj2phWMW5Izq4zkr6qWjvjrM2Xf9U1vtW5XvyxLZcOZLCb-HQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/vGlnm4Bp7z_lQchWFZfchXV6nI8kwYfeQcVl-8m4fXwmfDPoVFE9T5r3q_FmJYBL1FLL0NFPEQOvJCNEeCsyzgO2jk76yrVodlrgTvj45syDZ1sa7t__u3d7fkOsY-XvosuqXj_nlUM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/vGlnm4Bp7z_lQchWFZfchXV6nI8kwYfeQcVl-8m4fXwmfDPoVFE9T5r3q_FmJYBL1FLL0NFPEQOvJCNEeCsyzgO2jk76yrVodlrgTvj45syDZ1sa7t__u3d7fkOsY-XvosuqXj_nlUM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/qSJCqdIlUw2tStFxdcwcNZnORHntF3V1yy28LZ2O6bo1RPmjXo4jgmlcrXoDopv8ffN6LwWsDjdB5ivjxqNv_xRif6EQapAovzIiYzpMPqZrboVFDxuD65ra5pruuKZWRwT2dW5pcVM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/qSJCqdIlUw2tStFxdcwcNZnORHntF3V1yy28LZ2O6bo1RPmjXo4jgmlcrXoDopv8ffN6LwWsDjdB5ivjxqNv_xRif6EQapAovzIiYzpMPqZrboVFDxuD65ra5pruuKZWRwT2dW5pcVM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nDGohMh6ta0LlWFeCFpvgaqFxcNMlRMK2J27gAtyUwWTE3wndmH5Cgrgfvnxoh3MT0W0h7aV9IPnP6-B2a_3NDIAmEU_N12lbcu0lqsuzQslfhHPxlDhwsudEwi4xsZHeGxdzg-xDN0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nDGohMh6ta0LlWFeCFpvgaqFxcNMlRMK2J27gAtyUwWTE3wndmH5Cgrgfvnxoh3MT0W0h7aV9IPnP6-B2a_3NDIAmEU_N12lbcu0lqsuzQslfhHPxlDhwsudEwi4xsZHeGxdzg-xDN0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/635lfWknnXzA0EVfL63i1q2yx9JZCaN3Hk6QZclgPhn9l-7yd6aQY0A78EtXWSI4QbZlMaBknbsW5tjIuypYrzdYNzqeCzxUBoH8mDY2Y0BRsiDTMAX8nrRGMkVXizhCpZEGJ5mTIQc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/635lfWknnXzA0EVfL63i1q2yx9JZCaN3Hk6QZclgPhn9l-7yd6aQY0A78EtXWSI4QbZlMaBknbsW5tjIuypYrzdYNzqeCzxUBoH8mDY2Y0BRsiDTMAX8nrRGMkVXizhCpZEGJ5mTIQc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/xPTkUdBWoZdB6VkeemBI1c8dLN7AfBtRSV23DCLA3QxBxHkAcjrGCR5BEOvqDfnX9TYvhYpeJKpLWM2x14UDeP25m8YMcG4hp9w0ID3fErCXH9xFEmv3b9g4nFVw2ux0IecBnkCFSEY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/xPTkUdBWoZdB6VkeemBI1c8dLN7AfBtRSV23DCLA3QxBxHkAcjrGCR5BEOvqDfnX9TYvhYpeJKpLWM2x14UDeP25m8YMcG4hp9w0ID3fErCXH9xFEmv3b9g4nFVw2ux0IecBnkCFSEY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/0RGs6XBpKxPtTXvIzYicIpZcWXodcOfcbGcnVf8cpCJy5AIPoSBpphwBfrLOdLo7qgfhAme-H5jEKViJVggldtSimAPthp75ew82oMMTMlhd_jPTMvaCyAXGDaroA6fEkh3gKhtADhY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/0RGs6XBpKxPtTXvIzYicIpZcWXodcOfcbGcnVf8cpCJy5AIPoSBpphwBfrLOdLo7qgfhAme-H5jEKViJVggldtSimAPthp75ew82oMMTMlhd_jPTMvaCyAXGDaroA6fEkh3gKhtADhY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/XcRc9O62VBESpO2rNX0hhAOx88lFiOiU_NFO0PcTivkiiVE3Sd-G9z6cUMW8blHUH_N8CdcWG0CNWbMBkm-ztTCQzznJMzwDkVQi7q2hYLRt_mD_9ezP4TUoRt2gG5RYndaCPASpMq0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/XcRc9O62VBESpO2rNX0hhAOx88lFiOiU_NFO0PcTivkiiVE3Sd-G9z6cUMW8blHUH_N8CdcWG0CNWbMBkm-ztTCQzznJMzwDkVQi7q2hYLRt_mD_9ezP4TUoRt2gG5RYndaCPASpMq0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/bA76zXEv37WLfmNPQv9APWgAOiZDQDfFlqpiUcAvocDeg7I0IPjP4APAhvv2wTJOITr30tpQFnLe1RiKjx07nQtnwVvM3OxD5n57FO0ca_yyKouiz3M1Hyxos-vB_7l3McHJxVYUCxE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/bA76zXEv37WLfmNPQv9APWgAOiZDQDfFlqpiUcAvocDeg7I0IPjP4APAhvv2wTJOITr30tpQFnLe1RiKjx07nQtnwVvM3OxD5n57FO0ca_yyKouiz3M1Hyxos-vB_7l3McHJxVYUCxE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/oNMd9I4uQ-fE8bAqcrAC83Ha23PSSJ7onusFZFsJGeZEeRRUdzXsuVJNyXLurdX7s506_a7one_hG7Z9Nj8dzMgjthB0w6yembdrbk_mGDMXFXgVOjtFkii_0GZDATaFC0EN2dY_eXE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/oNMd9I4uQ-fE8bAqcrAC83Ha23PSSJ7onusFZFsJGeZEeRRUdzXsuVJNyXLurdX7s506_a7one_hG7Z9Nj8dzMgjthB0w6yembdrbk_mGDMXFXgVOjtFkii_0GZDATaFC0EN2dY_eXE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/OkAnVEu8UPQPUr1jIT89x9kPT3i_yPxcS2KF3584g3h3ZWZX-Rv2U8dav_TM3ri-RFV7Q47eIYGzetQc2JVNMma9WG8w7AzDosJYk4Wzt4LAUfMVJHU-mB0Tyju7e21g-tDTt6KzCD4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/OkAnVEu8UPQPUr1jIT89x9kPT3i_yPxcS2KF3584g3h3ZWZX-Rv2U8dav_TM3ri-RFV7Q47eIYGzetQc2JVNMma9WG8w7AzDosJYk4Wzt4LAUfMVJHU-mB0Tyju7e21g-tDTt6KzCD4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devil Survivor 2 The A',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 6.98',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/0wBEbp9a1V3OgZMLjN0Gi0xnUMLbEE3Z67iG8b5obABljjnHA93F81KBU5EzCeN5rm4TcbsoJ7w9WKINsS-ab3Yi_O-maHQDU929bj-_jsXg_UzE3i36S5OYIaJg4pGkp9bvT-k-U0I=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/0wBEbp9a1V3OgZMLjN0Gi0xnUMLbEE3Z67iG8b5obABljjnHA93F81KBU5EzCeN5rm4TcbsoJ7w9WKINsS-ab3Yi_O-maHQDU929bj-_jsXg_UzE3i36S5OYIaJg4pGkp9bvT-k-U0I=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
