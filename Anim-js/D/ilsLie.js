d20d7b394d1cb0f7951f8436129263b9fb519351
dunia-anime <adityha.on@gmail.com>
2019-11-28 09:32:27 +0800

    Create ilsLie.js

---
+++ Anim-js/D/ilsLie.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Devils Line',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/OaexDDVlGO3JzeGOYWz0gJleQ_ogwyWR9RWhR57jtUcs4dyYhXTl5vbl74TnxTDjMbd8yJo0YH9WF_h3cYL0soqEl0z_5V1_BhjUFxUe5wgK0rnPBsZRCM1_3vs0BdtnGMtvuKww4Gw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/OaexDDVlGO3JzeGOYWz0gJleQ_ogwyWR9RWhR57jtUcs4dyYhXTl5vbl74TnxTDjMbd8yJo0YH9WF_h3cYL0soqEl0z_5V1_BhjUFxUe5wgK0rnPBsZRCM1_3vs0BdtnGMtvuKww4Gw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SU22X1zaS2qac9egRKotHCko01zJMktOUpYfiZ8jsat_ogCn-Ujnb5TdSA8guFoZKm8WbOyH1fzbl7yGd-zekzGHjZBlaz76xlMv8N1JEDifCsur4wK_fyV24sXNcf-r9EeVWUpDSPA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SU22X1zaS2qac9egRKotHCko01zJMktOUpYfiZ8jsat_ogCn-Ujnb5TdSA8guFoZKm8WbOyH1fzbl7yGd-zekzGHjZBlaz76xlMv8N1JEDifCsur4wK_fyV24sXNcf-r9EeVWUpDSPA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/RJEl86QzrhK-hsSws6oSVtN9WlHoXfZmV-nY1MDs8GfwL6WHgps-_TCGjRQeeodSSa3G99OlhX53B0v596eiAkpSqxUPxxGd_RKwC0l-lueFz_y_4GlhLRu3hfNP_x0vtI2HPXDFPbE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/RJEl86QzrhK-hsSws6oSVtN9WlHoXfZmV-nY1MDs8GfwL6WHgps-_TCGjRQeeodSSa3G99OlhX53B0v596eiAkpSqxUPxxGd_RKwC0l-lueFz_y_4GlhLRu3hfNP_x0vtI2HPXDFPbE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/z4brDUca2m40cTYydfb59oYL6LPOe984uB8jf02sjsaQdPm6id5TCDMlTNQb13h8eGOA8RrQqgtbsy-11Zb2Py2eaUItRB_V8K-1WMt5BJP-_JX6OmjngQxUydmEsKDBf6EGVYrlQBA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/z4brDUca2m40cTYydfb59oYL6LPOe984uB8jf02sjsaQdPm6id5TCDMlTNQb13h8eGOA8RrQqgtbsy-11Zb2Py2eaUItRB_V8K-1WMt5BJP-_JX6OmjngQxUydmEsKDBf6EGVYrlQBA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZuHDREoMjcdbgUoYw0S9ZeTlYweRzu3K7jz57-MgUOFrJtCs31VrfGcg2yb1DEB6wm8a2rXlNq2s_S6GKG4ojUCGH4nustH3KBJhJYv5XQDSgTgBxXQ09ct02vyiMVEQ3Ja_pL1_10g=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZuHDREoMjcdbgUoYw0S9ZeTlYweRzu3K7jz57-MgUOFrJtCs31VrfGcg2yb1DEB6wm8a2rXlNq2s_S6GKG4ojUCGH4nustH3KBJhJYv5XQDSgTgBxXQ09ct02vyiMVEQ3Ja_pL1_10g=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/t5wd9RbKHNGt62T6U6dX3pYB4H08-X7cRhByFZUuYYKMSkRPOwGyET2-rW3xDAmCi6MbAtlIw74dEo2L_E7GAW-3j1kxvMeDnBM9E0AkrlN2xbT-CqljancTkY1cND_nal_p1ofA6LE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/t5wd9RbKHNGt62T6U6dX3pYB4H08-X7cRhByFZUuYYKMSkRPOwGyET2-rW3xDAmCi6MbAtlIw74dEo2L_E7GAW-3j1kxvMeDnBM9E0AkrlN2xbT-CqljancTkY1cND_nal_p1ofA6LE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/EDioqztFpXCUzGMnMlzNH-G-8PuzYF7elPkbFre4AM_lYn_PL7RBFgmMhg9HbmhyLsCQnT2diJ54qd8yGi7w_XiJpKkDXWH_O14xHZSOuWsWkOoPeMY8yqTJpiuzahlbJ6AbrieBT68=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/EDioqztFpXCUzGMnMlzNH-G-8PuzYF7elPkbFre4AM_lYn_PL7RBFgmMhg9HbmhyLsCQnT2diJ54qd8yGi7w_XiJpKkDXWH_O14xHZSOuWsWkOoPeMY8yqTJpiuzahlbJ6AbrieBT68=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nEeQ8IY9M0tqkhbrZ_lA3d6zh3ioXhLm2wyUTrPAHES-Ks98VA4PokUU-Rs0F1UJ6KYB3acGTqSO6v7QvOwsJ0oxQXc502d0tDuIZTlR-4DJYnfSDbvHcTt5G6IIRn3HS2cq_fTsZv8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nEeQ8IY9M0tqkhbrZ_lA3d6zh3ioXhLm2wyUTrPAHES-Ks98VA4PokUU-Rs0F1UJ6KYB3acGTqSO6v7QvOwsJ0oxQXc502d0tDuIZTlR-4DJYnfSDbvHcTt5G6IIRn3HS2cq_fTsZv8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6qTDI6uT-IipDnxrgtbxENMgmKt4HHaX7o7OxhVS3rhGDfpb3B1ZiZJRwM0WDN9C-0khdilPyK2x9faXVwaW99W4QGEHX0qmty2fSsFsAGSiQnlkMG0_6wMyFZUW6E8LBN-qERwrug4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6qTDI6uT-IipDnxrgtbxENMgmKt4HHaX7o7OxhVS3rhGDfpb3B1ZiZJRwM0WDN9C-0khdilPyK2x9faXVwaW99W4QGEHX0qmty2fSsFsAGSiQnlkMG0_6wMyFZUW6E8LBN-qERwrug4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZISCd4bBWQ0ZcYUFkHOfwcAPGfxW8Pa9xKp7Z4trGB5s-ffFRPStMBXCNB3kPFEvXt4dVDi4muUgCKtFaOO9rtqqeBY7W1ebzUaJLEdyL0WtqQbFx6cW-h_HYYfWLYQTZybX_NOOTSs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZISCd4bBWQ0ZcYUFkHOfwcAPGfxW8Pa9xKp7Z4trGB5s-ffFRPStMBXCNB3kPFEvXt4dVDi4muUgCKtFaOO9rtqqeBY7W1ebzUaJLEdyL0WtqQbFx6cW-h_HYYfWLYQTZybX_NOOTSs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Tdtkte0fe9sQMO7ouBwUKzWGtuhhYWsOFFEjdqiqjI4yMxVUcX-Q6gZIqAiidvpjPS92B4r8LdfWEiKQb4SdINpPhOswzRw0E0LKZf_wjE6tHlgl4q6f5zIub4J5T6CuH6sFJHpEzSE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Tdtkte0fe9sQMO7ouBwUKzWGtuhhYWsOFFEjdqiqjI4yMxVUcX-Q6gZIqAiidvpjPS92B4r8LdfWEiKQb4SdINpPhOswzRw0E0LKZf_wjE6tHlgl4q6f5zIub4J5T6CuH6sFJHpEzSE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Devils Line',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.87',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/zKBi6FhM4PstDnfy-4M9EK6sW46jt-seylIEOt80Awoy1AZV4Yb062HO-tLNMtlwbzpK0Qe6dKIS8t8Y12ftS7zpzrMT5q9C4KT2gwIp3PcbLwMXO7jOCXVEYHyCyc5scUaf8oa3axQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/zKBi6FhM4PstDnfy-4M9EK6sW46jt-seylIEOt80Awoy1AZV4Yb062HO-tLNMtlwbzpK0Qe6dKIS8t8Y12ftS7zpzrMT5q9C4KT2gwIp3PcbLwMXO7jOCXVEYHyCyc5scUaf8oa3axQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
