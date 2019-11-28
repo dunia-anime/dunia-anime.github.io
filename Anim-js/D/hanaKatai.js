cfff64003e9b8a9e129e14c27fdbc02e3758542b
dunia-anime <adityha.on@gmail.com>
2019-11-28 10:29:04 +0800

    Create hanaKatai.js

---
+++ Anim-js/D/hanaKatai.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/l-eH7AiPO5sgqTj-cvxvqD2Sub3Eal4Y20wRxyNAbr6OtzFBl6kDHiHS_SunpbPN5HNNiYTSvF06LQtXY_pZmhH_xVSmbREgmgyxYfV7b_Y-qF3cxZ-o3vLB-ElSE9f9fFrsEbBQZzo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/l-eH7AiPO5sgqTj-cvxvqD2Sub3Eal4Y20wRxyNAbr6OtzFBl6kDHiHS_SunpbPN5HNNiYTSvF06LQtXY_pZmhH_xVSmbREgmgyxYfV7b_Y-qF3cxZ-o3vLB-ElSE9f9fFrsEbBQZzo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/BNdXtB4ouyHOLdh4KAnQs5EeoivzAJCV9BId3X40iFso4kanvAulx9Lrcxir-dNcQviKdxU6nVENW2pDr4UbzdPMXjSM_UI6Kn7a5DKx0dZZGu7wCJOfYJtekk3E3O8S6kRzVqhytVg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/BNdXtB4ouyHOLdh4KAnQs5EeoivzAJCV9BId3X40iFso4kanvAulx9Lrcxir-dNcQviKdxU6nVENW2pDr4UbzdPMXjSM_UI6Kn7a5DKx0dZZGu7wCJOfYJtekk3E3O8S6kRzVqhytVg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AbcDD2wJ3-QOmCOzWN1g_mjbBNtJhEHOD_0yzmrQaLyP_SXzyYeVPVJVidxt_VOT9hMVjarCfKGFFMmM3J0_N0DYWrgt_Z-cpDOrRQGDl_I8AO-t3m59ALAoats7bBrtxCm_ga13kWA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AbcDD2wJ3-QOmCOzWN1g_mjbBNtJhEHOD_0yzmrQaLyP_SXzyYeVPVJVidxt_VOT9hMVjarCfKGFFMmM3J0_N0DYWrgt_Z-cpDOrRQGDl_I8AO-t3m59ALAoats7bBrtxCm_ga13kWA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/iEYu-9O_bDgLJSyrw_DX6Z8wfmjUL1W5Bp_rpLqLrdXQPNCIfj4tBmGKXo3SkAmoQjN5lo1uQ12Kg9-rZOlQBIAmkRgnChEgEGxm9xd-ZyRRipdiJsGcAuuJfbQgoefgRzTDJWsLXtI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/iEYu-9O_bDgLJSyrw_DX6Z8wfmjUL1W5Bp_rpLqLrdXQPNCIfj4tBmGKXo3SkAmoQjN5lo1uQ12Kg9-rZOlQBIAmkRgnChEgEGxm9xd-ZyRRipdiJsGcAuuJfbQgoefgRzTDJWsLXtI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Zt9oytuERi7CgriuCzqa_Gkm2PkiCLG_WwCaLNyfMTmfYCx7So0STXpvGTyQAHtaMDvbISKML1tp2CRWljollbT5E9QWjODWWSk1sEc9q4c3-2zzBZxu9M4NPIiRPTf2wxnTPAp1KiE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Zt9oytuERi7CgriuCzqa_Gkm2PkiCLG_WwCaLNyfMTmfYCx7So0STXpvGTyQAHtaMDvbISKML1tp2CRWljollbT5E9QWjODWWSk1sEc9q4c3-2zzBZxu9M4NPIiRPTf2wxnTPAp1KiE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/0A1PhTiScU-1xY72oKAwcOXDZJL9FfdUrcjw61Fp_SncuGK_3iwqpXbdNggoar1afh3IGvTyOCcqVoO4nb9uZSvUijwuqBQV6tcrm8ZsOtbpZONGxAVdGm85-utqQYtBMj3KzkLJGrc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/0A1PhTiScU-1xY72oKAwcOXDZJL9FfdUrcjw61Fp_SncuGK_3iwqpXbdNggoar1afh3IGvTyOCcqVoO4nb9uZSvUijwuqBQV6tcrm8ZsOtbpZONGxAVdGm85-utqQYtBMj3KzkLJGrc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/1kKTow2d4K13qA9tr8jc852MmUS2UNeNvMp5cjTp6bZvf3sY193nCwvYYdS363_cpKTzIirDXKWiSgNejAbMZ4Fers4yUmpg7DyhSgnJKFgWWAiFdPhveKKnR668Blf3vkwq1z8cyHI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/1kKTow2d4K13qA9tr8jc852MmUS2UNeNvMp5cjTp6bZvf3sY193nCwvYYdS363_cpKTzIirDXKWiSgNejAbMZ4Fers4yUmpg7DyhSgnJKFgWWAiFdPhveKKnR668Blf3vkwq1z8cyHI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6yn4ld5OkE3NpvSkjjNFtg50Z3-x-X6rtHQJPz06cITj1UJb3rqgGob-ce-q_4syD6-6sCeac3oGrxGmcwwzcWBjIZuR5xJPzdltUxwbdiS725B0shaOC2-ysm6Q5NIOZP049jLYt_Y=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6yn4ld5OkE3NpvSkjjNFtg50Z3-x-X6rtHQJPz06cITj1UJb3rqgGob-ce-q_4syD6-6sCeac3oGrxGmcwwzcWBjIZuR5xJPzdltUxwbdiS725B0shaOC2-ysm6Q5NIOZP049jLYt_Y=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6XXxRb-98_ktKka248p8V2pa7d3vAhz50QaXjbKA9kUqU0A1vSZSaVdGy8N1cSKCXFNGNvNceGNPl_rb1eLA644bzGGTkV5bJlG7Av5TtwMvx0whKXcJijupYwUhHJRRaOr2jpF5sgU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6XXxRb-98_ktKka248p8V2pa7d3vAhz50QaXjbKA9kUqU0A1vSZSaVdGy8N1cSKCXFNGNvNceGNPl_rb1eLA644bzGGTkV5bJlG7Av5TtwMvx0whKXcJijupYwUhHJRRaOr2jpF5sgU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/T1miZkE_sWOU_owX_iwMfhBA4WeklBL-TgeCZsaNucoXnMCvnUlkuF6DAZ94gJ13KzWMucLViJFi2fDsQJAUbMJSUJ6z4DbNkUXctQEA8fIq6fU4fECE1ZJaCZMH79WfI9SHknC0uqU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/T1miZkE_sWOU_owX_iwMfhBA4WeklBL-TgeCZsaNucoXnMCvnUlkuF6DAZ94gJ13KzWMucLViJFi2fDsQJAUbMJSUJ6z4DbNkUXctQEA8fIq6fU4fECE1ZJaCZMH79WfI9SHknC0uqU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZuGYWPRLeTFMNTRvHFYPoTBBe1UXhW9ijWtVkEru1bBA3PxrIVzLvgrZoMU8rI0ryL4oVVDhLHktI4-QEANdzMNe6x7DEovLi4JqBMDmmnjaOq3ugMJXhgcKIqF9oXvPtB7Bo6fnOh0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZuGYWPRLeTFMNTRvHFYPoTBBe1UXhW9ijWtVkEru1bBA3PxrIVzLvgrZoMU8rI0ryL4oVVDhLHktI4-QEANdzMNe6x7DEovLi4JqBMDmmnjaOq3ugMJXhgcKIqF9oXvPtB7Bo6fnOh0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Dfa66cOoArp-kUxcxrf3UfTu-4SXTcQ0f4aG9CTXF54YlUdxxHAKzFgagebZzGU9Sg46-btlax3OR_W6TzChgqpO43Zd48Tsyhxtz78ae0QMHi9-ANfHymn7m3JhTz3ZU_ivztoM-00=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Dfa66cOoArp-kUxcxrf3UfTu-4SXTcQ0f4aG9CTXF54YlUdxxHAKzFgagebZzGU9Sg46-btlax3OR_W6TzChgqpO43Zd48Tsyhxtz78ae0QMHi9-ANfHymn7m3JhTz3ZU_ivztoM-00=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Demi-chan wa Kataritai',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.72',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/1Qw_GCtvyWqmdP9DM0IpgHbsSw9jg3477NrPEnSW01fobVwpFgWZDUWvzs0nHKMh6pIGkFuiO2UQ6d0guLcsOMtK2oNRffwiJFgT_aq0p76OQvuyz3EGK7-qnH_EJoFKOzCJqHSvOoM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/1Qw_GCtvyWqmdP9DM0IpgHbsSw9jg3477NrPEnSW01fobVwpFgWZDUWvzs0nHKMh6pIGkFuiO2UQ6d0guLcsOMtK2oNRffwiJFgT_aq0p76OQvuyz3EGK7-qnH_EJoFKOzCJqHSvOoM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
