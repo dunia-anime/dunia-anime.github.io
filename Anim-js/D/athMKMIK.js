12bd0c57471a57e11cef134a9d4d39a7edc4f688
dunia-anime <adityha.on@gmail.com>
2019-11-28 10:50:06 +0800

    Create athMKMIK.js

---
+++ Anim-js/D/athMKMIK.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Death March kara HIK',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3MnVsdPJXkcM6e3H1yeuKIuHieOH304JnRM3UpiNiOMErISs4ngXFI0JW6I03J0iKrXGjVO9mmBu8JCmgEeJlZsciXO-VX9ytX8PLsrsGw7X5eLMhkoUVmzEya1SLX20flX0dOQdgnk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3MnVsdPJXkcM6e3H1yeuKIuHieOH304JnRM3UpiNiOMErISs4ngXFI0JW6I03J0iKrXGjVO9mmBu8JCmgEeJlZsciXO-VX9ytX8PLsrsGw7X5eLMhkoUVmzEya1SLX20flX0dOQdgnk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZORLtOICv1NKanFRL_eklKnocotPwQmqxT9iUWJ3O02Ygjeo7BNN_wl4nQAS8I_5ii6f4CJ9BHz4qhDsBbSj5WfzrZYsNWkgN-52XyU2XlkHicwqaparqpQprglwEtsWvNt2lF1yDjk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZORLtOICv1NKanFRL_eklKnocotPwQmqxT9iUWJ3O02Ygjeo7BNN_wl4nQAS8I_5ii6f4CJ9BHz4qhDsBbSj5WfzrZYsNWkgN-52XyU2XlkHicwqaparqpQprglwEtsWvNt2lF1yDjk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/S76IOE1BHFxgU-NmzRQjfeLj5U3xHJ8k9VRyU2_NibcPd8ufZGK0DBWwGOC-7CIEnZZZnMtEjT6UNWK1PYBXfV6mJT7w0yhlkdkvCvKApDERpDgLOEnUdNtFTYAp1kZNcgwSqnkpueU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/S76IOE1BHFxgU-NmzRQjfeLj5U3xHJ8k9VRyU2_NibcPd8ufZGK0DBWwGOC-7CIEnZZZnMtEjT6UNWK1PYBXfV6mJT7w0yhlkdkvCvKApDERpDgLOEnUdNtFTYAp1kZNcgwSqnkpueU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jKUfUtZsBLx0OQ4Q0QGExRnJDjx3nkE7qTef0oI1Sovx-HvcFoV91Cm16HhJF4lTojnIJidJG8LZP2RpREPSJrwXno6Sf2wrEtwhjdlDzZ4AlovS1Gn3UDWLMajVgRTd2D8lTd_chSs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jKUfUtZsBLx0OQ4Q0QGExRnJDjx3nkE7qTef0oI1Sovx-HvcFoV91Cm16HhJF4lTojnIJidJG8LZP2RpREPSJrwXno6Sf2wrEtwhjdlDzZ4AlovS1Gn3UDWLMajVgRTd2D8lTd_chSs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/eBYNzWUw1mO4kL4TYAMsVvyJQPqfsECv7Q6FEBpOOT6jUlkL8L-v-PThLE0Ruh2-5yEY_fobt4yerh5vQo_qmYNYZKEtE3BbRq20Uip6aBIvyeayzVtzz1mGXiO-QLlUm1wNn--WzaM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/eBYNzWUw1mO4kL4TYAMsVvyJQPqfsECv7Q6FEBpOOT6jUlkL8L-v-PThLE0Ruh2-5yEY_fobt4yerh5vQo_qmYNYZKEtE3BbRq20Uip6aBIvyeayzVtzz1mGXiO-QLlUm1wNn--WzaM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/zUssMvOdWNcasdMe71jAW9kXlRL7JmgeBRFS8l7hQFZzUn-8mhhAFJAWky7WLjw2A6YtYKLhHGnZ5R0beshBX4SOM3IYdeDiEq6mRjZEdI0y80PlWL1j-AHjRG92jM-5oa7Xym7PEvE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/zUssMvOdWNcasdMe71jAW9kXlRL7JmgeBRFS8l7hQFZzUn-8mhhAFJAWky7WLjw2A6YtYKLhHGnZ5R0beshBX4SOM3IYdeDiEq6mRjZEdI0y80PlWL1j-AHjRG92jM-5oa7Xym7PEvE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/mcZCqN8E6I1MmyBo8VUPxKXvxKS7pHablPj6n3qSGAy96B-SQlnLhYEPidqe7h_m27eh6oaHotKs7bL77txPgruwmfRcNsjUs0M9ey4Q8LqiDMwgZFdsZUXf8vyv4PDsnKk3KhfZLDc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/mcZCqN8E6I1MmyBo8VUPxKXvxKS7pHablPj6n3qSGAy96B-SQlnLhYEPidqe7h_m27eh6oaHotKs7bL77txPgruwmfRcNsjUs0M9ey4Q8LqiDMwgZFdsZUXf8vyv4PDsnKk3KhfZLDc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6Qb3kTGCsVi1tzetJVw0Dj1LI0TTFHEuf02pa2Oy-gvNTTH5hvqFtnLxY8uMX1JrXpa3ZtJqsMKfvMu_epeCypRige5OfwYKutnOb2J4V2WzbYwun6jaqBU7ggh4RxMB3IXyn5mv-ao=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6Qb3kTGCsVi1tzetJVw0Dj1LI0TTFHEuf02pa2Oy-gvNTTH5hvqFtnLxY8uMX1JrXpa3ZtJqsMKfvMu_epeCypRige5OfwYKutnOb2J4V2WzbYwun6jaqBU7ggh4RxMB3IXyn5mv-ao=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/4hEZUev3HXhejxOy7fNCeue5V36Y1z9duMZvyOe0-c-RmkG9hWvrzboxEHDdff1i6VK0ZwuDS35HpVOWIhnAlrq3NNrxQ9YWDIVZAHJcAR2KV5tVzAl9WWxk57rGZZ8nAx0jo6lRIvE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/4hEZUev3HXhejxOy7fNCeue5V36Y1z9duMZvyOe0-c-RmkG9hWvrzboxEHDdff1i6VK0ZwuDS35HpVOWIhnAlrq3NNrxQ9YWDIVZAHJcAR2KV5tVzAl9WWxk57rGZZ8nAx0jo6lRIvE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Lxwdj6PZptnxG2gTZjNMHtYuaCHD0jQsvKXeHX6iaiYsPeaekWSCqNsk5TkcZLCiaz_3D3QvgQTZWC2vmyRMq6VT9oOblStttobIcwgJ62rs9uorYyvG9DlJkfmX9uBusNPaqHcf5ao=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Lxwdj6PZptnxG2gTZjNMHtYuaCHD0jQsvKXeHX6iaiYsPeaekWSCqNsk5TkcZLCiaz_3D3QvgQTZWC2vmyRMq6VT9oOblStttobIcwgJ62rs9uorYyvG9DlJkfmX9uBusNPaqHcf5ao=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/PVsnQOB_x5b2qjFSHvJGUzh17R9oSguJWsbKK-RMCG6pEzxBApm-WAS12IBvs_v1R34vxmgPEPSoyAC_0kppRUITr1BINPh2RzFDHLtZF4vFtSQEOB-laeBwBDR_L7dkBUuHg8Mtqtk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/PVsnQOB_x5b2qjFSHvJGUzh17R9oSguJWsbKK-RMCG6pEzxBApm-WAS12IBvs_v1R34vxmgPEPSoyAC_0kppRUITr1BINPh2RzFDHLtZF4vFtSQEOB-laeBwBDR_L7dkBUuHg8Mtqtk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Death March kara HIK',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/97i_NOceBX0lrpKtyjECKLmKcwn1HKYMAM1YNAS8Mufruj_Oouf0XIWGxNMSb3ZYTtM037EWe29bIyVWrNRnYnWZaWH-SSgTnPuolmTJ3WQipiJgcz4mF4sci6z8QI8sxpidbzXYMuM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/97i_NOceBX0lrpKtyjECKLmKcwn1HKYMAM1YNAS8Mufruj_Oouf0XIWGxNMSb3ZYTtM037EWe29bIyVWrNRnYnWZaWH-SSgTnPuolmTJ3WQipiJgcz4mF4sci6z8QI8sxpidbzXYMuM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
