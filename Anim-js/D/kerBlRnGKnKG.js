46c00a86f89e1b0d04c77b2ea3952b99831659c7
dunia-anime <adityha.on@gmail.com>
2019-12-01 18:20:48 +0800

    Create kerBlRnGKnKG.js

---
+++ Anim-js/D/kerBlRnGKnKG.js
@@ -0,0 +1,128 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Darker than Black KnKG',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/BBf9-woD39HacU9__FLYfyUp5tpjrsSdhTcjYj1C121GemXGy7Yxb5cE9a9703r4KxfOrBkFCg1JKTz1gZEtfgmFTUhcFCANwPSrN3hwk3QPPI-AOWQu3j4fqpD2BwgjOpTbfH3x0rg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/BBf9-woD39HacU9__FLYfyUp5tpjrsSdhTcjYj1C121GemXGy7Yxb5cE9a9703r4KxfOrBkFCg1JKTz1gZEtfgmFTUhcFCANwPSrN3hwk3QPPI-AOWQu3j4fqpD2BwgjOpTbfH3x0rg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Darker than Black KnKG',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/77udYZdLGxVbsam5i8TlN0oQX7flBQ8t7CU9iRSwV7JABlN6zLUXd01uJ4ElzLnySsgBdg3j8olZCjMG8sci_tgtnTcb3SZYOsxPDPrgwGeQwSHWuTrjHHN2TuQbwHdlMwHRFpRhIFM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/77udYZdLGxVbsam5i8TlN0oQX7flBQ8t7CU9iRSwV7JABlN6zLUXd01uJ4ElzLnySsgBdg3j8olZCjMG8sci_tgtnTcb3SZYOsxPDPrgwGeQwSHWuTrjHHN2TuQbwHdlMwHRFpRhIFM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Darker than Black KnKG',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/TxWftRnhOoFF4DvUkX_FpMNwyxcO2uWUjiMCCvTRwIjyJ-qm68_EVuUOHsBJsF_IigJQ-aezIQ2PGWLDIu4p0yGz8BAWvjt8PMXdpOw9OGzAPEMA-dClSj7790L6ZtmowJ5R0Kfjj3k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/TxWftRnhOoFF4DvUkX_FpMNwyxcO2uWUjiMCCvTRwIjyJ-qm68_EVuUOHsBJsF_IigJQ-aezIQ2PGWLDIu4p0yGz8BAWvjt8PMXdpOw9OGzAPEMA-dClSj7790L6ZtmowJ5R0Kfjj3k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Darker than Black KnKG',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/qCOGpyz9tQYEnV_e9MDhnyKRV3nJl6TyID3nfYB4PskNYiPcK_scAYHxkl8eWbR1Fal8AzAXNGFeizGgR7wHcOBZmHRN9ekpTayvPZhBHVgCZmZBUYDogyVet4na0OTAbyMSInMyc84=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/qCOGpyz9tQYEnV_e9MDhnyKRV3nJl6TyID3nfYB4PskNYiPcK_scAYHxkl8eWbR1Fal8AzAXNGFeizGgR7wHcOBZmHRN9ekpTayvPZhBHVgCZmZBUYDogyVet4na0OTAbyMSInMyc84=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
