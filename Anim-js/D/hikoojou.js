099d3b95dc7d1ce9857ab49de289570d557d7c27
dunia-anime <adityha.on@gmail.com>
2019-12-01 19:19:47 +0800

    Create hikoojou.js

---
+++ Anim-js/D/hikoojou.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9dvDxB_jBzWo-42Imc92kQl-TXK53MNJjz0BJcL7g7QI0fD82dI6aMOtjHOOfXGm5OgF1366ICiUwT6QeW9Na5AXlbDcVFUuRTuI3jAFUvkjMMSm-4PIh6tqVL23MbL7O5EpbTGhaYE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9dvDxB_jBzWo-42Imc92kQl-TXK53MNJjz0BJcL7g7QI0fD82dI6aMOtjHOOfXGm5OgF1366ICiUwT6QeW9Na5AXlbDcVFUuRTuI3jAFUvkjMMSm-4PIh6tqVL23MbL7O5EpbTGhaYE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/pulO9An61F2K-DwBrAEqRvZYumTqEsLFXZobZcKFrJUfKiyF_ATtfBcB__Qigx6hndDzbqpZrNUEt139Io8Fhk0FV3LjT1fD6xWnLjDohJVk40LX9PJGFTlbsonBfmWTdJn1ZkSX5v0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/pulO9An61F2K-DwBrAEqRvZYumTqEsLFXZobZcKFrJUfKiyF_ATtfBcB__Qigx6hndDzbqpZrNUEt139Io8Fhk0FV3LjT1fD6xWnLjDohJVk40LX9PJGFTlbsonBfmWTdJn1ZkSX5v0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/kESfXTaDk66mJK4Ua4SslQSDRCv0ZKASh3Xz3CvX_1GdNVu_8lhh9L8_sxvJWhBYh4Jyi0ATo5DzhzkVhsIcINFWP7PG5v4_Y9H7bjQMfuteJaZd6ebHrsctb-lq--p7GuiF-6UL0AM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/kESfXTaDk66mJK4Ua4SslQSDRCv0ZKASh3Xz3CvX_1GdNVu_8lhh9L8_sxvJWhBYh4Jyi0ATo5DzhzkVhsIcINFWP7PG5v4_Y9H7bjQMfuteJaZd6ebHrsctb-lq--p7GuiF-6UL0AM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/yNOSX7xd0ffZnFnRQ_cqLhBNxZuWQATaM_FDAKN0pB5pYpe5jdkMwPb6ZD5KWkVdMmg3-smcIdFz341k0EPv733VonS2iPrIdL3obwvUAbLbG64Y1hdCKRwYZDrdkk0AldFKvG7sCP8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/yNOSX7xd0ffZnFnRQ_cqLhBNxZuWQATaM_FDAKN0pB5pYpe5jdkMwPb6ZD5KWkVdMmg3-smcIdFz341k0EPv733VonS2iPrIdL3obwvUAbLbG64Y1hdCKRwYZDrdkk0AldFKvG7sCP8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/r-pIFabhflUb6IK_8KXGuku7ddMwASXCQn-dXVoh1GD98IKOa4HAJ6MSO2aZtTAFltO-tcBi2nvzlKFcaP_oxSqg-g4cXsgOj2kOSyY6WfjT5vH9njk7UB_13K1e1cSShc6tMkiFeSA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/r-pIFabhflUb6IK_8KXGuku7ddMwASXCQn-dXVoh1GD98IKOa4HAJ6MSO2aZtTAFltO-tcBi2nvzlKFcaP_oxSqg-g4cXsgOj2kOSyY6WfjT5vH9njk7UB_13K1e1cSShc6tMkiFeSA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/mekqyigxkvPdYVXC5s78OnOEm9aQoAdc0FwYvrLqFBXJ6nzl3MKG_Q5hBqWFww1YZmFqCpCgKVR68WVCT5eMcaBwX7AkvrPNEjH42YZgzIvRWh-q6ZP_W-qHnwDYwNP8oK8a2v87egU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/mekqyigxkvPdYVXC5s78OnOEm9aQoAdc0FwYvrLqFBXJ6nzl3MKG_Q5hBqWFww1YZmFqCpCgKVR68WVCT5eMcaBwX7AkvrPNEjH42YZgzIvRWh-q6ZP_W-qHnwDYwNP8oK8a2v87egU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/DsbtHgKtmaZf2JFeF3mZABwo7k-Ma-aYagGq4LKeCd1oikB59ZvQu3obsQ8F4wLImvqbWTwNUzT5aj0eG9qGTcOcSvZVr9VSM52FipExVo955Abqyt2b2U7ZZZkmA0zn5JsHWeXXk24=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/DsbtHgKtmaZf2JFeF3mZABwo7k-Ma-aYagGq4LKeCd1oikB59ZvQu3obsQ8F4wLImvqbWTwNUzT5aj0eG9qGTcOcSvZVr9VSM52FipExVo955Abqyt2b2U7ZZZkmA0zn5JsHWeXXk24=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Z_Xbqba-ZqrEvvLUeghn82QTeFymrlv00_dMx9IsAsC8iYUZV3Z9lmaxANWiIK2cluqVeHWIbsvUJ42AxnNKXMi4wiupKEXHk0-KeKzafKFnZmsxcn3Ksqx9UGR4cY9iCstvHQQB_Hc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Z_Xbqba-ZqrEvvLUeghn82QTeFymrlv00_dMx9IsAsC8iYUZV3Z9lmaxANWiIK2cluqVeHWIbsvUJ42AxnNKXMi4wiupKEXHk0-KeKzafKFnZmsxcn3Ksqx9UGR4cY9iCstvHQQB_Hc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/pnDkyq4oH1k6CYQ27fyfj5xeg7MkyBzvAHIm7SGFPUdXw-DAjI9qeGRAjq8-mpL7a9tPuR_hr8tJvnnGCQUE59FF2a6vNzIDQSyviFLEVRETvPW_xIPOTQt8C1UmfBSGX1j_8sjIHnM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/pnDkyq4oH1k6CYQ27fyfj5xeg7MkyBzvAHIm7SGFPUdXw-DAjI9qeGRAjq8-mpL7a9tPuR_hr8tJvnnGCQUE59FF2a6vNzIDQSyviFLEVRETvPW_xIPOTQt8C1UmfBSGX1j_8sjIHnM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZArchEbznVDru_u2Cgvg0EEbtNdoXVBjsteNVAyWPEia-nKKfFsamf-RTn5bDb64z37q7R2S47IN8Hic3MnrQZ6mfScHHqOyo4wbX-Xasstf8JtdMd5KygQoF4wU5fYcPs5XPPkBWtw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZArchEbznVDru_u2Cgvg0EEbtNdoXVBjsteNVAyWPEia-nKKfFsamf-RTn5bDb64z37q7R2S47IN8Hic3MnrQZ6mfScHHqOyo4wbX-Xasstf8JtdMd5KygQoF4wU5fYcPs5XPPkBWtw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/rQLXYafB6qC_FGHTjrSnqkZojvxpmExdDcz6A7KP3zXAdXzfR136y-_Ipt3Y-UEIPM25jyec1S0_GKjJlVIWq_N8I73yeXAzYednmav8ZtUbxPOHZOnsl6djbwttZT1A4vpzcdIDPOM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/rQLXYafB6qC_FGHTjrSnqkZojvxpmExdDcz6A7KP3zXAdXzfR136y-_Ipt3Y-UEIPM25jyec1S0_GKjJlVIWq_N8I73yeXAzYednmav8ZtUbxPOHZOnsl6djbwttZT1A4vpzcdIDPOM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danshi Koukousei no Nichijou',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 8.32',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AXsHeFJBaOO6I0PP911jw0KzSyVrwUnnTy7JrrMd_QL005C1fuxPxHjGFkWJGbbTGcjumQYh_gc9cL3vUkEFdXNBVPYzShV2ISII37o9URX8OYR4WVL5vJXidC-jQ63oqwKPcP3tcj4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AXsHeFJBaOO6I0PP911jw0KzSyVrwUnnTy7JrrMd_QL005C1fuxPxHjGFkWJGbbTGcjumQYh_gc9cL3vUkEFdXNBVPYzShV2ISII37o9URX8OYR4WVL5vJXidC-jQ63oqwKPcP3tcj4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
