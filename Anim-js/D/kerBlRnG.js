f48a048b5202f0f4eb433a36687b6ca81b344055
dunia-anime <adityha.on@gmail.com>
2019-12-01 18:58:24 +0800

    Update kerBlRnG.js

--- Anim-js/D/kerBlRnG.js
+++ Anim-js/D/kerBlRnG.js
@@ -1,15 +1,15 @@
 var player = videojs('video')
 player.playlist([{
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 01.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 01.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 01',
-        name: 'Episode 01',
+        episode: '* Episode 13',
+        name: 'Episode 13',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/n03WCFUfizJ_mMt_ESzlfxY8-Cx9k2wExeQBhBjvcWRHiXmxO4wMyp3Ht5OKuPXstRbChdlUGKB1FgoKB9N40D9NIO_6V-a7xaBEiR-i7i2f8Jlexh1X1300nPCVoWSH3cdX7FWy6dc=m22',
@@ -36,15 +36,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 02.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 02.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 02',
-        name: 'Episode 02',
+        episode: '* Episode 14',
+        name: 'Episode 14',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/4r6vIEgaLV416jb1NjXTahCPYcOQTIgNbtyRIN358ZmlHzCPqA4D8bCS4_zxColCjpIovlDO_r1rerYg_kL_fzRkZAhg7KgHPrynpI8y1WM24wIbz6iAPqtLgg7Ht08pH4DvzVmMA2w=m22',
@@ -71,15 +71,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 03.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 03.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 03',
-        name: 'Episode 03',
+        episode: '* Episode 15',
+        name: 'Episode 15',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/o-zrBZN89KhZfr2oF16G_UuPUUx6fVZLgjice7sKqNn0_gPjxNH4j0b0KuPkVDfIBb6NX1ckeHMe8TC84tvNg1t2OYU4304HoUKKCJyOeWH3RrCImUl890AqQjQb_wGzv_7UwDOcEsM=m22',
@@ -106,15 +106,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 04.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 04.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 04',
-        name: 'Episode 04',
+        episode: '* Episode 16',
+        name: 'Episode 16',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/azX19-MxDNmZRNn9JTX8O-x9w4xgxjyLeeQ12fLtBZEyLqTTJKkocqg2UatfUwcgkm0ITLVLpjTUQTCOiKastM35VDjzUVQcXk3bJW6wpHApiAZoCu-Nq-LwoNBk45VYofQE08-epNc=m22',
@@ -141,15 +141,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 05.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 05.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 05',
-        name: 'Episode 05',
+        episode: '* Episode 17',
+        name: 'Episode 17',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/xgpbjrowJ6lIy_bQ1-ol8X57qGp5otGWhrmrjxj5HhepEmeqGwf5kS6kUhM_72sVndxk5ufC3h2D5u2hiETjDgZwJNqsXzHiLSVeaW5KF835e8BOvjqhWnhJZuLGjCmrzk-ohvLSSLU=m22',
@@ -176,15 +176,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 06.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 06.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 06',
-        name: 'Episode 06',
+        episode: '* Episode 18',
+        name: 'Episode 18',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/PbBTqgf9-Fdpm0nYjJYxT-u4sxz2OHGej6CaWfjnuTcVGTsKuvTkwz3NeV7gzFm6zNtyyaFMqrFOOQP5JfcCrANwPX9svWuldQiTRADpDtqD7ah5OdbM1baNQztIU_HYUOhsqS5ZNBg=m22',
@@ -211,15 +211,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 07.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 07.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 07',
-        name: 'Episode 07',
+        episode: '* Episode 19',
+        name: 'Episode 19',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/xSWKC5L5TJfHrXUv0IcmZsteWzksUODY6dT8acJQy4ZhQ03OQfFmwLuef-7n5JfkgwdOHUy6CVwF0tlUHELzQBV5QzTvf_VZeZk-0Cq2jexq-ToUven-egfsZZBjxp81zrI4fZvuVqU=m22',
@@ -246,15 +246,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 08.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 08.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 08',
-        name: 'Episode 08',
+        episode: '* Episode 20',
+        name: 'Episode 20',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/2oPAH6ME7VwovlS3VGZoc2rzUPMpR78nZALay5MYtlhEo8eWao1MN9cKvMWZTfo22yS1bBvUanDY9VuveSYf9qU-vH7VRnN0SU7veUN6lAKYOxrL7mFzzMBLlAxUpuBmNwXZiUg1Xpo=m22',
@@ -281,15 +281,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 09.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 09.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 09',
-        name: 'Episode 09',
+        episode: '* Episode 21',
+        name: 'Episode 21',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/12gz-W2XZJYzSRiGoOCJUll6MF1QXGrvMv75C34k3nZMATyzTWRyB_s7lKWbZInpDInapXlVr1W5d9EkxfKr5j6fZw9YiLMdBRhy9ggfPNnvugOiZoBJnOl0U3z3rj5weDAXERqogxM=m22',
@@ -316,15 +316,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 10.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 10.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 10',
-        name: 'Episode 10',
+        episode: '* Episode 22',
+        name: 'Episode 22',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/Kqw3mZpbsB2uLYMTurFNafWQvslbbQCEtyKX9oizgW_X1Bu9NRomoIbLVcLMv4itKUMzjlT0QlRnIafF7qlVPSeaBS0JN0cJRHbIo5PkjbV197XmPLouUOk6Z3tJV1MN5oNEhL6TzMo=m22',
@@ -351,15 +351,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 11.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 11.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 11',
-        name: 'Episode 11',
+        episode: '* Episode 23',
+        name: 'Episode 23',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/I-fodr6fPtctgtFddruMIqzVBI_j80PdOszvZCelIyCxi4GES9l7VT5Sbrte3Uyw4Xoa5t8QffnxlvtWdaWbFVrLarSaplIOCC3l0lswH165iefsMReZeP1i7_bqKOzcr9mMH_g0fbE=m22',
@@ -386,15 +386,15 @@ player.playlist([{
     },
     {
         textTracks: [{
-            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/[dunia-anime] Darker than Black Ryuusei no Gemini - 12.vtt',
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/D/Darker%20than%20Black%20Ryuusei%20no%20Gemini/%5Bdunia-anime%5D Darker than Black Ryuusei no Gemini - 12.vtt',
             kind: 'captions',
             srclang: 'id',
             label: 'Indonesia',
             'default': true
         }],
         judul: '* Darker than Black RnG',
-        episode: '* Episode 12',
-        name: 'Episode 12',
+        episode: '* Episode 24',
+        name: 'Episode 24',
         deskrip1: '* Rating : 7.53',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/x5DtX9aCF7GZw4Ws5DYCKSU5D2qYxVDhl5jy9BJr0HoOUdEin6KGHD68XtHRoGJ45ZfnpfGIZEMNkPuyd5f7liMdxfbaFQ_RbXZ0vOUAgmLlnGfALFZTrEHTpmhFiGaKczrVZlfbf2U=m22',
