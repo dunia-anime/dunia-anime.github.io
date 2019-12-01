bf222a9da8dcefb709fbcc4c5f735c32248829f1
dunia-anime <adityha.on@gmail.com>
2019-12-01 17:57:34 +0800

    Create teAveS1.js

---
+++ Anim-js/D/teAveS1.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/v85y-fGAXB29MeffxMNAlxIqQjUJ2K4oBNHZznZvO11DWi-ZKKwL3Q5p00_RDJDe9SLUL-aL0B5J_nBjylKIeKOJXK6TNYOg5-7WZumg7YxuIKtXkfDR55SBcqg7yMZhvm_4zYfQKTU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/v85y-fGAXB29MeffxMNAlxIqQjUJ2K4oBNHZznZvO11DWi-ZKKwL3Q5p00_RDJDe9SLUL-aL0B5J_nBjylKIeKOJXK6TNYOg5-7WZumg7YxuIKtXkfDR55SBcqg7yMZhvm_4zYfQKTU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/-VNFvqEv1CkNT_nLSkOJS8lNA3hCElufdLe3RjAb3pSn7nePCdQTr349zBPR1RX-PrdTAqL3tONgRYAXy1Rr2ddu2kfw0frsQJTcNRhUzMqzfpIQKilFIP62oX-HnTvP86iBOH7x0G0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/-VNFvqEv1CkNT_nLSkOJS8lNA3hCElufdLe3RjAb3pSn7nePCdQTr349zBPR1RX-PrdTAqL3tONgRYAXy1Rr2ddu2kfw0frsQJTcNRhUzMqzfpIQKilFIP62oX-HnTvP86iBOH7x0G0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/kP3ZqZkFdvhuOHJwWynHnqdjDCFXGtkTvfI_FTNTSdxhYAMUjsjTLlxiVcaP7Jjr3K29TRWr6ElhGNLx__8aZG9Zp_e78x9D6CcIB60q3rnKP_01Cd5mqtWek1UjcbQfJgrmkiQgCB4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/kP3ZqZkFdvhuOHJwWynHnqdjDCFXGtkTvfI_FTNTSdxhYAMUjsjTLlxiVcaP7Jjr3K29TRWr6ElhGNLx__8aZG9Zp_e78x9D6CcIB60q3rnKP_01Cd5mqtWek1UjcbQfJgrmkiQgCB4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/7FjuKof4kP6ASmaGai7x7GC7a-B6hYFLbWIIVrV-EeuFRW-tGIqSSxEqs2aFBz4doozFFaD3WpcDhWCHXrhMKJ7572FnWq2gq99mUjvV_zdUelCDy83A0tILLjd5es62FzP-KQdoKyk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/7FjuKof4kP6ASmaGai7x7GC7a-B6hYFLbWIIVrV-EeuFRW-tGIqSSxEqs2aFBz4doozFFaD3WpcDhWCHXrhMKJ7572FnWq2gq99mUjvV_zdUelCDy83A0tILLjd5es62FzP-KQdoKyk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/2vlGjROSp-n58i_GFYwhIHqMB6aGRY7E23N1ZkdbdFv0xQzNA3r6PI9-Z3EJs7fOz8v96p2c_KOeIY8ZGPIOJEc9OvKCjKE273pk3lCuGjsdgueAlILZSC9kQhDwe45za7Nwvmk4yPs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/2vlGjROSp-n58i_GFYwhIHqMB6aGRY7E23N1ZkdbdFv0xQzNA3r6PI9-Z3EJs7fOz8v96p2c_KOeIY8ZGPIOJEc9OvKCjKE273pk3lCuGjsdgueAlILZSC9kQhDwe45za7Nwvmk4yPs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/JJ5hEWppDTSEdomgEiX2-G8JCMcq6HJO-aZmzG1j3jdWOzB2Uv_GZHonWJ7pd5CUxxcw1k27FNPwVQ5ij96o8bqqEjphCp-WWPzyAtomy2a8pV1L65aUB4z9CF-YhuBPpNoHahHk1Pk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/JJ5hEWppDTSEdomgEiX2-G8JCMcq6HJO-aZmzG1j3jdWOzB2Uv_GZHonWJ7pd5CUxxcw1k27FNPwVQ5ij96o8bqqEjphCp-WWPzyAtomy2a8pV1L65aUB4z9CF-YhuBPpNoHahHk1Pk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/QbSi98IZojmg47q7Lt2r4rkvzYhPkF093bDABxD5zb6hkAuJaaJNN4cP8EVnG0TqyUj0BZQZ1dRkbDoW3j1T1A5nv2aaO12IGcMsq-Ik4I6-O1TsLeNb2fdi2h7xWtY2mrkmB3ch2dQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/QbSi98IZojmg47q7Lt2r4rkvzYhPkF093bDABxD5zb6hkAuJaaJNN4cP8EVnG0TqyUj0BZQZ1dRkbDoW3j1T1A5nv2aaO12IGcMsq-Ik4I6-O1TsLeNb2fdi2h7xWtY2mrkmB3ch2dQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/y3aFnucm2SYYZvrcm_sPNhrSjLgWlzwdgZCZFrpPnb2bHDw-rMOi6JgPxjrjh-87UkQYjJcvHVft-L_9xZybq_KTqGSi4cAusobRtA8XATWi3K_OZSlBSVEmQ4sMN_eSnOg7sW2YNBk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/y3aFnucm2SYYZvrcm_sPNhrSjLgWlzwdgZCZFrpPnb2bHDw-rMOi6JgPxjrjh-87UkQYjJcvHVft-L_9xZybq_KTqGSi4cAusobRtA8XATWi3K_OZSlBSVEmQ4sMN_eSnOg7sW2YNBk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/av4--1_OvZGgAbcSiKY6U7fxhmu2MkDVAfBOcFvm7y0zawSWtn0HkjL-fxLZHJ2Tfa6OPQc3xDuoXZGHWOgAfI0XS9AHih9JBBxVM-9_BrfOqo5dgAdBa1CNtWhBZeLKOcyUUyAO58I=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/av4--1_OvZGgAbcSiKY6U7fxhmu2MkDVAfBOcFvm7y0zawSWtn0HkjL-fxLZHJ2Tfa6OPQc3xDuoXZGHWOgAfI0XS9AHih9JBBxVM-9_BrfOqo5dgAdBa1CNtWhBZeLKOcyUUyAO58I=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/frVO1i6QGJu1_G9dSCJY9ob3UaCKtGozuWtNmiN0LglaqqoHHtk-tam5wuSwiKCPm6j-doqY4pPaGfxaXulZYUYI-W2zwidT3LDAnXXdag0O826WvasqkHdqmxRFxZjA4W-HgtUM5Ts=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/frVO1i6QGJu1_G9dSCJY9ob3UaCKtGozuWtNmiN0LglaqqoHHtk-tam5wuSwiKCPm6j-doqY4pPaGfxaXulZYUYI-W2zwidT3LDAnXXdag0O826WvasqkHdqmxRFxZjA4W-HgtUM5Ts=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Dgr45Cv5iPw4F4hUTpeemA5wqlmLQNLnjcPOl1HyHdnF0f-vcgVvMW02CPwtWhsy9oQA_ipPmo7mvRGWnoC3E874uZgxMZdWm0gL7DRyezoSH0r0UuInmRW45Ala5fZfPlS6JA5m1Fw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Dgr45Cv5iPw4F4hUTpeemA5wqlmLQNLnjcPOl1HyHdnF0f-vcgVvMW02CPwtWhsy9oQA_ipPmo7mvRGWnoC3E874uZgxMZdWm0gL7DRyezoSH0r0UuInmRW45Ala5fZfPlS6JA5m1Fw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/r4CnIXcgjDTdjogXXCK90TdzaJJ6gYqOsu3w7f1lCRct5ehzp6I3kH74qNjUUPJkrdBtP4NquqOvZoG7ikrFidkGIAYOwYvj7kCh5tbQE7PDLKt0Yhqx33gGsDIq1gedMr1vbP7Pduw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/r4CnIXcgjDTdjogXXCK90TdzaJJ6gYqOsu3w7f1lCRct5ehzp6I3kH74qNjUUPJkrdBtP4NquqOvZoG7ikrFidkGIAYOwYvj7kCh5tbQE7PDLKt0Yhqx33gGsDIq1gedMr1vbP7Pduw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Date A Live Season 1',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.39',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/KWG-9mXFbgWT0gleOdGlUv3vD3uMsMMZea2dqxUu9OR3iKLeEN4kO0x2N3kRu9xqGdA_AWG9qob95_no7RFUGP6e6Lcdbqlm9KeEqviyUdQMcjLXKIuiSTDNlQC-S_FN4z9IwdpBvuA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/KWG-9mXFbgWT0gleOdGlUv3vD3uMsMMZea2dqxUu9OR3iKLeEN4kO0x2N3kRu9xqGdA_AWG9qob95_no7RFUGP6e6Lcdbqlm9KeEqviyUdQMcjLXKIuiSTDNlQC-S_FN4z9IwdpBvuA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
