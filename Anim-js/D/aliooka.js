587aa949721138be8a4653b5a05a2366f334f2e0
dunia-anime <adityha.on@gmail.com>
2019-12-01 19:13:45 +0800

    Create aliooka.js

---
+++ Anim-js/D/aliooka.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/G0lgJXtUPDOOfoweLQUpqZxkQAyneki3QdRMI4dHasXK7P4nd4Pi64c7wfe05OaziG4lb-6ue0mO8sB5UBfMA_lVfAO-VXfJly_kzJ9wZpZmdbQBt4iqLGEwCbthU0_vKaRAbneV4Tk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/G0lgJXtUPDOOfoweLQUpqZxkQAyneki3QdRMI4dHasXK7P4nd4Pi64c7wfe05OaziG4lb-6ue0mO8sB5UBfMA_lVfAO-VXfJly_kzJ9wZpZmdbQBt4iqLGEwCbthU0_vKaRAbneV4Tk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_rJAgyZ2hF0eSzySuV0Bm2K88yX5hJ53y5hD5N5oVhOAcGkCDxwvBS55zNV4QGMWj0brjBLYNcZlP-26P8Vdpkq3tBkpiPTkcjjFtFCy9JOhcfIujmIxibZ8rKWeedZft_AlnxTMK08=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_rJAgyZ2hF0eSzySuV0Bm2K88yX5hJ53y5hD5N5oVhOAcGkCDxwvBS55zNV4QGMWj0brjBLYNcZlP-26P8Vdpkq3tBkpiPTkcjjFtFCy9JOhcfIujmIxibZ8rKWeedZft_AlnxTMK08=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/LSCIqK-04CLsFkkbqJUSWMIJRNFOP7D_MePyCzpiDPBCHKNNy1jTxgsB_yUadQ3Ee_Jk-d3YKAF2pSbxlXrEDfHePq1RmBezv0BDDonN_4l9C6RNeiwClKDVr4gtihz_sgaRP3tB-O0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/LSCIqK-04CLsFkkbqJUSWMIJRNFOP7D_MePyCzpiDPBCHKNNy1jTxgsB_yUadQ3Ee_Jk-d3YKAF2pSbxlXrEDfHePq1RmBezv0BDDonN_4l9C6RNeiwClKDVr4gtihz_sgaRP3tB-O0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/qJQYpZ3-9vqD_orG00-ww2KeL3dhIrJidz-hGCnVRIIrdqPJrmuuvYHp3HQQBtIxb_kOLe67ENSnjZCY499OJv1VhgG2WHJAv6WLmKe0emNRHriErm2nlKuYXnTQLAdXoSr9YMZyMUY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/qJQYpZ3-9vqD_orG00-ww2KeL3dhIrJidz-hGCnVRIIrdqPJrmuuvYHp3HQQBtIxb_kOLe67ENSnjZCY499OJv1VhgG2WHJAv6WLmKe0emNRHriErm2nlKuYXnTQLAdXoSr9YMZyMUY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/W1h0-_eEfLh3AaAm9gwIXt-Yapxhfk0hoP0RwyuLvNMkdJ9TYf_qDHQmw-ua9M9Aerask17AjcmkVfVGV4rw4RWM2-K8VYAb83fNShqvVgDLP3EgUDidKIl0m8YBlg2Mm2Zsl4JO-dA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/W1h0-_eEfLh3AaAm9gwIXt-Yapxhfk0hoP0RwyuLvNMkdJ9TYf_qDHQmw-ua9M9Aerask17AjcmkVfVGV4rw4RWM2-K8VYAb83fNShqvVgDLP3EgUDidKIl0m8YBlg2Mm2Zsl4JO-dA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/XIFSMsNBHz2EfYMHMG6AhiGrZb5C1X49RH3G9FPDtGLMulcZPEfFxahDdCIqlOUJ06gSlKba4O3-gZnYVwvLGKHQNueqtgJicF26n7TdmMQ1xQkfUP0vF8slEn8NM8kAP6BWyusycwk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/XIFSMsNBHz2EfYMHMG6AhiGrZb5C1X49RH3G9FPDtGLMulcZPEfFxahDdCIqlOUJ06gSlKba4O3-gZnYVwvLGKHQNueqtgJicF26n7TdmMQ1xQkfUP0vF8slEn8NM8kAP6BWyusycwk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Y026BgNGfOTv5_DiMMD3xsS2_yoCJL5QQLFXZKa4prm9gAdRJsWtgFHFdNznmkaXUJS9DXivRSl_069HidBOtAOuYn8loMJmt1C8CdDIbbHe_cmkrp0yYHaxZON9Aui01pyOeUisxl0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Y026BgNGfOTv5_DiMMD3xsS2_yoCJL5QQLFXZKa4prm9gAdRJsWtgFHFdNznmkaXUJS9DXivRSl_069HidBOtAOuYn8loMJmt1C8CdDIbbHe_cmkrp0yYHaxZON9Aui01pyOeUisxl0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9hNgrDzbP63Qyri7atDHpSJcH_RhsDS4ZQOcvNfUAgweJlDKgp88tji_HUXE47vpYdL_Ys-4sY5GUdWXajnvLJDz6umr6aeEPJZF9MvRZw7nIDNhHhfRVnWeM7fCyMA17m7DTBAn9rg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9hNgrDzbP63Qyri7atDHpSJcH_RhsDS4ZQOcvNfUAgweJlDKgp88tji_HUXE47vpYdL_Ys-4sY5GUdWXajnvLJDz6umr6aeEPJZF9MvRZw7nIDNhHhfRVnWeM7fCyMA17m7DTBAn9rg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/deODzPg5ke7MLAtidIUYcjtuTEzyu_cxuZqk0Oa6DgTKbn0RMwCbU8mm17gG6RPUuXSSry5gxHM5mAvTDcqCjjCxnE6lEvrnnqJp7a9Vb3WI0fTqUHfYmSKKKVpAHskqt81yqFouXoQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/deODzPg5ke7MLAtidIUYcjtuTEzyu_cxuZqk0Oa6DgTKbn0RMwCbU8mm17gG6RPUuXSSry5gxHM5mAvTDcqCjjCxnE6lEvrnnqJp7a9Vb3WI0fTqUHfYmSKKKVpAHskqt81yqFouXoQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/IYco1y7dZBjPQWSGSiIrhooskUB2uJsAhq5kodYcB_QqvJYIWrMV9mkh63sVSJXCjZLCgcOGX7W7k0lfeQNnb2RPqUGr2bhp9qaU46plRVuIhdLS6y8Wyl5auDDRUzi8wqf47lLmxGU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/IYco1y7dZBjPQWSGSiIrhooskUB2uJsAhq5kodYcB_QqvJYIWrMV9mkh63sVSJXCjZLCgcOGX7W7k0lfeQNnb2RPqUGr2bhp9qaU46plRVuIhdLS6y8Wyl5auDDRUzi8wqf47lLmxGU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Lk2EPGoWaH1co7MSUzIuheAB2rv_gKo2qhq63mwZvnVwj-Z0Ch9sW9eNC2eZAg1YzZ4uqij2sFFz21MgOkG_qCMLx5m5uCUadllvKEecqdlermrmc06j7WZwhrSbLD0onawnYx5mLOg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Lk2EPGoWaH1co7MSUzIuheAB2rv_gKo2qhq63mwZvnVwj-Z0Ch9sW9eNC2eZAg1YzZ4uqij2sFFz21MgOkG_qCMLx5m5uCUadllvKEecqdlermrmc06j7WZwhrSbLD0onawnYx5mLOg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Lwxe7dy3vW4KlbyhFnb1w4X8SGXPY0Nw2WxOjy4sIkvAHzqqDE_XpcqAclkmIcLAMru-6Q1hPAVSIHw8oWiukaSM071TB9oEjSADvgnPcvd-dL_GumZw5jxjGuI0EKryHcbVf3_UjUQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Lwxe7dy3vW4KlbyhFnb1w4X8SGXPY0Nw2WxOjy4sIkvAHzqqDE_XpcqAclkmIcLAMru-6Q1hPAVSIHw8oWiukaSM071TB9oEjSADvgnPcvd-dL_GumZw5jxjGuI0EKryHcbVf3_UjUQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dantalian no Shoka',
+        episode: '* Episode OVA',
+        name: 'Episode OVA',
+        deskrip1: '* Rating : 7.30',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/CEZWm1a3mKxfyt571BsvgmSR5pislAVTwnz5_FQO-XNa6999B0kKaxFkYaMdTedKcZmeUe6fkAZeevE8T6NmT-kOztBkUzARJP16Iv1FeR-YVEx1iNVFHx1BQOkqBARzfx74r4jPdOA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/CEZWm1a3mKxfyt571BsvgmSR5pislAVTwnz5_FQO-XNa6999B0kKaxFkYaMdTedKcZmeUe6fkAZeevE8T6NmT-kOztBkUzARJP16Iv1FeR-YVEx1iNVFHx1BQOkqBARzfx74r4jPdOA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
