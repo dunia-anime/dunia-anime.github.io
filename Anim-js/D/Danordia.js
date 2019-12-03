62baa497c610467872b45b0249e2f310117e7156
dunia-anime <adityha.on@gmail.com>
2019-12-03 11:25:12 +0800

    Create Danordia.js

---
+++ Anim-js/D/Danordia.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/tqJqBsr35WjqN8Wtg_GM2VkvG1wi7nUvJ-wo9Ulf3FUC-sqSs-mqFf45Sm5hCpcpkqUXCjXPLmHtnQ27RxqEkU9SZGKWq4Fke6eXYOj2PxhZXENkoUR3dHCtlbqrJBGuuR8vX0u30aE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/tqJqBsr35WjqN8Wtg_GM2VkvG1wi7nUvJ-wo9Ulf3FUC-sqSs-mqFf45Sm5hCpcpkqUXCjXPLmHtnQ27RxqEkU9SZGKWq4Fke6eXYOj2PxhZXENkoUR3dHCtlbqrJBGuuR8vX0u30aE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/bVbLMxFfp9HMTmLkXqQSszX3CgX24b1W-eoOOT3hdyyPzsqhfFCNVkew6lf2Xbs25LGwcaQ2bwNx9MiY8rFeiQYkOHEHRXIYGMNLu2YH3J7jvMePXOTp28syMq09jIbibkf-D8nqgo8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/bVbLMxFfp9HMTmLkXqQSszX3CgX24b1W-eoOOT3hdyyPzsqhfFCNVkew6lf2Xbs25LGwcaQ2bwNx9MiY8rFeiQYkOHEHRXIYGMNLu2YH3J7jvMePXOTp28syMq09jIbibkf-D8nqgo8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/kObQY1Yj3b2_SoiqMRSQ7QtEgoAyoU2MP-c3oLRkwJ5wuF4BU_YUE8dKJIsDb2457s0vDRFioBOx_4saTd6LoHP3SiIbnOfBnyEuwa9wdWjtvV74uJm_nVobWBeO2dVWRnJMlhzOLK0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/kObQY1Yj3b2_SoiqMRSQ7QtEgoAyoU2MP-c3oLRkwJ5wuF4BU_YUE8dKJIsDb2457s0vDRFioBOx_4saTd6LoHP3SiIbnOfBnyEuwa9wdWjtvV74uJm_nVobWBeO2dVWRnJMlhzOLK0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/c4REuztMaHaPR2rcJy7vcgbgdwvdCd5jQ9WPHvMpHRY7nhgvfv_VGDKa4lbolqrnFu8CoykfCwnAEmYA8lhQGnq87HxLWmZc9Vdpv0pEEMedfAcxEH0EKfnCPadqmf5sYXu3gR8UHaY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/c4REuztMaHaPR2rcJy7vcgbgdwvdCd5jQ9WPHvMpHRY7nhgvfv_VGDKa4lbolqrnFu8CoykfCwnAEmYA8lhQGnq87HxLWmZc9Vdpv0pEEMedfAcxEH0EKfnCPadqmf5sYXu3gR8UHaY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/mjLORGlboxQYKraYcR3ufwgf45PcFw00_HvkT-kX_3I7ESFhIp4ZZu4rxZkmC6vNUwYF6wwbU8UXnW21di49EuYS1TKuQJb2HLJ5ktZ364uD50yfWZczz_1-UKtKxzU8agjtL7RkagA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/mjLORGlboxQYKraYcR3ufwgf45PcFw00_HvkT-kX_3I7ESFhIp4ZZu4rxZkmC6vNUwYF6wwbU8UXnW21di49EuYS1TKuQJb2HLJ5ktZ364uD50yfWZczz_1-UKtKxzU8agjtL7RkagA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3mIxigXJB6RYCqluOu7RRanbLPo0WwccttJP6uH9H6BOM5mXd2ZoGgckAUK83rDz4tq_4bYoa-bb74j-80JLcCCd92MtrE-V4_eK5U7XIRMNhfZ5i_E6MxKJoPTFLlrhMBS1CrOE_gA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3mIxigXJB6RYCqluOu7RRanbLPo0WwccttJP6uH9H6BOM5mXd2ZoGgckAUK83rDz4tq_4bYoa-bb74j-80JLcCCd92MtrE-V4_eK5U7XIRMNhfZ5i_E6MxKJoPTFLlrhMBS1CrOE_gA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/kPVHinNXqSbUChnMiqP2fBsxzzN-F4w2U_EBoXpTmVIgoPMK6LIaroPDdk6EVQKJS-ZlPq4Mldf0qXPbFdLu8ZZlPMg-MZtzfij0ABKTh9r873jTn8tbTZ-MSZvb1z-Fiz6rW5RuaX8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/kPVHinNXqSbUChnMiqP2fBsxzzN-F4w2U_EBoXpTmVIgoPMK6LIaroPDdk6EVQKJS-ZlPq4Mldf0qXPbFdLu8ZZlPMg-MZtzfij0ABKTh9r873jTn8tbTZ-MSZvb1z-Fiz6rW5RuaX8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZvlrZTAOtpHLDINtZXqFSBNJGb4-VJ9BLvACIdDgwCGU99MRTxZuaHJdhAIHx95tlLgr6xtaBvtwxD6JFX5N83QSpUdK-K4C5W6l8bLMQnKgSx88z-jL-Jycr1EdkF5JzM0u_aB-sKI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZvlrZTAOtpHLDINtZXqFSBNJGb4-VJ9BLvACIdDgwCGU99MRTxZuaHJdhAIHx95tlLgr6xtaBvtwxD6JFX5N83QSpUdK-K4C5W6l8bLMQnKgSx88z-jL-Jycr1EdkF5JzM0u_aB-sKI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/pCYbLQG1Y8HWnxOv-aW0idggyXe14U8nD9oVXlt19JqyPvqGpGhm3_HGTN00GXw7IpVeK2PKDOKkYLIgCcjkqQKDDX-VL3Y167apNLuxRXSOyChyigrN-j63kjRMAsvPqdva0lHfwmo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/pCYbLQG1Y8HWnxOv-aW0idggyXe14U8nD9oVXlt19JqyPvqGpGhm3_HGTN00GXw7IpVeK2PKDOKkYLIgCcjkqQKDDX-VL3Y167apNLuxRXSOyChyigrN-j63kjRMAsvPqdva0lHfwmo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/h2IuLLRidwzFN_DwsnsrshanCts-st88zKJ8xfbp2elYfZozh4fXSmDt2wjIebV-oMgMTqy4Y3A-TyCWOlyCkeJuqKQwN7U7aPpBi3fwlrKpvSrRwuWX5CnUQ9wJw_c5jcufCg_2bMc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/h2IuLLRidwzFN_DwsnsrshanCts-st88zKJ8xfbp2elYfZozh4fXSmDt2wjIebV-oMgMTqy4Y3A-TyCWOlyCkeJuqKQwN7U7aPpBi3fwlrKpvSrRwuWX5CnUQ9wJw_c5jcufCg_2bMc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/7KyGVGj7P7qybtwrJRHhZ32Xazvex_6DBX1oH8ZvG-8EeUix8poVVCGc-KeLtfdVdhEPrbF4W4LXUoF-3LT56FZTM5WmoEXVH_T2HA6SW9fn0ifyfzvDvr0lZF4-bTOlzdm5pQjgRyQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/7KyGVGj7P7qybtwrJRHhZ32Xazvex_6DBX1oH8ZvG-8EeUix8poVVCGc-KeLtfdVdhEPrbF4W4LXUoF-3LT56FZTM5WmoEXVH_T2HA6SW9fn0ifyfzvDvr0lZF4-bTOlzdm5pQjgRyQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Danmachi: Sword Oratoria',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AUAumMMRzMiEBmcCQa8SBoXMNWtb4Ov0y_E0yCW4j0tQn6N0S40-hlf00LZ6hQ1ZRFQQXUheRaLl5yC9JKIiczLMjo-JS3iPJgMt-BuBqzO7oBrWlWGti5MUefU73aMya1oMAj8Scnw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AUAumMMRzMiEBmcCQa8SBoXMNWtb4Ov0y_E0yCW4j0tQn6N0S40-hlf00LZ6hQ1ZRFQQXUheRaLl5yC9JKIiczLMjo-JS3iPJgMt-BuBqzO7oBrWlWGti5MUefU73aMya1oMAj8Scnw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
