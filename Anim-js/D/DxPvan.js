2fbb6c0b5f548a5875515424846a89d51c1e61b2
dunia-anime <adityha.on@gmail.com>
2019-12-03 12:11:52 +0800

    Create DxPvan.js

---
+++ Anim-js/D/DxPvan.js
@@ -0,0 +1,415 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/TiGqLfSSNqbNFyU1EIPBRiaogc96yleh1yv8IvyvzwT-sCBEgmGVcgwHfponRsgaCYeN-lYQOVY6M4VY7a2iux1-fGSph6kvLWlGQ6OPecqQ5Y0goO9SL7zsC9LslmWF9vDdRLpIS4w=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/TiGqLfSSNqbNFyU1EIPBRiaogc96yleh1yv8IvyvzwT-sCBEgmGVcgwHfponRsgaCYeN-lYQOVY6M4VY7a2iux1-fGSph6kvLWlGQ6OPecqQ5Y0goO9SL7zsC9LslmWF9vDdRLpIS4w=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 02.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6jvCKQY7BKggJQXLcvY1DESVoasYTyGUXZlIuGxu-y2XgUCy_mIt4lui431v5ZF430xk6IGsBxPUr3LVrpHabT4ptViS1m-n9kTZGZMQKlg0WGvXGWSMTKaA0bs16LKjHUrtdXw8XUA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6jvCKQY7BKggJQXLcvY1DESVoasYTyGUXZlIuGxu-y2XgUCy_mIt4lui431v5ZF430xk6IGsBxPUr3LVrpHabT4ptViS1m-n9kTZGZMQKlg0WGvXGWSMTKaA0bs16LKjHUrtdXw8XUA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 03.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/qq9WWOH_74R_MgjPHLWEl4ebjZZ3T64krnEGs343ng6X62apamb8yIIf9mPlmtAAYMudqe4equ-mfc0R8QjaugmkwDSEMDiyeGktbT-l83FH0SAQKREcVmxDJ1mEfkd3-0sSdyCNVrc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/qq9WWOH_74R_MgjPHLWEl4ebjZZ3T64krnEGs343ng6X62apamb8yIIf9mPlmtAAYMudqe4equ-mfc0R8QjaugmkwDSEMDiyeGktbT-l83FH0SAQKREcVmxDJ1mEfkd3-0sSdyCNVrc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 04.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Q6DN4QRF0SDWBCxPSs2r4ol388DRtFwbN3AJ969D_6psm7oKkUqQBflXy50jy7LhRSDOBeq91BlGdwmqBfr_7u6mfkrbU21KZKsX8izSgXCu-ygbYrfdZnA6aIL-QkXh1k88zCO7TT8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Q6DN4QRF0SDWBCxPSs2r4ol388DRtFwbN3AJ969D_6psm7oKkUqQBflXy50jy7LhRSDOBeq91BlGdwmqBfr_7u6mfkrbU21KZKsX8izSgXCu-ygbYrfdZnA6aIL-QkXh1k88zCO7TT8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 05.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/A_JzUPafhavpEe1GIq9zXbNl_tIeGXvWqwne9g_OaMZfkM8f_7gGtoYeDBjVFeGhaDrh5LppZBZDvyxu2wzxsvdEYZWNmj5NZzKoeou7q9XBhm9zsaSGaeBBfYMrZv_qNtK-j9jbvhA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/A_JzUPafhavpEe1GIq9zXbNl_tIeGXvWqwne9g_OaMZfkM8f_7gGtoYeDBjVFeGhaDrh5LppZBZDvyxu2wzxsvdEYZWNmj5NZzKoeou7q9XBhm9zsaSGaeBBfYMrZv_qNtK-j9jbvhA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 06.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/N9LhxTZ4IiS1uev-qwmuYZr3xgYjKCFKwEVQm1k5czWRwThWOixwHH9dcg1EvAnENlGVahfXq02b-3RU7Bfg7KqimX-C717iYdX4KiDipRymWJZGCgeCbT60nI6tF08SSHR__wc6ucU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/N9LhxTZ4IiS1uev-qwmuYZr3xgYjKCFKwEVQm1k5czWRwThWOixwHH9dcg1EvAnENlGVahfXq02b-3RU7Bfg7KqimX-C717iYdX4KiDipRymWJZGCgeCbT60nI6tF08SSHR__wc6ucU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 07.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/vPKYUTXlvcNkQDvo428lhK5cFlLZ0s_9QUgNXopSNGmRJBW57EX4P3rmn5K74sAXmV2cPF0Eax-bEw-ctsN0HM-pVu9pGAm64WEoKXjJbvNdw8DYDA78Dw9rwfhj6dtk5J8Ku1a56v8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/vPKYUTXlvcNkQDvo428lhK5cFlLZ0s_9QUgNXopSNGmRJBW57EX4P3rmn5K74sAXmV2cPF0Eax-bEw-ctsN0HM-pVu9pGAm64WEoKXjJbvNdw8DYDA78Dw9rwfhj6dtk5J8Ku1a56v8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 08.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6D0T4l_bujrM8_7VoSmh_7XeIOsW1ioOopggVOdhgHWQ655jJ7Xcz26csTn0uqHUZLscDcK_slP2AQ1CvcPWahGaLGVR6xYswOSXHWlsROiUwN0sqqsdRrfcTXcONW7DCKO5DpBKJME=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6D0T4l_bujrM8_7VoSmh_7XeIOsW1ioOopggVOdhgHWQ655jJ7Xcz26csTn0uqHUZLscDcK_slP2AQ1CvcPWahGaLGVR6xYswOSXHWlsROiUwN0sqqsdRrfcTXcONW7DCKO5DpBKJME=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 09.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/c9LmBCg8lIdh_vpvmephjwpjuuf0S6a8f1oga_Q72PTkd92fWA7yLAxgfPaHj7X7lVuWy9MYS-WjmZNfMev_kD9idVHS4Hz8jj0rgGOYZogM61BiqQV9uyfj9hAbTAPO_0XNgtDcL60=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/c9LmBCg8lIdh_vpvmephjwpjuuf0S6a8f1oga_Q72PTkd92fWA7yLAxgfPaHj7X7lVuWy9MYS-WjmZNfMev_kD9idVHS4Hz8jj0rgGOYZogM61BiqQV9uyfj9hAbTAPO_0XNgtDcL60=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: '%5Bdunia-annime%5D Dame x Prince Anime Caravan - 10.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WH8ErbxD6-pp8EkI1NFCMkUwZ5bRK6r_9y89D0bUUvgheymB1hD-5zlqHfpF_mXB6OiSWY4VLx_qBi8r8iS-rH8wzpcW7yRJvJDjxJrBKIXKEA6iO23b1YYiteD72reXiRj5rsZhoxM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WH8ErbxD6-pp8EkI1NFCMkUwZ5bRK6r_9y89D0bUUvgheymB1hD-5zlqHfpF_mXB6OiSWY4VLx_qBi8r8iS-rH8wzpcW7yRJvJDjxJrBKIXKEA6iO23b1YYiteD72reXiRj5rsZhoxM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_zhD5YNTBdhdAnqV41BJSDt_L2rFYrqTeOCpm1XwOOEPP108-kFuzw_Nk0mVDZHAh8i5anWpbdn_wzNlQZE5sHnkbWCrDk2Qj0523Ea4CvtmAkyLyOZ9hc6apDFXE9c9l1X5k62rAd0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_zhD5YNTBdhdAnqV41BJSDt_L2rFYrqTeOCpm1XwOOEPP108-kFuzw_Nk0mVDZHAh8i5anWpbdn_wzNlQZE5sHnkbWCrDk2Qj0523Ea4CvtmAkyLyOZ9hc6apDFXE9c9l1X5k62rAd0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dame x Prince Anime Caravan',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.63',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/fYPV9iF8X43wRKjHkRrVFLNyoojHqpylfawwX_jp3PmTmiam3QHtv5ruiRPN1QfpazTuwBQp4IMm4SNfx9H-CIRMTDmfdUcrQDFbjlEp-F2VrLOH1Bsfk8PbuRebZG1hSVh7InA6U2A=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/fYPV9iF8X43wRKjHkRrVFLNyoojHqpylfawwX_jp3PmTmiam3QHtv5ruiRPN1QfpazTuwBQp4IMm4SNfx9H-CIRMTDmfdUcrQDFbjlEp-F2VrLOH1Bsfk8PbuRebZG1hSVh7InA6U2A=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
