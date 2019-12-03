e618c6005bedb265fb69beefecb1b0cdb7a4984f
dunia-anime <adityha.on@gmail.com>
2019-12-03 11:29:46 +0800

    Create Dahi.js

---
+++ Anim-js/D/Dahi.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* DanMachi',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/NqWg0rCKfMlelv8gOFcEPzMRwNyPbfqHTdF6inYboDHQxycAi-PZkG6W3gvYJDJtFevAFqn-jXO2Yu80TdE1cbVLYZeiBP99zFG_87Q85ceJTzSGCKa06PZ4s9m9SIYuFhsK6epSlTg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/NqWg0rCKfMlelv8gOFcEPzMRwNyPbfqHTdF6inYboDHQxycAi-PZkG6W3gvYJDJtFevAFqn-jXO2Yu80TdE1cbVLYZeiBP99zFG_87Q85ceJTzSGCKa06PZ4s9m9SIYuFhsK6epSlTg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/S_rOjz8N5ViSUKU9T-90PMw4tM_IWoyhUdlhCOe-srmDZROhRTtOyg4AawLrUZZEuefddMHSrnBnhwMhDPoe5qofLotoa8C-YwDNVVhY6HNAoyNTc-vkg6xtGWkhYfaO_qbbPCiwjn0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/S_rOjz8N5ViSUKU9T-90PMw4tM_IWoyhUdlhCOe-srmDZROhRTtOyg4AawLrUZZEuefddMHSrnBnhwMhDPoe5qofLotoa8C-YwDNVVhY6HNAoyNTc-vkg6xtGWkhYfaO_qbbPCiwjn0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/R4xwmIzqDO6b65VTzjZM5R4ioqENXFFj8CSL5u3LH04oLz9NuVCj7QD9eNAx5MgC3MScdccoV_aMNsA-_D_87C54-mzglPT6Ic-O70bWnHIjikhTcXZY-EyJWgvl3xsLm8Ttsmym86k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/R4xwmIzqDO6b65VTzjZM5R4ioqENXFFj8CSL5u3LH04oLz9NuVCj7QD9eNAx5MgC3MScdccoV_aMNsA-_D_87C54-mzglPT6Ic-O70bWnHIjikhTcXZY-EyJWgvl3xsLm8Ttsmym86k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Nz45VgcQR3XA0WCVegaJPNlZrKjv2uv-3hZi73ENoUWzvKLzEbezZQoVqoks31LrLNbOuvYnzC8lOsI3JsiPENIWb6-MNUlsZJqaHeqytxcjnavi_lsjup4ovBkZUt7q8p3_ukfsc0Y=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Nz45VgcQR3XA0WCVegaJPNlZrKjv2uv-3hZi73ENoUWzvKLzEbezZQoVqoks31LrLNbOuvYnzC8lOsI3JsiPENIWb6-MNUlsZJqaHeqytxcjnavi_lsjup4ovBkZUt7q8p3_ukfsc0Y=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SxOu66Nhd4ZTipMGSMC8X0y0faylFukOM1QbLBfcpR2LXLHYjYzfova3bFM_JmtRqQVZtGlSkhcqMKDUogdHuN9rMQWbOOVgVwVn7erWznJIPCAZNBUUkmdUuvJwYUir4a7JYiKwhHs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SxOu66Nhd4ZTipMGSMC8X0y0faylFukOM1QbLBfcpR2LXLHYjYzfova3bFM_JmtRqQVZtGlSkhcqMKDUogdHuN9rMQWbOOVgVwVn7erWznJIPCAZNBUUkmdUuvJwYUir4a7JYiKwhHs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jir6fMW7dmoFPONaLnHznsIYSxqJD2zfBBsBqRPTrwgb0pEk5jVCtQdYt0QKsXic-fwCkZT8MSAo3lDwi-nxj-wdPmPwh-rlruAKayl31jvvdZjiDUa93XDsFmsJ8HOQy7NjLfxc5I0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jir6fMW7dmoFPONaLnHznsIYSxqJD2zfBBsBqRPTrwgb0pEk5jVCtQdYt0QKsXic-fwCkZT8MSAo3lDwi-nxj-wdPmPwh-rlruAKayl31jvvdZjiDUa93XDsFmsJ8HOQy7NjLfxc5I0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/4r--_TEzXMeVdS7-Ot_vST_4OsmuymGbxVKY8CGPlRVnK8OPkN--o9tp2nDIhidv_N7ueOSQe0FceFnoBtEZTu0Vz8tqxuNBGIYCUfDu-4YJqcWMFqClmvcfE-mf8Ern6vknW0Dk9Kk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/4r--_TEzXMeVdS7-Ot_vST_4OsmuymGbxVKY8CGPlRVnK8OPkN--o9tp2nDIhidv_N7ueOSQe0FceFnoBtEZTu0Vz8tqxuNBGIYCUfDu-4YJqcWMFqClmvcfE-mf8Ern6vknW0Dk9Kk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/0LU31ic4hrDpx-fD20Q3xN7GG7EKX_8pwMSVSuRntJ49Zw6Sws-fT9JGP7bbbFES9TZo0koxm_zkS1h-w7flsRswZDLAYSgOjxSyQDMYYREbSTsu6cfLpj4JNrVlHLNNd_5U8qPvb64=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/0LU31ic4hrDpx-fD20Q3xN7GG7EKX_8pwMSVSuRntJ49Zw6Sws-fT9JGP7bbbFES9TZo0koxm_zkS1h-w7flsRswZDLAYSgOjxSyQDMYYREbSTsu6cfLpj4JNrVlHLNNd_5U8qPvb64=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Sv9YxRTG6N-Jn6lgnJG2qU6oywvEqxtc7h9HJGGx-gVZE3uE1CWritb-wKgShiijsogRSmAOqAirPQzn4qwVvHPSDwydtQPr4X-iPzB80K5ZZ5lARvRs7X1cvB5DGzo3CSR6iSYTSUA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Sv9YxRTG6N-Jn6lgnJG2qU6oywvEqxtc7h9HJGGx-gVZE3uE1CWritb-wKgShiijsogRSmAOqAirPQzn4qwVvHPSDwydtQPr4X-iPzB80K5ZZ5lARvRs7X1cvB5DGzo3CSR6iSYTSUA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/yEdrmNiwkKVbUoTpasLVLG8zqkbM-HSABVb1jkX4ZeBozRGMj2I6yLyV9hcDxaX2ScE7-NUP3Y832QyXY1_A8EeJnyCe9Xrq8zElPthpDBZQS1ORxMS9fUrInM2SPr_Ovckwsazpe2Y=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/yEdrmNiwkKVbUoTpasLVLG8zqkbM-HSABVb1jkX4ZeBozRGMj2I6yLyV9hcDxaX2ScE7-NUP3Y832QyXY1_A8EeJnyCe9Xrq8zElPthpDBZQS1ORxMS9fUrInM2SPr_Ovckwsazpe2Y=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/rGgs-27NTx0tIcqV7pzE4d51F9iwJrNturX19SmXHPuo1xgx3teoKqNHW4r32hLbSndsOLYlMJQ_e8YUn5s-s-8l6y32lw6v__3reMG74JhDQNvTDz--nWFEXsPqW2fmYm7uBw5pryI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/rGgs-27NTx0tIcqV7pzE4d51F9iwJrNturX19SmXHPuo1xgx3teoKqNHW4r32hLbSndsOLYlMJQ_e8YUn5s-s-8l6y32lw6v__3reMG74JhDQNvTDz--nWFEXsPqW2fmYm7uBw5pryI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_xWqd9_j1Kp4jzEGuuAFlnKl43DR_mnfIs-gqtyqWrMuDd0xBR1XYeJlwPUoEnjpipMyBIeg0XHgBlcGzMX_h6nG4EfKghBE0d1r3VgBA3FbGjZuCizktiJTTsaBM9CC3HsMHL_Rowo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_xWqd9_j1Kp4jzEGuuAFlnKl43DR_mnfIs-gqtyqWrMuDd0xBR1XYeJlwPUoEnjpipMyBIeg0XHgBlcGzMX_h6nG4EfKghBE0d1r3VgBA3FbGjZuCizktiJTTsaBM9CC3HsMHL_Rowo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* DanMachi',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.78',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/VzFxR1F5e7bwnJYES4jsZgq6-eP3PT4W5pxoFaAGR8sl32zVxZ3LMLItxRu9thkgbkVVDjR5N17nLicXid04rkxZATFgrVs3jhpG6SQl3k_CriocZF2FRUDj9LbEBlEQxC4rLkbe7Xc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/VzFxR1F5e7bwnJYES4jsZgq6-eP3PT4W5pxoFaAGR8sl32zVxZ3LMLItxRu9thkgbkVVDjR5N17nLicXid04rkxZATFgrVs3jhpG6SQl3k_CriocZF2FRUDj9LbEBlEQxC4rLkbe7Xc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
