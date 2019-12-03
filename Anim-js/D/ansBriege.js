a900e1ea046900788b591318d531d0e05072c7e2
dunia-anime <adityha.on@gmail.com>
2019-12-03 11:09:49 +0800

    Create ansBriege.js

---
+++ Anim-js/D/ansBriege.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 01',
+    name: 'Episode 01',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/Y4CFaXpC9gtpO9_T0o9FMAuB_qcERLqOzTSdshElrV7zI2qD-1-YapUJPmqxeoaHKkPGSwCbLdwVMPfBAgN8gCRh5x9dLUXnTwRlcS7WhsDnsPbosSfn96UE0MYgKp8Ms0B1Ah0ewmM=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/Y4CFaXpC9gtpO9_T0o9FMAuB_qcERLqOzTSdshElrV7zI2qD-1-YapUJPmqxeoaHKkPGSwCbLdwVMPfBAgN8gCRh5x9dLUXnTwRlcS7WhsDnsPbosSfn96UE0MYgKp8Ms0B1Ah0ewmM=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 02',
+    name: 'Episode 02',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/nkdgpVflf1C9mcIC0b0RFxTfoI8NLRDCoTWwVNDvghXKP1tIPbiCupwxp6Hr-gFsGUPu_hMAHiAveEO6xmSDBcRFquUc4xZMKv__SLAF97fdG9V7jIG9bfmH19BCx0ZgSlY-JIqNyQU=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/nkdgpVflf1C9mcIC0b0RFxTfoI8NLRDCoTWwVNDvghXKP1tIPbiCupwxp6Hr-gFsGUPu_hMAHiAveEO6xmSDBcRFquUc4xZMKv__SLAF97fdG9V7jIG9bfmH19BCx0ZgSlY-JIqNyQU=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 03',
+    name: 'Episode 03',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/7WffSBgyT_Ze_bKcjG9Yyh7spLeVcK1kwJVMAOqFUUhOlOMRN3eYSTUk_zFl8JHoagV-2F5z2eE1GIJYeEM0fyOcJtlh48PcgUZS6Dt6d77KT57efcF1IkexeJiWW560P2iCqxwB_es=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/7WffSBgyT_Ze_bKcjG9Yyh7spLeVcK1kwJVMAOqFUUhOlOMRN3eYSTUk_zFl8JHoagV-2F5z2eE1GIJYeEM0fyOcJtlh48PcgUZS6Dt6d77KT57efcF1IkexeJiWW560P2iCqxwB_es=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 04',
+    name: 'Episode 04',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/9OYwefxKkRMG6DSkRYmZ8f2qb08sUd-Qrz4MuOH06-HC5XR1ZmiLh1Pjxtpk06O2S8OVtJsNCxQoeeTSOaC-0CBe7a1E8x1HVlhe0QBHnothjVPM_-s41U1ztVOk4aN09E3ZSJTXkbA=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/9OYwefxKkRMG6DSkRYmZ8f2qb08sUd-Qrz4MuOH06-HC5XR1ZmiLh1Pjxtpk06O2S8OVtJsNCxQoeeTSOaC-0CBe7a1E8x1HVlhe0QBHnothjVPM_-s41U1ztVOk4aN09E3ZSJTXkbA=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 05',
+    name: 'Episode 05',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/FVCFNIpUtDL6kmDVtgBrMLAKA9trjw68cnpG885UM3e1i4KSqHMXEAtVueA_4lrZnhTkJj7oWrT_uogEvkxspnDL4X5ogq9Zwa3_DCYHbHBAfQ39h5AGWS-R_Eq24Vd-VnHf-kqq1tI=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/FVCFNIpUtDL6kmDVtgBrMLAKA9trjw68cnpG885UM3e1i4KSqHMXEAtVueA_4lrZnhTkJj7oWrT_uogEvkxspnDL4X5ogq9Zwa3_DCYHbHBAfQ39h5AGWS-R_Eq24Vd-VnHf-kqq1tI=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 06',
+    name: 'Episode 06',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/GZT2G6W65OdxBGX9gOnLrcAjO9pUC2-x9woJBWNHvXREpN83jXQMpZGFnwQXaJfo45cUW-lCswMy0dSfVY4DrYj5uZ5PgZ4AKSb3x0vF7ouXqBA5OVNetIiC5uYIq_7NgbzL6guX5n0=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/GZT2G6W65OdxBGX9gOnLrcAjO9pUC2-x9woJBWNHvXREpN83jXQMpZGFnwQXaJfo45cUW-lCswMy0dSfVY4DrYj5uZ5PgZ4AKSb3x0vF7ouXqBA5OVNetIiC5uYIq_7NgbzL6guX5n0=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 07',
+    name: 'Episode 07',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/5ZJqm5P2ReBsMwCNGDvATekHEttO8XZwXl3eXXKESnQgzXFBc4EN2WCUJtaPddF6q9M53aXVxL7wHrkYWiCqj6AQ_O9SlU3ffOxz_AXjfj0gLa5RxXyLDhTaN-bvJP-DlOA2heh5NS4=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/5ZJqm5P2ReBsMwCNGDvATekHEttO8XZwXl3eXXKESnQgzXFBc4EN2WCUJtaPddF6q9M53aXVxL7wHrkYWiCqj6AQ_O9SlU3ffOxz_AXjfj0gLa5RxXyLDhTaN-bvJP-DlOA2heh5NS4=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 08',
+    name: 'Episode 08',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/F13Wo9w4zr0ehD_L351C2c_MUA8Tg3usy3jW_iE2-tJzNksIV8nCgpl-wixEb9WejWATFdrS4fJFypuFTH5K3yvSkkDNm1NC3v0J5y1fq_mPF9QCsXu-5Sf0ARCgri5WjFMA8J7Wh4s=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/F13Wo9w4zr0ehD_L351C2c_MUA8Tg3usy3jW_iE2-tJzNksIV8nCgpl-wixEb9WejWATFdrS4fJFypuFTH5K3yvSkkDNm1NC3v0J5y1fq_mPF9QCsXu-5Sf0ARCgri5WjFMA8J7Wh4s=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 09',
+    name: 'Episode 09',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/qzFSBH8HWYkswIdrw2o4rH0s74k55xxXSxuHPokjbWerritzgXzeVmKV7QRj_I0yyYiZxE103f5MwbK7p2oMFfZt-QtXtt6GagxenCfkrhQTfMWXJa0W1PfWAUjm6JHgF2dN55h0cg4=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/qzFSBH8HWYkswIdrw2o4rH0s74k55xxXSxuHPokjbWerritzgXzeVmKV7QRj_I0yyYiZxE103f5MwbK7p2oMFfZt-QtXtt6GagxenCfkrhQTfMWXJa0W1PfWAUjm6JHgF2dN55h0cg4=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 10',
+    name: 'Episode 10',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/PzY-1IJk-Uf0PFeKwMw4aIjM_YU0GfE9LAkSte_hLxulcQCKtSjwGW3tABzPF3zTFsCad_vipTmACCsRo6Ex1GygDLBvBLsOMYp6WALgvs3ACp48AUm2iilUm2cSsJeCzrr520b_6Jc=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/PzY-1IJk-Uf0PFeKwMw4aIjM_YU0GfE9LAkSte_hLxulcQCKtSjwGW3tABzPF3zTFsCad_vipTmACCsRo6Ex1GygDLBvBLsOMYp6WALgvs3ACp48AUm2iilUm2cSsJeCzrr520b_6Jc=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 11',
+    name: 'Episode 11',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/nUp-bS4xuDx-fWYxchkIwNjRRs_acHXJOISMO9sfOcu3MdW3b3ISr_PztYkBygel-p5MpLoFOpUw8FewhMtt0oRfLIoYdfNxRFjiRq3__Jt564Pn5t_xdwC39NoMgtbfMuOuQz8cLDs=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/nUp-bS4xuDx-fWYxchkIwNjRRs_acHXJOISMO9sfOcu3MdW3b3ISr_PztYkBygel-p5MpLoFOpUw8FewhMtt0oRfLIoYdfNxRFjiRq3__Jt564Pn5t_xdwC39NoMgtbfMuOuQz8cLDs=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 12',
+    name: 'Episode 12',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/Rnsln9umG_pY77PqO6wcXvJx5IKqy0sUkFO0IQ3EmMySsDwEAjp9nJwWP1_NkpP3ywYj2WX7wLjoJ-xCBA_xd4cGXjGZ1daovurg3Pu2KR7_-5yCw1eb7--s5AByxQMzhmw8WswmXuM=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/Rnsln9umG_pY77PqO6wcXvJx5IKqy0sUkFO0IQ3EmMySsDwEAjp9nJwWP1_NkpP3ywYj2WX7wLjoJ-xCBA_xd4cGXjGZ1daovurg3Pu2KR7_-5yCw1eb7--s5AByxQMzhmw8WswmXuM=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  },
+  {
+    judul: '* Dansai Bunri no Crime Edge',
+    episode: '* Episode 13',
+    name: 'Episode 13',
+    deskrip1: '* Rating : 6.78',
+    sources: [{
+        src: 'https://lh3.googleusercontent.com/VUBUudsQUYqia6xPWjom4K3-__qLh2fGhje2sUTtIpLy2kyDq1CWw8HILCX5_szSI6c0G579uVlGib1pFZKKbhoAuR4J1VqzVHeKKhWlav9TbswSyd_zxe8wZwTvgTPTv5-tZpHuPjo=m22',
+        type: 'video/mp4',
+        label: '720p',
+        selected: 'false'
+      },
+      {
+        src: 'https://lh3.googleusercontent.com/VUBUudsQUYqia6xPWjom4K3-__qLh2fGhje2sUTtIpLy2kyDq1CWw8HILCX5_szSI6c0G579uVlGib1pFZKKbhoAuR4J1VqzVHeKKhWlav9TbswSyd_zxe8wZwTvgTPTv5-tZpHuPjo=m18',
+        type: 'video/mp4',
+        label: '480p',
+        selected: 'true'
+      }
+    ],
+    thumbnail: [{
+        srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+        type: 'image/jpeg',
+        media: '(min-width: 400px;)'
+      },
+      {
+        src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+      }
+    ]
+  }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+  var player = this;
+  player.controlBar.addChild('QualitySelector');
+  player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+  this.hotkeys({
+
+  });
+});
