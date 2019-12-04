d9ea7f324239c7fd83de0cb566a39acbe1843d33
dunia-anime <adityha.on@gmail.com>
2019-12-04 21:03:17 +0800

    Create Dag.js

---
+++ Anim-js/D/Dag.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* D-Frag!',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/LUo1Uywii8S9w22C5DGNYh7KTFEmg6yBzfTXSsNWpFVvNBrD8UA9yijdLz260A3XVtONWj7uH1ShQHdsOdCdYW3soFQU34T4oTHKMuBEyMv-wYjO6Ykh8F9Kk_zWVXxTL2Xn9x6kHzA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/LUo1Uywii8S9w22C5DGNYh7KTFEmg6yBzfTXSsNWpFVvNBrD8UA9yijdLz260A3XVtONWj7uH1ShQHdsOdCdYW3soFQU34T4oTHKMuBEyMv-wYjO6Ykh8F9Kk_zWVXxTL2Xn9x6kHzA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Fp0bd_3LilOTwyeFPzljPtgIb-5DQuRVKnwXjAt_vZWgc8UVTe0qnDngJ8qtw5lpq0BYQXGddOVbXpWAKnDZmqd7GC2iHxY-r5JFotaNFpPV6a5JfWVpi3EZ2KHBmGQWRUO2-ZpOkAg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Fp0bd_3LilOTwyeFPzljPtgIb-5DQuRVKnwXjAt_vZWgc8UVTe0qnDngJ8qtw5lpq0BYQXGddOVbXpWAKnDZmqd7GC2iHxY-r5JFotaNFpPV6a5JfWVpi3EZ2KHBmGQWRUO2-ZpOkAg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/JHu1_5-RctEhbyqZmvbMK3xEPB-nbFT8piGntfP8ctc2R4DYBgmFGOEoCUjYsFEiG3qgERXpYnrVkApFV41WSAmru4vAif03Jted-YTfKvH0hgBXIkt_g6CDtjrAVgS8FWGrYZcXpvk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/JHu1_5-RctEhbyqZmvbMK3xEPB-nbFT8piGntfP8ctc2R4DYBgmFGOEoCUjYsFEiG3qgERXpYnrVkApFV41WSAmru4vAif03Jted-YTfKvH0hgBXIkt_g6CDtjrAVgS8FWGrYZcXpvk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/KufR-RSr7Dze4SC0YFK1PsxB1NpimsCFbKbbGiYbdqVtJR3NIBcPcMeuIK0al22rd4tGGLxU9Fh5EOfEaFBs3lQjazA_YihE-40u9Ui51upe50WdtKnQrYf1_05KJW8_rXaPKouefjI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/KufR-RSr7Dze4SC0YFK1PsxB1NpimsCFbKbbGiYbdqVtJR3NIBcPcMeuIK0al22rd4tGGLxU9Fh5EOfEaFBs3lQjazA_YihE-40u9Ui51upe50WdtKnQrYf1_05KJW8_rXaPKouefjI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/VKu4xij3yV_bZV2uhVVLbrkqp-x99W6J7Rpd92bOFKAjpPcltUeh9YCnRSjM12aShO3s0Y7J70-ymw6U2Ay_EtYG8uRyJUHnfgZRJrSYmFPl2lb_SKhsDEw6e4Q2oqsnXzPv_DxZcnA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/VKu4xij3yV_bZV2uhVVLbrkqp-x99W6J7Rpd92bOFKAjpPcltUeh9YCnRSjM12aShO3s0Y7J70-ymw6U2Ay_EtYG8uRyJUHnfgZRJrSYmFPl2lb_SKhsDEw6e4Q2oqsnXzPv_DxZcnA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/quF7TRlBeGC7QGxOgYu5JpJVlaZD0i2I-x-NmrXDDDDsIURQHUHNyvLmoExNq4cZVc3KRJBQGBOvzMkEeK5hvyHaC-YXiDDwqTFO-p0lqpGQ1-dxWyvmkSn8mVpItAlM1FN22UlrXsk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/quF7TRlBeGC7QGxOgYu5JpJVlaZD0i2I-x-NmrXDDDDsIURQHUHNyvLmoExNq4cZVc3KRJBQGBOvzMkEeK5hvyHaC-YXiDDwqTFO-p0lqpGQ1-dxWyvmkSn8mVpItAlM1FN22UlrXsk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/C6c-EpAvk5wNnpX-LUJb2hmXIqUgFZ7dpllalBD5kl1_BR53iftcWgOu8SMZgmSH2CoOrft2lRnorukcym8fgBMaHsoi6DyvhvcTiu9cR4kRHJ5rHyvk5Tgj6-ysEQQbQyGBsGoH9s8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/C6c-EpAvk5wNnpX-LUJb2hmXIqUgFZ7dpllalBD5kl1_BR53iftcWgOu8SMZgmSH2CoOrft2lRnorukcym8fgBMaHsoi6DyvhvcTiu9cR4kRHJ5rHyvk5Tgj6-ysEQQbQyGBsGoH9s8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/FIxmQQMGds6pHUs0vUQwzrV7yM09wtj2ugKvs73e1WYfRpd0zIrtw9wpqNWoW_dFoi1Xk2dWjZfE0aUpbBf38A_cIozHEbjGkoYFETwljKjsbZJUCNcM4zmqr4DQxvNAcZavDUteF4g=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/FIxmQQMGds6pHUs0vUQwzrV7yM09wtj2ugKvs73e1WYfRpd0zIrtw9wpqNWoW_dFoi1Xk2dWjZfE0aUpbBf38A_cIozHEbjGkoYFETwljKjsbZJUCNcM4zmqr4DQxvNAcZavDUteF4g=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/74UOeN2k7qXxVvmdPijhjXS1yNjSLWKLB3fmhIMYPmNQKUseM1_mR8aIjIKLqtc5TW5OGywIHUL9J_lTSX0yMzDu5x09sZ34fK9bwA-_NyFa1vb3XjN9N6yJjpKOB3f4NfOfCAzaFD4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/74UOeN2k7qXxVvmdPijhjXS1yNjSLWKLB3fmhIMYPmNQKUseM1_mR8aIjIKLqtc5TW5OGywIHUL9J_lTSX0yMzDu5x09sZ34fK9bwA-_NyFa1vb3XjN9N6yJjpKOB3f4NfOfCAzaFD4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/iozQnhp7DxVs86nVckUw68tKN5z3BY3S7D6BdQ86VfDnUcB5VEuYbRiwI_XH-Ic_HuVJ8Mhfr88ekC5Dk08YHf6QjSqSUs4IzZyjwQsur_RvjQJi3Ikk1a6_JsFBITMLXqTRSchTkdA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/iozQnhp7DxVs86nVckUw68tKN5z3BY3S7D6BdQ86VfDnUcB5VEuYbRiwI_XH-Ic_HuVJ8Mhfr88ekC5Dk08YHf6QjSqSUs4IzZyjwQsur_RvjQJi3Ikk1a6_JsFBITMLXqTRSchTkdA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/u-eJbdBwctqqUaHg7JDprw5_Y_Oh1g1EE81BF4pWGJsuS_mn2klF0uwYdKjEPa9mPCZL7eKmlvsHA5f2Pdp2gfNFhqZoEHf_ogg_kGJkBMPuRsqeGi3PMk8rg95otZdyFJsUXiAIkXQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/u-eJbdBwctqqUaHg7JDprw5_Y_Oh1g1EE81BF4pWGJsuS_mn2klF0uwYdKjEPa9mPCZL7eKmlvsHA5f2Pdp2gfNFhqZoEHf_ogg_kGJkBMPuRsqeGi3PMk8rg95otZdyFJsUXiAIkXQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nUTEu8au4OHKq3VjLwXfwburtHfbtE53dqcZpAsQ57f9Dp5amtFlc6NxNdvOx5OUSjLs8udlcGA-Kxn-1PCgTSbk6uAhqPHCKCt0CoDpUYQ6qFzroJmtrhQFh1mATXEIUK0KxO1geDU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nUTEu8au4OHKq3VjLwXfwburtHfbtE53dqcZpAsQ57f9Dp5amtFlc6NxNdvOx5OUSjLs8udlcGA-Kxn-1PCgTSbk6uAhqPHCKCt0CoDpUYQ6qFzroJmtrhQFh1mATXEIUK0KxO1geDU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* D-Frag!',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.66',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/F2HA20UfqdjbkFsCn-e8YHppVQGdZaLTsqFL66Ks55jrAB9KKf4FYw-uQ3eqsUHG4WbN4msEGBQVrQ5qZgeKxHGGIUP8dZEn5w40QD9HE_naQCZ20BygJNnY-A4o_thMoVtJ6MlY1p8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/F2HA20UfqdjbkFsCn-e8YHppVQGdZaLTsqFL66Ks55jrAB9KKf4FYw-uQ3eqsUHG4WbN4msEGBQVrQ5qZgeKxHGGIUP8dZEn5w40QD9HE_naQCZ20BygJNnY-A4o_thMoVtJ6MlY1p8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
