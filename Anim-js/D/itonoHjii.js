24eb025437a05b8233136a18cc87db8297d51386
dunia-anime <adityha.on@gmail.com>
2019-12-04 20:38:59 +0800

    Update itonoHjii.js

--- Anim-js/D/itonoHjii.js
+++ Anim-js/D/itonoHjii.js
@@ -1,8 +1,8 @@
 var player = videojs('video')
 player.playlist([{
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 01',
-        name: 'Episode 01',
+        episode: '* Episode 13',
+        name: 'Episode 13',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/lpBhMeQDGHHhDr11bG0kjNRiprNEjCYncitokUA4yr-NQo0PM0EQLW5iAuNiJuqoHShjGBAiMZUgU-oE09LpgqXPu_9OEmaRMT7et1E_CUsluYvfVnpgJgon-3Kp40KfsAYt4zpir7U=m22',
@@ -29,8 +29,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 02',
-        name: 'Episode 02',
+        episode: '* Episode 14',
+        name: 'Episode 14',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/hjHe6mZlaorVhJmoSZnuE6QUlqWEASx9hx3nSOLzzBtW00DHYIvnADn_7-U-2n3hgWN7ZCKYHRZ-JaepbXDpOXNQQkQkRD0H4GpZ5Jr5K1xoOjYy8u9pJGDZdfuw-3E2v7-0asPM4UQ=m22',
@@ -57,8 +57,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 03',
-        name: 'Episode 03',
+        episode: '* Episode 15',
+        name: 'Episode 15',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/eagLDxevjATr-IpEuYg2jujV9X-L032IOPdIjRzTzEsuxTUAG3LJY6P7ymdHgwlmLG_fNS9uAebay1Ko-HcBZXy8I82jNy4fRPJel6Ufa80UgAwPNTN8ojZ9nWxU_prDFYvawPJsXZ4=m22',
@@ -85,8 +85,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 04',
-        name: 'Episode 04',
+        episode: '* Episode 16',
+        name: 'Episode 16',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/rnUJ6dUdv0WfP66oIbys7L9PsgmnNarP5SuFzjoRGoJvP-lCPPHRh08sI6gaOP7aLwDRgV6n8UBnmdkquLV5keDibLGnHKWsr9Z-59KNNjAezTdx2ot8HlYKZlWPd0goZpxXCGFutKQ=m22',
@@ -113,8 +113,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 05',
-        name: 'Episode 05',
+        episode: '* Episode 17',
+        name: 'Episode 17',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/mATJzLszraX8tau1ZKEfQwg3Kmut0QmwFCOslbOhLRar4dumYHe6L3EwIXpcDs0rt5n2iFsCChPW4wHpVJ_bRZcUWE-YTat0iPQfXbki2cAnz4DG-ll_C_TFS9mlliSuSHXMYhOhncw=m22',
@@ -141,8 +141,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 06',
-        name: 'Episode 06',
+        episode: '* Episode 18',
+        name: 'Episode 18',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/b6_04F9y_c6BR_kCSiHK82XCzOiTxnxrvhB4A_VioM8z3JVrWkm9dZpqp2aBnI2Yrg5iUL5Nb_OTmKbvriS7pPvhhijuszHvqQbVOcG65fDX1Auain3NHRJba3lLMrBJ8ebY3HeB8bM=m22',
@@ -169,8 +169,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 07',
-        name: 'Episode 07',
+        episode: '* Episode 19',
+        name: 'Episode 19',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/oFkzjTSet5V7lDZ23Wi2EUJRXrEyv19ANbQeeZXPhzobWQ6waJ4bH8tJDv8qIECRqkdJztjx--00kWSBDubjQZ7XlqNqs6RQnLbn5yTZ83dsQm2GlysAaU7kAxTRPbRcBPUb_ri5nu0=m22',
@@ -197,8 +197,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 08',
-        name: 'Episode 08',
+        episode: '* Episode 20',
+        name: 'Episode 20',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/PXOeJPOkEkFongW7NN7Qoj2OQIqeNhTxgfrm3Jiy6x6LV2EAxExEw4H9bUd-3PaG0Z7HB6lWpgWJoojUJvbyu_BNbkJUzMCyaFdliA8ZoFpy5adUVKPEzvFHHEcR3NNHz0YylxECM3E=m22',
@@ -225,8 +225,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 09',
-        name: 'Episode 09',
+        episode: '* Episode 21',
+        name: 'Episode 21',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/RtDfC_1PRCsRd3UWoPL4gPwcTdAPegCTmRe8RAHZWCUiRAdpeYhfL9fQ50LA3QxtvbggPUCyuWFtRs9cDMIoCElr2WsDL5Dh2k04bncqz-fYFYeSc0LX5WuUSeh7f5U-f7an9o7b2yc=m22',
@@ -253,8 +253,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 10',
-        name: 'Episode 10',
+        episode: '* Episode 22',
+        name: 'Episode 22',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/PqJVAi2AzZsmk0UAf_Hzh1XPN-Efhxo2xy25kbett7U-9r_Fm1rM19Mv4g0cyyWxCNK9BizHICE22D6V1qkgjLUuLuOUc1b3ovCJMDLhC_xkBHtPBw_MGnwSwUMWlT7YzQ3MYIk4uNc=m22',
@@ -281,8 +281,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 11',
-        name: 'Episode 11',
+        episode: '* Episode 23',
+        name: 'Episode 23',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/ptcJUlSfsxcL03CpUhdLY4mL3quURTcJlrgehlUHVEbjnVLZ8z3M6Byli2ka9aPpIcZE7eAyeiHBvofikORBzn3VS-e0VC5viqNb6M8cQylpYQzZ2_n4pbwZub5XTygN-c348NpJo2g=m22',
@@ -309,8 +309,8 @@ player.playlist([{
     },
     {
         judul: '* Daitoshokan no Hitsujikai',
-        episode: '* Episode 12',
-        name: 'Episode 12',
+        episode: '* Episode 24',
+        name: 'Episode 24',
         deskrip1: '* Rating : 7.02',
         sources: [{
                 src: 'https://lh3.googleusercontent.com/6nwj2H9Fwvrbvm7eACiCs6EZlVTtltuGnG06o-MohbxcT6V8VYFqQah6LU66rmqC0Bk3Fw2DiulisLlRwlNMzebj8FwSX8JFPNwExXKdw4urwyg7hfD3-CHDYiKJFeuRXs6zU2EmEOE=m22',
