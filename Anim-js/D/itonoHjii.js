af810d7eb75b23eb1699e4961113e6cbef31ab54
dunia-anime <adityha.on@gmail.com>
2019-12-04 20:31:13 +0800

    Create itonoHjii.js

---
+++ Anim-js/D/itonoHjii.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/lpBhMeQDGHHhDr11bG0kjNRiprNEjCYncitokUA4yr-NQo0PM0EQLW5iAuNiJuqoHShjGBAiMZUgU-oE09LpgqXPu_9OEmaRMT7et1E_CUsluYvfVnpgJgon-3Kp40KfsAYt4zpir7U=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/lpBhMeQDGHHhDr11bG0kjNRiprNEjCYncitokUA4yr-NQo0PM0EQLW5iAuNiJuqoHShjGBAiMZUgU-oE09LpgqXPu_9OEmaRMT7et1E_CUsluYvfVnpgJgon-3Kp40KfsAYt4zpir7U=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/hjHe6mZlaorVhJmoSZnuE6QUlqWEASx9hx3nSOLzzBtW00DHYIvnADn_7-U-2n3hgWN7ZCKYHRZ-JaepbXDpOXNQQkQkRD0H4GpZ5Jr5K1xoOjYy8u9pJGDZdfuw-3E2v7-0asPM4UQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/hjHe6mZlaorVhJmoSZnuE6QUlqWEASx9hx3nSOLzzBtW00DHYIvnADn_7-U-2n3hgWN7ZCKYHRZ-JaepbXDpOXNQQkQkRD0H4GpZ5Jr5K1xoOjYy8u9pJGDZdfuw-3E2v7-0asPM4UQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/eagLDxevjATr-IpEuYg2jujV9X-L032IOPdIjRzTzEsuxTUAG3LJY6P7ymdHgwlmLG_fNS9uAebay1Ko-HcBZXy8I82jNy4fRPJel6Ufa80UgAwPNTN8ojZ9nWxU_prDFYvawPJsXZ4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/eagLDxevjATr-IpEuYg2jujV9X-L032IOPdIjRzTzEsuxTUAG3LJY6P7ymdHgwlmLG_fNS9uAebay1Ko-HcBZXy8I82jNy4fRPJel6Ufa80UgAwPNTN8ojZ9nWxU_prDFYvawPJsXZ4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/rnUJ6dUdv0WfP66oIbys7L9PsgmnNarP5SuFzjoRGoJvP-lCPPHRh08sI6gaOP7aLwDRgV6n8UBnmdkquLV5keDibLGnHKWsr9Z-59KNNjAezTdx2ot8HlYKZlWPd0goZpxXCGFutKQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/rnUJ6dUdv0WfP66oIbys7L9PsgmnNarP5SuFzjoRGoJvP-lCPPHRh08sI6gaOP7aLwDRgV6n8UBnmdkquLV5keDibLGnHKWsr9Z-59KNNjAezTdx2ot8HlYKZlWPd0goZpxXCGFutKQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/mATJzLszraX8tau1ZKEfQwg3Kmut0QmwFCOslbOhLRar4dumYHe6L3EwIXpcDs0rt5n2iFsCChPW4wHpVJ_bRZcUWE-YTat0iPQfXbki2cAnz4DG-ll_C_TFS9mlliSuSHXMYhOhncw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/mATJzLszraX8tau1ZKEfQwg3Kmut0QmwFCOslbOhLRar4dumYHe6L3EwIXpcDs0rt5n2iFsCChPW4wHpVJ_bRZcUWE-YTat0iPQfXbki2cAnz4DG-ll_C_TFS9mlliSuSHXMYhOhncw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/b6_04F9y_c6BR_kCSiHK82XCzOiTxnxrvhB4A_VioM8z3JVrWkm9dZpqp2aBnI2Yrg5iUL5Nb_OTmKbvriS7pPvhhijuszHvqQbVOcG65fDX1Auain3NHRJba3lLMrBJ8ebY3HeB8bM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/b6_04F9y_c6BR_kCSiHK82XCzOiTxnxrvhB4A_VioM8z3JVrWkm9dZpqp2aBnI2Yrg5iUL5Nb_OTmKbvriS7pPvhhijuszHvqQbVOcG65fDX1Auain3NHRJba3lLMrBJ8ebY3HeB8bM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/oFkzjTSet5V7lDZ23Wi2EUJRXrEyv19ANbQeeZXPhzobWQ6waJ4bH8tJDv8qIECRqkdJztjx--00kWSBDubjQZ7XlqNqs6RQnLbn5yTZ83dsQm2GlysAaU7kAxTRPbRcBPUb_ri5nu0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/oFkzjTSet5V7lDZ23Wi2EUJRXrEyv19ANbQeeZXPhzobWQ6waJ4bH8tJDv8qIECRqkdJztjx--00kWSBDubjQZ7XlqNqs6RQnLbn5yTZ83dsQm2GlysAaU7kAxTRPbRcBPUb_ri5nu0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/PXOeJPOkEkFongW7NN7Qoj2OQIqeNhTxgfrm3Jiy6x6LV2EAxExEw4H9bUd-3PaG0Z7HB6lWpgWJoojUJvbyu_BNbkJUzMCyaFdliA8ZoFpy5adUVKPEzvFHHEcR3NNHz0YylxECM3E=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/PXOeJPOkEkFongW7NN7Qoj2OQIqeNhTxgfrm3Jiy6x6LV2EAxExEw4H9bUd-3PaG0Z7HB6lWpgWJoojUJvbyu_BNbkJUzMCyaFdliA8ZoFpy5adUVKPEzvFHHEcR3NNHz0YylxECM3E=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/RtDfC_1PRCsRd3UWoPL4gPwcTdAPegCTmRe8RAHZWCUiRAdpeYhfL9fQ50LA3QxtvbggPUCyuWFtRs9cDMIoCElr2WsDL5Dh2k04bncqz-fYFYeSc0LX5WuUSeh7f5U-f7an9o7b2yc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/RtDfC_1PRCsRd3UWoPL4gPwcTdAPegCTmRe8RAHZWCUiRAdpeYhfL9fQ50LA3QxtvbggPUCyuWFtRs9cDMIoCElr2WsDL5Dh2k04bncqz-fYFYeSc0LX5WuUSeh7f5U-f7an9o7b2yc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/PqJVAi2AzZsmk0UAf_Hzh1XPN-Efhxo2xy25kbett7U-9r_Fm1rM19Mv4g0cyyWxCNK9BizHICE22D6V1qkgjLUuLuOUc1b3ovCJMDLhC_xkBHtPBw_MGnwSwUMWlT7YzQ3MYIk4uNc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/PqJVAi2AzZsmk0UAf_Hzh1XPN-Efhxo2xy25kbett7U-9r_Fm1rM19Mv4g0cyyWxCNK9BizHICE22D6V1qkgjLUuLuOUc1b3ovCJMDLhC_xkBHtPBw_MGnwSwUMWlT7YzQ3MYIk4uNc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ptcJUlSfsxcL03CpUhdLY4mL3quURTcJlrgehlUHVEbjnVLZ8z3M6Byli2ka9aPpIcZE7eAyeiHBvofikORBzn3VS-e0VC5viqNb6M8cQylpYQzZ2_n4pbwZub5XTygN-c348NpJo2g=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ptcJUlSfsxcL03CpUhdLY4mL3quURTcJlrgehlUHVEbjnVLZ8z3M6Byli2ka9aPpIcZE7eAyeiHBvofikORBzn3VS-e0VC5viqNb6M8cQylpYQzZ2_n4pbwZub5XTygN-c348NpJo2g=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Daitoshokan no Hitsujikai',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.02',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6nwj2H9Fwvrbvm7eACiCs6EZlVTtltuGnG06o-MohbxcT6V8VYFqQah6LU66rmqC0Bk3Fw2DiulisLlRwlNMzebj8FwSX8JFPNwExXKdw4urwyg7hfD3-CHDYiKJFeuRXs6zU2EmEOE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6nwj2H9Fwvrbvm7eACiCs6EZlVTtltuGnG06o-MohbxcT6V8VYFqQah6LU66rmqC0Bk3Fw2DiulisLlRwlNMzebj8FwSX8JFPNwExXKdw4urwyg7hfD3-CHDYiKJFeuRXs6zU2EmEOE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
