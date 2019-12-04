2409bc768f2a396bd56f6fba339fa4b5570a998f
dunia-anime <adityha.on@gmail.com>
2019-12-04 20:25:27 +0800

    Update DakBoHDai.js

--- Anim-js/D/DakBoHDai.js
+++ Anim-js/D/DakBoHDai.js
@@ -5,12 +5,6 @@ player.playlist([{
         name: 'Episode 01',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/RzuENt9fAU1zs33l9YwLfTgbJWjOtYnpc6ERDJQglXxWBhLUIFFoa3B19pLqAXAOkB6LF_u1DzpIgoFahyusyXQUMAmG4LZmwW-TPU7uqcn3NQMXqx7pjKQ1I4Ip6MXQ9QcBKm9P-MY=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/RzuENt9fAU1zs33l9YwLfTgbJWjOtYnpc6ERDJQglXxWBhLUIFFoa3B19pLqAXAOkB6LF_u1DzpIgoFahyusyXQUMAmG4LZmwW-TPU7uqcn3NQMXqx7pjKQ1I4Ip6MXQ9QcBKm9P-MY=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -39,12 +33,6 @@ player.playlist([{
         name: 'Episode 02',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/WbjlZOy5GG-0unrxg37479ady4shE2-lFjpzWLhCtiNSeKt3c1THJaPu3eZE7D50eMfVyLSDnUe3QCA52h6Ahcxmm_POS53Tk5jmmZunGSRIKrJPMQ29JzrfoM_uAJKQVbx2oepzcYo=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/WbjlZOy5GG-0unrxg37479ady4shE2-lFjpzWLhCtiNSeKt3c1THJaPu3eZE7D50eMfVyLSDnUe3QCA52h6Ahcxmm_POS53Tk5jmmZunGSRIKrJPMQ29JzrfoM_uAJKQVbx2oepzcYo=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -73,12 +61,6 @@ player.playlist([{
         name: 'Episode 03',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/rd1LGmBneJ5QdmY75Os3YSK2-BMMqrJ5YUeIZzrLrhIaMsa7izhGzz5jHXx541hZAUd01YnKwfbu7D9zTZIUmS3Tta33a4_oBOMlJtk1SmuPwe94fw-VtMmkbfu5EP64JUqCi5MqzQA=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/rd1LGmBneJ5QdmY75Os3YSK2-BMMqrJ5YUeIZzrLrhIaMsa7izhGzz5jHXx541hZAUd01YnKwfbu7D9zTZIUmS3Tta33a4_oBOMlJtk1SmuPwe94fw-VtMmkbfu5EP64JUqCi5MqzQA=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -107,12 +89,6 @@ player.playlist([{
         name: 'Episode 04',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/ekKyCvqZj5yvP_HBX6y6QNlYFFDQW0zfv_NF31v25nSpXNGM3NEdidvQGsQGc1ZFgLXLR_Rdz5Qwz9YpWMnEFWRVd_h-3ukUrVXekOgR-Slx0iLZ4U3uZ0Ev7skhyI9x0a46kWzx01c=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/ekKyCvqZj5yvP_HBX6y6QNlYFFDQW0zfv_NF31v25nSpXNGM3NEdidvQGsQGc1ZFgLXLR_Rdz5Qwz9YpWMnEFWRVd_h-3ukUrVXekOgR-Slx0iLZ4U3uZ0Ev7skhyI9x0a46kWzx01c=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -141,12 +117,6 @@ player.playlist([{
         name: 'Episode 05',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/Eu4HiqfE0oJ2bDQQgmbZGG8PkGiSAW1F2XI3xUXxXIEeTP42jSA-J5V5hVkYndPqtcyaeUe5JyR3ANCiz7lDtg0dBKFpcDs_ofnK4FyIh69Usuzn56xprJEV2DRY0hMXZ5fiyCWWcTw=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/Eu4HiqfE0oJ2bDQQgmbZGG8PkGiSAW1F2XI3xUXxXIEeTP42jSA-J5V5hVkYndPqtcyaeUe5JyR3ANCiz7lDtg0dBKFpcDs_ofnK4FyIh69Usuzn56xprJEV2DRY0hMXZ5fiyCWWcTw=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -175,12 +145,6 @@ player.playlist([{
         name: 'Episode 06',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/7KVXKFZdZAOueOAJcOSyNP9ddtvX-llteLGKVkc13tLqkOUr3F5C0f-Bqvqu-_c6VDcTquR48u_0nNiCBqzxe2pwPxvngOskJJq6xzueN5kcn0nzm-LfPR1KDSUlZFcQtLmC12NtURk=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/7KVXKFZdZAOueOAJcOSyNP9ddtvX-llteLGKVkc13tLqkOUr3F5C0f-Bqvqu-_c6VDcTquR48u_0nNiCBqzxe2pwPxvngOskJJq6xzueN5kcn0nzm-LfPR1KDSUlZFcQtLmC12NtURk=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -209,12 +173,6 @@ player.playlist([{
         name: 'Episode 07',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/GG_01L28s8IPCzvmJUYTyiCYKaPr329NxqBXZBhqgNB94qNXqDllZX8ETl9k24Yr01UzcImcvNwvbr4Rq0VCMV1g0PH2v3JrsZKgs_Jke8SF1Gxezn0jzWKasfnagX61dHGn9I5pRK8=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/GG_01L28s8IPCzvmJUYTyiCYKaPr329NxqBXZBhqgNB94qNXqDllZX8ETl9k24Yr01UzcImcvNwvbr4Rq0VCMV1g0PH2v3JrsZKgs_Jke8SF1Gxezn0jzWKasfnagX61dHGn9I5pRK8=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -243,12 +201,6 @@ player.playlist([{
         name: 'Episode 08',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/yu6coYCl_f20DYEH1JB84pnQEXlhbUFBylh08CC0tYItu72SS06OTJHyJX8jPFHI3agPgMZkXhCnGe5YcZVinPeRcxykLHrSONBq_lABHCQwlZHv3NwxlU8ILcz1IhDnDDr9ERB-UVc=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/yu6coYCl_f20DYEH1JB84pnQEXlhbUFBylh08CC0tYItu72SS06OTJHyJX8jPFHI3agPgMZkXhCnGe5YcZVinPeRcxykLHrSONBq_lABHCQwlZHv3NwxlU8ILcz1IhDnDDr9ERB-UVc=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -277,12 +229,6 @@ player.playlist([{
         name: 'Episode 09',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/WKCZLicE7msroH0DYJyK-GUtHWdqjfofaIqb-WX4ISBCMgVm21PAiSjgPbc7Uo183DOWz0bvs7yMBGwaSbTJGVXfIHmMJRUyVDNj49z65l67Xkl9T-B8LFRiXiUA3ogfkjwi70VJmPs=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/WKCZLicE7msroH0DYJyK-GUtHWdqjfofaIqb-WX4ISBCMgVm21PAiSjgPbc7Uo183DOWz0bvs7yMBGwaSbTJGVXfIHmMJRUyVDNj49z65l67Xkl9T-B8LFRiXiUA3ogfkjwi70VJmPs=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -311,12 +257,6 @@ player.playlist([{
         name: 'Episode 10',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/mKWpil9xPqLVLllmrgD-sSxNS2PI_G2_ga2rxA8JhKvrCczfIKM14SdT7OZfm7Gujir0tcKgX3sIZzweTyBSdICXk5cdEzm0NpSrHmLGx0z555rhzSPlZB8saegVbuhrS5G4_xRBptQ=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/mKWpil9xPqLVLllmrgD-sSxNS2PI_G2_ga2rxA8JhKvrCczfIKM14SdT7OZfm7Gujir0tcKgX3sIZzweTyBSdICXk5cdEzm0NpSrHmLGx0z555rhzSPlZB8saegVbuhrS5G4_xRBptQ=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -345,12 +285,6 @@ player.playlist([{
         name: 'Episode 11',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/tD6Bsr5O9guntDz-AkvkoUaBipImYDcVBtGEdhTemhd8EgsS1wyZL-MlIMZATMc9QDbLaMygPswkAw59pnmIsnNbYe2o6TfeHMkmoZOHzWBRP39QLWEwqM0zXvGJuh2eiiKeYt_kRvU=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/tD6Bsr5O9guntDz-AkvkoUaBipImYDcVBtGEdhTemhd8EgsS1wyZL-MlIMZATMc9QDbLaMygPswkAw59pnmIsnNbYe2o6TfeHMkmoZOHzWBRP39QLWEwqM0zXvGJuh2eiiKeYt_kRvU=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -379,12 +313,6 @@ player.playlist([{
         name: 'Episode 12',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/FP3omm3YcIK-6bha0zXMtsl497eaVVuWPvHm1Y8PhgjY1CHnxcwVRCjrHYXsZUwK-bEGa9vCCssCkj3tOulnDdUSgMcvz964ZOdoVeoJLpuVOUh0-zQsC4oeN3lubHMRaUlDkBXA0so=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/FP3omm3YcIK-6bha0zXMtsl497eaVVuWPvHm1Y8PhgjY1CHnxcwVRCjrHYXsZUwK-bEGa9vCCssCkj3tOulnDdUSgMcvz964ZOdoVeoJLpuVOUh0-zQsC4oeN3lubHMRaUlDkBXA0so=m22',
                 type: 'video/mp4',
                 label: '720p',
@@ -413,12 +341,6 @@ player.playlist([{
         name: 'Episode 13',
         deskrip1: '* Rating : 6.79',
         sources: [{
-                src: 'https://lh3.googleusercontent.com/xkBtWICeI6IWmY7CIBb0MaArEVrMQhVBzuTtsIauwSVYIsJj2mr0j1SfTG2fuqDtlT_FG7vQy43YBLxxJ9e5q5sUYfLoUI1iMhWuVKuspnBEl4btuCWGrmkZjICLp9hSAaRw5zHRK8U=m37',
-                type: 'video/mp4',
-                label: '1080p',
-                selected: 'false'
-            },
-            {
                 src: 'https://lh3.googleusercontent.com/xkBtWICeI6IWmY7CIBb0MaArEVrMQhVBzuTtsIauwSVYIsJj2mr0j1SfTG2fuqDtlT_FG7vQy43YBLxxJ9e5q5sUYfLoUI1iMhWuVKuspnBEl4btuCWGrmkZjICLp9hSAaRw5zHRK8U=m22',
                 type: 'video/mp4',
                 label: '720p',
