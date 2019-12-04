0d1a6dd321d58b4323d3fbc83d9ac31c80cd8dac
dunia-anime <adityha.on@gmail.com>
2019-12-04 20:49:47 +0800

    Create aassiS1.js

---
+++ Anim-js/D/aassiS1.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/xaNkPmtxRymSssJQ0FW5n5aAUG7TGjOfn1xPuWE-tKDlD5W3oO-YFq3dIm6qigO6t6aNeRD8Cv55cm6aGVWjjHwlVBfrpUi95RQ7ff64TUP4EFQkCgxXW7ZFAz7d6Rcw0nSQ7p_SLlw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/xaNkPmtxRymSssJQ0FW5n5aAUG7TGjOfn1xPuWE-tKDlD5W3oO-YFq3dIm6qigO6t6aNeRD8Cv55cm6aGVWjjHwlVBfrpUi95RQ7ff64TUP4EFQkCgxXW7ZFAz7d6Rcw0nSQ7p_SLlw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/fdME-WjRyndVcnVgW3f3GcFbVuYIDXhfoLHKdvx3siKsxSc2wfHpGc9GrwIGv0TcMTXOohc8FlzIesWyMsxLA_fJpDC999uZoHnen90-3cIo6a5BYaqIrYGY4hMYPMa67VpYSZP7Ymk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/fdME-WjRyndVcnVgW3f3GcFbVuYIDXhfoLHKdvx3siKsxSc2wfHpGc9GrwIGv0TcMTXOohc8FlzIesWyMsxLA_fJpDC999uZoHnen90-3cIo6a5BYaqIrYGY4hMYPMa67VpYSZP7Ymk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ynEEHjFvdFZrhyNmQLxhY2JltdMKvZ-Iv10qYSNsy7DI3IKB0HMZee8oolyaT3X_Fo02fQF2Riiz0VKKKTYTuAWt5eXSvHfMw1Ndu7Qc0sMATbPM2d5u86kgDpDn_PMLre01hlaBvnQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ynEEHjFvdFZrhyNmQLxhY2JltdMKvZ-Iv10qYSNsy7DI3IKB0HMZee8oolyaT3X_Fo02fQF2Riiz0VKKKTYTuAWt5eXSvHfMw1Ndu7Qc0sMATbPM2d5u86kgDpDn_PMLre01hlaBvnQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/aTHHPZudzwA6sMYRTgATnKRo5ukRsHjSbWnAs7OXDrfme-Mpe-wxnGipYBn0WYtGzusur_sya_C9Gs18IVbrSuNVSPI_iaN2FaAjfIgRdZB5wcOvzjObOFWmN7IfF2H-AN1VQDYeyIk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/aTHHPZudzwA6sMYRTgATnKRo5ukRsHjSbWnAs7OXDrfme-Mpe-wxnGipYBn0WYtGzusur_sya_C9Gs18IVbrSuNVSPI_iaN2FaAjfIgRdZB5wcOvzjObOFWmN7IfF2H-AN1VQDYeyIk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/l5eRuO7XHWO1U2wKsmDF0sv5z1SxHQCZTk2QK9P5v-PISXeqMDTms5jWU33zaaIyUjZiuldbwzI9Y-U3UAXLz06foGUH1Ym0_tUyhOHo9-bNKXS2R2-EZuvymjk0B7GokcwTsWDJeeo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/l5eRuO7XHWO1U2wKsmDF0sv5z1SxHQCZTk2QK9P5v-PISXeqMDTms5jWU33zaaIyUjZiuldbwzI9Y-U3UAXLz06foGUH1Ym0_tUyhOHo9-bNKXS2R2-EZuvymjk0B7GokcwTsWDJeeo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/U6HfteBPHC4A9cy1MYR0SL_fwvD5v49RKZLm0LE5Zi4GTePWXSB06KaZMfFpiSg89aqdgbdP7o7szWPSxW4UDwLC_vuUwm80g8rl1UxJPUdSyurCYovz3bVVEUPjhYfNUn_fB_EW-3k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/U6HfteBPHC4A9cy1MYR0SL_fwvD5v49RKZLm0LE5Zi4GTePWXSB06KaZMfFpiSg89aqdgbdP7o7szWPSxW4UDwLC_vuUwm80g8rl1UxJPUdSyurCYovz3bVVEUPjhYfNUn_fB_EW-3k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/dp_YVCfsGIH5OtpszQOC-JYPhoLM9u967b5Hyns--en3tOIBl2cg-iS653OxFoSIfsbIAHkHyXndQGKMf7VMo-rMA9_n-89VXEJf9S6H52-dSncvy9PC4sjia29xFwbQioMSlAGdjNI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/dp_YVCfsGIH5OtpszQOC-JYPhoLM9u967b5Hyns--en3tOIBl2cg-iS653OxFoSIfsbIAHkHyXndQGKMf7VMo-rMA9_n-89VXEJf9S6H52-dSncvy9PC4sjia29xFwbQioMSlAGdjNI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/-7CGb855b5WSvRgpskqmZ0hWQstL81oe4ewe2QI7CULLSBlpqHBKYEfOUiQIZKl0emn7IsP9l5KWbhlb9uRQBvF1Jtb0_lzu3oEHILI0mQBhDflLMQg_lpgAjszPKHEYixhBDBygyjE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/-7CGb855b5WSvRgpskqmZ0hWQstL81oe4ewe2QI7CULLSBlpqHBKYEfOUiQIZKl0emn7IsP9l5KWbhlb9uRQBvF1Jtb0_lzu3oEHILI0mQBhDflLMQg_lpgAjszPKHEYixhBDBygyjE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/tYr9mTzy604VKsgP0KXSjdmlUcaMWKk1whIGvvQHDUu_RC0NTHz9YxzVx1ePDsVxBAwVmuPyibbn_xfv6dv6rz8OtZXn9dBd6gRfssER7YnUEeyu33uOT7ox-aFL0ka8baKp0yB2TWY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/tYr9mTzy604VKsgP0KXSjdmlUcaMWKk1whIGvvQHDUu_RC0NTHz9YxzVx1ePDsVxBAwVmuPyibbn_xfv6dv6rz8OtZXn9dBd6gRfssER7YnUEeyu33uOT7ox-aFL0ka8baKp0yB2TWY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/cgfws3qINYeAu0rJLbNEcQGZpmpRSmhAE8FaMhDNpVlhDT-gM7SxyB9UkrVBGzfUva-nxrppsFVHaVo4Y1Spp5AI5A8b_CbiPBtAK7J1l8mjbtlwbKtz9PjFTaDwZ11LQGAHY7pPWEk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/cgfws3qINYeAu0rJLbNEcQGZpmpRSmhAE8FaMhDNpVlhDT-gM7SxyB9UkrVBGzfUva-nxrppsFVHaVo4Y1Spp5AI5A8b_CbiPBtAK7J1l8mjbtlwbKtz9PjFTaDwZ11LQGAHY7pPWEk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/a_4NGDuaC5CXoRhm-XbAHrW0EjMvEFPPRseYxQZclwjEOU3BVK-26sm-IWUMNCogln64T7sl-_S0F60hNdGpvXUOGk2MKtUw6XbG0v-iZ5nxz0XFgey7BkkiLEdw0APGJBFyfWqMFB0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/a_4NGDuaC5CXoRhm-XbAHrW0EjMvEFPPRseYxQZclwjEOU3BVK-26sm-IWUMNCogln64T7sl-_S0F60hNdGpvXUOGk2MKtUw6XbG0v-iZ5nxz0XFgey7BkkiLEdw0APGJBFyfWqMFB0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Dagashi Kashi S1',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.69',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/YVCgslFvOtgtueWFUSdwSqV78H1pzCcKa506rkv4t_nrV9HunTVSt3stGK_OCPHug34PQ2feY36N1RF-7gname509gT070SbzYv0x7IKkPdYPjkmrIg0eFqovGzyGCIumW7ALX-6Ywo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/YVCgslFvOtgtueWFUSdwSqV78H1pzCcKa506rkv4t_nrV9HunTVSt3stGK_OCPHug34PQ2feY36N1RF-7gname509gT070SbzYv0x7IKkPdYPjkmrIg0eFqovGzyGCIumW7ALX-6Ywo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
