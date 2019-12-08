d20ff7ac0b496fe900602d26b2f99954100edc98
dunia-anime <adityha.on@gmail.com>
2019-12-08 10:53:58 +0800

    Create eroManoo.js

---
+++ Anim-js/D/eroManoo.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/x5h1NdPYZQu4CVtRzth9yMAYGMSoZPHlJByHrF34sHQmb7bbvWS6L6dioJZfUEIMCU6OjiMD6wOGDBv7W14H-SL56_rZ1C1nezhJLsiWrXJxPDW1x-C7FjyNTMYDh0S4_e4lz-9-9t8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/x5h1NdPYZQu4CVtRzth9yMAYGMSoZPHlJByHrF34sHQmb7bbvWS6L6dioJZfUEIMCU6OjiMD6wOGDBv7W14H-SL56_rZ1C1nezhJLsiWrXJxPDW1x-C7FjyNTMYDh0S4_e4lz-9-9t8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/O9Sh0nMxKJ-e3F4Bn0gKK9Pglx8j3BZINe7qfikOequYh0sbmXHOkGkpcUS-BZarQkreUmluXTjZqgYGEq6ETMu9w3-b5N_ViYk7sVltAaRkESv0TzaubEyyN6BojOUEQLdmgaEuYdw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/O9Sh0nMxKJ-e3F4Bn0gKK9Pglx8j3BZINe7qfikOequYh0sbmXHOkGkpcUS-BZarQkreUmluXTjZqgYGEq6ETMu9w3-b5N_ViYk7sVltAaRkESv0TzaubEyyN6BojOUEQLdmgaEuYdw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/hqw7mcNw43m0jn-QZxrU_3v2l69ew4u7z9z3B3tz_xOGpajYPR9aAb2iE6_SqQzasKl7uYbWEZCVHbvjr8GnFs9sLNtAgHnh07I5hZNF-w-yY4-E_xleVyhXTbiDcfhq_zeJVDS_h30=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/hqw7mcNw43m0jn-QZxrU_3v2l69ew4u7z9z3B3tz_xOGpajYPR9aAb2iE6_SqQzasKl7uYbWEZCVHbvjr8GnFs9sLNtAgHnh07I5hZNF-w-yY4-E_xleVyhXTbiDcfhq_zeJVDS_h30=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/8QUaLEzvvaJcKBSKkOa46ZTAT5HqIiq9tp6Z9woe2aM1xqj7jx9RMDmE_uDWti9r1-yvq6YXCZP4kXuDQjFxtGKaLHlBqZnRIAAnJfOMGsvQVEEKHo94vyoFgVr5B-fC4ALUMzwAHP8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/8QUaLEzvvaJcKBSKkOa46ZTAT5HqIiq9tp6Z9woe2aM1xqj7jx9RMDmE_uDWti9r1-yvq6YXCZP4kXuDQjFxtGKaLHlBqZnRIAAnJfOMGsvQVEEKHo94vyoFgVr5B-fC4ALUMzwAHP8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/d45rr6_rpArGa8KGJYDfq_TjltTAU40IBfghE6H0Pgnd6oUodKwMj36zvuhuvjVwP7GjphjfUGejo--c0oOdX8rzgeWosgCxKD24MDrIBtnKmQJ4vJa06XAhyzciLIw2e0ljQyAyThI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/d45rr6_rpArGa8KGJYDfq_TjltTAU40IBfghE6H0Pgnd6oUodKwMj36zvuhuvjVwP7GjphjfUGejo--c0oOdX8rzgeWosgCxKD24MDrIBtnKmQJ4vJa06XAhyzciLIw2e0ljQyAyThI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/DbPVJ9JP2NB4vLxeArBdvKmBiAavVwVRM6WoRnwdGOsfxtG4aVhVOqLbUm5va8_UW3ks3VCyIQ9zIX5KKNgv6M19ez9kfTCkcWMv4pvBddax8HnxW6VJdBTyaomM5XyyY8cg6wlbloU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/DbPVJ9JP2NB4vLxeArBdvKmBiAavVwVRM6WoRnwdGOsfxtG4aVhVOqLbUm5va8_UW3ks3VCyIQ9zIX5KKNgv6M19ez9kfTCkcWMv4pvBddax8HnxW6VJdBTyaomM5XyyY8cg6wlbloU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/US_2tlFgyOTZ8LSlX5PMvngYITpyNisDLDssZKMfXzSoEqW9ifdkmHYfOo7ZpU4oZtOxZMa_-4b7IthK1e3VLpPe2MNa3p-sWgPvcobRsusAdy77Xfsi4kx8nxer0YXULf6lULB8234=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/US_2tlFgyOTZ8LSlX5PMvngYITpyNisDLDssZKMfXzSoEqW9ifdkmHYfOo7ZpU4oZtOxZMa_-4b7IthK1e3VLpPe2MNa3p-sWgPvcobRsusAdy77Xfsi4kx8nxer0YXULf6lULB8234=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6x9tMxCKhdLoo6VNHPG3jqCAH5q-oq3q1CPW7jtRdDPLWtJsXzqeGG9mkfSwE1pZmLDqTVkNMsRSYkqoPep6blDGVlIM-IALSb1x6d5UumKmEZQXfnkES9wJKCSiC0OAOH-AXHwj9xE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6x9tMxCKhdLoo6VNHPG3jqCAH5q-oq3q1CPW7jtRdDPLWtJsXzqeGG9mkfSwE1pZmLDqTVkNMsRSYkqoPep6blDGVlIM-IALSb1x6d5UumKmEZQXfnkES9wJKCSiC0OAOH-AXHwj9xE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/-YVqVCY3ukff4Bb7X3U2aeijzEBlk381gVRTSgvS0N7u6T6FUlYizbFP9ysUbfpCdWWZbZpxis2dbL5xH6NXWeYKkXOdLrsD9MeL094T-1SPd-DNCsCsMbgFo25AQU_yzljtnm95eA0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/-YVqVCY3ukff4Bb7X3U2aeijzEBlk381gVRTSgvS0N7u6T6FUlYizbFP9ysUbfpCdWWZbZpxis2dbL5xH6NXWeYKkXOdLrsD9MeL094T-1SPd-DNCsCsMbgFo25AQU_yzljtnm95eA0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/hD9eM6Du25LITfrfNIXwkaGdEZOqHKjfTYMsfUla-5xFT7Rn90LhefjVNZht7mB2NvA8zto9x3wxVfC1DWX8hyNdlgyheD_LMQCFYdC4B2HS0cmd-0R1n1BciysLYd9tRlpeOJtCqu0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/hD9eM6Du25LITfrfNIXwkaGdEZOqHKjfTYMsfUla-5xFT7Rn90LhefjVNZht7mB2NvA8zto9x3wxVfC1DWX8hyNdlgyheD_LMQCFYdC4B2HS0cmd-0R1n1BciysLYd9tRlpeOJtCqu0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/N3G3F28m9g4UrwiNGugXO6fBEMqE3Lq6J_9y4-O-kdovSzZ5j35surJEpvE6IMQrz5AQw5ZUP3feLoIf5WszjwvWGgRGnofJwRE4Q1PZz4nE5ZD_hxDO-AelCDYkPH3zwiZofMqDy4A=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/N3G3F28m9g4UrwiNGugXO6fBEMqE3Lq6J_9y4-O-kdovSzZ5j35surJEpvE6IMQrz5AQw5ZUP3feLoIf5WszjwvWGgRGnofJwRE4Q1PZz4nE5ZD_hxDO-AelCDYkPH3zwiZofMqDy4A=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zero kara Hajimeru MnS',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/r3lx_adPlcfHy6NSPt8lL1938h4OqSGD8TSmJS7RTsLl04Ng_yd0SgYbamMGcQDxzk9Wc2wGbNBkQ3WthXQChXaKYzRGiuVCOq9CnPOFGv8mnO3ewqibnSwsBSz-zurj_O0-0DIsrqA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/r3lx_adPlcfHy6NSPt8lL1938h4OqSGD8TSmJS7RTsLl04Ng_yd0SgYbamMGcQDxzk9Wc2wGbNBkQ3WthXQChXaKYzRGiuVCOq9CnPOFGv8mnO3ewqibnSwsBSz-zurj_O0-0DIsrqA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
