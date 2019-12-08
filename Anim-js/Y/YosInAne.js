b0420e879b99537c32c5168910ef385c1ea6b3de
dunia-anime <adityha.on@gmail.com>
2019-12-08 11:31:58 +0800

    Update

---
+++ Anim-js/Y/YosInAne.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/qufULKO3qAjIE1nrzjR-u8wATg3AsSpJQR-U-4464g9Bf6jTDVF5SGm4VoxLb4bx_SxOrx5uLJuapKbRjWKQKarJiI7_qz9N5c_sjL3wBXrUsPp93Slmfr7odu-M5C6hvmWpqaBB9KY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/qufULKO3qAjIE1nrzjR-u8wATg3AsSpJQR-U-4464g9Bf6jTDVF5SGm4VoxLb4bx_SxOrx5uLJuapKbRjWKQKarJiI7_qz9N5c_sjL3wBXrUsPp93Slmfr7odu-M5C6hvmWpqaBB9KY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SyzhIJlMkIf4lOqiA5ET_M8-2h86WmRkdIuryHWwIfyqWEliURwVXGpgxvfErZAPTWSaJ-qkuKMDO3jQV9iCYJVQCS1ki0J0vZESXFFWVcvotL6WQu_VzB5a99MHEl7dDU3Cd7MRxiU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SyzhIJlMkIf4lOqiA5ET_M8-2h86WmRkdIuryHWwIfyqWEliURwVXGpgxvfErZAPTWSaJ-qkuKMDO3jQV9iCYJVQCS1ki0J0vZESXFFWVcvotL6WQu_VzB5a99MHEl7dDU3Cd7MRxiU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nTzflJ-xgJLfkNwKXnzFAG7CqA5xaFB899Ld3DuIvyqDb5QapOIQF6d9yxzc8bbg-5kKtcX-_sMt85pwDKlO-gRCGu33WRe29oyTOOmYwVlFqPLbJSs7gjE_dblOwMFolv9VXxhZCDc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nTzflJ-xgJLfkNwKXnzFAG7CqA5xaFB899Ld3DuIvyqDb5QapOIQF6d9yxzc8bbg-5kKtcX-_sMt85pwDKlO-gRCGu33WRe29oyTOOmYwVlFqPLbJSs7gjE_dblOwMFolv9VXxhZCDc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jzHLFpIR0bnrYZMtPZ9nfdg1K3B8BmXfQpLHzOzpJ98eSjZRMwmz-1xHLeuyG74-QEMmQT2w7sWHEDk2bN3L6yKm86QxkJM6G_DUk6BckrGqL-if9NF72x7C1HDEMd-TuInYVv3V9Fc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jzHLFpIR0bnrYZMtPZ9nfdg1K3B8BmXfQpLHzOzpJ98eSjZRMwmz-1xHLeuyG74-QEMmQT2w7sWHEDk2bN3L6yKm86QxkJM6G_DUk6BckrGqL-if9NF72x7C1HDEMd-TuInYVv3V9Fc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9K_a0WEGQGff5MP4Snj9XTdROPD4o5apke4vTbYkvAYxzUQHBSI-vnZmQ_l_24Dc6VCkdx7tc0MkDEiJzsS14nrTqdzmot93JFWAQhaHzA_MmZc22Q6m2RB6J5kyYI9NogrY17peIEg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9K_a0WEGQGff5MP4Snj9XTdROPD4o5apke4vTbYkvAYxzUQHBSI-vnZmQ_l_24Dc6VCkdx7tc0MkDEiJzsS14nrTqdzmot93JFWAQhaHzA_MmZc22Q6m2RB6J5kyYI9NogrY17peIEg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/8_X26-ySNG1fTbEhdXcGCrtwd4oDpWPP9glKb67BAl9CG3Zrmd5EzbLt_hXlZe2CP23ad1Glp8C1x0eij0_Rtn82oie3EFrYV_NEnrBHjPOiMHFmr5xIXuqz26OMaR8FRXCMfUFYuT8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/8_X26-ySNG1fTbEhdXcGCrtwd4oDpWPP9glKb67BAl9CG3Zrmd5EzbLt_hXlZe2CP23ad1Glp8C1x0eij0_Rtn82oie3EFrYV_NEnrBHjPOiMHFmr5xIXuqz26OMaR8FRXCMfUFYuT8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SbhvabRqMvS3yZfkwocPlSH7b1SfRJmEUcCul8WMTvC8MhqX67Vkt3O-c1DGCz54KI37lAdutWarWXA5K49DVFo6B7S3FRg9TuuTOTbkE53Zu7YE6Lhz36EyoRQriQXGd-J7v07RL3M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SbhvabRqMvS3yZfkwocPlSH7b1SfRJmEUcCul8WMTvC8MhqX67Vkt3O-c1DGCz54KI37lAdutWarWXA5K49DVFo6B7S3FRg9TuuTOTbkE53Zu7YE6Lhz36EyoRQriQXGd-J7v07RL3M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_hIOX2ZJxB1HDvr-YPgE0kVOMIz2OhVPtOQ7Aq6Ibyz3c9D5W4qkSoQLl0froq6d5G2T7UteO2Wwp0-YJNjGf3d9dJo-ds3llUZ4jsgagi5RFEOH9YEW868WKTPRE5j6SboO4WxY8BU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_hIOX2ZJxB1HDvr-YPgE0kVOMIz2OhVPtOQ7Aq6Ibyz3c9D5W4qkSoQLl0froq6d5G2T7UteO2Wwp0-YJNjGf3d9dJo-ds3llUZ4jsgagi5RFEOH9YEW868WKTPRE5j6SboO4WxY8BU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/N2V6FG1s1OWbfCvY3VOUzot7ajFiMQzuOd8dRL2ImgUAlnqamhduNckxQhRSpRAgtCWQw8tD7bjXor_oiCsLMSua9_cyswU6HJ9wlbqc-TT5r_sf_xxrD6tbGLEn6PrMwrdARtT2yZA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/N2V6FG1s1OWbfCvY3VOUzot7ajFiMQzuOd8dRL2ImgUAlnqamhduNckxQhRSpRAgtCWQw8tD7bjXor_oiCsLMSua9_cyswU6HJ9wlbqc-TT5r_sf_xxrD6tbGLEn6PrMwrdARtT2yZA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/w0_LrZc4AfUf6tYO7Ov0KNpS2pANzQeMBx41jO9s4htdKYIFIBFJ7KytSgBRgiTbA2pg-sIG1xz7V9UP8wKDnpCXZdQ-Hf8duIiuIQyi72tC4AFBXX2Rpd-qcAaBG1Zi-t8dKFLghtw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/w0_LrZc4AfUf6tYO7Ov0KNpS2pANzQeMBx41jO9s4htdKYIFIBFJ7KytSgBRgiTbA2pg-sIG1xz7V9UP8wKDnpCXZdQ-Hf8duIiuIQyi72tC4AFBXX2Rpd-qcAaBG1Zi-t8dKFLghtw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Vkc5-YuIszvyLZN3rJ7yNj0QO3jbaEJyJnK9O9Fj6qjEtUr5EwiDFv42L-khLG_QbV7prugTbYjSWX_vL2sdr188Su6meeePfTF1NN2YKHqBMFy24GeO_7bJCUCkjk6tMfeIcb3e7Vo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Vkc5-YuIszvyLZN3rJ7yNj0QO3jbaEJyJnK9O9Fj6qjEtUr5EwiDFv42L-khLG_QbV7prugTbYjSWX_vL2sdr188Su6meeePfTF1NN2YKHqBMFy24GeO_7bJCUCkjk6tMfeIcb3e7Vo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Yosuga no Sora IS WWALA',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.47',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/n8FEWsiN6krbblhn2GNpRZjwnvvzzLumxm_bb7L1yJ55BGQPg3CKOO4ZFXqPNGZ9hQuSd5bDVW3dsJ6dm2iyLmLVa4ecs5InDnPuKUxfzLXG59KId0wtpEHp_YZOPHigfNxjp_oxG9U=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/n8FEWsiN6krbblhn2GNpRZjwnvvzzLumxm_bb7L1yJ55BGQPg3CKOO4ZFXqPNGZ9hQuSd5bDVW3dsJ6dm2iyLmLVa4ecs5InDnPuKUxfzLXG59KId0wtpEHp_YZOPHigfNxjp_oxG9U=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
