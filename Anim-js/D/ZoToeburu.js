d7db1022f8b0eb6dfd74bb68e2204256cced217d
dunia-anime <adityha.on@gmail.com>
2019-12-08 10:34:17 +0800

    Create ZoToeburu.js

---
+++ Anim-js/D/ZoToeburu.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/-WdA4cSGLItfg1H1KjKnyPapPTNFPJwL9SKCNiIwM1Gar071PIq2U9zbrmZFUrv8x3nRaQr10pKGfKbJiWaYJ7pgCKtLvHPUrxCcpa9PrMlvCDtiZMeRACnCtxhXRovGiswtGjqXeqk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/-WdA4cSGLItfg1H1KjKnyPapPTNFPJwL9SKCNiIwM1Gar071PIq2U9zbrmZFUrv8x3nRaQr10pKGfKbJiWaYJ7pgCKtLvHPUrxCcpa9PrMlvCDtiZMeRACnCtxhXRovGiswtGjqXeqk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/vPSJuePtw8Z0EEajCAQLYPhRCC13_dsA6j2avCAef5FQ3JlZX7bU21l7VJ-FqgBUnm2CGlXJWeUy2eXfV8Nk_rC6fjKLr0BvMu8tmSleKdp1AfE7i1x0x4DsC_rwbJ_DkW3CpUCEZkM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/vPSJuePtw8Z0EEajCAQLYPhRCC13_dsA6j2avCAef5FQ3JlZX7bU21l7VJ-FqgBUnm2CGlXJWeUy2eXfV8Nk_rC6fjKLr0BvMu8tmSleKdp1AfE7i1x0x4DsC_rwbJ_DkW3CpUCEZkM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/R3SNeCSieyQr8k2z6qiK_j60gTWN4ZYsYArwdC-OKJY1JrV0vZdRuXSwiAiI6fzMywVBJcl71HU-hk6_6usbgr-jqib0VIFGUMYhXwTUAW0YsPRARYH5G7o9xFXxfgooqq67vO79n5M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/R3SNeCSieyQr8k2z6qiK_j60gTWN4ZYsYArwdC-OKJY1JrV0vZdRuXSwiAiI6fzMywVBJcl71HU-hk6_6usbgr-jqib0VIFGUMYhXwTUAW0YsPRARYH5G7o9xFXxfgooqq67vO79n5M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AxKbr1022okw2tkuQOBCDb24fAQyr3AbWo9aNW23j5EMYivgGQJ6lE8ghCzfGkRdSwNvExlLc151thpJh6aSAQnF7q5TbC6iLfMY_2j-IFs1Vs-DO2ouxTDuuz5CUleKCPL9elmJVpM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AxKbr1022okw2tkuQOBCDb24fAQyr3AbWo9aNW23j5EMYivgGQJ6lE8ghCzfGkRdSwNvExlLc151thpJh6aSAQnF7q5TbC6iLfMY_2j-IFs1Vs-DO2ouxTDuuz5CUleKCPL9elmJVpM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/t95J9P6lj0DG2iyMCgn_eXkTZQSlxW3a9OWNCIWOH-8cFkWUtw3flDKH-f-LGXc1h9WLU83YI-XbNyeCHOSQMzHq206SOusQVx6yhJZPIMYBbA9hKF5CiRLLN1jCQARv1BGLLA0ChEY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/t95J9P6lj0DG2iyMCgn_eXkTZQSlxW3a9OWNCIWOH-8cFkWUtw3flDKH-f-LGXc1h9WLU83YI-XbNyeCHOSQMzHq206SOusQVx6yhJZPIMYBbA9hKF5CiRLLN1jCQARv1BGLLA0ChEY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/8tpqxN8EXQdmct_8Ux42sGJKzelQimu2pPeoRcgWaGEV03FCjxdzsrm-ZxQm4oYHumgm522S74lAYZTgc65HKNVep4g1MufJ91M-T35egQWf_9GQefqcLh0yGUs13mA8rgJ1RM9PlRM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/8tpqxN8EXQdmct_8Ux42sGJKzelQimu2pPeoRcgWaGEV03FCjxdzsrm-ZxQm4oYHumgm522S74lAYZTgc65HKNVep4g1MufJ91M-T35egQWf_9GQefqcLh0yGUs13mA8rgJ1RM9PlRM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WyCQDJ23M4tMvyHJkf4gqcn1i_qj8FItY8CgZxmmCZ2ZmtS9CpZI9BwC0o60PNhrjxWT5Py635mUw66g_ki40I2gfM6o7twtRtTgSTg2Bg2LwXuk75TxgnXFuYJxjQiQala4CC73Iy4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WyCQDJ23M4tMvyHJkf4gqcn1i_qj8FItY8CgZxmmCZ2ZmtS9CpZI9BwC0o60PNhrjxWT5Py635mUw66g_ki40I2gfM6o7twtRtTgSTg2Bg2LwXuk75TxgnXFuYJxjQiQala4CC73Iy4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/97osRjDXQbACakw2BWRpBz0qazQRHkZ0OejTHZ65YrQMf2584R9hQaRc4__4mSDDW14xbSy4JvNP-UN4BohUhgM8o9FNgvMKO97Wl6PjitIHpE_uof-JzkaeAiot2ow6ooYIYnPyNz4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/97osRjDXQbACakw2BWRpBz0qazQRHkZ0OejTHZ65YrQMf2584R9hQaRc4__4mSDDW14xbSy4JvNP-UN4BohUhgM8o9FNgvMKO97Wl6PjitIHpE_uof-JzkaeAiot2ow6ooYIYnPyNz4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6G7dA4-g7b2BKyrnLduFoUVlYVFZjigtzrblaCwjWy3V3ut-wcSPG8PGbMUWZrgYuVU_I5YbZOBPYtOQoEHCMq6zREunLtBWt5NMFc1BzfBiHgXr0rkkf47bv3rFEo7_OkMrH2cOwyI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6G7dA4-g7b2BKyrnLduFoUVlYVFZjigtzrblaCwjWy3V3ut-wcSPG8PGbMUWZrgYuVU_I5YbZOBPYtOQoEHCMq6zREunLtBWt5NMFc1BzfBiHgXr0rkkf47bv3rFEo7_OkMrH2cOwyI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/RgOjFaxlgyJPsgl20qOQM4dBynozF5BVZe6V-nuaetGYA6d7X94Nwk-RARTQ--awG9QN2i6bqI-mrC9C57Pqg0CyyML29ckcK3dBRxQ6hs2Wnysh_hjXduZB9Ttra6s-3qnFeg9mhpg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/RgOjFaxlgyJPsgl20qOQM4dBynozF5BVZe6V-nuaetGYA6d7X94Nwk-RARTQ--awG9QN2i6bqI-mrC9C57Pqg0CyyML29ckcK3dBRxQ6hs2Wnysh_hjXduZB9Ttra6s-3qnFeg9mhpg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/eLaUEBRQz2Wss_4s8jVdf_WPDQ_-K-8IoGqv-TiVZXvzUyJEGmrAC7_xu8N1uhaSB1ybU-7unlrFjFUMNEwkLI8AloTgRi0_ecaByE5a3sTRw64JOqQftEHDmTMRNwq21JtRWVVZ81k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/eLaUEBRQz2Wss_4s8jVdf_WPDQ_-K-8IoGqv-TiVZXvzUyJEGmrAC7_xu8N1uhaSB1ybU-7unlrFjFUMNEwkLI8AloTgRi0_ecaByE5a3sTRw64JOqQftEHDmTMRNwq21JtRWVVZ81k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zoku Touken Ranbu: Hanamaru',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.38',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/DAxrwWiYlzc-3YWiJwhFiuHSzJX6hk2leHyQyTRFS16EjszOv9bkNiQdJ3AFF4V8-dT0jKBwHiC6wsbfsNLdU5hT52FPjL_gT38bGzg_Q3WjfzSOClMFEHe_v9vVQFSRceVCnT67gJU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/DAxrwWiYlzc-3YWiJwhFiuHSzJX6hk2leHyQyTRFS16EjszOv9bkNiQdJ3AFF4V8-dT0jKBwHiC6wsbfsNLdU5hT52FPjL_gT38bGzg_Q3WjfzSOClMFEHe_v9vVQFSRceVCnT67gJU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
