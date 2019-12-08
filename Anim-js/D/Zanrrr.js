96240e2776e8006cb854e485cddb8f5e46268989
dunia-anime <adityha.on@gmail.com>
2019-12-08 10:57:08 +0800

    Create Zanrrr.js

---
+++ Anim-js/D/Zanrrr.js
@@ -0,0 +1,324 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Yql9KRA-f0TsY5IyQpvcmAjxDCX4-XjG6rYpXpHfFRQA6jnyhqhlqnDwz4_JU3ERAvpOEpVfr70gKpUhjnV9KlI9A6OMAn1pHv3Ia7-FO4smoJRjjWJkb7zVZxe5LNFVDarPn3yL7L8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Yql9KRA-f0TsY5IyQpvcmAjxDCX4-XjG6rYpXpHfFRQA6jnyhqhlqnDwz4_JU3ERAvpOEpVfr70gKpUhjnV9KlI9A6OMAn1pHv3Ia7-FO4smoJRjjWJkb7zVZxe5LNFVDarPn3yL7L8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WdE_feDS4nWav6murvV8V0rNgcn3ez64ZPlKb0feC8HGwrjKPAxRnJWdk8zCO8jXk91UysIt1o0ZUQpGd0ewilWbmmOmPD1U0cJc6xYXd_y11N87TBVbZAPk39ZlTYWyU2PGgRp8ERw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WdE_feDS4nWav6murvV8V0rNgcn3ez64ZPlKb0feC8HGwrjKPAxRnJWdk8zCO8jXk91UysIt1o0ZUQpGd0ewilWbmmOmPD1U0cJc6xYXd_y11N87TBVbZAPk39ZlTYWyU2PGgRp8ERw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/QyWOSbmAKf-ESqhgarS5kQwrrjZzZyzt7INiMKRmtVo99W0T5wWcGSjEU5HBYE50K5yOAUseJ2EcklozOQ9Rt1g2OLEgoWguHvLm3i2N0nFOuA67gjs3VyFsXQepovYDtWu7UO3fwug=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/QyWOSbmAKf-ESqhgarS5kQwrrjZzZyzt7INiMKRmtVo99W0T5wWcGSjEU5HBYE50K5yOAUseJ2EcklozOQ9Rt1g2OLEgoWguHvLm3i2N0nFOuA67gjs3VyFsXQepovYDtWu7UO3fwug=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/UA3CjC_UCI7ri5JoTVejR_s2C7y7P1gaaIT2JscK5y1BWgb3NSeTiJT5HbcJ4ZLsBYxLw5iuV-nM5c3yX3PGBMQfICzTNPCDn-Ib7Ch6dazHo_u06kIk-U0uOwMm34NgzfGUJJPHvQ8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/UA3CjC_UCI7ri5JoTVejR_s2C7y7P1gaaIT2JscK5y1BWgb3NSeTiJT5HbcJ4ZLsBYxLw5iuV-nM5c3yX3PGBMQfICzTNPCDn-Ib7Ch6dazHo_u06kIk-U0uOwMm34NgzfGUJJPHvQ8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/OWWZROD1zkv4J0VCdN_JkDoxmCrm7K4_aKHdaR3RQutl_HxVl8BHlOs4_vUdPApMxxrtVtrzL4rT5BlUieA9JPKyvbTVmwRLWKn1KRLK40Aoz9TC3nf5YQy-cUPQgR-AUjdeFRY0pKc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/OWWZROD1zkv4J0VCdN_JkDoxmCrm7K4_aKHdaR3RQutl_HxVl8BHlOs4_vUdPApMxxrtVtrzL4rT5BlUieA9JPKyvbTVmwRLWKn1KRLK40Aoz9TC3nf5YQy-cUPQgR-AUjdeFRY0pKc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6CX1JYrneFeIIY5U-e7SIPU3s36SSgDcZN_rIHg1G5XU41PXJmFGvXaCKkIh0_d1c1oXfrhDGvgzqsWNdQWXzdu09LqlgZkVr7hAMY-dkdob0V4qH_jOM_jRZhlko0OzyE1TALX4kOI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6CX1JYrneFeIIY5U-e7SIPU3s36SSgDcZN_rIHg1G5XU41PXJmFGvXaCKkIh0_d1c1oXfrhDGvgzqsWNdQWXzdu09LqlgZkVr7hAMY-dkdob0V4qH_jOM_jRZhlko0OzyE1TALX4kOI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/2ZMv99zmS351YAsqQUoem7dLALlDKzQY3q7Pd0M7xoDaFRt7VSL0qC_22WHpo6R38OA-s4DXveBYAX2XEmPc5hM_Cv0u1LiGINTKQM49kDJjXdqqqdJ2jeORtKc9N-aKL3Lz_TWiElQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/2ZMv99zmS351YAsqQUoem7dLALlDKzQY3q7Pd0M7xoDaFRt7VSL0qC_22WHpo6R38OA-s4DXveBYAX2XEmPc5hM_Cv0u1LiGINTKQM49kDJjXdqqqdJ2jeORtKc9N-aKL3Lz_TWiElQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/wOMhgWScCvkH1xn4d51ztDQTnO1a7xebU9BeE--wF18Fjl3najvAIVcLRmvTAgUB6lEFrQxg5eUw5zu6rlazbr6CtijUTZZGYghmRgsegN03gAAXR40EdEOzVt5wzusBA1zztVVr1Uw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/wOMhgWScCvkH1xn4d51ztDQTnO1a7xebU9BeE--wF18Fjl3najvAIVcLRmvTAgUB6lEFrQxg5eUw5zu6rlazbr6CtijUTZZGYghmRgsegN03gAAXR40EdEOzVt5wzusBA1zztVVr1Uw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/D46ek9oidoLHircaVXpmk7jU5e4doalQUSS34IQCyWUaF9DUq2IxpjPjlL7iXNspVa4XKRnKkWGgNZli-SkGX-iBkTDNa-J-p6NTjCoSqfsAxjATrYP44S55I5ZfSL8uXz5YAALKs1k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/D46ek9oidoLHircaVXpmk7jU5e4doalQUSS34IQCyWUaF9DUq2IxpjPjlL7iXNspVa4XKRnKkWGgNZli-SkGX-iBkTDNa-J-p6NTjCoSqfsAxjATrYP44S55I5ZfSL8uXz5YAALKs1k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/OfF3xjMKBoIe-YEWbZQhli9z9dPrLi9O6mg04_wcol5kNqJ5mZq4Y-1ZaBcDq7W7B3H1ZS71ot0F-29ZR2JRqwdgcI43s0ZtOlZnp9HnWnuuk4UeI65D4KBE-VFjGf5iVg9sQUJsYbM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/OfF3xjMKBoIe-YEWbZQhli9z9dPrLi9O6mg04_wcol5kNqJ5mZq4Y-1ZaBcDq7W7B3H1ZS71ot0F-29ZR2JRqwdgcI43s0ZtOlZnp9HnWnuuk4UeI65D4KBE-VFjGf5iVg9sQUJsYbM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Zankyou no Terror',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 8.17',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/PLUKW_t80sX2SrtkHb_IEuI3cKX-CZiYSx5_ytL-gNCEIHNceeGeXWSgZS_JKnXCTMrCI3o7agmG8St7kCsVNvzM3fXsdtf1T5XdkSrLvr1r2zp0MClDq3bD8wsPWDmCmzbG-D8VDsU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/PLUKW_t80sX2SrtkHb_IEuI3cKX-CZiYSx5_ytL-gNCEIHNceeGeXWSgZS_JKnXCTMrCI3o7agmG8St7kCsVNvzM3fXsdtf1T5XdkSrLvr1r2zp0MClDq3bD8wsPWDmCmzbG-D8VDsU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
