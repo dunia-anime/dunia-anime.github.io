931e0aba63d766600c9e37b7ead8d1ffd3a5761d
dunia-anime <adityha.on@gmail.com>
2019-12-09 15:33:27 +0800

    Update uIe.js

---
+++ Anim-js/Y/uIe.js
@@ -0,0 +1,436 @@
+var player = videojs('video')
+player.playlist([{
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 01.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/iV8M-AEFIR9IPvdyKc5Pc4FNwaZvE9d0DXdsTu6oadl4t78afCs1i-JccyDSgJNyEgbTVNTaeIyUcbSibNyV2QdTmUNMJnoSzZ-P_HiLkJcz-mofr0iDus5_H62UIjGwY8E3fKd_x5w=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/iV8M-AEFIR9IPvdyKc5Pc4FNwaZvE9d0DXdsTu6oadl4t78afCs1i-JccyDSgJNyEgbTVNTaeIyUcbSibNyV2QdTmUNMJnoSzZ-P_HiLkJcz-mofr0iDus5_H62UIjGwY8E3fKd_x5w=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 02.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/tn-1j8jWoOofDGk6eDAOuoxETQSspWz6q2Ns9NwKKPrhG6Lg_LY3tgArGj-uBje4-OlrlNRCuvHKF5pqiCPXMVl6ZNJ9ynhtq0wucfCaW5RucjIwdtSDMPrvcf9ERpR3bQ510Vs2D2I=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/tn-1j8jWoOofDGk6eDAOuoxETQSspWz6q2Ns9NwKKPrhG6Lg_LY3tgArGj-uBje4-OlrlNRCuvHKF5pqiCPXMVl6ZNJ9ynhtq0wucfCaW5RucjIwdtSDMPrvcf9ERpR3bQ510Vs2D2I=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 03.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6vJMQOxQu4WFTykALII4j9QYeXHdWz3WaK-wvuU198Uk7OFQGZ9cuPqw8XZw-VPzQsNo96REpTVW9dpePyxNmbbd6Gs5W88YmPuM8GkafOm_pNN2niRo4mOaj_3OUhwXb2h5ZU6BT5o=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6vJMQOxQu4WFTykALII4j9QYeXHdWz3WaK-wvuU198Uk7OFQGZ9cuPqw8XZw-VPzQsNo96REpTVW9dpePyxNmbbd6Gs5W88YmPuM8GkafOm_pNN2niRo4mOaj_3OUhwXb2h5ZU6BT5o=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 04.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/sTS-7sGfKbx-_jhUtdMq2LzBQ8nbEXmoahjVfEt6cviX7mrnaz0jUq-2ZEUL9FYuHAHU9y057m8jpnF-rlkK4eRap7e1NGUeOU7fwFFVG6MwW9yxngCp4OSpIyc7Gz2ufvW_F_H2tvk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/sTS-7sGfKbx-_jhUtdMq2LzBQ8nbEXmoahjVfEt6cviX7mrnaz0jUq-2ZEUL9FYuHAHU9y057m8jpnF-rlkK4eRap7e1NGUeOU7fwFFVG6MwW9yxngCp4OSpIyc7Gz2ufvW_F_H2tvk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 05.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/1J6chW7ivFmA4qRXHT63bJFVz6IbfgF5fIdGQkokpGKFHGXIjeBgjg2JEYokDKAzZ1cGtqPJ3CICaoQ7m5aXFIMAewGE3-aatH7nDYoTlfu2Pb1D-M6G3J44B65_36EHmQ2tow1eIfw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/1J6chW7ivFmA4qRXHT63bJFVz6IbfgF5fIdGQkokpGKFHGXIjeBgjg2JEYokDKAzZ1cGtqPJ3CICaoQ7m5aXFIMAewGE3-aatH7nDYoTlfu2Pb1D-M6G3J44B65_36EHmQ2tow1eIfw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 06.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/mmOL6z3ma22f3Vt9StWcPXV1k2zyUiyF6DWFT3pDi660FhmbnuMbYO9_cEZUr8SfdI5cnbjCN2X7yPXxZrLDbbqk_eX1mWcQibCHU4121fQYFq0IkFGHR2km3SAL6Rbuqb5G4f1bvRc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/mmOL6z3ma22f3Vt9StWcPXV1k2zyUiyF6DWFT3pDi660FhmbnuMbYO9_cEZUr8SfdI5cnbjCN2X7yPXxZrLDbbqk_eX1mWcQibCHU4121fQYFq0IkFGHR2km3SAL6Rbuqb5G4f1bvRc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 07.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/MkqJ_wUzRq12FZJZtFIJwUc_fIxYHD37Ah09v8Rxn5iDHwqJHbP1e2JA01jE8sbc0wuBkJHmjC2pA4rSUikKihm6vP3mZNSQ2N1_bMVuYsPSkXbjDlqajM6DrdIU-YqUFkkXjfI1bG4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/MkqJ_wUzRq12FZJZtFIJwUc_fIxYHD37Ah09v8Rxn5iDHwqJHbP1e2JA01jE8sbc0wuBkJHmjC2pA4rSUikKihm6vP3mZNSQ2N1_bMVuYsPSkXbjDlqajM6DrdIU-YqUFkkXjfI1bG4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 08.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/_7JeL2TbjwfWPHcar7S-Uw-fVAspijhdyrLz_6zl_w0qQAwxITnh8J5PP0QL0ogiaFFyQccPbwdA_QWinrmVLDTmdi8IFAAm5ytFCz1xRK0SHTpOjXhbFV6r8suCt7DNM0xvdhCjdzs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/_7JeL2TbjwfWPHcar7S-Uw-fVAspijhdyrLz_6zl_w0qQAwxITnh8J5PP0QL0ogiaFFyQccPbwdA_QWinrmVLDTmdi8IFAAm5ytFCz1xRK0SHTpOjXhbFV6r8suCt7DNM0xvdhCjdzs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 09.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3IxEYH0qo1TqSWxc8zRJvRj4_p6P1OXysoPVJul6CT83xQYp-tXwBkSNg4khC0-h70OBMoLqAzDiVM4Fbo2_njcy39T3ALZwteeAQNv7-bilQ2YfmepVLUymCw4H3yhO8scyRRzAqkE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3IxEYH0qo1TqSWxc8zRJvRj4_p6P1OXysoPVJul6CT83xQYp-tXwBkSNg4khC0-h70OBMoLqAzDiVM4Fbo2_njcy39T3ALZwteeAQNv7-bilQ2YfmepVLUymCw4H3yhO8scyRRzAqkE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 10.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/EYrVKku684LV4panrBmGwfCkbtr8njN9P9-UTrT9d7kGfIipxiqpore4qGIMbRlcRfuR_HtoMvIdF1o3R8M25Wevb9cZjXHCeUX6R1X5mBaAQXKn6XhICHDu8X-QKE6wbBtdCtUzmNc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/EYrVKku684LV4panrBmGwfCkbtr8njN9P9-UTrT9d7kGfIipxiqpore4qGIMbRlcRfuR_HtoMvIdF1o3R8M25Wevb9cZjXHCeUX6R1X5mBaAQXKn6XhICHDu8X-QKE6wbBtdCtUzmNc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 11.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/xBp-ZAOmsodcKCKcLoIgyk5EKD8pck-qRyEGF6x8Xm6eDIcWOpyGRcDIrm6DJ7Qg1NnyIi1G7LOyYBrBzxVVC63meGKRFJo0iG-9MySCHBvRl-8CaxZyWgLdGbfWcDsws9u0fOGt-ac=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/xBp-ZAOmsodcKCKcLoIgyk5EKD8pck-qRyEGF6x8Xm6eDIcWOpyGRcDIrm6DJ7Qg1NnyIi1G7LOyYBrBzxVVC63meGKRFJo0iG-9MySCHBvRl-8CaxZyWgLdGbfWcDsws9u0fOGt-ac=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Y/Yuri!!!%20on%20Ice/%5Bdunia-anime.id%5D Yuri!!! on Ice - 12.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Yuri!!! on Ice',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 8.08',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/YHkM6GkIKJ4PFU84brewcnj0hHS5OPOF2YzNEr4sP0o7JobQr0pyPsMUXt865V5Y1QjewMRLhA77KWsY1d3Gi7Nmhu4xF9Grp5d583PIiXt-bDT8j3d-CDmq-oHTk-igmto8JAB92PU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/YHkM6GkIKJ4PFU84brewcnj0hHS5OPOF2YzNEr4sP0o7JobQr0pyPsMUXt865V5Y1QjewMRLhA77KWsY1d3Gi7Nmhu4xF9Grp5d583PIiXt-bDT8j3d-CDmq-oHTk-igmto8JAB92PU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
