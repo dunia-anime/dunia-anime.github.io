55e2bbc56fe25f8663b423f830309612ad986e73
dunia-anime <adityha.on@gmail.com>
2019-12-09 15:21:15 +0800

    Create ZXIion.js

---
+++ Anim-js/Z/ZXIion.js
@@ -0,0 +1,436 @@
+var player = videojs('video')
+player.playlist([{
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 01.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3LVepbuuRcqvxyFmI-7WKOkmHCUuEJRmApDdfZYFzYv9gK-Qfj6x2bje59IuMeOq_YnVdwUA98-iKnmZkRENBIx_n6oovj3MnWhGJfaGHy5zPfEHLB0VCD6YGiHMoCPTnAfwyc0ln8w=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3LVepbuuRcqvxyFmI-7WKOkmHCUuEJRmApDdfZYFzYv9gK-Qfj6x2bje59IuMeOq_YnVdwUA98-iKnmZkRENBIx_n6oovj3MnWhGJfaGHy5zPfEHLB0VCD6YGiHMoCPTnAfwyc0ln8w=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 02.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SYdyXJ1bw_F5AH5tMq0dKpO954PcrxsXPAI29t8AJFrP1WAzp3Lk2GlijXahaMUcA36nZSgDggW_siSehglq4u1YqVPhkHXyfIIg-5BgAoqEOg-1vQzxk4GF-PlRbTnISXKk2DOHwmA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SYdyXJ1bw_F5AH5tMq0dKpO954PcrxsXPAI29t8AJFrP1WAzp3Lk2GlijXahaMUcA36nZSgDggW_siSehglq4u1YqVPhkHXyfIIg-5BgAoqEOg-1vQzxk4GF-PlRbTnISXKk2DOHwmA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 03.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/I20pvUalT0GeaqLVTTIeE8BZv9cl5ED2MMJXxnEK3sIN6Wb7jqhFBwomEj9zWiV5QVg1u_ONePXcLZTsO8DTv4XzKfSHsVSm1rjlb0s8PeGR-kGHMe6WRq-X5uDXt3X0mrSDtPlvHlE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/I20pvUalT0GeaqLVTTIeE8BZv9cl5ED2MMJXxnEK3sIN6Wb7jqhFBwomEj9zWiV5QVg1u_ONePXcLZTsO8DTv4XzKfSHsVSm1rjlb0s8PeGR-kGHMe6WRq-X5uDXt3X0mrSDtPlvHlE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 04.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/gtNDG4gL7F2DKJzbONQ706pthqT47OTHKHkhY5cGgbdzCYQ5-lm9Rxa2npSFgM_goUefxuoVcL2cWkH37oEBg2cV47MOJH395FWRxc-nfyq2Rvbz9TzjOdxOjoWoTxWE7cQJWuKlubA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/gtNDG4gL7F2DKJzbONQ706pthqT47OTHKHkhY5cGgbdzCYQ5-lm9Rxa2npSFgM_goUefxuoVcL2cWkH37oEBg2cV47MOJH395FWRxc-nfyq2Rvbz9TzjOdxOjoWoTxWE7cQJWuKlubA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 05.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/G3RMHK65c5zkXOs40OwaeTvDQz4HqR0JzZ1D5-M_HacLBpe7o3jFxCq2_jLJtNbECtWxuk8j-MuTCpr_aRIsAab_eU9zI5QzJDVhjqSUIxHkw6SkijgrE6ti4wSc2Fe0gjV8Xgwo958=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/G3RMHK65c5zkXOs40OwaeTvDQz4HqR0JzZ1D5-M_HacLBpe7o3jFxCq2_jLJtNbECtWxuk8j-MuTCpr_aRIsAab_eU9zI5QzJDVhjqSUIxHkw6SkijgrE6ti4wSc2Fe0gjV8Xgwo958=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 06.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ViQDGsvh_ztXletMzUQlarzYfzi7ey7SFdIoGMtgzpqobp5eknQDvCYRG6JcNAZjGTAWsljLLK0ifGYDwRz_fuE1wmngxS0XqndEAcXkccWcF_OXqfrOZErZFx-HrzTCOHhpSEmheuE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ViQDGsvh_ztXletMzUQlarzYfzi7ey7SFdIoGMtgzpqobp5eknQDvCYRG6JcNAZjGTAWsljLLK0ifGYDwRz_fuE1wmngxS0XqndEAcXkccWcF_OXqfrOZErZFx-HrzTCOHhpSEmheuE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 07.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/h0At6xbiANBCrnrNRt6RmCR37OvkEvSimbB0-3g4vKbcDnelkWNbfy5ZtuRwA8wiCDN0aH1NCyLiYNI6AcXZGjB5xvKVFQn3OETuMtbwlQ-hMycSq3x4B-FOkDDFKnUOhEJeSN2UwaA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/h0At6xbiANBCrnrNRt6RmCR37OvkEvSimbB0-3g4vKbcDnelkWNbfy5ZtuRwA8wiCDN0aH1NCyLiYNI6AcXZGjB5xvKVFQn3OETuMtbwlQ-hMycSq3x4B-FOkDDFKnUOhEJeSN2UwaA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 08.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/xVz4ovsCLaJteN94m7G_U9FirvL2r7he9WbUc1-yOlBHYXnZdPWH10ZDAHSqEe-YAd8H6_as_eK5ioaiWDc8SZAFEsWe84UGl-II_Yq-iLOBLfFJgY9vEWExkgwlgp0TsavVQtI5gFY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/xVz4ovsCLaJteN94m7G_U9FirvL2r7he9WbUc1-yOlBHYXnZdPWH10ZDAHSqEe-YAd8H6_as_eK5ioaiWDc8SZAFEsWe84UGl-II_Yq-iLOBLfFJgY9vEWExkgwlgp0TsavVQtI5gFY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 09.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/IN7IoKpOFFQUTt3DlKFkTzjC930cAXZRysS4UakVfBXsEGqhO0SK-vbW67Bx5sAewHxf07DkzhIMaBRhCBW7jC7F3-O6MyKxOyOJCEgy4nd9ZtDHrvohbzH3Zjt2ClQ_MawKbcXGKMM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/IN7IoKpOFFQUTt3DlKFkTzjC930cAXZRysS4UakVfBXsEGqhO0SK-vbW67Bx5sAewHxf07DkzhIMaBRhCBW7jC7F3-O6MyKxOyOJCEgy4nd9ZtDHrvohbzH3Zjt2ClQ_MawKbcXGKMM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 10.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WIPCkAuR5JdYcDd18WtpqJKz5i8USTwCMOH_8V8a3Z72MsHitv9fjI2saC4sSlXL80DqoP-91K3wxhSlybkpiRF39HZ4B8nvMMB8ucXKzA_rALluLX-0JZ0Ubxb8BslgW0xNStoEaA4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WIPCkAuR5JdYcDd18WtpqJKz5i8USTwCMOH_8V8a3Z72MsHitv9fjI2saC4sSlXL80DqoP-91K3wxhSlybkpiRF39HZ4B8nvMMB8ucXKzA_rALluLX-0JZ0Ubxb8BslgW0xNStoEaA4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 11.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jIP5juLDFr_CCpcTz0J6cO03qHBxAEt5fNJMtzTyetDcYRo4ARSMY59mDj-N5fBlccAjKtSlalsnTHwFhjv6FXGnOxDgJkcbbraYEmE5-WFM2j-RgeZPH0cg9mCR_wzTmhrvd5wGZn4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jIP5juLDFr_CCpcTz0J6cO03qHBxAEt5fNJMtzTyetDcYRo4ARSMY59mDj-N5fBlccAjKtSlalsnTHwFhjv6FXGnOxDgJkcbbraYEmE5-WFM2j-RgeZPH0cg9mCR_wzTmhrvd5wGZn4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        textTracks: [{
+            src: 'https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/Z/ZX%20Ignition/%5Bdunia-anime.id%5D ZX Ignition - 12.vtt',
+            kind: 'captions',
+            srclang: 'id',
+            label: 'Indonesia',
+            'default': true
+        }],
+        judul: '* Z/X: Ignition',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/a5O0emD9L2vrIc5OKTYMKPXyLlKO51RprJpBSB9iTdD1FAHRfUP9aA2NrqaEQxkYVIkWs8UHmnLN03W5OLwBU0hMvaMfaAZA_2IEwTsjX2IQRdyRprdevPhkaHuDQVSGogmMWhdMI4c=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/a5O0emD9L2vrIc5OKTYMKPXyLlKO51RprJpBSB9iTdD1FAHRfUP9aA2NrqaEQxkYVIkWs8UHmnLN03W5OLwBU0hMvaMfaAZA_2IEwTsjX2IQRdyRprdevPhkaHuDQVSGogmMWhdMI4c=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
