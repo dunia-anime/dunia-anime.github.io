7920afc48552ec34414b94e144e6ae6440d416fd
dunia-anime <adityha.on@gmail.com>
2019-12-09 19:36:06 +0800

    Create WagDKaui.js

---
+++ Anim-js/W/WagDKaui.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* WataMote',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WQIbMajjHZ7Ep04GxcjMWlMQd-7BMp-nZJ96b7cvydOkjwwhoUPK2iDol1ozuhNGj-Jb-voNTtuG5DR8o3cDvaunlwIr-6SAYH0UhnetTeW622AVAJW7KUSvNHX0uNvWAMwCdWfwBRo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WQIbMajjHZ7Ep04GxcjMWlMQd-7BMp-nZJ96b7cvydOkjwwhoUPK2iDol1ozuhNGj-Jb-voNTtuG5DR8o3cDvaunlwIr-6SAYH0UhnetTeW622AVAJW7KUSvNHX0uNvWAMwCdWfwBRo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/zZV7HDhQQ6AfPCp8H_UY5kq_o-W3prakVdqt4SRf1jn-6hy2klLW_5ywIRZGqS2u-rmSbdx74wqbG1iPPurp4JlTvSGIW5BIGr-X6jDQbuOdoAIqw2xZaxUJKKTrKrbsDDI8fVIjeM8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/zZV7HDhQQ6AfPCp8H_UY5kq_o-W3prakVdqt4SRf1jn-6hy2klLW_5ywIRZGqS2u-rmSbdx74wqbG1iPPurp4JlTvSGIW5BIGr-X6jDQbuOdoAIqw2xZaxUJKKTrKrbsDDI8fVIjeM8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/B8hq2VI4DVtiQ3vudacynQGFgmAaIwlv70hG_RDU42ZfIm8sDlRQsHGsTBlzSGAJZF2GU-4KDiKmFZAVSVkj3lCxhgCoXypThv6KQcg9w3JwufCqW6iNm7BD9ILVVcWZxR-VNbDQe6g=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/B8hq2VI4DVtiQ3vudacynQGFgmAaIwlv70hG_RDU42ZfIm8sDlRQsHGsTBlzSGAJZF2GU-4KDiKmFZAVSVkj3lCxhgCoXypThv6KQcg9w3JwufCqW6iNm7BD9ILVVcWZxR-VNbDQe6g=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/bYGvIbA4Udao9E_Qri22n7NSGQQk_vyULAXXTKMPFBc4cWAgdtWVoZd-ALznMU7oqnCKdE6Dy9rRDHE0Enl8QOHi5vHzWNjLb8HPSK1jv_Cy9-zPTrmAuYaUSMHD1iUpz-x6joyfWJ4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/bYGvIbA4Udao9E_Qri22n7NSGQQk_vyULAXXTKMPFBc4cWAgdtWVoZd-ALznMU7oqnCKdE6Dy9rRDHE0Enl8QOHi5vHzWNjLb8HPSK1jv_Cy9-zPTrmAuYaUSMHD1iUpz-x6joyfWJ4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WQaScU6xm8Kt-WlIU_VtBx1g67YN7LbcquczR8pPXtmAIXjOXm0m3LX6_dJ09PK29Oc0eLQYOeFCKWVFVLdkcLk1Cerm9SAriLi4n4rDpcAtAjRfGvlIAhZFZRmrOY-6_0ctN3-ZlxE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WQaScU6xm8Kt-WlIU_VtBx1g67YN7LbcquczR8pPXtmAIXjOXm0m3LX6_dJ09PK29Oc0eLQYOeFCKWVFVLdkcLk1Cerm9SAriLi4n4rDpcAtAjRfGvlIAhZFZRmrOY-6_0ctN3-ZlxE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9YJ7bfzPZcLPy6BbKVkPgaWX3J3E8hpav83SitFtNuAHot0aXsn595DOER1Yj5GsLvAzqCamwIj9CwYIrs1ltm9GiS5lwa54zgPd80dqV2ynFmtnhnUfRDHvNlQTpDhyfxFfCMCVzmA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9YJ7bfzPZcLPy6BbKVkPgaWX3J3E8hpav83SitFtNuAHot0aXsn595DOER1Yj5GsLvAzqCamwIj9CwYIrs1ltm9GiS5lwa54zgPd80dqV2ynFmtnhnUfRDHvNlQTpDhyfxFfCMCVzmA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/U5YgzBLQNB3Ty04mSB4haCzX98h6AsBdPr2cyZV2XhleHwUfLIHqXOC8vQkIxShcltD4cgK3WWL34WAGqmfK_YprsYFPL4QFu0kuDvPBb1_JfN9L1Xd4YpTXK-hzKzmhrS6rDblXKHQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/U5YgzBLQNB3Ty04mSB4haCzX98h6AsBdPr2cyZV2XhleHwUfLIHqXOC8vQkIxShcltD4cgK3WWL34WAGqmfK_YprsYFPL4QFu0kuDvPBb1_JfN9L1Xd4YpTXK-hzKzmhrS6rDblXKHQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/pQTrD7CCdusatNLxymcsf5EvTL387haECzOFmf3UOD1UCABezopeCFyEesam2szFpFnezMZFyt640jsFRt5B9EaNeyxxdfGWwgw0klGq1AQXEh2jhlivf4au4sskestQeSIDFA56N3k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/pQTrD7CCdusatNLxymcsf5EvTL387haECzOFmf3UOD1UCABezopeCFyEesam2szFpFnezMZFyt640jsFRt5B9EaNeyxxdfGWwgw0klGq1AQXEh2jhlivf4au4sskestQeSIDFA56N3k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/yVMD3bnBctRibCjcCXZdMIdIdme1-y6VeTcf4LWsiN1FREZOPjirktxlmpJpH4Acve3q908O1jVnFYxUMFijM4m6IkbQJvTcgk8OA_aCtm7UfNXKmNfhuXX0zv_TO6mo2uVWzBZkabI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/yVMD3bnBctRibCjcCXZdMIdIdme1-y6VeTcf4LWsiN1FREZOPjirktxlmpJpH4Acve3q908O1jVnFYxUMFijM4m6IkbQJvTcgk8OA_aCtm7UfNXKmNfhuXX0zv_TO6mo2uVWzBZkabI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/kZIJ0xzILNjgRW4jcX37-l9HFOHHjH3LUT5IVU2o_UNw-SjcH-TVbT7Qk6UcFawBI7VoBpy-ngr_RKroVweOa0Y9Niol5q5fqJN-RbR8yh5lIyzVco4rwwWnHRpVrlI1XKkimiBBB4U=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/kZIJ0xzILNjgRW4jcX37-l9HFOHHjH3LUT5IVU2o_UNw-SjcH-TVbT7Qk6UcFawBI7VoBpy-ngr_RKroVweOa0Y9Niol5q5fqJN-RbR8yh5lIyzVco4rwwWnHRpVrlI1XKkimiBBB4U=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jUZoZKw4-JvQR50HqHHW1VAz-MjOcOPYfhnkSNEVU0JB6pmgYJT-i75L4D4IfF_mPBmjtohJ9oN8iHMnnXaFI6E5n-q4vcrxT6YrVeY6ViOE1zP_-rjAU1TD8uflI1b5tpz2jb8gYvw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jUZoZKw4-JvQR50HqHHW1VAz-MjOcOPYfhnkSNEVU0JB6pmgYJT-i75L4D4IfF_mPBmjtohJ9oN8iHMnnXaFI6E5n-q4vcrxT6YrVeY6ViOE1zP_-rjAU1TD8uflI1b5tpz2jb8gYvw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/gKhxuU4iuXeppIVMK73AGFLMnWhvLPi9w7e1mBNDoxmvIKD1r5fO9gbwc_nZod7QuLhLdRVPpSQVTQVBMft1O2739lBrDt40tJLbIXaiFGI6DHKhWKyR9CTny89btlIaLHvkg5LSGLE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/gKhxuU4iuXeppIVMK73AGFLMnWhvLPi9w7e1mBNDoxmvIKD1r5fO9gbwc_nZod7QuLhLdRVPpSQVTQVBMft1O2739lBrDt40tJLbIXaiFGI6DHKhWKyR9CTny89btlIaLHvkg5LSGLE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* WataMote',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.18',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/FUJLIz9p_-qoziHPgg2gi19-qslYONm80sTre1273tXKExNXIdf3Ucpe32nyhJ9mwob8KhIlNCLXiEmedy7Ynb_h-i5oIvr6N5BdYLdZS8omsQpki4RuOO4eSYZkmuqPCapq3SrIKGE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/FUJLIz9p_-qoziHPgg2gi19-qslYONm80sTre1273tXKExNXIdf3Ucpe32nyhJ9mwob8KhIlNCLXiEmedy7Ynb_h-i5oIvr6N5BdYLdZS8omsQpki4RuOO4eSYZkmuqPCapq3SrIKGE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
