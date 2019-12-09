682b524823f8f3669db24ec717cadc8358d5ceed
dunia-anime <adityha.on@gmail.com>
2019-12-09 19:30:42 +0800

    Create Whlm2.js

---
+++ Anim-js/W/Whlm2.js
@@ -0,0 +1,289 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* White Album 2',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/AbCo099iDT4RxRzRZ6EqmfCzlNVYEB4XxD5P_MuIEZQiQevI_bt9AkmfAlHn4WWJU4hi7u1MM1nnTKEgmK8GnhvsjH66-SNbVSKpwWjkiTRGmEla6YW4NPqbvhM9ubRytoUXynfSVzk=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/IlKePI35BQXUcSuRkOvJDaVDnM2AKu2Vr7T7ltFK7LYPrs-0PeWik5PVustmJzpCwBbMjPwYdv313IUh-_gHoUZNUgJCV5G32QAx_lgaoge_IsFcB7LnawpZpuFYvKbcN6dq5NzvgeA=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/cfZkcUA6BRsd7xRpQUD8ibVYyTkV87zMi7IiVJLF8_V2aV8SL6pbKT6znQ1Uu1EtxMEQcLKKCKa7Ea5Rwg4_7iE9FxdzenxLSWkuSmVrF0eFyigBWD6o_YUBCJlas_8sNw0m_SK12aI=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/sjcujPMJncoahM5sBldpF82B_t-Q-2mQSmZo1IwBHbX0--EObjsEknhrgQnszWslQdTkOjpr4ir36nVBr3Us9V088ASWd4dG0XCoKD5ks917k7LAWv0x3Znj3ojvKUKv7V-xDVBkQU0=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/MP3zuXc23U3e7SDHzN5dVqJx0Wi76w4hu60mkQk-6MzGlXu96AQku7w9WRYCctzJcsMLHzFLC4DD3ujECGvvx4Xj369JjTik2dKxHmrb9uvnmSPv9cXsgboEMYjiSAlv8jbW4jRZySA=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/c0bf5JbwAIFhO3g0xCUQuAnlY26shhQIGcBooLsAtciU9ELfqfIXI8AzLrvAFQSzBxROPZmUKYHnC0iuHhQqQb7qE1Min1nmlieuymJORtDZh44TnBqE_IdCWtyfJyOukGo8iQIcDIo=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/zQlmmyhJ_5Lnmj5JqS4I_HgLMgCYCFWnHxXmclqKCIQ42VYDPH_U5VRoxD-hCZvV1DU98nG4uYsmTMClzC179XBqIS-ANdQN1CAdti4L9CPJuDzIuu2wCt1qktjgR9zUkFTxDfsgCCU=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/NM02BoSIYg-Px3zQs1rhBEkowWsyazdz5EXu5oO9vGoGYZDDY_TKKs1Ru5ML8xkogX92FLwSUCmmts1q-CB9XutJMdh-snm73ic6E9crd2ThMdTKeihNWA1h8kn_e4A7-oayeKjK5iw=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/pOdf31TbPqBJXtfVTMQxFbEFbBY3B-kNqe8q4WaNfFDQBbYKQ8cUtnQFKsS6EyjD00SvOSR3mjvcFKtdhjEM6RqMjYBujPOoCjJWE9iJXVEASpEVSm3vTcWElLJ3rW8bk5e8fE_70VY=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/HRDhLd1Llo-Z8aZ0LleXuhPa3qYb_zISf2tx7VZzWdDMPVucSKse29otWxQFLvHs1m_fUSJZUuQtAK0nWeNtW54sUEQ6So-28f2RkzXB7S2oP5uUW5KSNvZjeZplz92CMzNhuq_tv00=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/n9_m9rsX5l1kqLbnJzIiBMm2tKesuV7aM8ZKUa6E6ONvl67X_gOL7UMRxtiGXLVPqADJTPf8OhKkIzZb6LQQOUPaLa0xFl5N8TU-G0-V0XliwTijN1p50hbAH5QARIoGUKlr_58bZ-k=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/3y4ea8LcUkQHXIKbRoyCzldzd3hZJEygPrw9OLo5fKHOA3g7__fbwbPSHh2MYoDy0pMCmOHKOFFp5kRE7Cs_EVMschQ0-q2xC7BzwoYKVE7Y4DXrIaxGlmq2SSVpdvhP1qdbzj6FKGM=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* White Album 2',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+            src: 'https://lh3.googleusercontent.com/sRP6lJ9WZgTjqtW70MrA9K-H9jNUQPZjHuSRckFIR1H3DxPmVULiOAY_XIOylAHRmG3StFRuRyZ_NtHbukHAnPcJblimF-8tOCP8ph0vQBB4rq4Ir5HgDYVnT0ode-UqcoRcpSxisR8=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
