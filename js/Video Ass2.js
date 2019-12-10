c7e41a2b858c71dd4b4361f3725a9ea71d8b74ca
dunia-anime <adityha.on@gmail.com>
2019-12-10 20:44:29 +0800

    Update Video Ass2.js

--- js/Video Ass2.js
+++ js/Video Ass2.js
@@ -1,19 +1,45 @@
-var imported1 = document.createElement('script');
-imported.src = 'VideoAss/video.js';
-document.head.appendChild(imported);
+videojs('player1', {
+    controls: true,
+    nativeControlsForTouch: false,
+    width: 640,
+    height: 360,
+    fluid: true,
+    plugins: {
+        ass: {
+            'src': ["https://raw.githubusercontent.com/dunia-anime/Sub-Anim/master/zTest/jiraiya0.ass"],
+            'delay': -0.1,
+        }
+    },
+    techOrder: ["html5"],
+    sources: [{
+            src: 'https://lh3.googleusercontent.com/hClKwg0oYofI3uR6rjV-8Abkov14E6vwOpsvzAJ-xauRfla5gxv3IDMeAX23U4EIrk-mdP2ecnW2CP2LOviTtYPqteuUGdzr3JiB-ik-Ph6a3qcOGfQeyCrGtQ2F31wWVsMi76yAwD8=m37',
+            type: 'video/mp4',
+            label: '1080p',
+            selected: 'false'
+        },
+        {
+            src: 'https://lh3.googleusercontent.com/hClKwg0oYofI3uR6rjV-8Abkov14E6vwOpsvzAJ-xauRfla5gxv3IDMeAX23U4EIrk-mdP2ecnW2CP2LOviTtYPqteuUGdzr3JiB-ik-Ph6a3qcOGfQeyCrGtQ2F31wWVsMi76yAwD8=m22',
+            type: 'video/mp4',
+            label: '720p',
+            selected: 'false'
+        },
+        {
+            src: 'https://lh3.googleusercontent.com/hClKwg0oYofI3uR6rjV-8Abkov14E6vwOpsvzAJ-xauRfla5gxv3IDMeAX23U4EIrk-mdP2ecnW2CP2LOviTtYPqteuUGdzr3JiB-ik-Ph6a3qcOGfQeyCrGtQ2F31wWVsMi76yAwD8=m18',
+            type: 'video/mp4',
+            label: '480p',
+            selected: 'true'
+        }
+    ]
+}, );
 
-var imported2 = document.createElement('script');
-imported.src = 'VideoAss/videojs-resolution-switcher.js';
-document.head.appendChild(imported);
+videojs("player1", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
 
-var imported3 = document.createElement('script');
-imported.src = 'VideoAss/libjass.js';
-document.head.appendChild(imported);
+videojs('player1').ready(function () {
+    this.hotkeys({
 
-var imported4 = document.createElement('script');
-imported.src = 'VideoAss/videojs.ass.js';
-document.head.appendChild(imported);
-
-var imported5 = document.createElement('script');
-imported.src = 'VideoAss/videojs-landscape-fullscreen.js';
-document.head.appendChild(imported);
+    });
+});
