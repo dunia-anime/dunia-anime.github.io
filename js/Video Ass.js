3288f28ceb35b64845021526b0098e5288a56f33
dunia-anime <adityha.on@gmail.com>
2019-12-10 17:15:59 +0800

    Update

---
+++ js/Video Ass.js
@@ -0,0 +1,19 @@
+var imported1 = document.createElement('script');
+imported.src = 'VideoAss/video.js';
+document.head.appendChild(imported);
+
+var imported2 = document.createElement('script');
+imported.src = 'VideoAss/videojs-resolution-switcher.js';
+document.head.appendChild(imported);
+
+var imported3 = document.createElement('script');
+imported.src = 'VideoAss/libjass.js';
+document.head.appendChild(imported);
+
+var imported4 = document.createElement('script');
+imported.src = 'VideoAss/videojs.ass.js';
+document.head.appendChild(imported);
+
+var imported5 = document.createElement('script');
+imported.src = 'VideoAss/videojs-landscape-fullscreen.js';
+document.head.appendChild(imported);
