8f5edd85a1040840e2a22974b9a892534bc94c8d
dunia-anime <adityha.on@gmail.com>
2019-12-10 17:27:46 +0800

    Update Video Ass.js

--- js/Video Ass.js
+++ js/Video Ass.js
@@ -1,5 +1,19 @@
-document.writeln("<script type='text/javascript' src='VideoAss/video.js'></script>");
-document.writeln("<script type='text/javascript' src='VideoAss/videojs-resolution-switcher.js'></script>");
-document.writeln("<script type='text/javascript' src='VideoAss/libjass.js'></script>");
-document.writeln("<script type='text/javascript' src='VideoAss/videojs.ass.js'></script>");
-document.writeln("<script type='text/javascript' src='VideoAss/videojs-landscape-fullscreen.js'></script>");
+var imported1 = document.createElement('script');
+imported.src = 'VideoAss/video.js';
+document.head.appendChild(imported);
+
+var imported2 = document.createElement('script');
+imported.src = 'VideoAss/videojs-resolution-switcher.js';
+document.head.appendChild(imported);
+
+var imported3 = document.createElement('script');
+imported.src = 'VideoAss/libjass.js';
+document.head.appendChild(imported);
+
+var imported4 = document.createElement('script');
+imported.src = 'VideoAss/videojs.ass.js';
+document.head.appendChild(imported);
+
+var imported5 = document.createElement('script');
+imported.src = 'VideoAss/videojs-landscape-fullscreen.js';
+document.head.appendChild(imported);
