61de9f7d40242aebb5c17b2418204e673165218e
dunia-anime <adityha.on@gmail.com>
2019-12-11 13:41:41 +0800

    Create ToEP.js

---
+++ Anim-js/T/ToEP.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Tokyo ESP',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/roiX0-y1djKHnrrFpAHgxM1B6uz-nJOk5HAgxYEnF5MJRx5kms85_8J3CAb4GwcHEgwscWxmgXLwCZwBvQmmQN-vbj0lucN3Rh1LRs5j8R-J9Ed7ABMWndvRkj6kP8LsYIv71Yp9sfA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/roiX0-y1djKHnrrFpAHgxM1B6uz-nJOk5HAgxYEnF5MJRx5kms85_8J3CAb4GwcHEgwscWxmgXLwCZwBvQmmQN-vbj0lucN3Rh1LRs5j8R-J9Ed7ABMWndvRkj6kP8LsYIv71Yp9sfA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/f7uYWF72SCLqGqa9CsDhe6WlaUpSC5wavunyVlx-ubC6sS7TdeFiyTdgY2ubAD6xOrbzJaqOdnXeWkgt5ng0eDC2rKZdpYnsczs_LCg6FTRL5fASuZgTqSoYLTM1UOMUT3SUQRHPAHk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/f7uYWF72SCLqGqa9CsDhe6WlaUpSC5wavunyVlx-ubC6sS7TdeFiyTdgY2ubAD6xOrbzJaqOdnXeWkgt5ng0eDC2rKZdpYnsczs_LCg6FTRL5fASuZgTqSoYLTM1UOMUT3SUQRHPAHk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/taYTdX5wXoMQZsQpYXfrJrR2A9ubZHFLOdYsg0ngzB8HqsuZGROvQfDbajxR50jHj061SrWmVhBEhILMIljCSXGGt1jBy_7Kyj7azWoINACTsDUmK7VJT7q9UgD9rDF7QR-msnXkZBc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/taYTdX5wXoMQZsQpYXfrJrR2A9ubZHFLOdYsg0ngzB8HqsuZGROvQfDbajxR50jHj061SrWmVhBEhILMIljCSXGGt1jBy_7Kyj7azWoINACTsDUmK7VJT7q9UgD9rDF7QR-msnXkZBc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/DPnR3tR_0BYZzB9QFSqboMfGd_9W_NlKNSzt7app0zOCe2kfiym17HbfIm__67MMEZFJjcqft5mXUtp8g3REY21qEUcnBrWT68mvstA22hY-OTIp3RxjthYcW5bb0a8mOTeaKo60R14=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/DPnR3tR_0BYZzB9QFSqboMfGd_9W_NlKNSzt7app0zOCe2kfiym17HbfIm__67MMEZFJjcqft5mXUtp8g3REY21qEUcnBrWT68mvstA22hY-OTIp3RxjthYcW5bb0a8mOTeaKo60R14=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/YcsoMmZ4gFoBdiHrdu2LTQTCR6N7W8cZg5amQ3K-kHSmZIpKeQ68oBadYkx6tyw7YHtIuTm1GbxeJ5TBB-H01WontVjY97ODSVFOKfQObnRUBiJ-IUpl89-SsNi8KdE8JnyCzP9rf_0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/YcsoMmZ4gFoBdiHrdu2LTQTCR6N7W8cZg5amQ3K-kHSmZIpKeQ68oBadYkx6tyw7YHtIuTm1GbxeJ5TBB-H01WontVjY97ODSVFOKfQObnRUBiJ-IUpl89-SsNi8KdE8JnyCzP9rf_0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/EGWJkiT1b9Us0OAslPQujarFsOsKkUkm_39NfZSeTGK5bgEhlcn-qfv_fT1v3zW9gl3gIpF20tXi-E_V6JHigblgACzSK9dkamYQLrEhXw80EXXcuD4m0ps5UCh7csFYpwSJBtgo4iE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/EGWJkiT1b9Us0OAslPQujarFsOsKkUkm_39NfZSeTGK5bgEhlcn-qfv_fT1v3zW9gl3gIpF20tXi-E_V6JHigblgACzSK9dkamYQLrEhXw80EXXcuD4m0ps5UCh7csFYpwSJBtgo4iE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Mgv990gW2KFRleDkg6WX0mJ4kSpXXCj6Gg_nskOWOTL08nDeY84Ciqu4Fx_L0HegZSicK1pL8LdVuouqtRII7tzSJC3SJg014wIsyWB6tqI0RK0XytGr4TnwyX8rPiAWmTRvYAnSlEs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Mgv990gW2KFRleDkg6WX0mJ4kSpXXCj6Gg_nskOWOTL08nDeY84Ciqu4Fx_L0HegZSicK1pL8LdVuouqtRII7tzSJC3SJg014wIsyWB6tqI0RK0XytGr4TnwyX8rPiAWmTRvYAnSlEs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/TMs2yd8QBePC36D_UY7t7E1k1IwRS2fYrJEMLFPPEQGzsB6CQmQAj1oiI3_u6KVqUJxws5wPJcDoe8Q8z9u-1vT5Z9Qm4YIV8FJpfLA0cvgY3vdZzSXj7CjZFEVeueaMD1vb67vubhA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/TMs2yd8QBePC36D_UY7t7E1k1IwRS2fYrJEMLFPPEQGzsB6CQmQAj1oiI3_u6KVqUJxws5wPJcDoe8Q8z9u-1vT5Z9Qm4YIV8FJpfLA0cvgY3vdZzSXj7CjZFEVeueaMD1vb67vubhA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/oF_x-cmmYiwj2GSJiJ7KYrQfQSUBiiOKIsG91IEi4aviVU4ghNuH0tl02cP9OzsrGwdWvEdbHDudZqBiD2-0MhJTt9FJV5I79tl7njkXgmP90JLmq56WBl8VQgc5-stYPLR35TwISuc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/oF_x-cmmYiwj2GSJiJ7KYrQfQSUBiiOKIsG91IEi4aviVU4ghNuH0tl02cP9OzsrGwdWvEdbHDudZqBiD2-0MhJTt9FJV5I79tl7njkXgmP90JLmq56WBl8VQgc5-stYPLR35TwISuc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/9gGIia6XtU3utliWH5q2zE06PFNoJMNZgm5wDR_ZTZ1TAQUOTS4sw14GfNxH30T6tkNF7VmxQinC65s2QPMcegVUuczzwoJu1ppO8uBAJa4D5pePj3FCr_TN_HvYuDRO8PptYmD5O7c=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/9gGIia6XtU3utliWH5q2zE06PFNoJMNZgm5wDR_ZTZ1TAQUOTS4sw14GfNxH30T6tkNF7VmxQinC65s2QPMcegVUuczzwoJu1ppO8uBAJa4D5pePj3FCr_TN_HvYuDRO8PptYmD5O7c=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/yOk9EVsGSHGfkrOz8lq6qxkVYFTKc8NwR59iEfouhU3Xo0BTCfkVADa7lr_zjv8ACFeV_vp1RSDcNcmWx3ZboJw_2udmItEr7zHQY9v3KWeDrIlk_fktp-9ScCP0vJS7aY7TBIb2Q1k=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/yOk9EVsGSHGfkrOz8lq6qxkVYFTKc8NwR59iEfouhU3Xo0BTCfkVADa7lr_zjv8ACFeV_vp1RSDcNcmWx3ZboJw_2udmItEr7zHQY9v3KWeDrIlk_fktp-9ScCP0vJS7aY7TBIb2Q1k=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo ESP',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 6.61',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/-MQQiT2knvNvTRg0N3HMOsj-nFzKvHeEBmaPfcofvzoMptXsZ5iEegMQI0xddVi2DkjA4fa53cAXSdMHI8TWPsdqhPI_4Z1WRGsZ6pahikouOQCK94m1bZuz7VLmpHgpHaXtdFPT0gA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/-MQQiT2knvNvTRg0N3HMOsj-nFzKvHeEBmaPfcofvzoMptXsZ5iEegMQI0xddVi2DkjA4fa53cAXSdMHI8TWPsdqhPI_4Z1WRGsZ6pahikouOQCK94m1bZuz7VLmpHgpHaXtdFPT0gA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
