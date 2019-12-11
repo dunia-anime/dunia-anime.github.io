3079ad7fabbfc8fe578a7a2cc88a5a13566693aa
dunia-anime <adityha.on@gmail.com>
2019-12-11 14:47:22 +0800

    Create Tanowai.js

---
+++ Anim-js/T/Tanowai.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3EIBBk1t1KuVY3v7vlqdUJthjTeWZexehG1KeUg4O2SWLF5N_aRJD3OZQATStOI6oLUw-yByRkuixzgZqArAoIiNFTDAQqbtnaMGL4W6i72bUYVGFC5UJgW_r_WpNiS34BVRu-oRBkY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3EIBBk1t1KuVY3v7vlqdUJthjTeWZexehG1KeUg4O2SWLF5N_aRJD3OZQATStOI6oLUw-yByRkuixzgZqArAoIiNFTDAQqbtnaMGL4W6i72bUYVGFC5UJgW_r_WpNiS34BVRu-oRBkY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/iLBrEknwlCGRC8okCmXdfJ8gxwOGCeqI8WSlqQQ-uT2ZONRfmplg87NALpiWWoO71izOKDpCf-b8pcr7yT31n9zcUC5KLMROiHZI5rX9I7c7g3d0JKu0GP0ThdYN1kmJDpV3fIS9yWY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/iLBrEknwlCGRC8okCmXdfJ8gxwOGCeqI8WSlqQQ-uT2ZONRfmplg87NALpiWWoO71izOKDpCf-b8pcr7yT31n9zcUC5KLMROiHZI5rX9I7c7g3d0JKu0GP0ThdYN1kmJDpV3fIS9yWY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/a2fhrQ_dGz7ZfrUirGHaGzg0lCaA2h77WIKKN--dyROl0DDEVkdTlsUt7xbUpiN__ZNm_pGqjdB-yM3Xp1XnEtj0VG_KNOPOUcAfucda_zznpqLy6RVFmLPOw19ITq9x1atSoh-DUjg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/a2fhrQ_dGz7ZfrUirGHaGzg0lCaA2h77WIKKN--dyROl0DDEVkdTlsUt7xbUpiN__ZNm_pGqjdB-yM3Xp1XnEtj0VG_KNOPOUcAfucda_zznpqLy6RVFmLPOw19ITq9x1atSoh-DUjg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/L2l9GgRcFl6q-Gik1l70MTzJk1Y9WL4_3Jj2JcO0MdV3k0VHjMhDiTg5o9FN1gCm-9jb8C1H8oqUL9GsYHCpjQxYZiWxP8lda45pjiJ3MnXkXoFwMwqIIZGyZR7eL7DKwD5qLZXBPyg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/L2l9GgRcFl6q-Gik1l70MTzJk1Y9WL4_3Jj2JcO0MdV3k0VHjMhDiTg5o9FN1gCm-9jb8C1H8oqUL9GsYHCpjQxYZiWxP8lda45pjiJ3MnXkXoFwMwqIIZGyZR7eL7DKwD5qLZXBPyg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/IplSTX4bPpPSyc7CPfTUDjIdZC4_Si_s1W4Qv_oIyAOjW281fOu2YSDe3Rad40Vp5SRZDDtZvvSXbYL4kp-utl7h8E7g92uF34Zu3j1g7Okp9nuD_pHlu7Z7SHauJsCbFWxAugbHm18=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/IplSTX4bPpPSyc7CPfTUDjIdZC4_Si_s1W4Qv_oIyAOjW281fOu2YSDe3Rad40Vp5SRZDDtZvvSXbYL4kp-utl7h8E7g92uF34Zu3j1g7Okp9nuD_pHlu7Z7SHauJsCbFWxAugbHm18=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/78ami6wLJsVw4VL8YUMYz85acFKV0ZiJtHruNbfsjdPqO_hkzyxOsN-aCHUhObUblPR4MpeKH68kMO0haL8NqyypXj3H6gMfbIlLRXdlLzgWHAE8p2lN5bc_bdrzuhrnQnTodNgnj4s=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/78ami6wLJsVw4VL8YUMYz85acFKV0ZiJtHruNbfsjdPqO_hkzyxOsN-aCHUhObUblPR4MpeKH68kMO0haL8NqyypXj3H6gMfbIlLRXdlLzgWHAE8p2lN5bc_bdrzuhrnQnTodNgnj4s=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/HoJlz5qgHXjfqBWEEx3481-ERcb_ROQoFt7sASOWgBzMoy8brBq75gjI2AhZxBWTLB8We7j9ArM774xwkijmqYx6w6txbuz4C5hlZHcbhgGus2_qKX_MWztd3VorOZEHyJ47X7BAnTg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/HoJlz5qgHXjfqBWEEx3481-ERcb_ROQoFt7sASOWgBzMoy8brBq75gjI2AhZxBWTLB8We7j9ArM774xwkijmqYx6w6txbuz4C5hlZHcbhgGus2_qKX_MWztd3VorOZEHyJ47X7BAnTg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WaGYmte8OEmXCvc5Kl8yKK-fsLZdz_a7n3UL7mGT6ITROdKUo8v1tRxO6WNcotTM2oQk7rdlO-WCOiIyQcFljWP241-DZSfeHUb_sBVi6yqv_7_xvRHRErJ0aZNIfvIvl107vjIUa9c=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WaGYmte8OEmXCvc5Kl8yKK-fsLZdz_a7n3UL7mGT6ITROdKUo8v1tRxO6WNcotTM2oQk7rdlO-WCOiIyQcFljWP241-DZSfeHUb_sBVi6yqv_7_xvRHRErJ0aZNIfvIvl107vjIUa9c=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/jtgWKdp287PiwtPRv8YUDfMR_Dgj8sP4B52iR6EPR2mcrG-4Ml-OFtr2ddDq5jhTcEn1iW8WT93HIUVrOqBOxrPp6vDOMnExN0AF21Fp_5NsrTlBL5uhSEHrSqgfcw3cFtar5T-JbC0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/jtgWKdp287PiwtPRv8YUDfMR_Dgj8sP4B52iR6EPR2mcrG-4Ml-OFtr2ddDq5jhTcEn1iW8WT93HIUVrOqBOxrPp6vDOMnExN0AF21Fp_5NsrTlBL5uhSEHrSqgfcw3cFtar5T-JbC0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/CmB2CX0eaoND9zY9otC5BieXR-nUEtUgCOiFR_omI0m10Ypwlq1r96HWyBPPdZwfKUKVFBS5pOCimZ-kRJT5ZkYvSLHYa2u_X6N6TL4vBriRcPRfDoXHhRCKkcjl_lOG02Ui1CHtYXw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/CmB2CX0eaoND9zY9otC5BieXR-nUEtUgCOiFR_omI0m10Ypwlq1r96HWyBPPdZwfKUKVFBS5pOCimZ-kRJT5ZkYvSLHYa2u_X6N6TL4vBriRcPRfDoXHhRCKkcjl_lOG02Ui1CHtYXw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/c_GavZNY4ykr2eMi10jtNl5HjSzdrRHbwyWJeP0ztV8nsr3XAraEwdqCom__j-V1vamrrCAhwrg5Kx4H1icMzLjSAM-VFVxN2EWOoBT9Nqr6WL80aCoDStF5Jvcfb6eEWdeTEd_BqEk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/c_GavZNY4ykr2eMi10jtNl5HjSzdrRHbwyWJeP0ztV8nsr3XAraEwdqCom__j-V1vamrrCAhwrg5Kx4H1icMzLjSAM-VFVxN2EWOoBT9Nqr6WL80aCoDStF5Jvcfb6eEWdeTEd_BqEk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6kx6pugTNd054YBt-mGsnNtxtGASwp1bmkZe4ush-VSEQStPcQPoUEKBuZkhodirh7SuIdnvpf995f8UvWwzj1WnaAHChPygyIbcX7yrpgAaL9T1cz-HI94qP0xMrR8layk1iTPevE0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6kx6pugTNd054YBt-mGsnNtxtGASwp1bmkZe4ush-VSEQStPcQPoUEKBuZkhodirh7SuIdnvpf995f8UvWwzj1WnaAHChPygyIbcX7yrpgAaL9T1cz-HI94qP0xMrR8layk1iTPevE0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tada-kun wa Koi wo Shinai',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.59',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/MwC0EnoJKNXIUKP46vwEmMbHTbsq0JN-eA7z78VVfqh3rRyVSYahp-9LZYveXQjfy410G6-CjNokIg7zpbhEpWvVZcLPl1oPoTazmi2eTa1TWAT0rbWdQbv-wpy7TC8T5Av-2rV_cY4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/MwC0EnoJKNXIUKP46vwEmMbHTbsq0JN-eA7z78VVfqh3rRyVSYahp-9LZYveXQjfy410G6-CjNokIg7zpbhEpWvVZcLPl1oPoTazmi2eTa1TWAT0rbWdQbv-wpy7TC8T5Av-2rV_cY4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
