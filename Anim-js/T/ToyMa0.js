e6695ee5ba17f6b39e7015f9e85cff3d200b45c9
dunia-anime <adityha.on@gmail.com>
2019-12-11 13:28:00 +0800

    Create ToyMa0.js

---
+++ Anim-js/T/ToyMa0.js
@@ -0,0 +1,324 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/94LWnvdPAJp1Ka1O_lDXyOHqdxKRFRBwkljKdXZ4Zi5QyD3YMV8UaXXcWlmYdAkbiwm7kkRhbLO0XwLNGaRJ9E4dn59gvHM3FJc73f4CV5zfNqFV1oGcNcE5ABZKp5RhN835yKLtm20=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/94LWnvdPAJp1Ka1O_lDXyOHqdxKRFRBwkljKdXZ4Zi5QyD3YMV8UaXXcWlmYdAkbiwm7kkRhbLO0XwLNGaRJ9E4dn59gvHM3FJc73f4CV5zfNqFV1oGcNcE5ABZKp5RhN835yKLtm20=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AXEYEt_0j_Vbwa3KOiqSOnKszbY9iNUjFfeQn4lpaqa597O6O_TNlQaqXzWRRj3O1xnl-eSP37DpHTzCAWNqp5Rev_ZtZjjlNAozTn-z5j_OGQn2Ht5rCLcYHBVUCcUXMTPIY-zi8Cc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AXEYEt_0j_Vbwa3KOiqSOnKszbY9iNUjFfeQn4lpaqa597O6O_TNlQaqXzWRRj3O1xnl-eSP37DpHTzCAWNqp5Rev_ZtZjjlNAozTn-z5j_OGQn2Ht5rCLcYHBVUCcUXMTPIY-zi8Cc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/WIs_VPZNr_QEnbwTVahYFc1alDnG34hdQllT7odx-L3on1CQ5TDR53t_667sj5K01wlg0XmirbZ0qoRKjnYkmj6xqYWJms2O9ZqtGnsYiQdl6kG0uMyd5V3SkXWWvbB6vaIp_nrLuP0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/WIs_VPZNr_QEnbwTVahYFc1alDnG34hdQllT7odx-L3on1CQ5TDR53t_667sj5K01wlg0XmirbZ0qoRKjnYkmj6xqYWJms2O9ZqtGnsYiQdl6kG0uMyd5V3SkXWWvbB6vaIp_nrLuP0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/hUdOn3K0lOHXR2UkEZMhR-ek2OaykJo-CW3NHofsyOnAdYWYmhICnXNfuliGWpa2us-jD_XLUMU2n_ZLJojInaG-K6rRgUOW1Dj556Y5ulpqpyGZmKAqR8akVz8agPVJrJHl7y8Kf2s=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/hUdOn3K0lOHXR2UkEZMhR-ek2OaykJo-CW3NHofsyOnAdYWYmhICnXNfuliGWpa2us-jD_XLUMU2n_ZLJojInaG-K6rRgUOW1Dj556Y5ulpqpyGZmKAqR8akVz8agPVJrJHl7y8Kf2s=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Huzjt2Yfilh4foeaDzlbrKSrriNUZbXYGzI0ac5jORGYVnjifGU_PzYl_24l3nnKdU5gVCNlEhkRfigsUB8RVCGOWJyM8jG2mErhI4vJqPDjUT8YYXQxAJ0d9agbit3yk9ONGk_6wlk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Huzjt2Yfilh4foeaDzlbrKSrriNUZbXYGzI0ac5jORGYVnjifGU_PzYl_24l3nnKdU5gVCNlEhkRfigsUB8RVCGOWJyM8jG2mErhI4vJqPDjUT8YYXQxAJ0d9agbit3yk9ONGk_6wlk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/1sqgDbN53l0oNLg0Ctg1E_HuhwgtZ7niJ-rggulEUOfOHDl1OIeYQaTOTpw6uGWD9zMDYSKsQpjbNCXzZcrr7oRbJnOLODcGWPfC22SmaE9zf2bI17GOMP8pcyjwKBcwp16qB6TEfRU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/1sqgDbN53l0oNLg0Ctg1E_HuhwgtZ7niJ-rggulEUOfOHDl1OIeYQaTOTpw6uGWD9zMDYSKsQpjbNCXzZcrr7oRbJnOLODcGWPfC22SmaE9zf2bI17GOMP8pcyjwKBcwp16qB6TEfRU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/zfYr2FQ7BGCCpmKDi-XBgcW_ZEivwubscHTEyaZqsSY9jKgdQbpqS3j0xMMY82i85n_D0IpIePbasf1DA8cTvtC7AMJfPoXqWqJLkb2SuW5DslNf7JasfWoHcbugtsz-vExi3H84t9o=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/zfYr2FQ7BGCCpmKDi-XBgcW_ZEivwubscHTEyaZqsSY9jKgdQbpqS3j0xMMY82i85n_D0IpIePbasf1DA8cTvtC7AMJfPoXqWqJLkb2SuW5DslNf7JasfWoHcbugtsz-vExi3H84t9o=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/LpHEsI9fAMOuHyLspNEw82hHsBLjtNb1v5F5O-Ift5f1y1efQDmsNFsjl-zabygsACqBe53lkGx08qpOzLsio2Xp_bcteDm73PnBhppkJE-UhsWBoga54pX_6D-V43yCQgRhgn21KZE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/LpHEsI9fAMOuHyLspNEw82hHsBLjtNb1v5F5O-Ift5f1y1efQDmsNFsjl-zabygsACqBe53lkGx08qpOzLsio2Xp_bcteDm73PnBhppkJE-UhsWBoga54pX_6D-V43yCQgRhgn21KZE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/audqOEYTuKixXatemK2AeE4ucl6EsuBlvj5OkK0h6jGn-WE_g8esTmvNHibBLK4LSQmGWx6XTjyi8JkQzfjK7Rf3BLVqzTC4oiXgqJiuWoIt1Z2CW-7tr20Bz4OBdb44Fp7Nw5EwYMw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/audqOEYTuKixXatemK2AeE4ucl6EsuBlvj5OkK0h6jGn-WE_g8esTmvNHibBLK4LSQmGWx6XTjyi8JkQzfjK7Rf3BLVqzTC4oiXgqJiuWoIt1Z2CW-7tr20Bz4OBdb44Fp7Nw5EwYMw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/dMYfX4WM9X9WAEwEdS3rBsTUKqPcTs3WuipLi-XdYrWnnvRCITHYYWh1z7rEGYGBNsXXz1D1ICDtO2d3Dm9zQtadSz6xDFKmuuHW2BMdoRm_65oQ4l6VUobvCmtSMAB9pGY23UC2XFk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/dMYfX4WM9X9WAEwEdS3rBsTUKqPcTs3WuipLi-XdYrWnnvRCITHYYWh1z7rEGYGBNsXXz1D1ICDtO2d3Dm9zQtadSz6xDFKmuuHW2BMdoRm_65oQ4l6VUobvCmtSMAB9pGY23UC2XFk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Magnitude 8.0',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 8.12',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/-v55w4ncdB-XGEvRzdaJ-B7agP18aeGseMlGUNw7OD7-46jlYaAd4bMhX85r_3RXozXAib-njpsgSy-FGEXqVhRE6q-D73JEylsAU7FLWygcz3BdsFAsFV2qT6jUuN8f5MPPi-U6Cmw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/-v55w4ncdB-XGEvRzdaJ-B7agP18aeGseMlGUNw7OD7-46jlYaAd4bMhX85r_3RXozXAib-njpsgSy-FGEXqVhRE6q-D73JEylsAU7FLWygcz3BdsFAsFV2qT6jUuN8f5MPPi-U6Cmw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
