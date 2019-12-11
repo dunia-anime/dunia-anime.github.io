0125324a5da13a6ce042cdb280499e0b812e1022
dunia-anime <adityha.on@gmail.com>
2019-12-11 13:35:22 +0800

    Create okul.js

---
+++ Anim-js/T/okul.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/aY8pSj9SgRqvVErp63auBiQXtKX-V4pk0_nulF5-G3dHh8ImrFBjR_PkE-5ZA7I9A2brey0otlTHVD1A2f-Pty2E7pgiT-RtKAKm7rBslJ0zXFlPPc597QicXu10pH-QYDrhu81fuWE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/aY8pSj9SgRqvVErp63auBiQXtKX-V4pk0_nulF5-G3dHh8ImrFBjR_PkE-5ZA7I9A2brey0otlTHVD1A2f-Pty2E7pgiT-RtKAKm7rBslJ0zXFlPPc597QicXu10pH-QYDrhu81fuWE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/GPC0oM7GV5MgjYmxICqte5wukyWyDxR1_MvEeLW8rFR3aG3KRjBatk0cHE_bemdX2MPxPcbJamUilO0rrdCeNmsoUaIkFmQrGKozeNTq8VFtl7nGf3D5KlpNWW-Vn-QqRzSTFFKftw4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/GPC0oM7GV5MgjYmxICqte5wukyWyDxR1_MvEeLW8rFR3aG3KRjBatk0cHE_bemdX2MPxPcbJamUilO0rrdCeNmsoUaIkFmQrGKozeNTq8VFtl7nGf3D5KlpNWW-Vn-QqRzSTFFKftw4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/GiFa1hfxmhdY7H3x8fejxMphbB1kBQ3pPDzUGLit3-dKapfzJ8e94hkMonyOGkSukggiwctxYd2TfVXynK06wxIY3702Q9gr3gZkJTc0czPnET9kevIH9zqm08z9t2rmUAcIpN0FlOc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/GiFa1hfxmhdY7H3x8fejxMphbB1kBQ3pPDzUGLit3-dKapfzJ8e94hkMonyOGkSukggiwctxYd2TfVXynK06wxIY3702Q9gr3gZkJTc0czPnET9kevIH9zqm08z9t2rmUAcIpN0FlOc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/BR9TLuV1_sVG8-VikFF87L52ehQSxclAvdr0R_6CmxB_twmKS5qR-WO8WhdoxVWRwXAijXqyRGkvyp5_dwj6DMUWubn8UPmp6PjLQhJDq4TuQ6u6-XwWmi6aHSPi1p_u_CfmHjWEWCo=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/BR9TLuV1_sVG8-VikFF87L52ehQSxclAvdr0R_6CmxB_twmKS5qR-WO8WhdoxVWRwXAijXqyRGkvyp5_dwj6DMUWubn8UPmp6PjLQhJDq4TuQ6u6-XwWmi6aHSPi1p_u_CfmHjWEWCo=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/sLk9LRLmLxKeEHcEmVMdHFEz2IFgJ2tlSN6bO9PiucpQRlN2DSQ251-PGBhKspFlkpeU8ETGCUmWZJAm9BbmcX-G06eZYh1WD8A20kzsJ4EsZhD6YrXucI9-tM4HEOyZydnzWTbjlSE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/sLk9LRLmLxKeEHcEmVMdHFEz2IFgJ2tlSN6bO9PiucpQRlN2DSQ251-PGBhKspFlkpeU8ETGCUmWZJAm9BbmcX-G06eZYh1WD8A20kzsJ4EsZhD6YrXucI9-tM4HEOyZydnzWTbjlSE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/NritT7AH5K0_aaPsf3IkMMiIsgu3HINyB8hCJnzGYs-N3oUcp1t2Ij_VDed0JrhvS6ANctE2gxlinHKM-CiM6VnpApblnlyx3pznNl6fBtSRiTuumsCEX8e0c8uqrXgL42t8Jl6bTWY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/NritT7AH5K0_aaPsf3IkMMiIsgu3HINyB8hCJnzGYs-N3oUcp1t2Ij_VDed0JrhvS6ANctE2gxlinHKM-CiM6VnpApblnlyx3pznNl6fBtSRiTuumsCEX8e0c8uqrXgL42t8Jl6bTWY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/99T0r2znFwetBIZeahh2H6_FAjbNH_HOLYo2GYTG9dPEodhVL7-BYaQo9kHkGBwvdAZKfvVF6ayKpYw9i062fPZzTcPFjj0rHn_33U68nD9y393Uzn44Dho6M1gHuvMOgItRRLjV3KI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/99T0r2znFwetBIZeahh2H6_FAjbNH_HOLYo2GYTG9dPEodhVL7-BYaQo9kHkGBwvdAZKfvVF6ayKpYw9i062fPZzTcPFjj0rHn_33U68nD9y393Uzn44Dho6M1gHuvMOgItRRLjV3KI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AmbSeYAZseAXlT1r1fhK-jCgNHrgGsRTDBrkL_lVRYM2oOW9Cvvam2MJ0Za2Ac8zW3R5U-bEEAgiAfb0PNcmJWQCtgv_G9GD6T1HhxavyS5HQg6-HxygvLkpfh2cur_-mUwUb6K1Pkw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AmbSeYAZseAXlT1r1fhK-jCgNHrgGsRTDBrkL_lVRYM2oOW9Cvvam2MJ0Za2Ac8zW3R5U-bEEAgiAfb0PNcmJWQCtgv_G9GD6T1HhxavyS5HQg6-HxygvLkpfh2cur_-mUwUb6K1Pkw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/wAOXapE_AxrcDyy_avqM6VJoEWahBzYGEAEg8BNE2BH1OURTsMIQuZLeJvKy7RJ1v-gmAKY2_hrqi7ezwWCR1Hol8OqMtEsG6MTHNk5LOdYn-sJXcGIkVTb_Pi4zqotMu1ciuEuL-0M=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/wAOXapE_AxrcDyy_avqM6VJoEWahBzYGEAEg8BNE2BH1OURTsMIQuZLeJvKy7RJ1v-gmAKY2_hrqi7ezwWCR1Hol8OqMtEsG6MTHNk5LOdYn-sJXcGIkVTb_Pi4zqotMu1ciuEuL-0M=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ALvhqPnBM3MHqO-rb7MNF-pRAQ89hC9AKChwe-uh39SYOm_DzIuYvtfLws_Ys8Dqh7jMrEOa6t1gNhgjf_TM7HG2vDd0fNTXFF1UQva_r4FkkD1jalfEt0rjsbvltaWTgyPy7JlvnFs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ALvhqPnBM3MHqO-rb7MNF-pRAQ89hC9AKChwe-uh39SYOm_DzIuYvtfLws_Ys8Dqh7jMrEOa6t1gNhgjf_TM7HG2vDd0fNTXFF1UQva_r4FkkD1jalfEt0rjsbvltaWTgyPy7JlvnFs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/50rYcrU5aVEJd1P7I2w3zQrbJr8abkDUCVtSNvqvqqQXMZHfUD9sGSeOYpdEWtiaCmw6mCqLNKT6iLsfM_c1Px2Ob1Fnkj7R_HieSSPXy0JG7kf-O0XzXfd7hp6w0PgbtVKK9p37rkw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/50rYcrU5aVEJd1P7I2w3zQrbJr8abkDUCVtSNvqvqqQXMZHfUD9sGSeOYpdEWtiaCmw6mCqLNKT6iLsfM_c1Px2Ob1Fnkj7R_HieSSPXy0JG7kf-O0XzXfd7hp6w0PgbtVKK9p37rkw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tokyo Ghoul',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.93',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/7xs1j-eLflqauUrVwgiehOTl7PavxXAzwm3VKMPkN2vgeIBo8fXxIvJvg3EDvQF4ne9xxRxD4Gg1HjFCml9LiBQ9SVTzlIp99_0lKnB7lYLamYtClKWpFNaT9RaHUjzCW0JKz8E3qVY=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/7xs1j-eLflqauUrVwgiehOTl7PavxXAzwm3VKMPkN2vgeIBo8fXxIvJvg3EDvQF4ne9xxRxD4Gg1HjFCml9LiBQ9SVTzlIp99_0lKnB7lYLamYtClKWpFNaT9RaHUjzCW0JKz8E3qVY=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
