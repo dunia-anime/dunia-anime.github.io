f353179024d96f63bf99eb2800a83bdbc1b2aedc
dunia-anime <adityha.on@gmail.com>
2019-12-11 13:22:58 +0800

    Create inin.js

---
+++ Anim-js/T/inin.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Trinity Seven',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/PCaTgaDykT23ne2f6z8264YJKZ9qAY8oQpEmRl6KGnTvO-WTWjEy8O4AH7-EkBRTIseBAtKEc-r7mioJl2ZKnQaSsllGedjuvILCHq35BZI_9YisjttxqsR2MNzdOwll5mepD7m9xUU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/PCaTgaDykT23ne2f6z8264YJKZ9qAY8oQpEmRl6KGnTvO-WTWjEy8O4AH7-EkBRTIseBAtKEc-r7mioJl2ZKnQaSsllGedjuvILCHq35BZI_9YisjttxqsR2MNzdOwll5mepD7m9xUU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/un3_xKQgff3kU1b282ypixI8z9UJnKjvZUjw4X565eXnoVvTxzCCMt9cZLGdwvBC2XbvhXc49KJ1e0FxJX1QVePrVwSealMfIyqgSSwvy8E22CAriyiWAbGAdUnEWeQ955Q_ntxgHnc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/un3_xKQgff3kU1b282ypixI8z9UJnKjvZUjw4X565eXnoVvTxzCCMt9cZLGdwvBC2XbvhXc49KJ1e0FxJX1QVePrVwSealMfIyqgSSwvy8E22CAriyiWAbGAdUnEWeQ955Q_ntxgHnc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/zCHEu15U8vzIWKJoCSGVsr1l0xtNTRTKJafkPdlccmr-qF9G1kURXlmItGEDN5E0plwLNdMltPwyRz7f7dwXd6qztpwMoXtOJgwUiBWTIYy4s_B1-saeR7fbUqHURvNCWwf5OBq06Ds=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/zCHEu15U8vzIWKJoCSGVsr1l0xtNTRTKJafkPdlccmr-qF9G1kURXlmItGEDN5E0plwLNdMltPwyRz7f7dwXd6qztpwMoXtOJgwUiBWTIYy4s_B1-saeR7fbUqHURvNCWwf5OBq06Ds=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/bt4YRPGpFuE4ULfdmtmy0I8_OdbXFRaO6COYs1TqgqlWsUk8OlXvt5K0nWcAu7YYMIyclp8d4hh9DtBvS4efqPgETFE8f7YIJvVgKBD6IuvkfD-4GFVBJd3X3oRJtoNcybRrhOiECtk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/bt4YRPGpFuE4ULfdmtmy0I8_OdbXFRaO6COYs1TqgqlWsUk8OlXvt5K0nWcAu7YYMIyclp8d4hh9DtBvS4efqPgETFE8f7YIJvVgKBD6IuvkfD-4GFVBJd3X3oRJtoNcybRrhOiECtk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Ml2Ug6SLX52p8gbAUFx_mfFhPlzqXPeIm3ta7YjrdK0cOw7i45kzbT6cpVk5PDaRtos7ciTA-A2Hf4xPZ7qLpkf9INOUCmORkBU3y77CqDgiq03B7gnn7eZvQ9UyaydA8Q68LcLkhT0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Ml2Ug6SLX52p8gbAUFx_mfFhPlzqXPeIm3ta7YjrdK0cOw7i45kzbT6cpVk5PDaRtos7ciTA-A2Hf4xPZ7qLpkf9INOUCmORkBU3y77CqDgiq03B7gnn7eZvQ9UyaydA8Q68LcLkhT0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/0isF_JdZUiu3WOSi2H1XDX-WaggV85R9sjs4ZUjiBmtCzSCmWrgvgdWjDIBok8lLNGHIlnLHRAc7XeS_3xo-h0DpkYS8hVf0RXspChK8SFCx2JZg0zRDCfx8VH7tjyo9qU02JsnGWTk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/0isF_JdZUiu3WOSi2H1XDX-WaggV85R9sjs4ZUjiBmtCzSCmWrgvgdWjDIBok8lLNGHIlnLHRAc7XeS_3xo-h0DpkYS8hVf0RXspChK8SFCx2JZg0zRDCfx8VH7tjyo9qU02JsnGWTk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/PYhbOKcJz8K7JW1HSM-GiY8-G6KNdyxr6iYHXPWuxHR7xRKMCF4t2o4oSTpv6yyBy7-KKt-JrZ-rYmsJleHnQjBlLTAX5SpUNhoa2upfl5Ct1UA8Tx5RTDU6h5yoY8NdfAf8JLaVoUI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/PYhbOKcJz8K7JW1HSM-GiY8-G6KNdyxr6iYHXPWuxHR7xRKMCF4t2o4oSTpv6yyBy7-KKt-JrZ-rYmsJleHnQjBlLTAX5SpUNhoa2upfl5Ct1UA8Tx5RTDU6h5yoY8NdfAf8JLaVoUI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/frwtUPbishq4KnGTzPcPhwa39PdCOP7bxdFVkkjpIK_aB6kBcYI9ldKWMHzsZpFR8Hbll_WV6g1lCvUJfFZ0bRCvIbvwOJv5q3QiFeY1jT5e7zCalZM5XYpnK5JnmQKu4q1bGD36agA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/frwtUPbishq4KnGTzPcPhwa39PdCOP7bxdFVkkjpIK_aB6kBcYI9ldKWMHzsZpFR8Hbll_WV6g1lCvUJfFZ0bRCvIbvwOJv5q3QiFeY1jT5e7zCalZM5XYpnK5JnmQKu4q1bGD36agA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/5T2FFrsobmpVDRJKpM5YeODgdR2Hl2ddSJPEXBSM6zaRPbo9GWORherEG4roCH3vG3snJPeBHl34SgciukzYheFNVQrN6pcy_V0HnXI9ZgrRbxXJXWma_wMQzRYb8lTujqAawN8oO7s=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/5T2FFrsobmpVDRJKpM5YeODgdR2Hl2ddSJPEXBSM6zaRPbo9GWORherEG4roCH3vG3snJPeBHl34SgciukzYheFNVQrN6pcy_V0HnXI9ZgrRbxXJXWma_wMQzRYb8lTujqAawN8oO7s=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/lN_7WM2DimX8mxfjMKOLZb9GeANtf8CwiYKvhKmhnvvOqAlS9zuQdU7TnWOCaYgZ2CTKXseNU06eKHD_B0CQPRCZZSs3OGdwk6F05FxHvAm08CErh2n539ESSlyD6v63g_Vf660U1BM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/lN_7WM2DimX8mxfjMKOLZb9GeANtf8CwiYKvhKmhnvvOqAlS9zuQdU7TnWOCaYgZ2CTKXseNU06eKHD_B0CQPRCZZSs3OGdwk6F05FxHvAm08CErh2n539ESSlyD6v63g_Vf660U1BM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/FpB2KbUE71xfCYuWo66J3Hv7pnHLhQIMNpUOBsmYf-d3r9WTLQQ3fVP42ghDhLSCXd_7nYsx7ASW05Cxh-euiPc1AX0f2A64lqNIHQrqoXkFjnKwYAJ_XDYlfDRojZRZO75VHUnT9RU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/FpB2KbUE71xfCYuWo66J3Hv7pnHLhQIMNpUOBsmYf-d3r9WTLQQ3fVP42ghDhLSCXd_7nYsx7ASW05Cxh-euiPc1AX0f2A64lqNIHQrqoXkFjnKwYAJ_XDYlfDRojZRZO75VHUnT9RU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/i-HLYL-IAoUTCOlqTfGRHF0HYSoKHuAdYwynWSoRa4sRi6dduQh97QmdfaClFYOF8aPiwKuti3dlHBYOAv4LAjT6FKf-7JD5AwdMids5KvAxRFFK0o-dm_x6A1wiD4t1u1htoO42_vk=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/i-HLYL-IAoUTCOlqTfGRHF0HYSoKHuAdYwynWSoRa4sRi6dduQh97QmdfaClFYOF8aPiwKuti3dlHBYOAv4LAjT6FKf-7JD5AwdMids5KvAxRFFK0o-dm_x6A1wiD4t1u1htoO42_vk=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Trinity Seven',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.34',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AItsILoL2TxBk_DHbE0ual6E0aKO0TbAmstCvoKLvpfJf14g2-ecR683Ak7EhlHgwNRfkMhtGhHypzlvgRyEzmvFUOEXqFj-ACW4KHT8GXtTmA7djFc6bYrMmB2UhcztxWtMjUhNMRg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AItsILoL2TxBk_DHbE0ual6E0aKO0TbAmstCvoKLvpfJf14g2-ecR683Ak7EhlHgwNRfkMhtGhHypzlvgRyEzmvFUOEXqFj-ACW4KHT8GXtTmA7djFc6bYrMmB2UhcztxWtMjUhNMRg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
