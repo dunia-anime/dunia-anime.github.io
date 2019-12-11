c4e3bfa1f374f0b0133a1af6f9875c3b45a9f4bd
dunia-anime <adityha.on@gmail.com>
2019-12-11 12:37:52 +0800

    Create UMutyby.js

---
+++ Anim-js/U/UMutyby.js
@@ -0,0 +1,380 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/aWFmJDFX45OTEmPPGjoCGaNSOdGK_9a3-ZV6Oau7P0_NhISpJcc903fh6sOF5GGgWZFhIEX5e9YfSqRRYhDNkcL5W3Clws0So-znCX6y2BhyrQ0EUfJhdCpLPyXfstxE7wnnYEWIl68=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/aWFmJDFX45OTEmPPGjoCGaNSOdGK_9a3-ZV6Oau7P0_NhISpJcc903fh6sOF5GGgWZFhIEX5e9YfSqRRYhDNkcL5W3Clws0So-znCX6y2BhyrQ0EUfJhdCpLPyXfstxE7wnnYEWIl68=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/tySQsTvc_Gjjj_68pXknvvDluRTa0dc3d8xpn1AUlZ2iO6vQS-9Yvx4J4PgkVRxq1M1m6XXXu2730eeSn5YZUTB54ecGxPyzJf-IeJXlL9OupULcqzkFvQFIsmZSH2FdFy-pF3UTR8U=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/tySQsTvc_Gjjj_68pXknvvDluRTa0dc3d8xpn1AUlZ2iO6vQS-9Yvx4J4PgkVRxq1M1m6XXXu2730eeSn5YZUTB54ecGxPyzJf-IeJXlL9OupULcqzkFvQFIsmZSH2FdFy-pF3UTR8U=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/F1b_MG465JH8td56JPqHGwRXjzZPrSWD7m63EPlL-u2soQu4qwnl18ffu9Bl66V_GGzcSLcQL755586YuZtKKZoLajWP9QBE10Ju05Bad8phiPSNxtrk66WEt3aUY76wDXi1Ijsd81A=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/F1b_MG465JH8td56JPqHGwRXjzZPrSWD7m63EPlL-u2soQu4qwnl18ffu9Bl66V_GGzcSLcQL755586YuZtKKZoLajWP9QBE10Ju05Bad8phiPSNxtrk66WEt3aUY76wDXi1Ijsd81A=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SE8j52gqjOr9IynL2vfserZ8WEFU3zs9JBO_oqj0RG1Xg05lvoqRSifkAyYevnXrN0MjNir1C35wfVgC8P9-ukDTDfPmSv35vWFAKC7_3_tn0RBQrwwGB-8co_ZVgfW8EEHXIiJGkYM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SE8j52gqjOr9IynL2vfserZ8WEFU3zs9JBO_oqj0RG1Xg05lvoqRSifkAyYevnXrN0MjNir1C35wfVgC8P9-ukDTDfPmSv35vWFAKC7_3_tn0RBQrwwGB-8co_ZVgfW8EEHXIiJGkYM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/6fmwzzUlSXsCR-l5MqjJsqe8DKywgL0zlq9owAlMLyLuZjq8Kc17_-O3KRQhlcKeNDQZmzoaLDXlLYjenI4VU2BEhTt2GnxV48EkRrMSv-4N6YHpn5ZHSyPnvnuFMsZfNOc9c24BnV4=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/6fmwzzUlSXsCR-l5MqjJsqe8DKywgL0zlq9owAlMLyLuZjq8Kc17_-O3KRQhlcKeNDQZmzoaLDXlLYjenI4VU2BEhTt2GnxV48EkRrMSv-4N6YHpn5ZHSyPnvnuFMsZfNOc9c24BnV4=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/axNvEkdUVcrno5-sxX6BA_tl5F2KEqXcwsEy00NFyw3MQ50Wm8-mn0_wI_8fQlMXRMzYlD1GeQ7PmSjXoguzcZqA3_7bk9hBzcMNo-YNF3J18-g-RQygtdoivildCtThh6AOaFb8t44=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/axNvEkdUVcrno5-sxX6BA_tl5F2KEqXcwsEy00NFyw3MQ50Wm8-mn0_wI_8fQlMXRMzYlD1GeQ7PmSjXoguzcZqA3_7bk9hBzcMNo-YNF3J18-g-RQygtdoivildCtThh6AOaFb8t44=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/nBEeWEpIB1mjdaW03r-2b-nZsQQC1MNK-j5YBTpgAjiUIj13fpxNEtAs7XDsHA0mSO6qq0WwXwpOyrmp62kpElVoMiHVH_upIm7AFctF5D6L6CQWcDrAjzhF6dcMgt39SGCGVqu0pSc=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/nBEeWEpIB1mjdaW03r-2b-nZsQQC1MNK-j5YBTpgAjiUIj13fpxNEtAs7XDsHA0mSO6qq0WwXwpOyrmp62kpElVoMiHVH_upIm7AFctF5D6L6CQWcDrAjzhF6dcMgt39SGCGVqu0pSc=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/3030DPhcDmRrVC2gCCvfbpvfH7WkkFkJHzYKT4bw8nuz2-TsIFnc4K42J8qhouZwUrO2bVZno7RB9WwtZpopuLjXoeLQP3YbV2Zie2njKflUQsakCtTgFjC3vl-zuqymz3Bf1yUHejU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/3030DPhcDmRrVC2gCCvfbpvfH7WkkFkJHzYKT4bw8nuz2-TsIFnc4K42J8qhouZwUrO2bVZno7RB9WwtZpopuLjXoeLQP3YbV2Zie2njKflUQsakCtTgFjC3vl-zuqymz3Bf1yUHejU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/vAObq0P-M0Ze_zRb7Hs7oyT7WPqJ-DMMl4Ivijr9UtW3KhnZzK-GrDBOfo3Ga2h-jHL6goKRNox5A_TAtGiJ62VVfD4WznHNhWjq7vf84o_JrofdQV1lLE9n1sQPYty_eD3cWe-cCY8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/vAObq0P-M0Ze_zRb7Hs7oyT7WPqJ-DMMl4Ivijr9UtW3KhnZzK-GrDBOfo3Ga2h-jHL6goKRNox5A_TAtGiJ62VVfD4WznHNhWjq7vf84o_JrofdQV1lLE9n1sQPYty_eD3cWe-cCY8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/BQLQ3AkvhypTE4a-xpdXK0oqI-7-XAzAu9YGjpeKCcrXPNijt3OVweRATUQMgsX9_6k2MvyHfrfH4eF0DWW7X0RDWx4gsWvceB9emhu5Y-G6jaiJcGj7lz75u9Xrr5x7ocxU1SHDyPg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/BQLQ3AkvhypTE4a-xpdXK0oqI-7-XAzAu9YGjpeKCcrXPNijt3OVweRATUQMgsX9_6k2MvyHfrfH4eF0DWW7X0RDWx4gsWvceB9emhu5Y-G6jaiJcGj7lz75u9Xrr5x7ocxU1SHDyPg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/g5ZD5YA4N7Tl9JU2ELI4OnTrOy6hAvvE1tQ7bw8FbIZ2ZxxGjMdT1CtSxzYLfTk8uh-2UJY1rZoVljlbmCdRa8cF0kmzVx5lEegu9LzDPb7CSRK9fvmt-EP2uLIjWejsp_6vofXmSZ0=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/g5ZD5YA4N7Tl9JU2ELI4OnTrOy6hAvvE1tQ7bw8FbIZ2ZxxGjMdT1CtSxzYLfTk8uh-2UJY1rZoVljlbmCdRa8cF0kmzVx5lEegu9LzDPb7CSRK9fvmt-EP2uLIjWejsp_6vofXmSZ0=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/OOb49xA1U6SZCj_KNNW0BI4YcN88E8W84-JEhCT7vC-7CTDWTwixyHtwSbkomP-T4g8a7KwWqaK1LtSz6CCjmv2aR0rSzK0M9ka9ro1f89aOfo6FJljzrg8gvxanv7B6kOLyW4XLuYg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/OOb49xA1U6SZCj_KNNW0BI4YcN88E8W84-JEhCT7vC-7CTDWTwixyHtwSbkomP-T4g8a7KwWqaK1LtSz6CCjmv2aR0rSzK0M9ka9ro1f89aOfo6FJljzrg8gvxanv7B6kOLyW4XLuYg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Uma Musume: Pretty Derby',
+        episode: '* Episode 13',
+        name: 'Episode 13',
+        deskrip1: '* Rating : 7.41',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/NVgodyOmZiaiHUgp1bw3dmU-LgUlDBaCb4-wRoPfAdtR1WhPIsVXcAsgAsoLcp7WQCldTpaQ_F7r0LlhZmYl-CLlr3u-d2gXGETw1Wm-E5baCxQ56Xopg1o_jOkOsCoEdAlpx67f8f8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/NVgodyOmZiaiHUgp1bw3dmU-LgUlDBaCb4-wRoPfAdtR1WhPIsVXcAsgAsoLcp7WQCldTpaQ_F7r0LlhZmYl-CLlr3u-d2gXGETw1Wm-E5baCxQ56Xopg1o_jOkOsCoEdAlpx67f8f8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
