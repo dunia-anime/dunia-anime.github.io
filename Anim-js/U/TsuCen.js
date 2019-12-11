b8053d90bbc8aa85740973d9024a5e9e7d2786f6
dunia-anime <adityha.on@gmail.com>
2019-12-11 12:58:10 +0800

    Create TsuCen.js

---
+++ Anim-js/U/TsuCen.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Tsurezure Children',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/sB1FVKXhTsA-Nk1IRdAMEwpM8n8AE-iFaf86ru20nF1tram4AHzZtQeVRQa_NzcZj6VPlTNSKWa6bXnimiCGDsRL03pYyf7XgB3zcUTJlg0EkATf69u9z7xpw3Ss677vZbS-69G2wF8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/sB1FVKXhTsA-Nk1IRdAMEwpM8n8AE-iFaf86ru20nF1tram4AHzZtQeVRQa_NzcZj6VPlTNSKWa6bXnimiCGDsRL03pYyf7XgB3zcUTJlg0EkATf69u9z7xpw3Ss677vZbS-69G2wF8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/OvDtNXOuBz-kuH6oyQv7G96h1wh-1zl1tplIcYQ4WRdBkzl7UIOBUgjUufme_TVKkXMrJ02rYTY7I1tTP0xnC3wXSLcP7ZB2fymA0ZBUzQnhz2u21txNxWx_ruCyG-tLtY-YNVRyydw=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/OvDtNXOuBz-kuH6oyQv7G96h1wh-1zl1tplIcYQ4WRdBkzl7UIOBUgjUufme_TVKkXMrJ02rYTY7I1tTP0xnC3wXSLcP7ZB2fymA0ZBUzQnhz2u21txNxWx_ruCyG-tLtY-YNVRyydw=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/GxGoPtmbJ4bvtDLORzCwyNeRiVM3uxGL6G_54GhI9wDfMkmD24iYgJUOxQv7hjM--CuQtJ2SgotSYhvjJKSx2MTGkds-47f6ckUvIfpvNf9HnZ5ecA1rhGBIaKd22ZQcikY8bjJR2LI=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/GxGoPtmbJ4bvtDLORzCwyNeRiVM3uxGL6G_54GhI9wDfMkmD24iYgJUOxQv7hjM--CuQtJ2SgotSYhvjJKSx2MTGkds-47f6ckUvIfpvNf9HnZ5ecA1rhGBIaKd22ZQcikY8bjJR2LI=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/oKlFVxs03nrBNy3PfK4FCrhSqVXmtv1999Mn6nHaJ_hr_rUvZmelS3fAvN3Dp95HRT7N4d7RZtEFHVPCQkvMjTD-iBbUp6dcTGwoezfeY34FzUBwESCgFAUgCTXp7xMSbqO-viqwvag=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/oKlFVxs03nrBNy3PfK4FCrhSqVXmtv1999Mn6nHaJ_hr_rUvZmelS3fAvN3Dp95HRT7N4d7RZtEFHVPCQkvMjTD-iBbUp6dcTGwoezfeY34FzUBwESCgFAUgCTXp7xMSbqO-viqwvag=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/AF8aOyyGZBqL5SzMbQ54iSslHT8bakDh5uojJKLwt_AzfK-sXM09W4UeTMR5chUV6mZ74ZINMjNI0jBrmi35x94LFSlQWPeSp0BTGQsTGE553kovlVN7Gb0Moa1c1BBMgEycIelvRVQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/AF8aOyyGZBqL5SzMbQ54iSslHT8bakDh5uojJKLwt_AzfK-sXM09W4UeTMR5chUV6mZ74ZINMjNI0jBrmi35x94LFSlQWPeSp0BTGQsTGE553kovlVN7Gb0Moa1c1BBMgEycIelvRVQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Lhw9k4zAEdwA_Hog6fz050OY2ZoutSpo_x9KBjD6jzWvtg0-fEiWZqze6FVUdO04jGX1bZ5l9Wi5qwJCTziD3-E0dnL7oXpn7F4KKolizfAk-9eb7WLTNbR5FIIvfnYIPY-KueXVcOE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Lhw9k4zAEdwA_Hog6fz050OY2ZoutSpo_x9KBjD6jzWvtg0-fEiWZqze6FVUdO04jGX1bZ5l9Wi5qwJCTziD3-E0dnL7oXpn7F4KKolizfAk-9eb7WLTNbR5FIIvfnYIPY-KueXVcOE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Mln5zO39smI0v20cYDafAxEop7BjD_5uVmabFoS39k5IWbkEybQDek3O_W6BeeOly449P94ytAOI5RFTAiplmggtuxqxgh4-ZizMpKk1NYGjqJzZvQn2um-40x8Eul7YS5i_6a0dQz8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Mln5zO39smI0v20cYDafAxEop7BjD_5uVmabFoS39k5IWbkEybQDek3O_W6BeeOly449P94ytAOI5RFTAiplmggtuxqxgh4-ZizMpKk1NYGjqJzZvQn2um-40x8Eul7YS5i_6a0dQz8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/rQnOyRh8cSlr95LHOUuFUwd_y88py-X-vBYB3qnfHpQTqwBTZ0EYunjAtLU25EYW-LE9Y4NYgcadiNxAUmpg_PcFgYFzUXQlNGdOMnqG6l2xo_TKFEonzd116aJgyUWRoEX0zdevKCg=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/rQnOyRh8cSlr95LHOUuFUwd_y88py-X-vBYB3qnfHpQTqwBTZ0EYunjAtLU25EYW-LE9Y4NYgcadiNxAUmpg_PcFgYFzUXQlNGdOMnqG6l2xo_TKFEonzd116aJgyUWRoEX0zdevKCg=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/xV1KGLvTz91t2_9ZOHipbP0J9sjQ5zt0UoPFNGWnlPFjDHrHK7P8J9coBGH3QYsNNI543MXyJa-x36FUi7tuhSbNOj2CYmviEaoMJTwJURI_DVVsC0pIQRl6ETVagi0nPxwA9u1Lp_8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/xV1KGLvTz91t2_9ZOHipbP0J9sjQ5zt0UoPFNGWnlPFjDHrHK7P8J9coBGH3QYsNNI543MXyJa-x36FUi7tuhSbNOj2CYmviEaoMJTwJURI_DVVsC0pIQRl6ETVagi0nPxwA9u1Lp_8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/hxuAuMyyeSTA_qPAZTvd9p-5meiWUlJeA1fO1kBlEoBB7-pEeRcd4m9meiXbTzHPR2Oeq7xGXCSRbtMhIhna_uDjsHmtUGFch-s5w2AjlxOJI-c2Mv8DklR5CfhtxQfd6kIE285HVyA=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/hxuAuMyyeSTA_qPAZTvd9p-5meiWUlJeA1fO1kBlEoBB7-pEeRcd4m9meiXbTzHPR2Oeq7xGXCSRbtMhIhna_uDjsHmtUGFch-s5w2AjlxOJI-c2Mv8DklR5CfhtxQfd6kIE285HVyA=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/UVxPZRR88EPVFN5zHyFFuViAekbbWRh5S6aJhhiCYI8T1tF6m064BAETIv_pP_vHd1G_bosaIcctFktuc8c43vEZpYzzYf52yAG6enU4Sc5CIFnVvfVBekVzVEzWGaJPczzv2dlM38s=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/UVxPZRR88EPVFN5zHyFFuViAekbbWRh5S6aJhhiCYI8T1tF6m064BAETIv_pP_vHd1G_bosaIcctFktuc8c43vEZpYzzYf52yAG6enU4Sc5CIFnVvfVBekVzVEzWGaJPczzv2dlM38s=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Tsurezure Children',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.81',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/SN261D3O1BKZMJoAgabAoES3alMJqY56k_gyDnf-l2t1VYJvfv4ZEVt-dKE1LmgkojNORV4MMQ2eQC14HqRwueSVjERIisS90RCXMqYHHQSA3_hdztl-wpsf01m7KpgF-uq02LQ_FSE=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/SN261D3O1BKZMJoAgabAoES3alMJqY56k_gyDnf-l2t1VYJvfv4ZEVt-dKE1LmgkojNORV4MMQ2eQC14HqRwueSVjERIisS90RCXMqYHHQSA3_hdztl-wpsf01m7KpgF-uq02LQ_FSE=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
