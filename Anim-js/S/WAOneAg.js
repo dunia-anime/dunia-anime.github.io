13eb19b379ea4e44127f70507dacb25a96ada360
dunia-anime <adityha.on@gmail.com>
2019-12-12 17:35:40 +0800

    Create WAOneAg.js

---
+++ Anim-js/S/WAOneAg.js
@@ -0,0 +1,352 @@
+var player = videojs('video')
+player.playlist([{
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 01',
+        name: 'Episode 01',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/UPuonTZOk_HYpX9DvqnDBjcAaLLfhMcPbrSyTazliPufuIHp4dLnZ7-MubbEKw7hmMvnJqG7v8XQgf_JemXX913Aehc0JacIHxbEQRnnfZgQ7c6bVJsWaiK-QcazeF-cHgLFvperE-g=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/UPuonTZOk_HYpX9DvqnDBjcAaLLfhMcPbrSyTazliPufuIHp4dLnZ7-MubbEKw7hmMvnJqG7v8XQgf_JemXX913Aehc0JacIHxbEQRnnfZgQ7c6bVJsWaiK-QcazeF-cHgLFvperE-g=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 02',
+        name: 'Episode 02',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/63FIZQeWi6giNRZ1XGvgMOoy-l07ayrB4BeecsuQcZDRGkVGB0kNJgqC_weylah-GKrhjpP1YyxyGq53c6KTY6ASv543adswj2EEz762YbbK5rjK2r5A9dQO4ligXdMBRNxKErkiJq8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/63FIZQeWi6giNRZ1XGvgMOoy-l07ayrB4BeecsuQcZDRGkVGB0kNJgqC_weylah-GKrhjpP1YyxyGq53c6KTY6ASv543adswj2EEz762YbbK5rjK2r5A9dQO4ligXdMBRNxKErkiJq8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 03',
+        name: 'Episode 03',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/2pdxgF4RdJbJz99zskkDWO_QygYDlmSRSC6jEpe-awUFMFNzHGkOLJDYnjC3-YS28yNK-lBhF3dIAb3GbA_rm1O6lvA77Pmrj0CkbeoyXh70w4v22H3CZHSaIxilx0ELpYBE8-iT5BU=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/2pdxgF4RdJbJz99zskkDWO_QygYDlmSRSC6jEpe-awUFMFNzHGkOLJDYnjC3-YS28yNK-lBhF3dIAb3GbA_rm1O6lvA77Pmrj0CkbeoyXh70w4v22H3CZHSaIxilx0ELpYBE8-iT5BU=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 04',
+        name: 'Episode 04',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/bxDGc8ebsDpzNUXlVo0q28TxH2rYRgnHxXISxjr9Ek6E3GIHzgZQGvy3eM9SiBcOUgTvheCTeLp4KUchvq6JX3Lf8_mPXOY9U8-Qx-dWES255wPmfe1DV2fcFqeT7SL7c0qhxtDj11w=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/bxDGc8ebsDpzNUXlVo0q28TxH2rYRgnHxXISxjr9Ek6E3GIHzgZQGvy3eM9SiBcOUgTvheCTeLp4KUchvq6JX3Lf8_mPXOY9U8-Qx-dWES255wPmfe1DV2fcFqeT7SL7c0qhxtDj11w=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 05',
+        name: 'Episode 05',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/ZBMO4ccR6mlTGePpOusOUd0D6Pz91i865gG1Hi2UoX8echs59AFMKv4w1ffpOH5yUpDd1n4-mU1-LW7BQlhtkywcQCSGL1D-QzLDdXzNT5_rnzs5pXVbTFwc45Kc22S8D7aLELBNdfQ=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/ZBMO4ccR6mlTGePpOusOUd0D6Pz91i865gG1Hi2UoX8echs59AFMKv4w1ffpOH5yUpDd1n4-mU1-LW7BQlhtkywcQCSGL1D-QzLDdXzNT5_rnzs5pXVbTFwc45Kc22S8D7aLELBNdfQ=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 06',
+        name: 'Episode 06',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/tLp3DthwNuFNgC4-kTr35Bdb5vlnX-j0bs33kcOv_PhqL20EVpqrhgF4NaSiUOCencMm1ctrqfnPhxnDwPjSTKVCSHviwIUoP3WSAyqrqkLtjtWXqF_hPEmyWWR1gyJBqUqdFibLty8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/tLp3DthwNuFNgC4-kTr35Bdb5vlnX-j0bs33kcOv_PhqL20EVpqrhgF4NaSiUOCencMm1ctrqfnPhxnDwPjSTKVCSHviwIUoP3WSAyqrqkLtjtWXqF_hPEmyWWR1gyJBqUqdFibLty8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 07',
+        name: 'Episode 07',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/IliK-hiDNl0e2Qo6f0pkzJjonjCrBbKgoO6IrsitR0CWVJBy5jIf4JXDcVeQ0GV7AMd-MhXT5SAaYoW9fdl3YRasqpZqrPQ6jHREm7liN1q6HH0tpvqxNS94oAbERBKd5YodMvh8GyM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/IliK-hiDNl0e2Qo6f0pkzJjonjCrBbKgoO6IrsitR0CWVJBy5jIf4JXDcVeQ0GV7AMd-MhXT5SAaYoW9fdl3YRasqpZqrPQ6jHREm7liN1q6HH0tpvqxNS94oAbERBKd5YodMvh8GyM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 08',
+        name: 'Episode 08',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/tY1Oz0AzWMuyPJ6Xy6jUUEgfl2LK6z2PMFj2obvoKRGSZqSgpJRrzh59R_QAiEQBr0dp-0oxjD6SE8UFIo7nrAP6yEAzHW45__jDU9n894Rl1rU3M18wVQPbouYWGog_nhqN-aoEmlM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/tY1Oz0AzWMuyPJ6Xy6jUUEgfl2LK6z2PMFj2obvoKRGSZqSgpJRrzh59R_QAiEQBr0dp-0oxjD6SE8UFIo7nrAP6yEAzHW45__jDU9n894Rl1rU3M18wVQPbouYWGog_nhqN-aoEmlM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 09',
+        name: 'Episode 09',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Zsffo-4lEEFpMTF7Kg-GUzBm7dK7bOxp7rN1mEHQgcnqyDvr-2epnUE9-bMJy7PMKLoq6v6I9KUl8FhIroMEKUmIuBPQbAznQyQlxOTgm649EylU5coDRq0HbK-9iUp4V9sC68Qmwjs=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Zsffo-4lEEFpMTF7Kg-GUzBm7dK7bOxp7rN1mEHQgcnqyDvr-2epnUE9-bMJy7PMKLoq6v6I9KUl8FhIroMEKUmIuBPQbAznQyQlxOTgm649EylU5coDRq0HbK-9iUp4V9sC68Qmwjs=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 10',
+        name: 'Episode 10',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/Zb-xPHMtYQAz0L5eDBxqCy5TPtgMJEuRZ4p-HW3Z2brrppgnAhSeGiIiLuBId5G00zPipaS1rYZM7OZvJKtBvBBeBnr0bQUdTSGHb0Ycl-zhIuq-Z47nOhQ-aBYV2gStqfch_F90Fi8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/Zb-xPHMtYQAz0L5eDBxqCy5TPtgMJEuRZ4p-HW3Z2brrppgnAhSeGiIiLuBId5G00zPipaS1rYZM7OZvJKtBvBBeBnr0bQUdTSGHb0Ycl-zhIuq-Z47nOhQ-aBYV2gStqfch_F90Fi8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 11',
+        name: 'Episode 11',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/8VdVgP8BRwQfIo3z7pxEmhWso-q-vf-40kBHUsEzfzHj08Kw9eykNXD3Pdyd9zG7wl3HmG6ITrduxUD4yQZML2tljGqLsCGLZ6x1hTD09-Ac6a4u2hiPd7Df7J5-iXUwqDrIzVAaHlM=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/8VdVgP8BRwQfIo3z7pxEmhWso-q-vf-40kBHUsEzfzHj08Kw9eykNXD3Pdyd9zG7wl3HmG6ITrduxUD4yQZML2tljGqLsCGLZ6x1hTD09-Ac6a4u2hiPd7Df7J5-iXUwqDrIzVAaHlM=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    },
+    {
+        judul: '* Sword Art Online A: GGO',
+        episode: '* Episode 12',
+        name: 'Episode 12',
+        deskrip1: '* Rating : 7.25',
+        sources: [{
+                src: 'https://lh3.googleusercontent.com/taHpVyQw74wxpdy6S14na4LyryhThrOyKHbj2u6Hk0ZyB_wFXaRu_9tBcu7yTIoZn-dEUFkXZD0dlo31Ijqhn13eyS0155aUfEjXlU-3vdLkDg5bKUzrSXpp8pIo6NyDoU_OI7BhLv8=m22',
+                type: 'video/mp4',
+                label: '720p',
+                selected: 'false'
+            },
+            {
+                src: 'https://lh3.googleusercontent.com/taHpVyQw74wxpdy6S14na4LyryhThrOyKHbj2u6Hk0ZyB_wFXaRu_9tBcu7yTIoZn-dEUFkXZD0dlo31Ijqhn13eyS0155aUfEjXlU-3vdLkDg5bKUzrSXpp8pIo6NyDoU_OI7BhLv8=m18',
+                type: 'video/mp4',
+                label: '480p',
+                selected: 'true'
+            }
+        ],
+        thumbnail: [{
+                srcset: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                src: 'https://1.bp.blogspot.com/-gxrecw-tn5k/XdEKFIfdc7I/AAAAAAAABk4/yZd7T6-5QDodFismcHENHOfUAlDrfpo_gCLcBGAsYHQ/s10/Background.jpg'
+            }
+        ]
+    }
+]);
+// Initialize the playlist-ui plugin with no option (i.e. the defaults).
+player.playlistUi();
+
+videojs("video", {}, function () {
+    var player = this;
+    player.controlBar.addChild('QualitySelector');
+    player.landscapeFullscreen();
+});
+
+videojs('video').ready(function () {
+    this.hotkeys({
+
+    });
+});
