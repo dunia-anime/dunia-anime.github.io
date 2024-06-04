f37139c475c76042234d4419fb76e59de09a4a6d
Author Sulttra <43660030+dunia-anime@users.noreply.github.com>
2024-06-04 15:41:02 +0000

    Update script.js

--- Anim-js/zTest/script.js
+++ Anim-js/zTest/script.js
@@ -1,3 +1,10 @@
-function playVideo(source) {
-    document.getElementById('current-video').src = source;
+window.onload = function() {
+    const thumbnails = document.querySelectorAll('.thumbnail');
+    
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.onclick = function() {
+            const src = this.getAttribute('data-src');
+            document.getElementById('current-video').src = src;
+        }
+    });
 }
